58d439b171267b8f6eae9080ca0340eb6bb2ff8a
Orr Gottlieb <orr@monday.com>
2020-10-07 11:43:00 +0300

    lint

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,31 +3,31 @@ import PropTypes from "prop-types";
 import "./Loader.scss";
 
 const Loader = ({ svgClassName }) => {
-    return (
-        <div className="monday-loader-component">
-            <svg
-                className={`circle-loader-spinner ${svgClassName}`}
-                viewBox="0 0 50 50"
-            >
-                <circle
-                    className="circle-loader-spinner-path"
-                    cx="25"
-                    cy="25"
-                    r="20"
-                    fill="none"
-                    strokeWidth="5"
-                />
-            </svg>
-        </div>
-    );
+  return (
+    <div className="monday-loader-component">
+      <svg
+        className={`circle-loader-spinner ${svgClassName}`}
+        viewBox="0 0 50 50"
+      >
+        <circle
+          className="circle-loader-spinner-path"
+          cx="25"
+          cy="25"
+          r="20"
+          fill="none"
+          strokeWidth="5"
+        />
+      </svg>
+    </div>
+  );
 };
 
 Loader.propTypes = {
-    svgClassName: PropTypes.string
+  svgClassName: PropTypes.string
 };
 
 Loader.defaultProps = {
-    svgClassName: ""
+  svgClassName: ""
 };
 
 export default Loader;
