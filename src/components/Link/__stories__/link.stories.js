0b9d4240967a84841ea0a9e6f0e91a4f448f9210
Orr Gottlieb <orr@monday.com>
2020-10-11 14:21:31 +0300

    update stories

--- src/components/Link/__stories__/link.stories.js
+++ src/components/Link/__stories__/link.stories.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { text, select } from "@storybook/addon-knobs";
 import Link from "../Link";
+import "./link-story.scss";
+import DescriptionLabel from "../../storybook-helpers/description-label/description-label";
+import StoryLine from "../../../StoryBookComponents/StoryLink/StoryLine";
+import { FlexLayout } from "../../storybook-helpers";
 
 export const Sandbox = () => (
   <div className="width-35">
@@ -20,6 +24,84 @@ export const Sandbox = () => (
     />
   </div>
 );
+
+export const States = () => {
+  return (
+    <div className="links-list">
+      <DescriptionLabel>
+        When adding links please make sure to pass a short but descriptive{" "}
+        <code>ariaLabelDescription</code> prop in order to better support screen
+        readers
+      </DescriptionLabel>
+      <FlexLayout>
+        <StoryLine title="Start Icon">
+          <Link
+            id="normal"
+            text="Read more"
+            iconPosition={Link.position.START}
+            href="https://www.monday.com"
+            iconName="fa fa-info"
+            ariaLabelDescription="Read more about monday,con"
+          />
+        </StoryLine>
+      </FlexLayout>
+      <FlexLayout>
+        <StoryLine title="End Icon">
+          <Link
+            id="end"
+            text="Read more"
+            iconPosition={Link.position.END}
+            href="https://www.monday.com"
+            iconName="fa fa-link"
+            ariaLabelDescription="Read more about monday,con"
+          />
+        </StoryLine>
+      </FlexLayout>
+      <FlexLayout>
+        <StoryLine title="Icon only">
+          <Link
+            id="just-icon"
+            href="https://www.monday.com"
+            iconPosition={Link.position.START}
+            iconName="fa fa-star"
+            ariaLabelDescription="Read more about monday,con"
+          />
+        </StoryLine>
+      </FlexLayout>
+    </div>
+  );
+};
+
+export const RTLSupport = () => {
+  return (
+    <div>
+      <FlexLayout>
+        <StoryLine title="Start Icon" wrapperClassName="links-story-trl">
+          <Link
+            id="normal-rtl"
+            text="קרא עוד"
+            iconPosition={Link.position.START}
+            href="https://www.monday.com"
+            iconName="fa fa-info"
+            ariaLabelDescription="Read more about monday,con"
+          />
+        </StoryLine>
+      </FlexLayout>
+      <FlexLayout>
+        <StoryLine title="End Icon" wrapperClassName="links-story-trl">
+          <Link
+            id="end-rtl"
+            text="اقرأ أكثر"
+            iconPosition={Link.position.END}
+            href="https://www.monday.com"
+            iconName="fa fa-link"
+            ariaLabelDescription="Read more about monday,con"
+          />
+        </StoryLine>
+      </FlexLayout>
+    </div>
+  );
+};
 
 export default {
   title: "Components/Link",
