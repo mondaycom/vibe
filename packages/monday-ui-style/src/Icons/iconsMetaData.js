1d0e56ea844a33f93ad46f9d4db86198621342d3
Orr Gottlieb <orr@monday.com>
2020-10-11 17:04:36 +0300

    feat: Icons initial commit

---
+++ packages/monday-ui-style/src/Icons/iconsMetaData.js
@@ -0,0 +1,15 @@
+export const iconsMetaData = [
+  {
+    name: "Group",
+    description: "Adding a group to a board",
+  },
+  {
+    name: "Close",
+    description: "Close icons for inputs",
+  },
+  {
+    name: "SearchThin",
+    description:
+      "Magnifier icons - for search inputs and general search icon buttons",
+  },
+];
