09ea369c318a16b1a535018116166dffed472e95
Orr Gottlieb <orr@monday.com>
2020-10-15 16:12:26 +0300

    Icons

--- src/Icons/iconsMetaData.js
+++ src/Icons/iconsMetaData.js
@@ -1,24 +1,60 @@
 export const iconsMetaData = [
-  {
-    name: "Group",
-    file: "Group.svg",
-    description: "Adding a group to a board",
-  },
-  {
-    name: "Close",
-    file: "Close.svg",
-    description: "Close",
-  },
-  {
-    name: "Search Thin",
-    file: "SearchThin.svg",
-    description: "Search Items",
-  },
-  {
-    name: "Question",
-    file: "Question.svg",
-    description: "Add a question",
-  },
   // DO NOT REMOVE THIS FOLLOWING LINE!
   // plop_marker:icon_metadata
+  {
+      name: "NavigationArrowLeft",
+      file: "",
+      description: "Navigation, Arrow, Left",
+      tags: "Navigation, Arrow, Left"
+  },
+
+  {
+      name: "NavigationArrowRight",
+      file: "",
+      description: "Navigation Arrow Right",
+      tags: "Navigation, Arrow, Right"
+  },
+
+  {
+      name: "NavigationArrowDown",
+      file: "",
+      description: "Navigtion Arrow Down",
+      tags: "Navigation, Arrow, Down"
+  },
+
+  {
+      name: "NavigationArrowUp",
+      file: "",
+      description: "Naviation Arrow",
+      tags: "Navigation, Arrow, Up"
+  },
+
+  {
+      name: "Notification",
+      file: "",
+      description: "Bell for notifictions",
+      tags: "Notifications, Reminders, Bell"
+  },
+
+  {
+      name: "Search",
+      file: "",
+      description: "Magnifier Glass",
+      tags: "Search, Find, Zoom"
+  },
+
+  {
+      name: "Talk",
+      file: "",
+      description: "Talk Bubble",
+      tags: "Chat, Bubble, Talk, Post, Comment"
+  },
+
+  {
+      name: "Workspaces",
+      file: "",
+      description: "Workspaces Icon",
+      tags: ""
+  },
+
 ];
