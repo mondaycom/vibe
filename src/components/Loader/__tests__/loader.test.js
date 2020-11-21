7bc92e0d9298f56e86baf64a03510d7cf70fdd5a
Orr Gottlieb <orr@monday.com>
2020-11-21 21:42:40 +0200

    test: initial jest config

---
+++ src/components/Loader/__tests__/loader.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Loader from "../Loader";
+
+it("renders correctly with empty props", () => {
+  const tree = renderer.create(<Loader />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders correctly with custom class name", () => {
+  const tree = renderer
+    .create(<Loader svgClassName="dummy-class-name" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
