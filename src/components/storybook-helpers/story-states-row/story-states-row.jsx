792c2284c2097ab52b8567501166abdcfe39fdfa
Orr Gottlieb <orr@monday.com>
2020-12-08 16:42:52 +0200

    circular dependency

--- src/components/storybook-helpers/story-states-row/story-states-row.jsx
+++ src/components/storybook-helpers/story-states-row/story-states-row.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react";
 import FlexLayout from "../flex-layout/flex-layout";
-import {ComponentStateDescription} from "../index";
+import ComponentStateDescription from "../component-state-description/ComponentStateDescription";
 
 export default class StoryStateRow extends PureComponent {
   render() {
