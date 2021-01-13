620d6b9185377d75d85226cdfcf2d73d78a3d0ab
Orr Gottlieb <orr@monday.com>
2021-01-13 10:46:27 +0200

    wip: colors page

--- src/general-stories/colors/colors.stories.js
+++ src/general-stories/colors/colors.stories.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { colorsMap } from "./colors-vars-map";
+import { allMondayColors, colorsMap } from "./colors-vars-map";
 import "./ColorItem.scss";
+import Sun from "../../components/Icon/Icons/components/Sun";
+import Moon from "../../components/Icon/Icons/components/Moon";
 
-const ColorItem = ({ color }) => {
+const KeyColorItem = ({ color }) => {
   return (
     <section className="color-item-component">
       <span className="light-app-theme" style={{ height: "100%" }}>
@@ -16,10 +18,39 @@ const ColorItem = ({ color }) => {
   );
 };
 
+const ColorItem = ({ color }) => {
+  return (
+    <section className="all-color-item">
+      <div className="all-color-element" style={{ backgroundColor: `var(${color})` }} />
+      <span className="all-color-name">{color}</span>
+    </section>
+  );
+};
+
 export const ColorKeys = () => {
   return (
     <div className="colors-container">
+      <div className="themes-icon-container">
+        <span className="theme-container">
+          <Sun className="theme-icon" />
+          Light Theme
+        </span>
+        <span className="theme-container">
+          <Moon className="theme-icon" />
+          Dark Theme
+        </span>
+      </div>
       {colorsMap.map(colorName => (
+        <KeyColorItem color={colorName} />
+      ))}
+    </div>
+  );
+};
+
+export const MondayColors = () => {
+  return (
+    <div className="all-colors-container">
+      {allMondayColors.map(colorName => (
         <ColorItem color={colorName} />
       ))}
     </div>
