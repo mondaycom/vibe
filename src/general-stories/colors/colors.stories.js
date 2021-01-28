d3f5405ee55a1af7c4e06f09197e17026098196a
Orr Gottlieb <orr@monday.com>
2021-01-28 14:07:44 +0200

    colors story

--- src/general-stories/colors/colors.stories.js
+++ src/general-stories/colors/colors.stories.js
@@ -3,17 +3,62 @@ import { allMondayColors, colorsMap } from "./colors-vars-map";
 import "./ColorItem.scss";
 import Sun from "../../components/Icon/Icons/components/Sun";
 import Moon from "../../components/Icon/Icons/components/Moon";
+import DescriptionLabel from "../../components/storybook-helpers/description-label/description-label";
+import StoryWrapper from "../../StoryBookComponents/StoryWrapper/StoryWrapper";
+import {
+  buildColorsStory,
+  codingColors,
+  darkColors,
+  greyColors,
+  mainColors,
+  mainColors2,
+  mainColors3
+} from "./colors-helper";
+import StoryTitle from "../../components/storybook-helpers/story-title/story-title";
 
-const KeyColorItem = ({ color }) => {
+const KeyColorItem = ({ color, description }) => {
   return (
     <section className="color-item-component">
-      <span className="light-app-theme" style={{ height: "100%" }}>
-        <div className="color-element" style={{ backgroundColor: `var(${color})` }} />
-      </span>
-      <span className="color-name">{color}</span>
-      <span className="dark-app-theme" style={{ height: "100%" }}>
-        <div className="color-element" style={{ backgroundColor: `var(${color})` }} />
-      </span>
+      <div className="color-name">
+        <span style={{ padding: "0 8px", fontSize: "16px" }}>{color}</span>
+        <DescriptionLabel className="color-name-description">{description}</DescriptionLabel>
+      </div>
+
+      <div className="light-app-theme" style={{ height: "80%" }}>
+        <div
+          className="color-element"
+          style={{
+            width: "150px",
+            backgroundColor: `var(${color})`,
+            borderRadius: "4px 0px 0px 4px",
+            border: "1px solid var(--ui-border-color)",
+            borderRight: "none"
+          }}
+        />
+      </div>
+      <div
+        className="dark-app-theme"
+        style={{
+          width: "175px",
+          height: "100%",
+          backgroundColor: "var(--primary-background-color)",
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        <div style={{ height: "80%" }}>
+          <div
+            className="color-element"
+            style={{
+              width: "150px",
+              backgroundColor: `var(${color})`,
+              borderRadius: "0px 4px 4px 0px",
+              border: "1px solid var(--ui-border-color)",
+              borderLeft: "none"
+            }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
@@ -27,35 +72,56 @@ const ColorItem = ({ color }) => {
   );
 };
 
-export const ColorKeys = () => {
+export const Colors = () => {
   return (
-    <div className="colors-container">
-      <div className="themes-icon-container">
-        <span className="theme-container">
-          <Sun className="theme-icon" />
-          Light Theme
-        </span>
-        <span className="theme-container">
-          <Moon className="theme-icon" />
-          Dark Theme
-        </span>
+    <StoryWrapper>
+      <StoryTitle text="Main Colors" />
+      {buildColorsStory(mainColors)}
+      {buildColorsStory(mainColors2)}
+      {buildColorsStory(mainColors3)}
+      {buildColorsStory(greyColors)}
+      <div style={{ margin: "24px 0" }}>
+        <br />
       </div>
-      {colorsMap.map(colorName => (
-        <KeyColorItem color={colorName} />
-      ))}
-    </div>
+      <StoryTitle text="Colors Keys" />
+      <div className="colors-container">
+        <div className="themes-icon-container">
+          <div className="theme-name-spacer">Color Keys</div>
+          <span className="theme-container">
+            <Sun className="theme-icon" /> Light
+          </span>
+          <span className="theme-container dark-app-theme" style={{ width: "175" }}>
+            <Moon className="theme-icon" /> Dark
+          </span>
+        </div>
+        {colorsMap.map(({ color, description }) => (
+          <KeyColorItem color={color} description={description} />
+        ))}
+      </div>
+
+      <div style={{ margin: "24px 0" }}>
+        <br />
+      </div>
+      <StoryTitle text="Status Colors" />
+      <DescriptionLabel>
+        These colours are used only for color coding purposes like groups colours, statuses timeline bars etc.. this
+        gives understanding and indication of orientation and belonging. The board’s main strength is its simple and
+        visual appearance. That’s why the status colors should appear on the board and nowhere else in the UI.
+      </DescriptionLabel>
+      {buildColorsStory(codingColors, true)}
+    </StoryWrapper>
   );
 };
 
-export const MondayColors = () => {
-  return (
-    <div className="all-colors-container">
-      {allMondayColors.map(colorName => (
-        <ColorItem color={colorName} />
-      ))}
-    </div>
-  );
-};
+// export const MondayColors = () => {
+//   return (
+//     <div className="all-colors-container">
+//       {allMondayColors.map(colorName => (
+//         <ColorItem color={colorName} />
+//       ))}
+//     </div>
+//   );
+// };
 
 export default {
   title: "Foundations|Colors"
