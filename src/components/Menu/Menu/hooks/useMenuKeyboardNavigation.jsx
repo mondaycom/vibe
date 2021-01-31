c10eaf29ddee9c284083e260422d32c376a0c558
Omer Doron <omer@monday.com>
2021-01-31 20:56:22 +0200

    refactor

--- src/components/Menu/Menu/hooks/useMenuKeyboardNavigation.jsx
+++ src/components/Menu/Menu/hooks/useMenuKeyboardNavigation.jsx
@@ -1,6 +1,11 @@
 import { useCallback } from "react";
 import useKeyEvent from "../../../../hooks/useKeyEvent";
 
+const ARROW_DIRECTIONS = {
+  UP: "up",
+  DOWN: "down"
+};
+
 const isChildSelectable = (newIndex, children) => {
   const child = children[newIndex];
   return child.type.isSelectable && !child.props.disabled;
@@ -13,31 +18,38 @@ export default function useMenuKeyboardNavigation(
   setActiveItemIndex,
   isVisible
 ) {
-  const onArrowUp = useCallback(() => {
-    let newIndex;
-    if (hasOpenSubMenu) return false;
-    for (let offset = children.length - 1; offset > 0; offset--) {
-      newIndex = (activeItemIndex + offset) % children.length;
-      if (isChildSelectable(newIndex, children)) {
-        break;
+  const onArrowKeyEvent = useCallback(
+    direction => {
+      let newIndex;
+      if (hasOpenSubMenu) return false;
+
+      if (direction === ARROW_DIRECTIONS.DOWN) {
+        for (let offset = 1; offset <= children.length; offset++) {
+          newIndex = (activeItemIndex + offset) % children.length;
+          if (isChildSelectable(newIndex, children)) {
+            break;
+          }
+        }
+      } else if (direction === ARROW_DIRECTIONS.UP) {
+        for (let offset = children.length - 1; offset > 0; offset--) {
+          newIndex = (activeItemIndex + offset) % children.length;
+          if (isChildSelectable(newIndex, children)) {
+            break;
+          }
+        }
       }
-    }
-    (newIndex || newIndex === 0) && setActiveItemIndex(newIndex);
-  }, [setActiveItemIndex, children, activeItemIndex, hasOpenSubMenu]);
+
+      if (newIndex || newIndex === 0) setActiveItemIndex(newIndex);
+    },
+    [activeItemIndex, children, hasOpenSubMenu, setActiveItemIndex]
+  );
+  const onArrowUp = useCallback(() => {
+    onArrowKeyEvent(ARROW_DIRECTIONS.UP);
+  }, [onArrowKeyEvent]);
 
   const onArrowDown = useCallback(() => {
-    if (hasOpenSubMenu) return false;
-    let newIndex;
-
-    if (!children) return;
-    for (let offset = 1; offset <= children.length; offset++) {
-      newIndex = (activeItemIndex + offset) % children.length;
-      if (isChildSelectable(newIndex, children)) {
-        break;
-      }
-    }
-    (newIndex || newIndex === 0) && setActiveItemIndex(newIndex);
-  }, [setActiveItemIndex, children, activeItemIndex, hasOpenSubMenu]);
+    onArrowKeyEvent(ARROW_DIRECTIONS.DOWN);
+  }, [onArrowKeyEvent]);
 
   const onEnterClickCallback = useCallback(
     _event => {
