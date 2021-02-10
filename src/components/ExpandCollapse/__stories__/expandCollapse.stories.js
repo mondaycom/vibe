686eced3a37e7a24d72c879bec39303fd1cd7fd9
Amit Hanoch <amitha@monday.com>
2021-02-10 18:37:27 +0200

    change stories, use knobs

--- src/components/ExpandCollapse/__stories__/expandCollapse.stories.js
+++ src/components/ExpandCollapse/__stories__/expandCollapse.stories.js
@@ -1,46 +1,45 @@
 import React from "react";
 import { action } from '@storybook/addon-actions';
-import { text, boolean, number, select } from "@storybook/addon-knobs";
+import { text, number } from "@storybook/addon-knobs";
 import { withPerformance } from "storybook-addon-performance";
 import ExpandCollapse from "../ExpandCollapse";
-import { StoryStateRow, StoryStateColumn, ComponentStateDescription, FlexLayout, Divider } from "../../storybook-helpers";
-import DarkThemeContainer from "../../../StoryBookComponents/DarkThemeContainer/DarkThemeContainer";
-import StoryWrapper from "../../../StoryBookComponents/StoryWrapper/StoryWrapper";
 import IconTitleHeaderComponent from "../components/IconTitleHeader/IconTitleHeaderComponent";
 import Icon from "../../Icon/Icon";
 import Robot from "../../Icon/Icons/components/Robot";
 import "./expandCollapse.stories.scss"
 
-export const Sandbox = () => (
-    <div>
+export const Sandbox = () => {
+    const width = number("width", 500);
+    const height = number("height", 200);
+
+    return (<div style={{ width, height }}>
         <ExpandCollapse 
-        className="expandCollapse"
-        headerComponentRenderer={IconTitleHeaderComponent("Header Text", 
-        "Some subtext to describe stuff...")}>
-            <p>child 1</p>
-            <h2>child 2</h2>
+        headerComponentRenderer={() => <p>Knobs doen't support functions - <b>insert any component you want</b></p>}>
+            <p>Knobs doen't support functions</p>
+            <h2>insert any component you want</h2>
+            <p>here is a robot for you</p>
             <Icon iconType={Icon.type.SVG} icon={Robot} iconSize={"52px"} tabindex="-1" clickable={true} />
         </ExpandCollapse>
-    </div>
-);
+    </div>);
+};
 
 export const OpenByDefault = () => (
     <div>
         <ExpandCollapse 
         className="expandCollapse"
         defaultOpenState={true}
-        headerComponentRenderer={IconTitleHeaderComponent("Header Text", 
-        "Some subtext to describe stuff...")}>
+        headerComponentRenderer={() => <h1>Any component you want</h1>}>
             <Icon iconType={Icon.type.SVG} icon={Robot} iconSize={"52px"} tabindex="-1" clickable={true} />
         </ExpandCollapse>
     </div>
 );
 
-export const CustomHeader = () => (
+export const IconTitleHeader = () => (
     <div>
         <ExpandCollapse 
         className="expandCollapse"
-        headerComponentRenderer={() => <h1>Any component you want</h1>}>
+        headerComponentRenderer={IconTitleHeaderComponent("Header Text", 
+        "Some subtext to describe stuff...")}>
             <Icon iconType={Icon.type.SVG} icon={Robot} iconSize={"52px"} tabindex="-1" clickable={true} />
         </ExpandCollapse>
     </div>
