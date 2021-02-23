f5fae145537176b9073871a96967ca634404671a
Moshe Zemah <moshe@monday.com>
2021-02-23 11:55:23 +0200

    Heading fixes

--- src/components/EditableHeading/__stories__/editableHeading.stories.js
+++ src/components/EditableHeading/__stories__/editableHeading.stories.js
@@ -5,7 +5,10 @@ import { TYPES } from "../../Heading/HeadingConstants";
 
 export const Sandbox = () => (
   <div style={{ width: "200px", height: "500px", padding: "16px", border: "1px solid" }}>
-    <EditableHeading type={TYPES.h1} value="Edit H1" />
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <EditableHeading type={TYPES.h1} value="Edit H1 Long text test" />
+      <div style={{ flex: "0 0 30px" }}>icon</div>
+    </div>
     <br />
     <EditableHeading type={TYPES.h2} value="Edit H2 tooltip" tooltip="Click to edit" />
     <br />
