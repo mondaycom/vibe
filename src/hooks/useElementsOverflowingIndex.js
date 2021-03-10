a3914c90d0f253b9676fce88af7977c24928ea78
Orr Gottlieb <orr@monday.com>
2021-03-10 12:17:30 +0200

    feat: remove redundant useState

--- src/hooks/useElementsOverflowingIndex.js
+++ src/hooks/useElementsOverflowingIndex.js
@@ -1,41 +1,41 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import useResizeObserver from "./useResizeObserver";
 
 // Use this hook when you want to get the index of the child which should be hidden from
 
 function useElementsOverflowingIndex({ ref, children, paddingSize, resizeDebounceTime }) {
-  const [size, setSize] = useState(null);
+  const [indexToSplit, setIndexToSplit] = useState(-1);
+  const aggregatedChildLengths = useRef([]);
 
   const onResize = useCallback(
     ({ borderBoxSize }) => {
-      setSize(borderBoxSize.inlineSize);
+      setIndexToSplit(
+        aggregatedChildLengths.current.findIndex(
+          ({ totalLength }) => totalLength > borderBoxSize.inlineSize - paddingSize
+        )
+      );
     },
-    [setSize]
+    [aggregatedChildLengths, setIndexToSplit, paddingSize]
   );
+
   useResizeObserver({
     ref,
     callback: onResize,
     debounceTime: resizeDebounceTime
   });
 
-  const [aggregatedChildLengths, setAggregatedChildLengths] = useState([]);
-  const [indexToSplit, setIndexToSplit] = useState(-1);
-
-  useEffect(() => {
-    setIndexToSplit(aggregatedChildLengths.findIndex(({ totalLength }) => totalLength > size - paddingSize));
-  }, [aggregatedChildLengths, size, setIndexToSplit, paddingSize]);
-
   useEffect(() => {
     if (!ref.current) return;
     const childLengthsArray = [];
     let totalLength = 0;
-    ref.current.childNodes.forEach(node => {
+    ref.current.children.forEach(node => {
       const childLength = node.clientWidth;
       totalLength += childLength;
       childLengthsArray.push({ childLength, totalLength });
     });
-    setAggregatedChildLengths(childLengthsArray);
-  }, [children, ref, setAggregatedChildLengths]);
+
+    aggregatedChildLengths.current = childLengthsArray;
+  }, [children, ref, aggregatedChildLengths]);
 
   return indexToSplit;
 }
