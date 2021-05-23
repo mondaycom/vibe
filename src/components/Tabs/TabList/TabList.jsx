13e320ffbd18ddbc437ceb503969dd318fb1b7cc
Liat Maor <liat@monday.com>
2021-05-23 14:27:51 +0300

    multiple tab comps inside tab list

--- src/components/Tabs/TabList/TabList.jsx
+++ src/components/Tabs/TabList/TabList.jsx
@@ -1,71 +1,42 @@
-import React, { useRef, forwardRef, useState, useMemo, useEffect } from "react";
+import React, { useRef, forwardRef, useState } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import useMergeRefs from "../../../hooks/useMergeRefs";
-import Tooltip from "../../Tooltip/Tooltip";
 import "./TabList.scss";
-import { VelocityTransitionGroup } from "velocity-react";
 
-function renderTab(tabKey, tabConfig, activeTab, onTabClick) {
-  const isActive = tabKey === activeTab;
+function renderTab(tab, tabId, activeTabId, onTabClick) {
+  const isActive = tabId === activeTabId;
 
   return (
-    <li key={tabConfig.name}>
-      <Tooltip content={tabConfig.tooltip}>
-        <a
-          onClick={() => !tabConfig.disabled && onTabClick(tabKey)}
-          className={cx({
-            active: isActive,
-            disabled: tabConfig.disabled
-          })}
-          href="javascript:void(0)"
-        >
-          {tabConfig.name}
-        </a>
-      </Tooltip>
+    <li key={tabId}>
+      <a
+        onClick={() => onTabClick(tabId)}
+        className={cx({ active: isActive })}
+        href="javascript:void(0)"
+      >
+        {tab}
+      </a>
     </li>
   );
 }
 
-function renderTabs(tabs, activeTab, onTabClick) {
-  return Object.keys(tabs).map(tabKey => renderTab(tabKey, tabs[tabKey], activeTab, onTabClick));
+function renderTabs(tabs, activeTabId, onTabClick) {
+  return <ul>{tabs.map((tab, index) => renderTab(tab, index, activeTabId, onTabClick))}</ul>;
 }
 
-function renderTabContainer(tab) {
-  const [enter, setEnter] = useState("slideUp");
-  const [leave, setLeave] = useState("slideDown");
-
-  return React.createElement(
-    VelocityTransitionGroup,
-    {
-      className: "tab-container",
-      enter: { animation: enter, duration: 100 },
-      leave: { animation: leave, duration: 100 },
-      runOnMount: true
-    },
-    tab.content
-  );
-}
-
-const TabList = forwardRef(({ className, id, tabs, activeTabKey, onTabChange }, ref) => {
+const TabList = forwardRef(({ className, id, onTabChange, activeTabId, children }, ref) => {
     const componentRef = useRef(null);
     const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
 
-    const firstTabKey = Object.keys(tabs)[0];
-    const [activeTab, setActiveTab] = useState(activeTabKey || firstTabKey);
-    function onTabClick(tabKey) {
-      setActiveTab(tabKey);
-      onTabChange && onTabChange(tabKey)
+    const [activeTab, setActiveTab] = useState(activeTabId || 0);
+    function onTabClick(tabId) {
+      setActiveTab(tabId);
+      onTabChange && onTabChange(tabId)
     }
 
-    const isOnlySingleTab = Object.keys(tabs).length === 1;
-
     return (
-    <div ref={mergedRef} className={cx("tabs--wrapper", className, {
-        "disabled": isOnlySingleTab,
-    })} id={id}>
-      <ul>{renderTabs(tabs, activeTab, onTabClick)}</ul>
-      {renderTabContainer(tabs[activeTab])}
+    <div ref={mergedRef} className={cx("tabs--wrapper", className)} id={id}>
+      {renderTabs(children, activeTab, onTabClick)}
     </div>
   );
 });
@@ -73,15 +44,12 @@ const TabList = forwardRef(({ className, id, tabs, activeTabKey, onTabChange }, 
 TabList.propTypes = {
   className: PropTypes.string,
   id: PropTypes.string,
-  tabs: PropTypes.object,
-  activeTabKey: PropTypes.string,
-  onTabChange: PropTypes.func
+  onTabChange: PropTypes.func,
+  activeTabId: PropTypes.number
 };
 TabList.defaultProps = {
   className: "",
   id: "",
-  tabs: {},
-  activeTabKey: "",
   onTabChange: () => {}
 };
 
--- src/components/Tabs/TabList/__stories__/tabList.stories.js
+++ src/components/Tabs/TabList/__stories__/tabList.stories.js
@@ -6,28 +6,16 @@ import TabList from "../TabList";
 import { StoryStateRow, StoryStateColumn, ComponentStateDescription, FlexLayout, Divider } from "../../../storybook-helpers";
 import DarkThemeContainer from "../../../../StoryBookComponents/DarkThemeContainer/DarkThemeContainer";
 import StoryWrapper from "../../../../StoryBookComponents/StoryWrapper/StoryWrapper";
+import Tab from "../../Tab/Tab";
 
 
 export const Sandbox = () => (
     <div>
-        {/*<TabList*/}
-        {/*    id="Knobs"*/}
-        {/*    tabs={{*/}
-        {/*      "first": { name: "First", tooltip: "first tab", content: <div>First tab content</div> },*/}
-        {/*      "second": { name: "Second", tooltip: "second tab", content: <div>Second tab content</div> },*/}
-        {/*      "third": { name: "Third", tooltip: "third tab", content: <div>Third tab content</div> }*/}
-        {/*    }}*/}
-        {/*    activeTab="first"*/}
-        {/*/>*/}
-      <TabList
-        id="Knobs"
-        tabs={{
-          "first": { name: "First", content: <div>First tab content</div> },
-          "second": { name: "Second", content: <div>Second tab content</div> },
-          "third": { name: "Third", content: <div>Third tab content</div>, disabled: true }
-        }}
-        activeTab="first"
-      />
+      <TabList id="Knobs">
+        <Tab>First</Tab>
+        <Tab>Second</Tab>
+        <Tab>Third</Tab>
+      </TabList>
     </div>
 );
 
