d8899976732a569f037acc156ad116bfdc740a88
Liat Maor <liat@monday.com>
2021-06-06 15:03:15 +0300

    add more snapshot tests for tab

--- src/components/Tabs/Tab/__tests__/tab.jest.js
+++ src/components/Tabs/Tab/__tests__/tab.jest.js
@@ -2,7 +2,31 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Tab from "../Tab";
 
-it("renders correctly with empty props", () => {
-  const tree = renderer.create(<Tab />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe("<Tab />", () => {
+  describe("Snapshot tests", () => {
+    it("renders correctly with empty props", () => {
+      const tree = renderer.create(<Tab/>).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly disabled tab", () => {
+      const tree = renderer.create(<Tab disabled />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly active tab", () => {
+      const tree = renderer.create(<Tab active />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly focus tab", () => {
+      const tree = renderer.create(<Tab focus />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("renders correctly focus and active tab", () => {
+      const tree = renderer.create(<Tab focus active />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
 });
