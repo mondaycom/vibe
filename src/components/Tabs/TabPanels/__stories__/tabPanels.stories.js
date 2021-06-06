d96eb53d9050a00927ec13c27b18d28e258e9cd5
Liat Maor <liat@monday.com>
2021-06-06 13:20:06 +0300

    stories for tab panels

--- src/components/Tabs/TabPanels/__stories__/tabPanels.stories.js
+++ src/components/Tabs/TabPanels/__stories__/tabPanels.stories.js
@@ -2,15 +2,54 @@ import React from "react";
 import { withPerformance } from "storybook-addon-performance";
 import TabPanels from "../TabPanels";
 import TabPanel from "../../TabPanel/TabPanel";
+import DescriptionLabel from "../../../storybook-helpers/description-label/description-label";
+import { ComponentStateDescription, StoryStateRow, Divider } from "../../../storybook-helpers";
 
-export const Sandbox = () => (
-  <div>
-    <TabPanels>
+export const DefaultStory = () => (
+  <TabPanels>
+    <TabPanel>First tab panel</TabPanel>
+    <TabPanel>Second tab panel</TabPanel>
+    <TabPanel>Third tab panel</TabPanel>
+  </TabPanels>
+);
+
+export const Active = () => (
+  <section>
+    <ComponentStateDescription title="Custom active"/>
+    <DescriptionLabel>
+      Pass the specific tab id that is active
+    </DescriptionLabel>
+
+    <TabPanels activeTabId={1}>
       <TabPanel>First tab panel</TabPanel>
       <TabPanel>Second tab panel</TabPanel>
       <TabPanel>Third tab panel</TabPanel>
     </TabPanels>
-  </div>
+  </section>
+);
+
+export const Animations = () => (
+  <section>
+    <ComponentStateDescription title="Left to Right"/>
+    <StoryStateRow>
+      <TabPanels animationDirection="ltr">
+        <TabPanel>First tab panel</TabPanel>
+        <TabPanel>Second tab panel</TabPanel>
+        <TabPanel>Third tab panel</TabPanel>
+      </TabPanels>
+    </StoryStateRow>
+
+    <Divider />
+
+    <ComponentStateDescription title="Right to Left"/>
+    <StoryStateRow>
+      <TabPanels animationDirection="rtl">
+        <TabPanel>First tab panel</TabPanel>
+        <TabPanel>Second tab panel</TabPanel>
+        <TabPanel>Third tab panel</TabPanel>
+      </TabPanels>
+    </StoryStateRow>
+  </section>
 );
 
 export default {
