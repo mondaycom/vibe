37f5b42806e4d1452e9c545844b67b6f6af4fc4d
Moshe Zemah <moshe@monday.com>
2021-06-07 13:48:00 +0300

    Combobox fixes (#140)

--- src/components/Combobox/__stories__/combobox.stories.js
+++ src/components/Combobox/__stories__/combobox.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { action } from "@storybook/addon-actions";
 import { text, boolean, number, select } from "@storybook/addon-knobs";
 import { withPerformance } from "storybook-addon-performance";
@@ -20,29 +20,44 @@ export const Sandbox = () => (
   </div>
 );
 
-const getOptions = selectedId => {
-  return [
-    { id: "1", label: "first", leftIcon: "fa fa-star-o", selected: selectedId === "1" },
-    { id: "2", label: "second", rightIcon: "fa fa-star-o", selected: selectedId === "2" },
-    { id: "3", label: "disabled", disabled: true, rightIcon: "fa fa-star-o", selected: selectedId === "3" },
-    { id: "4", label: "fourth", selected: selectedId === "4" }
-  ];
+const getOptions = (selectedId, additionalOptions = []) => {
+  const options = [
+    { id: "1", label: "first", leftIcon: "fa fa-star-o" },
+    { id: "2", label: "second", rightIcon: "fa fa-star-o" },
+    { id: "3", label: "disabled", disabled: true, rightIcon: "fa fa-star-o" },
+    { id: "4", label: "fourth" }
+  ].concat(additionalOptions);
+
+  options.forEach(option => {
+    option.selected = option.id === selectedId;
+  });
+  return options;
 };
 
 const ComboboxWrapper = () => {
   const [selectedId, setSelectedId] = useState("2");
+  const [addedItems, setAddedItems] = useState([]);
+
+  const addNewItem = useCallback(
+    filterValue => {
+      const id = (5 + addedItems.length).toString();
+      setAddedItems(addedItems.concat([{ id, label: filterValue }]));
+      setSelectedId(id);
+    },
+    [addedItems, setAddedItems, setSelectedId]
+  );
 
   return (
     <div className="combobox-wrapper">
       <Combobox
         placeholder="Search for content"
-        onAddNew={value => console.log("Add new ", value)}
+        onAddNew={addNewItem}
         addNewLabel={value => `+ Add new ${value}`}
         onClick={option => {
           console.log("Clicked on ", option.label);
           setSelectedId(option.id);
         }}
-        options={getOptions(selectedId)}
+        options={getOptions(selectedId, addedItems)}
       />
     </div>
   );
