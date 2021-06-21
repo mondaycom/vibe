ea2c1d4fe09564043dc472b3c56a9bbb9006c10d
Dor Shaked <dorsh@monday.com>
2021-06-21 10:02:43 +0300

    fix: change apps icon svg

--- src/components/Icon/Icons/components/Radio.js
+++ src/components/Icon/Icons/components/Radio.js
@@ -3,9 +3,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 const Radio = ({size, ...props}) => (
-  <svg viewBox="0 0 16 16" fill="currentColor" width={ size || "16" } height={ size || "16" } {...props}>
-    <rect x=".75" y=".75" width="14.5" height="14.5" rx="7.25" stroke="currentColor" strokeWidth="1.5" />
-    <circle cx="8" cy="8" r="3" fill="currentColor" />
+  <svg viewBox="0 0 20 20" fill="currentColor" width={ size || "20" } height={ size || "20" } {...props}>
+    <path d="M10 16.5C6.41015 16.5 3.5 13.5899 3.5 10C3.5 6.41015 6.41015 3.5 10 3.5C13.5899 3.5 16.5 6.41015 16.5 10C16.5 13.5899 13.5899 16.5 10 16.5ZM2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10ZM10 13C11.6569 13 13 11.6569 13 10C13 8.34315 11.6569 7 10 7C8.34315 7 7 8.34315 7 10C7 11.6569 8.34315 13 10 13Z"
+      fill="currentColor" fillRule="evenodd" clipRule="evenodd" />
   </svg>
 );
 Radio.displayName = 'Radio';
