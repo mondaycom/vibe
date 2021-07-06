2beb0d02fdddb75d17c1afd2656088fa9105bbb4
Tom Bogin <tombo@monday.com>
2021-07-06 17:32:51 +0300

    more actions icon

--- src/Icons/iconsMetaData.js
+++ src/Icons/iconsMetaData.js
@@ -5,6 +5,13 @@ export const iconsMetaData = [
   // DO NOT REMOVE THIS FOLLOWING LINE!
   // plop_marker:icon_metadata
   {
+      name: "MoreActions",
+      file: "MoreActions.svg",
+      description: "Icons used for dropdown of more actions",
+      tags: "more,actions,dropdown,arrow"
+  },
+
+  {
       name: "Apps",
       file: "Apps.svg",
       description: "monday apps section",
