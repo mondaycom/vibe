87c07e52d7e0cca21ab6fea308bca25bbf8a8982
Hadas Farhi <hadasfa@monday.com>
2021-07-06 13:50:34 +0300

    feature: support type and size constants and add proptypes

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,2 +1,29 @@
-export const Avatar = ({ className, alt, size, src, icon }) => {
+import cx from "classnames";
+import PropTypes from "prop-types";
+import {
+  AVATAR_ICON_TYPE,
+  AVATAR_IMAGE_TYPE,
+  AVATAR_LARGE_SIZE,
+  AVATAR_MEDIUM_SIZE,
+  AVATAR_SMALL_SIZE,
+  AVATAR_TEXT_TYPE
+} from "./AvatarConstants";
+
+export const AVATAR_CSS_BASE_CLASS = "monday-style-avatar";
+export const Avatar = ({ className, alt, size, type, isSupportEmptyState }) => {
+  return <div className={cx(AVATAR_CSS_BASE_CLASS)} />;
+};
+Avatar.PropTypes = {
+  className: PropTypes.string,
+  alt: PropTypes.string,
+  size: PropTypes.oneOf([AVATAR_LARGE_SIZE, AVATAR_MEDIUM_SIZE, AVATAR_SMALL_SIZE]),
+  type: PropTypes.oneOf([AVATAR_ICON_TYPE, AVATAR_IMAGE_TYPE, AVATAR_TEXT_TYPE]),
+  isSupportEmptyState: PropTypes.bool
+};
+Avatar.defaultProps = {
+  className: "",
+  alt: "",
+  size: AVATAR_MEDIUM_SIZE,
+  type: AVATAR_IMAGE_TYPE,
+  isSupportEmptyState: true
 };
---
+++ src/components/Avatar/AvatarConstants.jsx
@@ -0,0 +1,6 @@
+export const AVATAR_SMALL_SIZE = "small";
+export const AVATAR_MEDIUM_SIZE = "medium";
+export const AVATAR_LARGE_SIZE = "large";
+export const AVATAR_IMAGE_TYPE = "image";
+export const AVATAR_TEXT_TYPE = "text";
+export const AVATAR_ICON_TYPE = "icon";
