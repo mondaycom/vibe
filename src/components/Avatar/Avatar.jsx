e9ba56e57a849428d5fb948c0603616e4a10ee5f
Hadas Farhi <hadasfa@monday.com>
2021-07-07 09:10:49 +0300

    wip

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,40 +1,38 @@
-import cx from "classnames";
 import PropTypes from "prop-types";
+import cx from "classnames";
+import { useMemo } from "react";
 import { BEMClass } from "../../helpers/bem-helper";
-import Icon from "../Icon/Icon";
 import { AVATAR_SIZES } from "./AvatarConstants";
 import "./Avatar.scss";
+import { getCSSVar } from "../../services/themes";
+import { getSelectedColor } from "../../general-stories/colors/colors-vars-map";
 
 const AVATAR_CSS_BASE_CLASS = "monday-style-avatar";
 const bemHelper = BEMClass(AVATAR_CSS_BASE_CLASS);
 
 // TODO: still need to figure it out: color
-export const Avatar = ({
-  className,
-  size,
-  img,
-  text,
-  icon,
-  iconProps,
-  isSupportEmptyState,
-  role,
-  ariaLabel,
-  backgroundColor
-}) => {
-  // Get avatar content
-  let avatarContent;
-  if (img) avatarContent = <img role={role} alt={ariaLabel} src={img} />;
-  else if (icon) avatarContent = <Icon {...iconProps} ariaLabel={ariaLabel} role={role} />;
-  else if (text === "" && isSupportEmptyState) <div />;
-  else
-    avatarContent = (
-      <span aria-label={ariaLabel} role={role}>
-        {text}
-      </span>
-    );
+export const Avatar = ({ className, size, img, text, isSupportEmptyState, role, ariaLabel, backgroundColor }) => {
+  let avatarContent = useMemo(() => {
+    if (img) avatarContent = <img role={role} alt={ariaLabel} src={img} />;
+    else if (text === "" && isSupportEmptyState) <div />;
+    else
+      avatarContent = (
+        <span aria-label={ariaLabel} role={role}>
+          {text}
+        </span>
+      );
+    return avatarContent;
+  });
+
+  const backgroundColorStyle = useMemo(() => {
+    return { backgroundColor: getSelectedColor(backgroundColor) };
+  }, [backgroundColor]);
 
   return (
-    <div className={cx(AVATAR_CSS_BASE_CLASS, bemHelper({ state: size }), className)} style={{ backgroundColor }}>
+    <div
+      className={cx(AVATAR_CSS_BASE_CLASS, bemHelper({ state: size }), className)}
+      style={{ backgroundColor: backgroundColorStyle }}
+    >
       {avatarContent}
     </div>
   );
