13870a6f13e29a2e9fad80141e92284143f30623
Tom Bogin <tombo@monday.com>
2021-07-28 10:54:57 +0300

    variable

--- scripts/generate-meta.js
+++ scripts/generate-meta.js
@@ -10,7 +10,8 @@ async function addFiles() {
     if(fileWithNoMeta && fileWithNoMeta.length > 0) {
         for(const file of fileWithNoMeta) {
             console.log(`generating meta for file: ${file}`);
-            const answers = await IconGenerator.runPrompts([file.substr(0, file.indexOf(".")), file]);
+            const iconName = file.substr(0, file.indexOf("."));
+            const answers = await IconGenerator.runPrompts([iconName, file]);
             await IconGenerator.runActions(answers);
         }
     }
