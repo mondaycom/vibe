50e110c56e1609dbd34e2d67004fb5f48bfdb332
Tom Bogin <tombo@monday.com>
2021-08-02 10:00:30 +0300

    fixes

--- jest.init.js
+++ jest.init.js
@@ -2,11 +2,15 @@ import 'regenerator-runtime/runtime'
 import ReactDOM from "react-dom";
 
 ReactDOM.createPortal = node => node;
+const { testing } = process.env;
+const TESTING_STORYBOOK = testing === "storybook";
 
-jest.mock('react-transition-group', () => {
-  const FakeTransition = jest.fn(({children}) => children)
-  const FakeCSSTransition = jest.fn(props =>
-    props.in ? <FakeTransition>{props.children}</FakeTransition> : null,
-  )
-  return {CSSTransition: FakeCSSTransition, Transition: FakeTransition, SwitchTransition: FakeTransition}
-})
+if (TESTING_STORYBOOK) {
+  jest.mock('react-transition-group', () => {
+    const FakeTransition = jest.fn(({children}) => children)
+    const FakeCSSTransition = jest.fn(props =>
+      props.in ? <FakeTransition>{props.children}</FakeTransition> : null,
+    )
+    return {CSSTransition: FakeCSSTransition, Transition: FakeTransition, SwitchTransition: FakeTransition}
+  })
+}
