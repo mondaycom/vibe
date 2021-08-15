787d152b1c04246df2d9b94279fb90d69de05c26
Ayelet Holtzman <ayeleth@monday.com>
2021-08-15 13:32:55 +0300

    Ayelet/combobox/combobox ellipsis (#198)
    * add more padding to the categories
    
    * fix the categories sort bug
    
    * add ellipsis on long names
    
    * add tests for the service

--- src/components/Combobox/__stories__/combobox.stories.js
+++ src/components/Combobox/__stories__/combobox.stories.js
@@ -43,7 +43,9 @@ const getOptions = (selectedId, additionalOptions = []) => {
     { id: "3", label: "disabled", disabled: true, rightIcon: "fa fa-star-o" },
     { id: "4", label: "custom left icon", leftIcon: iconRenderer, leftIconType: Combobox.iconTypes.RENDERER },
     { id: "5", label: "custom right icon", rightIcon: iconRenderer, rightIconType: Combobox.iconTypes.RENDERER },
-    { id: "6", label: "no icon" }
+    { id: "6", label: "no icon" },
+    { id: "7", label: "with left icon and a very very long name", leftIcon: "fa fa-star-o" },
+    { id: "8", label: "with right icon and a very very long name", rightIcon: "fa fa-star-o" }
   ].concat(additionalOptions);
 
   options.forEach(option => {
