47243543852eaf595772580ccec1ccc136d6c3b3
Liat Maor <liat@monday.com>
2021-10-11 12:00:11 +0300

    add dark mode for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -53,5 +53,5 @@ module.exports = {
   webpackFinal: async config => {
     return buildConfig(config);
   },
-  addons: ["storybook-addon-themes", "@storybook/addon-a11y", "storybook-addon-performance/register"]
+  addons: ["storybook-addon-themes", "@storybook/addon-a11y", "storybook-addon-performance/register", "storybook-dark-mode"]
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,7 +12,11 @@ addParameters({
     { name: "Light", class: "light-app-them", color: "#ffffff", default: true },
     { name: "Dark", class: "dark-app-theme", color: "#1C1F3B" },
     { name: "Black", class: "black-app-theme", color: "#111111" }
-  ]
+  ],
+  darkMode: {
+    // Set the initial theme
+    current: 'light'
+  }
 });
 
 const storiesLoaderFunction = () => [...loadFoundationsStories()];
