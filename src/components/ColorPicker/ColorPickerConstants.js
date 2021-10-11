bd53a7434881c52f4537c144ef41ac4d302b342c
Amit Hanoch <amitha@monday.com>
2021-10-11 15:27:19 +0300

    pr notes 1

--- src/components/ColorPicker/ColorPickerConstants.js
+++ src/components/ColorPicker/ColorPickerConstants.js
@@ -1,9 +1,11 @@
+import { SIZES } from "../../constants/sizes";
+
 export const DEFAULT_NUMBER_OF_COLORS_IN_LINE = 5;
 
 export const COLOR_SIZES = {
-  small: 24,
-  medium: 32,
-  large: 40
+  [SIZES.SMALL]: 24,
+  [SIZES.MEDIUM]: 32,
+  [SIZES.LARGE]: 40
 };
 
 export const COLOR_PADDING = 4;
