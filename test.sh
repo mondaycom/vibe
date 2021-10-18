9e7ea5195ae2353ab286214008a1a5892ed38e8c
Sahar Brodbeker <saharonb@gmail.com>
2021-10-18 17:32:36 +0300

    Remove test file

--- test.sh
+++
@@ -1,4 +0,0 @@
-echo $(npm view monday-ui-react-core version)
-echo $(node -e "console.log(require('./package.json').version)")
-
-[[ $(npm view monday-ui-react-core version) = $(node -e "console.log(require('./package.json').version)") ]] && echo "yesy" && echo "123" || echo "/no"
