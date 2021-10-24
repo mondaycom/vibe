643d365128e73c9a847a2ac2b88388f939bc53f7
Sahar Brodbeker <saharonb@gmail.com>
2021-10-24 19:27:47 +0300

    Fix script

--- scripts/release.js
+++ scripts/release.js
@@ -64,12 +64,7 @@ function formatChanges(changelogText) {
 }
 
 function buildChangelogSinceLastVersion() {
-  const { stdout } = execa.sync("npx", [
-    "lerna-changelog",
-    "--from",
-    `v${require("../package.json").version}`,
-    "--next-version-from-metadata"
-  ]);
+  const { stdout } = execa.sync("npx", ["lerna-changelog", "--from", require("../package.json").version]);
 
   return stdout;
 }
@@ -93,7 +88,7 @@ function getCurrentVersion() {
 }
 
 function bumpVersion(strategy) {
-  execa.sync("npm", ["version", strategy, "--git-tag-version=false"]);
+  execa.sync("npm", ["version", strategy]);
 }
 
 function logNothingToDo() {
