5d67c85c1ca6e0c82bd61320048cc43b46f0634a
Hadas Farhi <hadasfa@monday.com>
2021-10-25 16:06:26 +0300

    fix snapshots tests

--- src/components/Accordion/Accordion/__tests__/accordion.jest.js
+++ src/components/Accordion/Accordion/__tests__/accordion.jest.js
@@ -3,33 +3,35 @@ import renderer from "react-test-renderer";
 import Accordion from "../Accordion";
 import AccordionItem from "../../AccordionItem/AccordionItem";
 
-it("renders correctly with empty props", () => {
-  const tree = renderer.create(<Accordion />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe("Accordion snapshot tests", () => {
+  it("renders correctly with empty props", () => {
+    const tree = renderer.create(<Accordion />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it("renders correctly with accordion items", () => {
-  const tree = renderer
-    .create(
-      <Accordion>
-        <AccordionItem />
-        <AccordionItem />
-        <AccordionItem />
-      </Accordion>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it("renders correctly with accordion items", () => {
+    const tree = renderer
+      .create(
+        <Accordion>
+          <AccordionItem />
+          <AccordionItem />
+          <AccordionItem />
+        </Accordion>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it("renders correctly with defaultIndex prop", () => {
-  const tree = renderer
-    .create(
-      <Accordion defaultIndex={[0, 2]}>
-        <AccordionItem />
-        <AccordionItem />
-        <AccordionItem />
-      </Accordion>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  it("renders correctly with defaultIndex prop", () => {
+    const tree = renderer
+      .create(
+        <Accordion defaultIndex={[0, 2]}>
+          <AccordionItem />
+          <AccordionItem />
+          <AccordionItem />
+        </Accordion>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
