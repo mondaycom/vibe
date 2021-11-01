9f23919819a3cd5c6ab404216d6c72785e30f81a
Hadas Farhi <hadasfa@monday.com>
2021-11-01 14:17:38 +0200

    remove component name attention box dependency

--- src/storybook/components/component-name/component-name.jsx
+++ src/storybook/components/component-name/component-name.jsx
@@ -1,18 +1,5 @@
-import { AttentionBox } from "../../../components";
-import { BEMClass } from "../../../helpers/bem-helper";
 import "./component-name.scss";
 
-const CSS_BASE_CLASS = "monday-storybook-component-name";
-const bemHelper = BEMClass(CSS_BASE_CLASS);
-
 export const ComponentName = ({ children }) => {
-  return (
-    <AttentionBox
-      icon={null}
-      componentClassName={CSS_BASE_CLASS}
-      ariaLevel={1}
-      titleClassName={bemHelper({ element: "title" })}
-      title={children}
-    />
-  );
+  return <h1 className="monday-storybook-component-name">{children}</h1>;
 };
