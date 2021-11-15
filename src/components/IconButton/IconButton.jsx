b086ca23bf3f06b5f1debe32d4cdecb4ea4c450a
Orr Gottlieb <orr@monday.com>
2021-11-15 15:14:51 +0200

    tests!

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, forwardRef, useMemo } from "react";
 import cx from "classnames";
 import PropTypes from "prop-types";
+import NOOP from "lodash/noop";
 import useMergeRefs from "../../hooks/useMergeRefs";
 import ToolTip from "../Tooltip/Tooltip";
 import Button from "../Button/Button";
@@ -11,7 +12,7 @@ import { SIZES } from "../../constants/sizes";
 import "./IconButton.scss";
 
 const IconButton = forwardRef(
-  ({ className, id, icon, size, tooltipContent, ariaLabel, kind, active, disabled, disabledReason }, ref) => {
+  ({ className, id, icon, size, tooltipContent, ariaLabel, kind, active, disabled, disabledReason, onClick }, ref) => {
     const componentRef = useRef(null);
     const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
     const buttonAriaLabel = useMemo(() => {
@@ -59,6 +60,7 @@ const IconButton = forwardRef(
     return (
       <ToolTip content={content}>
         <Button
+          onClick={onClick}
           disabled={disabled}
           kind={kind}
           ariaLabel={buttonAriaLabel}
@@ -86,10 +88,29 @@ IconButton.sizes = SIZES;
 IconButton.kinds = Button.kinds;
 
 IconButton.propTypes = {
+  /**
+   * id to be added to the element
+   */
   id: PropTypes.string,
+  /**
+   * callback function when clicking the icon button
+   */
+  onClick: PropTypes.func,
+  /**
+   * class to be added to the element
+   */
   className: PropTypes.string,
+  /**
+   * Icon to be rendered
+   */
   icon: PropTypes.oneOfType([PropTypes.string, PropTypes.node, PropTypes.element, PropTypes.func]),
+  /**
+   * a11y property to be added, used for screen reader to know what kind of button it is
+   */
   ariaLabel: PropTypes.string,
+  /**
+   * Size of the icon
+   */
   size: PropTypes.oneOf([
     IconButton.sizes.XXS,
     IconButton.sizes.XS,
@@ -97,14 +118,27 @@ IconButton.propTypes = {
     IconButton.sizes.MEDIUM,
     IconButton.sizes.LARGE
   ]),
+  /**
+   * Tooltip wraps the button icon, it will display in the tooltip, if not present the aria label will be shown
+   */
   tooltipContent: PropTypes.string,
+  /**
+   * Kind of button - like <Button />
+   */
   kind: PropTypes.oneOf([IconButton.kinds.PRIMARY, IconButton.kinds.SECONDARY, IconButton.kinds.TERTIARY]),
+  /**
+   * disabled state
+   */
   disabled: PropTypes.bool,
+  /**
+   * if disabled - this will be shown in the tooltip
+   */
   disabledReason: PropTypes.string
 };
 
 IconButton.defaultProps = {
   className: undefined,
+  onClick: NOOP,
   id: undefined,
   icon: AddSmall,
   ariaLabel: undefined,
