dca3ddea90b9f09d894a029efe9c4c7b202f1b1d
Kandio16 <kandio1604@gmail.com>
2021-11-25 10:46:26 +0200

    stories/split-btn

--- src/components/SplitButton/__stories__/SplitButtonExampleDialog.jsx
+++ src/components/SplitButton/__stories__/SplitButtonExampleDialog.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
-import { Check, Search, Filter } from "../../Icon/Icons";
+import { Check } from "../../Icon/Icons";
 import Icon from "../../Icon/Icon";
 import "./splitButton.stories.scss";
 
@@ -8,29 +9,30 @@ const SplitButtonExampleOption = ({ option }) => {
   const text = option.text;
 
   return (
-    <div class="monday-storybook-split-button_dialog_option">
-      {icon 
-        ? <Icon 
-          iconType={Icon.type.ICON_FONT} 
-          clickable={false} 
+    <div className="monday-storybook-split-button_dialog_option">
+      {icon ? (
+        <Icon
+          iconType={Icon.type.ICON_FONT}
+          clickable={false}
           icon={icon}
           className="monday-storybook-split-button_dialog_icon-spacing"
           iconSize={20}
-          ignoreFocusStyle />
-        : null
-      }
+          ignoreFocusStyle
+        />
+      ) : null}
       {text}
     </div>
   );
 };
 
-const SplitButtonExampleDialog = ({ options = [{icon: Check, text: 'Hey'}] }) => {
-  return <div className="monday-storybook-split-button_dialog">
-    {!options 
-      ? <>I would be anything you want to be</>
-      : options.map(option => <SplitButtonExampleOption option={option} />)
-    }
-  </div>;
+const SplitButtonExampleDialog = ({ options = [{ icon: Check, text: 'Hey' }] }) => {
+  return (
+    <div className="monday-storybook-split-button_dialog">
+      {!options
+        ? <>I would be anything you want to be</>
+        : options.map(option => <SplitButtonExampleOption option={option} />)}
+    </div>
+  );
 };
 
 export default SplitButtonExampleDialog;
