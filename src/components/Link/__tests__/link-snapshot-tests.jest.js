15d07c6c4fc6f9e678e5d92aa38cc467f9bdbe9b
Kandio16 <kandio1604@gmail.com>
2021-11-30 16:37:50 +0200

    stories/add-link-tests

---
+++ src/components/Link/__tests__/link-snapshot-tests.jest.js
@@ -0,0 +1,30 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Link from "../Link";
+
+describe("Link renders correctly", () => {
+  it("with icon", () => {
+    const tree = renderer.create(<Link icon="fa fa-star" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with right icon", () => {
+    const tree = renderer.create(<Link icon="fa fa-star" iconPosition={Link.position.END} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with id", () => {
+    const tree = renderer.create(<Link id="TestId" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with componentClassName", () => {
+    const tree = renderer.create(<Link componentClassName="testClassName" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with text", () => {
+    const tree = renderer.create(<Link text="Link" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
