8ce6055a4e62c8a8a4181a765fc24430c9565c18
Kandio16 <kandio1604@gmail.com>
2021-12-01 11:02:35 +0200

    stories/add-text-field-snapshot-tests

--- src/components/TextField/__tests__/textField-snapshot-tests.jest.js
+++ src/components/TextField/__tests__/textField-snapshot-tests.jest.js
@@ -2,49 +2,74 @@ import React from "react";
 import renderer from "react-test-renderer";
 import TextField from "../TextField";
 
-/**
- * There are cases where the component we want to test in the snapshot test will contain additional components.
- We do not want changes to the additional components to fail our component snapshot's test.
- Therefore, we will replace the instances of the other external components in the snapshot test with mock/stub components in these cases.
- */
-
-/** example for external library
- jest.mock("react-transition-group", () => {
-  const FakeTransition = jest.fn(({ children }) => children);
-  const FakeSwitchTransition = jest.fn(({ children }) => children);
-  const FakeCSSTransition = jest.fn(({ children }) => children);
-
-  // We return here the instance of the mock / stub library object content
-  return {
-    CSSTransition: FakeCSSTransition,
-    Transition: FakeTransition,
-    SwitchTransition: FakeSwitchTransition
-  };
-});
- **/
-
-/** example for internal component
-jest.mock("../../Button/Button", () => {
-  // We return here the instance of the mock / stub component
-  return ({ onClick }) => (
-    <div data-testid="cancel-button" {...(onClick && { "data-onclick": "onclick-provided" })} />
-  );
-});
-**/
-
 describe("TextField renders correctly", () => {
-    it("with empty props", () => {
-      const tree = renderer.create(<TextField />).toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+  it("with placeholder", () => {
+    const tree = renderer.create(<TextField placeholder="placeholder" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-    it("with x", () => {
-      const tree = renderer.create(<TextField />).toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+  it("with value", () => {
+    const tree = renderer.create(<TextField value="value" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-    it("with y", () => {
-          const tree = renderer.create(<TextField />).toJSON();
-          expect(tree).toMatchSnapshot();
-    });
+  it("when disabled", () => {
+    const tree = renderer.create(<TextField disabled />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with wrapperClassName", () => {
+    const tree = renderer.create(<TextField wrapperClassName="testClassName" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with className", () => {
+    const tree = renderer.create(<TextField className="testClassName" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with id", () => {
+    const tree = renderer.create(<TextField id="testId" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with validation", () => {
+    const tree = renderer.create(<TextField validation={{ status: "error", text: "error" }} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with showCharCount", () => {
+    const tree = renderer.create(<TextField showCharCount />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("when loading", () => {
+    const tree = renderer.create(<TextField loading />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with another type", () => {
+    const tree = renderer.create(<TextField type={TextField.types.PASSWORD} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with large size", () => {
+    const tree = renderer.create(<TextField size={TextField.sizes.LARGE} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+  it("with icon", () => {
+    const tree = renderer.create(<TextField iconName="fa-star" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+  it("with  labelIconName", () => {
+    const tree = renderer.create(<TextField labelIconName="fa-star" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+  it("when readonly", () => {
+    const tree = renderer.create(<TextField readonly />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
