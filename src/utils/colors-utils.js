a0f3f7b39618ee6ade838a72bd2ecfae5408c8a1
Hadas Farhi <hadasfa@monday.com>
2021-12-14 10:44:37 +0200

    export color utils as default object (for fixing error while trying to create new version) (#387)

--- src/utils/colors-utils.js
+++ src/utils/colors-utils.js
@@ -1,4 +1,8 @@
 import { COLOR_STYLES } from "../general-stories/colors/colors-vars-map";
 
-export const getMondayColorAsStyle = (color, mode = COLOR_STYLES.REGULAR, withVar = true) =>
-  `${withVar ? "var(" : ""}--color-${color}${mode !== COLOR_STYLES.REGULAR ? `-${mode}` : ""}${withVar ? ")" : ""}`;
+const ColorUtils = {
+  getMondayColorAsStyle: (color, mode = COLOR_STYLES.REGULAR, withVar = true) =>
+    `${withVar ? "var(" : ""}--color-${color}${mode !== COLOR_STYLES.REGULAR ? `-${mode}` : ""}${withVar ? ")" : ""}`
+};
+
+export default Object.freeze(ColorUtils);
