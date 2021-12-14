09ed6a41ac14c547df2225e585a5e8081329af82
Hadas Farhi <hadasfa@monday.com>
2021-12-14 17:11:06 +0200

    add temp assets until receiving good assets

--- src/storybook/stand-alone-documentaion/spacing/spacing-sizes/spacing-sizes.jsx
+++ src/storybook/stand-alone-documentaion/spacing/spacing-sizes/spacing-sizes.jsx
@@ -1,8 +1,9 @@
 import SpacingComponent from "./spacing-component/spacing-component";
+import { Frame } from "../../../components";
 import "./spacing-sizes.scss";
 
 export const SpacingSizes = () => (
-  <div className="monday-storybook-spacing-sizes">
+  <Frame className="monday-storybook-spacing-sizes">
     <SpacingComponent title="XS" sizeInPx="4px" spacingClass="spacing-xs" />
     <SpacingComponent title="S" sizeInPx="8px" spacingClass="spacing-small" />
     <SpacingComponent title="MD" sizeInPx="16px" spacingClass="spacing-medium" />
@@ -10,5 +11,5 @@ export const SpacingSizes = () => (
     <SpacingComponent title="XL" sizeInPx="32px" spacingClass="spacing-xl" />
     <SpacingComponent title="XXL" sizeInPx="48px" spacingClass="spacing-xxl" />
     <SpacingComponent title="XXXL" sizeInPx="64px" spacingClass="spacing-xxxl" />
-  </div>
+  </Frame>
 );
