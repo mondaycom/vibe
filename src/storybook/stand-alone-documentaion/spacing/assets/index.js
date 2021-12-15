8b4782aecb659b171b17aef641f04e6289d9f405
Hadas Farhi <hadasfa@monday.com>
2021-12-15 10:44:18 +0200

    fix spacing assets

--- src/storybook/stand-alone-documentaion/spacing/assets/index.js
+++ src/storybook/stand-alone-documentaion/spacing/assets/index.js
@@ -1,5 +1,5 @@
 import menuExample from "./menuExample.png";
 import tipseenExample from "./tipseenExample.png";
-import toolbarExample from "./toolbarExample.jpg";
+import toolbarExample from "./toolbarExample.png";
 
 export { menuExample, tipseenExample, toolbarExample };
