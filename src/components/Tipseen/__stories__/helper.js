d902afacdb7e62d06064e4e14e916bac80b9b456
Ivan Harasymchuk <ivan7790@gmail.com>
2021-12-15 14:48:13 +0200

    Leany labs/storybook 6 (#380)
    * Leany labs/storybook 1 (#1)
    
    * story: avatar-init
    
    * stories: chips
    
    * stories/delete-old-stories
    
    * stories/add-label
    
    * stories/toggle-init
    
    * stories/add-ArgsTable
    
    * stories/toggle-init
    
    * stories/add-checkbox
    
    * stories/add-radioButton
    
    * stories/fix-btn-group
    
    * stories/add-src
    
    * stories/fix
    
    * stories/add-search-bar-ui
    
    * stories/search-person-column
    
    * stories/fix
    
    * stories/fix
    
    * stories/fix
    
    * chip/test/init
    
    * test/add-chips-tests
    
    * clean up
    
    * stories/add-icons
    
    * stories/add-components
    
    * stories/fix-icons-name
    
    * stories/small-refactoring
    
    * stories/little-fix
    
    * stories/fix
    
    * test/add-tests-plop
    
    * change styling
    
    * remove unused files
    
    * add spaces
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>
    
    * Leany labs/storybook 1 (#2)
    
    docs: create new storybooks for Avatar, Chip, Toggle
    tests: change style and expend tests for Avatar, Chip, Toggle, Button
    
    * remove obsolete snapshot
    
    * revert Button test
    
    * remove spaces
    
    * chips: style tests
    
    * LeanyLabs/storybook 2 (#3)
    
    * story: avatar-init
    
    * stories: chips
    
    * stories/delete-old-stories
    
    * stories/add-label
    
    * stories/toggle-init
    
    * stories/add-ArgsTable
    
    * stories/toggle-init
    
    * stories/add-checkbox
    
    * stories/add-radioButton
    
    * stories/fix-btn-group
    
    * stories/add-src
    
    * stories/fix
    
    * stories/add-search-bar-ui
    
    * stories/search-person-column
    
    * stories/fix
    
    * stories/fix
    
    * stories/fix
    
    * chip/test/init
    
    * test/add-chips-tests
    
    * clean up
    
    * stories/add-icons
    
    * stories/add-components
    
    * stories/fix-icons-name
    
    * stories/small-refactoring
    
    * stories/little-fix
    
    * stories/fix
    
    * test/add-tests-plop
    
    * stories/add-link
    
    * stories/label-init
    
    * stories/add-radio-button
    
    * stories/add-RC
    
    * stories/add-checkbox
    
    * stories/fix-comments
    
    * stories/add
    
    * stories/fix
    
    * stories/merge
    
    * stories/some-fix
    
    * tests/add-tests
    
    * stories/add-link
    
    * stories/fix
    
    * stories/add-size
    
    * fix: small changes in checkbox stories
    
    * polishig
    
    * renamed file according to styles
    
    * polishing
    
    * polishing
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>
    
    * Leany labs/storybook 2a (#4)
    
    * fix/fix-coments
    
    * minor changes
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>
    
    * Leany labs/storybook 2b (#5)
    
    * stories/fix-comments
    
    * stories/add-checkBox-name
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>
    
    * fix: names for checkbox
    
    * fix: add missing link
    
    * Leany labs/storybook 3 (#6)
    
    docs: create new storybooks for Attention box, Alert banner, Toast
    tests: change style and expand tests for Attention box, Alert banner, Toast
    
    * Leany labs/storybook 3a (#7)
    
    * fix
    
    * stories/fix-comments
    
    * stories/add-toast-test
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>
    
    * Leany labs/storybook 3b (#8)
    
    * stories-3b/init
    
    * stories-3b/toasts
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>
    
    * stories/fix-demo-comments
    
    * refactor: remove line before closing bracket
    
    * stories/steps-init
    
    * stories/tipseen-init
    
    * steps/tests-init
    
    * stories/tooltip-init
    
    * stories/tipseen-styles-init
    
    * stories/styles-tooltip
    
    * stories/tooltip-tests-init
    
    * stories/steps-clear
    
    * stories/steps-fix
    
    * stories/tipseen-styles
    
    * stories/tooltip
    
    * stories/tooltip-styles
    
    * RC-Links
    
    * fix-steps
    
    * fix-styles-steps
    
    * tipseen-snapshot-tests
    
    * fix-after-chromatic-review
    
    * remove-class
    
    * tipseen-clear
    
    * fix
    
    * tooltip-tests
    
    * fix: rename files
    
    * fix: line gaps
    
    * fix-coments
    
    * fix-comments
    
    * tipseen-fix-children
    
    Co-authored-by: Kandio16 <kandio1604@gmail.com>

---
+++ src/components/Tipseen/__stories__/helper.js
@@ -0,0 +1,14 @@
+export const modifiers = [
+  {
+    name: "preventOverflow",
+    options: {
+      mainAxis: false
+    }
+  },
+  {
+    name: "flip",
+    options: {
+      fallbackPlacements: []
+    }
+  }
+];
