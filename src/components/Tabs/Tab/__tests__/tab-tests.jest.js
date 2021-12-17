3e893f3abcf263fcfa8340d3560e47a144d87a98
Kandio16 <kandio1604@gmail.com>
2021-12-17 10:20:07 +0200

    tab-tests

---
+++ src/components/Tabs/Tab/__tests__/tab-tests.jest.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Tab from "../Tab";
+
+
+
+describe("Chips tests", () => {
+  let onClickMock;
+  const text = "tab"
+  let tabComponent;
+  
+  beforeEach(() => {
+    onClickMock = jest.fn();
+    tabComponent = render(
+      <Tab onClick={onClickMock}>{text}</Tab>
+    );
+  });
+
+  it("should call the click callback when clicked", () => {
+    fireEvent.click(screen.queryByText(text));
+    expect(onClickMock.mock.calls.length).toBe(1);
+  });
+});
