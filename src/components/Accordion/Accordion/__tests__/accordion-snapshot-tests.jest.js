a12ac3250fdb15deaafecb9fd0aa26aaeef953f4
Kandio16 <kandio1604@gmail.com>
2021-12-17 10:01:53 +0200

    accordin-tests

--- src/components/Accordion/Accordion/__tests__/accordion-snapshot-tests.jest.js
+++ src/components/Accordion/Accordion/__tests__/accordion-snapshot-tests.jest.js
@@ -3,117 +3,58 @@ import renderer from "react-test-renderer";
 import Accordion from "../Accordion";
 import AccordionItem from "../../AccordionItem/AccordionItem";
 
-
-describe("Snapshot tests", () => {
-	describe("Accordion renders correctly", () => {
-		it("with empty props", () => {
-			const tree = renderer.create(<Accordion />).toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-	
-		it("with accordion items", () => {
-			const tree = renderer
-				.create(
-					<Accordion>
-						<AccordionItem />
-						<AccordionItem />
-						<AccordionItem />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-	
-		it("with defaultIndex prop", () => {
-			const tree = renderer
-				.create(
-					<Accordion defaultIndex={[0]}>
-						<AccordionItem />
-						<AccordionItem />
-						<AccordionItem />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-	
-		it("with className", () => {
-			const tree = renderer.create(<Accordion className="test" />).toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-	
-		it("with id", () => {
-			const tree = renderer.create(<Accordion id="test" />).toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-	
-		it("with allowMultiple", () => {
-			const tree = renderer
-				.create(
-					<Accordion allowMultiple defaultIndex={[0, 2]}>
-						<AccordionItem />
-						<AccordionItem />
-						<AccordionItem />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
+describe("Accordion renders correctly", () => {
+	it("with empty props", () => {
+		const tree = renderer.create(<Accordion />).toJSON();
+		expect(tree).toMatchSnapshot();
 	});
-	describe("AccordionItem renders correctly", () => {
-		it("with className", () => {
-			const tree = renderer
-				.create(
-					<Accordion defaultIndex={[0]}>
-						<AccordionItem className="test" />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
 
-		it("with id", () => {
-			const tree = renderer
-				.create(
-					<Accordion defaultIndex={[0]}>
-						<AccordionItem id="test" />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-
-		it("with title", () => {
-			const tree = renderer
-				.create(
-					<Accordion defaultIndex={[0]}>
-						<AccordionItem title="Test title" />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-
-		it("with children", () => {
-			const tree = renderer
-				.create(
-					<Accordion defaultIndex={[0]}>
-						<AccordionItem>Test Children</AccordionItem>
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-
-		it("with iconSize", () => {
-			const tree = renderer
-				.create(
-					<Accordion defaultIndex={[0]}>
-						<AccordionItem iconSize={36} />
-					</Accordion>
-				)
-				.toJSON();
-			expect(tree).toMatchSnapshot();
-		});
+	it("with accordion items", () => {
+		const tree = renderer
+			.create(
+				<Accordion>
+					<AccordionItem />
+					<AccordionItem />
+					<AccordionItem />
+				</Accordion>
+			)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
 	});
-})
+
+	it("with defaultIndex prop", () => {
+		const tree = renderer
+			.create(
+				<Accordion defaultIndex={[0]}>
+					<AccordionItem />
+					<AccordionItem />
+					<AccordionItem />
+				</Accordion>
+			)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it("with className", () => {
+		const tree = renderer.create(<Accordion className="test" />).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it("with id", () => {
+		const tree = renderer.create(<Accordion id="test" />).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it("with allowMultiple", () => {
+		const tree = renderer
+			.create(
+				<Accordion allowMultiple defaultIndex={[0, 2]}>
+					<AccordionItem />
+					<AccordionItem />
+					<AccordionItem />
+				</Accordion>
+			)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
---
+++ src/components/Accordion/AccordionItem/__tests__/accordionItem-tests.jest.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Accordion from "../../Accordion/Accordion.jsx";
+import AccordionItem from "../AccordionItem";
+import { render, fireEvent, screen } from "@testing-library/react";
+
+describe("Chips tests", () => {
+  let onClickMock;
+  let accordionComponent;
+  
+  beforeEach(() => {
+    onClickMock = jest.fn();
+    accordionComponent = render(
+      <Accordion >
+        <AccordionItem onClick={onClickMock} />
+      </Accordion>
+    );
+  });
+
+  it("should call the click callback when clicked", () => {
+    fireEvent.click(screen.queryByRole('button'));
+    expect(onClickMock.mock.calls.length).toBe(1);
+  });
+});
