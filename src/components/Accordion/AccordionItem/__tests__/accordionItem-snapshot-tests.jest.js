fd57715bdf324d820cfbc7ded36603116825ed6f
Ivan Harasymchuk <ivan7790@gmail.com>
2021-12-20 15:08:38 +0200

    fix: identations

--- src/components/Accordion/AccordionItem/__tests__/accordionItem-snapshot-tests.jest.js
+++ src/components/Accordion/AccordionItem/__tests__/accordionItem-snapshot-tests.jest.js
@@ -7,55 +7,55 @@ describe("AccordionItem renders correctly", () => {
   it("with className", () => {
     const tree = renderer
       .create(
-          <Accordion defaultIndex={[0]}>
-              <AccordionItem className="test" />
-          </Accordion>
+        <Accordion defaultIndex={[0]}>
+          <AccordionItem className="test" />
+        </Accordion>
       )
       .toJSON();
-      expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
   });
 
   it("with id", () => {
     const tree = renderer
-        .create(
-            <Accordion defaultIndex={[0]}>
-                <AccordionItem id="test" />
-            </Accordion>
-        )
-        .toJSON();
+      .create(
+        <Accordion defaultIndex={[0]}>
+          <AccordionItem id="test" />
+        </Accordion>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with title", () => {
     const tree = renderer
-        .create(
-            <Accordion defaultIndex={[0]}>
-                <AccordionItem title="Test title" />
-            </Accordion>
-        )
-        .toJSON();
+      .create(
+        <Accordion defaultIndex={[0]}>
+          <AccordionItem title="Test title" />
+        </Accordion>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with children", () => {
     const tree = renderer
-        .create(
-            <Accordion defaultIndex={[0]}>
-                <AccordionItem>Test Children</AccordionItem>
-            </Accordion>
-        )
-        .toJSON();
+      .create(
+        <Accordion defaultIndex={[0]}>
+          <AccordionItem>Test Children</AccordionItem>
+        </Accordion>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with iconSize", () => {
     const tree = renderer
-        .create(
-            <Accordion defaultIndex={[0]}>
-                <AccordionItem iconSize={36} />
-            </Accordion>
-        )
-        .toJSON();
+      .create(
+        <Accordion defaultIndex={[0]}>
+          <AccordionItem iconSize={36} />
+        </Accordion>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
