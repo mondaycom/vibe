181e20682e237229ed6a714af426e47cd484d15f
Hadas Farhi <hadasfa@monday.com>
2021-12-20 12:17:48 +0200

    update template according to new branding part 1

--- src/storybook/components/component-name/component-name.jsx
+++ src/storybook/components/component-name/component-name.jsx
@@ -1,6 +1,14 @@
 import cx from "classnames";
 import "./component-name.scss";
 
-export const ComponentName = ({ children, className }) => {
-  return <h1 className={cx("monday-storybook-component-name", className)}>{children}</h1>;
+export const ComponentName = ({ children, className, isFoundation = false, overrideBackground }) => {
+  return (
+    <h1
+      className={cx("monday-storybook-component-name", className, {
+        "monday-storybook-component-name--foundation": isFoundation
+      })}
+    >
+      {children}
+    </h1>
+  );
 };
