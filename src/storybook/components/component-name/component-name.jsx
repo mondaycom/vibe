77a9476e8c528e55a2b9f67382eefa0e8ee3407d
Hadas Farhi <hadasfa@monday.com>
2021-12-20 15:37:37 +0200

    code review fix

--- src/storybook/components/component-name/component-name.jsx
+++ src/storybook/components/component-name/component-name.jsx
@@ -1,11 +1,11 @@
 import cx from "classnames";
 import "./component-name.scss";
 
-export const ComponentName = ({ children, className, isFoundation = false, overrideBackground }) => {
+export const ComponentName = ({ children, className, withFoundationBackground = false }) => {
   return (
     <h1
       className={cx("monday-storybook-component-name", className, {
-        "monday-storybook-component-name--foundation": isFoundation
+        "monday-storybook-component-name--foundation": withFoundationBackground
       })}
     >
       {children}
