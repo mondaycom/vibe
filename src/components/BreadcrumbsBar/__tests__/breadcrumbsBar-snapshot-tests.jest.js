f5a49930ccfe760656ec69a8f32607310182d38e
Ivan Harasymchuk <ivan7790@gmail.com>
2021-12-23 15:11:34 +0200

    fix: replace formating

--- src/components/BreadcrumbsBar/__tests__/breadcrumbsBar-snapshot-tests.jest.js
+++ src/components/BreadcrumbsBar/__tests__/breadcrumbsBar-snapshot-tests.jest.js
@@ -4,28 +4,28 @@ import BreadcrumbsBar from "../BreadcrumbsBar";
 import BreadcrumbItem from "../BreadcrumbItem/BreadcrumbItem";
 
 describe("BreadcrumbsBar renders correctly", () => {
-	it("with empty props", () => {
-		const tree = renderer.create(<BreadcrumbsBar />).toJSON();
-		expect(tree).toMatchSnapshot();
-	});
-
-	it("with bar type", () => {
-		Object.values(BreadcrumbsBar.types).forEach(type => {
-			const tree = renderer.create(<BreadcrumbsBar type={type} />).toJSON();
-			expect(tree).toMatchSnapshot();
-		});
-	});
-
-	it("with children", () => {
-		const tree = renderer
-			.create(
-				<BreadcrumbsBar>
-					<BreadcrumbItem text="Workspace" />
-					<BreadcrumbItem text="Board" />
-				</BreadcrumbsBar>
-			)
-			.toJSON();
-		expect(tree).toMatchSnapshot();
-	});
+  it("with empty props", () => {
+    const tree = renderer.create(<BreadcrumbsBar />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
+
+  it("with bar type", () => {
+    Object.values(BreadcrumbsBar.types).forEach(type => {
+      const tree = renderer.create(<BreadcrumbsBar type={type} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  it("with children", () => {
+    const tree = renderer
+      .create(
+        <BreadcrumbsBar>
+          <BreadcrumbItem text="Workspace" />
+          <BreadcrumbItem text="Board" />
+        </BreadcrumbsBar>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
   
