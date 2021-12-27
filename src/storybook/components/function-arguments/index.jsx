6e92948488751e0148d202d13fdf0a5cf266a393
Sahar Brodbeker <saharonb@gmail.com>
2021-12-27 13:37:05 +0200

    Add `useDebounceEvent` stories (#410)

---
+++ src/storybook/components/function-arguments/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Code } from "@storybook/components"; // eslint-disable-line import/no-extraneous-dependencies
+import classes from "./index.module.scss";
+
+export const FunctionArgument = ({ children, name, type, description, default: defaultValue }) => (
+  <li className={classes.argument}>
+    <code className={classes["argument-name"]}>{name}</code>
+    <code className={classes["argument-type"]}>{type}</code>
+    {description && <> - {description}</>}
+    {defaultValue && (
+      <>
+        {" "}
+        Defaults to: <Code>{defaultValue}</Code>
+      </>
+    )}
+    {children && <ul>{children}</ul>}
+  </li>
+);
+
+export const FunctionArguments = ({ children }) => {
+  return <ul>{children}</ul>;
+};
+
+FunctionArguments.propTypes = {
+  children: PropTypes.oneOfType([FunctionArgument, PropTypes.arrayOf(FunctionArgument)])
+};
+
+FunctionArguments.defaultProps = {
+  children: []
+};
