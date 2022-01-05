46e1b12fa2997d06f90a893afa08730fc586b9f1
Moshe Zemah <moshe@monday.com>
2022-01-05 07:31:26 +0200

    Enhance use key event (#432)
    * Enhance use key event
    
    * Enhance use key event

--- src/hooks/__tests__/useKeyEvent.jest.js
+++ src/hooks/__tests__/useKeyEvent.jest.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { renderHook, cleanup, act, waitFor } from "@testing-library/react-hooks";
 import { fireEvent } from "@testing-library/react";
 import useKeyEvent from "../useKeyEvent";
@@ -52,6 +51,135 @@ describe("useKeyEvent", () => {
           key: keys[0]
         });
       });
+      expect(callbackStub.mock.calls.length).toEqual(0);
+    });
+  });
+
+  describe("single key with ALT modifier", () => {
+    const keys = ["Enter"];
+    beforeEach(() => {
+      callbackStub = jest.fn();
+      element = document.createElement("div");
+      document.body.appendChild(element);
+      renderHook(() =>
+        useKeyEvent({
+          keys,
+          keyEventName: "keyup",
+          ref: { current: element },
+          callback: callbackStub,
+          modifier: useKeyEvent.modifiers.ALT
+        })
+      );
+    });
+
+    afterEach(() => {
+      element.remove();
+      cleanup();
+    });
+
+    it(`should call the callback with the ${keys[0]} key and ALT key`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0],
+          altKey: true
+        });
+      });
+      expect(callbackStub.mock.calls.length).toEqual(1);
+    });
+
+    it(`should not call the callback with the key but without modifiers`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0]
+        });
+      });
+
+      expect(callbackStub.mock.calls.length).toEqual(0);
+    });
+
+    it(`should not call the callback with the key but with other modifier`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0],
+          shiftKey: true
+        });
+      });
+
+      expect(callbackStub.mock.calls.length).toEqual(0);
+    });
+  });
+
+  describe("single key with CTRL_OR_META modifier", () => {
+    const keys = ["Enter"];
+    beforeEach(() => {
+      callbackStub = jest.fn();
+      element = document.createElement("div");
+      document.body.appendChild(element);
+      renderHook(() =>
+        useKeyEvent({
+          keys,
+          keyEventName: "keyup",
+          ref: { current: element },
+          callback: callbackStub,
+          modifier: useKeyEvent.modifiers.CTRL_OR_META
+        })
+      );
+    });
+
+    afterEach(() => {
+      element.remove();
+      cleanup();
+    });
+
+    it(`should call the callback with the ${keys[0]} key and CTRL key`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0],
+          ctrlKey: true
+        });
+      });
+      expect(callbackStub.mock.calls.length).toEqual(1);
+    });
+
+    it(`should call the callback with the ${keys[0]} key and META key`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0],
+          metaKey: true
+        });
+      });
+      expect(callbackStub.mock.calls.length).toEqual(1);
+    });
+
+    it(`should not call the callback with the key but without modifiers`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0]
+        });
+      });
+
+      expect(callbackStub.mock.calls.length).toEqual(0);
+    });
+
+    it(`should not call the callback with the key but with other modifier`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: keys[0],
+          shiftKey: true
+        });
+      });
+
+      expect(callbackStub.mock.calls.length).toEqual(0);
+    });
+
+    it(`should not call the callback with other key but with modifiers`, () => {
+      act(() => {
+        fireEvent.keyUp(element, {
+          key: "Esc",
+          metaKey: true
+        });
+      });
+
       expect(callbackStub.mock.calls.length).toEqual(0);
     });
   });
