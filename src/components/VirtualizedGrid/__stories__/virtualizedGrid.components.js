216f20215f0b95e3b41ea1b1769f5cc41e26612d
Or Nuri <ornu@monday.com>
2022-01-09 15:56:16 +0200

    docs: Added VirtualizedGrid storybook

---
+++ src/components/VirtualizedGrid/__stories__/virtualizedGrid.components.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const generateItems = (height = 30, width = "100%", itemsCount) => {
+  const items = [];
+  for (let i = 0; i < itemsCount; i++) {
+    items.push({ value: `Item ${i}`, height, width, id: i });
+  }
+  return items;
+};
+
+export const itemRenderer = (item, index, style) => {
+  const backgroundColor = index % 2 === 0 ? "white" : "#f8f8f0";
+  return (
+    <div key={index} style={style}>
+      <div
+        style={{
+          backgroundColor,
+          height: item.height,
+          width: item.width,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        {item.value}
+      </div>
+    </div>
+  );
+};
