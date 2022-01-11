33687cd78cd251f8f16f2c3cbcea1e15bde31a41
Ron Nachmany <ronna@monday.com>
2022-01-11 15:11:42 +0200

    add description icon (#85)

--- src/Icons/iconsMetaData.js
+++ src/Icons/iconsMetaData.js
@@ -5,6 +5,13 @@ export const iconsMetaData = [
   // DO NOT REMOVE THIS FOLLOWING LINE!
   // plop_marker:icon_metadata
   {
+      name: "description",
+      file: "description.svg",
+      description: "description",
+      tags: "description"
+  },
+
+  {
       name: "LearnMore",
       file: "LearnMore.svg",
       description: "Learn More",
