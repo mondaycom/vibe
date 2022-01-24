48dcd723c7b734cac0da1109e903ae580aa11571
roniCohen123 <ronic@monday.com>
2022-01-24 14:55:05 +0200

    lint fix

--- src/components/Combobox/components/ComboboxOption/ComboboxOption.jsx
+++ src/components/Combobox/components/ComboboxOption/ComboboxOption.jsx
@@ -105,7 +105,8 @@ const ComboboxOption = ({
         {label}
       </div>
       {rightIcon && renderIcon(rightIcon, rightIconType, "right")}
-    </>);
+    </>
+  );
 
   return (
     // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
