d2d4bf9c2e40aee3d4417270dbf17f94c7f261d0
Omri Lavi <omrila@monday.com>
2022-01-30 17:19:23 +0200

    added useLastNavigationDirection

---
+++ src/components/Menu/Menu/hooks/__tests__/useLastNavigationDirection.jest.js
@@ -0,0 +1,74 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import userEvent from "@testing-library/user-event";
+import { NAV_DIRECTIONS } from "../../../../../hooks/useFullKeyboardListeners";
+import { useLastNavigationDirection } from "../useLastNavigationDirection";
+
+describe("useLastNavigationDirection", () => {
+  it("should return undefined when no direction key was pressed yet", () => {
+    const { result } = renderHookForTest();
+
+    expect(result.current.lastNavigationDirection).toBeUndefined();
+  });
+
+  it("should return undefined when only non-direction keys were pressed", () => {
+    const { result } = renderHookForTest();
+
+    act(() => {
+      userEvent.keyboard("f");
+    });
+
+    expect(result.current.lastNavigationDirection).toBeUndefined();
+  });
+
+  [
+    { key: "ArrowUp", direction: NAV_DIRECTIONS.UP },
+    { key: "ArrowLeft", direction: NAV_DIRECTIONS.LEFT },
+    { key: "ArrowRight", direction: NAV_DIRECTIONS.RIGHT },
+    { key: "ArrowDown", direction: NAV_DIRECTIONS.DOWN }
+  ].forEach(({ key, direction }) => {
+    it(`should return direction "${direction}" when pressing the key "${key}"`, () => {
+      const { result } = renderHookForTest();
+
+      act(() => {
+        userEvent.keyboard(`{${key}}`);
+      });
+
+      expect(result.current.lastNavigationDirection).toBe(direction);
+    });
+
+    it(`should return direction "${direction}" when pressing the key "${key}" after another direction key`, () => {
+      const otherDirectionKey = key === "ArrowUp" ? "ArrowDown" : "ArrowUp";
+      const { result } = renderHookForTest();
+
+      act(() => {
+        userEvent.keyboard(`{${otherDirectionKey}}{${key}}`);
+      });
+
+      expect(result.current.lastNavigationDirection).toBe(direction);
+    });
+
+    it(`should return direction "${direction}" when pressing the key "${key}" after another non-direction keys`, () => {
+      const { result } = renderHookForTest();
+
+      act(() => {
+        userEvent.keyboard(`something{${key}}`);
+      });
+
+      expect(result.current.lastNavigationDirection).toBe(direction);
+    });
+
+    it(`should return direction "${direction}" when pressing the key "${key}" and THEN non-direction keys`, () => {
+      const { result } = renderHookForTest();
+
+      act(() => {
+        userEvent.keyboard(`{${key}}something`);
+      });
+
+      expect(result.current.lastNavigationDirection).toBe(direction);
+    });
+  });
+
+  function renderHookForTest() {
+    return renderHook(() => useLastNavigationDirection());
+  }
+});
