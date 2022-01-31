d9c23af24ca3a57670cef8f31c4373f22c3d8ea7
Hadas Farhi <hadasfa@monday.com>
2022-01-31 13:58:19 +0200

    fix code review notes

--- src/components/Dropdown/hooks/useHiddenOptionsData.js
+++ src/components/Dropdown/hooks/useHiddenOptionsData.js
@@ -28,6 +28,7 @@ export function useHiddenOptionsData({ isMultiline, ref, selectedOptionsCount, c
 
     setOverflowIndex(finalOverflowingIndex);
   }, [ref, isMultiline, selectedOptionsCount, chipClassName, setOverflowIndex]);
+
   const hiddenOptionsCount = overflowIndex > -1 ? selectedOptionsCount - overflowIndex : 0;
   return { overflowIndex, hiddenOptionsCount };
 }
