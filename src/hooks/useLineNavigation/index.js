f118efcd22d4919c0d4064074b99d70ecae8d73a
Hadas Farhi <hadasfa@monday.com>
2022-03-08 19:31:06 +0200

    undo not needed change

--- src/hooks/useLineNavigation/index.js
+++
@@ -1,6 +0,0 @@
-import { useEffect, useState } from "react";
-import { useEventListener } from "hooks";
-
-export function useLineNavigation({ elementRefs, isVertical = false }) {
-  useEventListener({eventName: "keydown");
-}
