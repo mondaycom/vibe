caaf9b44bdcbf8df4e1a7133ad5335313ce948ff
Moshe Zemah <moshe@monday.com>
2022-03-13 11:17:30 +0200

    Interaction tests (#582)
    * Interaction tests
    
    * Interaction tests
    
    * Interaction tests
    
    * Interaction tests - test failure
    
    * Interaction tests

---
+++ src/__tests__/interactions-helper.js
@@ -0,0 +1,62 @@
+import { within, userEvent } from "@storybook/testing-library";
+import { getTestId, ELEMENT_TYPES as types } from "../utils/test-utils";
+
+export const ELEMENT_TYPES = types;
+
+function getWithin(canvasOrValidTestElement) {
+  if (canvasOrValidTestElement.getByRole) return canvasOrValidTestElement;
+  return within(canvasOrValidTestElement);
+}
+
+export const testFunctionWrapper = testFunc => {
+  return async ({ canvasElement }) => {
+    // Starts querying the component from its root element
+    const canvas = getWithin(canvasElement);
+    return testFunc(canvas);
+  };
+};
+
+export const getByTestId = (rootElement, elementType, id = "") => {
+  const dataTestId = getTestId(elementType, id);
+  return getWithin(rootElement).getByTestId(dataTestId);
+};
+
+export const getByText = (rootElement, text) => {
+  return getWithin(rootElement).getByText(text);
+};
+
+export const getByPlaceholderText = (rootElement, text) => {
+  return getWithin(rootElement).getByPlaceholderText(text);
+};
+
+export const getByClassName = className => {
+  return document.getElementsByClassName(className)[0];
+};
+
+export const getByRole = (rootElement, role) => {
+  return getWithin(rootElement).getByRole(role);
+};
+
+export const getByLabelText = (rootElement, text) => {
+  return getWithin(rootElement).getByLabelText(text);
+};
+
+export const clickElement = element => {
+  return userEvent.click(element);
+};
+
+export const typeText = async (element, text, waitForDebounceMs = 250) => {
+  let promise = userEvent.type(element, text, {
+    delay: 50
+  });
+  const result = await promise;
+  await delay(waitForDebounceMs);
+  return result;
+};
+
+function delay(timeout) {
+  return new Promise(resolve => {
+    if (!timeout) return resolve();
+    setTimeout(resolve, timeout);
+  });
+}
