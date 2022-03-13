7fcb5a7fa8184c90f6e633b3648030f3cd452f2e
yonatan lev ari <43612427+3dyonic@users.noreply.github.com>
2022-03-13 19:00:28 +0200

    PR: Documentation ,Ay11 foundation page (#579)
    * # ay11 Foundation page
    * Add content structure
    * Add content images
    * Add accessibility principles component
    * Add modifiers to the frames "no-gutter, no-border"
    * Revise attention-box <a> styles
    
    * # ay11 Foundation page Rejects: remove attentionBox overrides.
    
    * # ay11 Foundation page Rejects: connect "monday-storybook-paragraph" styles to markdown <p>
    
    * # ay11 Foundation page Rejects: add unstyled lists
    * Add unstyled-list.jsx and styles
    * Add unstyled-list-item.jsx and styles
    
    * # ay11 Foundation page Rejects: Add Props to Frame component
    * Add noBorder prop mapped to "no-border" modifier
    * Add noGutter prop mapped to "no-gutter" modifier
    
    * # ay11 Foundation page Rejects: Add Checklist PDF
    
    * # ay11 Foundation page Rejects:convert _bold_ to ### h3

--- src/storybook/components/paragraph/paragraph.jsx
+++ src/storybook/components/paragraph/paragraph.jsx
@@ -1,4 +1,5 @@
 import cx from "classnames";
+import "./paragraph.scss";
 
 export const Paragraph = ({ children, className }) => (
   <p className={cx("monday-storybook-paragraph", className)}>{children}</p>
