bd4bfa7ec8703e8bdc728eaa56b696e05f571ddc
Hadas Farhi <hadasfa@monday.com>
2022-03-16 15:25:34 +0200

    undo

--- src/components/BreadcrumbsBar/BreadcrumbItem/BreadcrumbContent/BreadcrumbContent.jsx
+++ src/components/BreadcrumbsBar/BreadcrumbItem/BreadcrumbContent/BreadcrumbContent.jsx
@@ -5,47 +5,36 @@ import "./BreadcrumbContent.scss";
 const ENTER_KEY = "Enter";
 const SPACE_KEY = " ";
 
-export const BreadcrumbContent = forwardRef(
-  ({ className, isClickable, link, onClick, text, icon, isCurrent, buttonRef }, ref) => {
-    const Icon = icon;
-    const onKeyDown = useCallback(
-      event => {
-        if (event.key === ENTER_KEY || event.key === SPACE_KEY) {
-          link ? (window.parent.location.href = link) : onClick();
-        }
-      },
-      [onClick, link]
-    );
-
-    if (isClickable && (link || onClick)) {
-      if (link) {
-        return (
-          <a className={className} href={link} onKeyDown={onKeyDown} aria-current={isCurrent ? "page" : undefined}>
-            {Icon && <Icon className="breadcrumb-icon" size="14" clickable={false} />}
-            <span ref={ref} className="breadcrumb-text">
-              {text}
-            </span>
-          </a>
-        );
+export const BreadcrumbContent = forwardRef(({ className, isClickable, link, onClick, text, icon, isCurrent }, ref) => {
+  const Icon = icon;
+  const onKeyDown = useCallback(
+    event => {
+      if (event.key === ENTER_KEY || event.key === SPACE_KEY) {
+        link ? (window.parent.location.href = link) : onClick();
       }
+    },
+    [onClick, link]
+  );
+
+  if (isClickable && (link || onClick)) {
+    if (link) {
       return (
-        <span
-          className={className}
-          ref={buttonRef}
-          onClick={onClick}
-          onKeyDown={onKeyDown}
-          tabIndex="0"
-          aria-current={isCurrent ? "page" : undefined}
-        >
+        <a className={className} href={link} onKeyDown={onKeyDown} aria-current={isCurrent ? "page" : undefined}>
           {Icon && <Icon className="breadcrumb-icon" size="14" clickable={false} />}
           <span ref={ref} className="breadcrumb-text">
             {text}
           </span>
-        </span>
+        </a>
       );
     }
     return (
-      <span className={className} aria-disabled="true" tabIndex="0" aria-current={isCurrent ? "page" : undefined}>
+      <span
+        className={className}
+        onClick={onClick}
+        onKeyDown={onKeyDown}
+        tabIndex="0"
+        aria-current={isCurrent ? "page" : undefined}
+      >
         {Icon && <Icon className="breadcrumb-icon" size="14" clickable={false} />}
         <span ref={ref} className="breadcrumb-text">
           {text}
@@ -53,4 +42,12 @@ export const BreadcrumbContent = forwardRef(
       </span>
     );
   }
-);
+  return (
+    <span className={className} aria-disabled="true" tabIndex="0" aria-current={isCurrent ? "page" : undefined}>
+      {Icon && <Icon className="breadcrumb-icon" size="14" clickable={false} />}
+      <span ref={ref} className="breadcrumb-text">
+        {text}
+      </span>
+    </span>
+  );
+});
