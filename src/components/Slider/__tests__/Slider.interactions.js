fae884b5474ec8e097c11070710cf0f73731f5a9
Nik Savchenko <niksa@monday.com>
2022-03-29 17:49:37 +0300

    test(slider): fix render waiting
     - waitForElementVisible - add failed expectations at log :(
     - waitForElementVisible replaced by correct findByRole
     - fix minor typos at comments

--- src/components/Slider/__tests__/Slider.interactions.js
+++ src/components/Slider/__tests__/Slider.interactions.js
@@ -1,6 +1,6 @@
 import { expect } from "@storybook/jest";
 import { within, userEvent } from "@storybook/testing-library";
-import { delay, interactionSuite, resetFocus, waitForElementVisible } from "../../../__tests__/interactions-helper";
+import { delay, interactionSuite, resetFocus } from "../../../__tests__/interactions-helper";
 
 const CHANGES_DELAY = 1;
 
@@ -9,16 +9,16 @@ const changeSliderValueByClickingOnTrackTest = async canvas => {
   // prepare: take sizes of slider and waiting for render
   const elRail = canvas.getByTestId("monday-slider-show-value-s__rail");
   const rect = elRail.getBoundingClientRect();
-  const elThumb = await waitForElementVisible(() => within(elRail).getByRole("slider"));
+  const elThumb = await within(elRail).findByRole("slider");
   // go to start
   userEvent.click(elRail, { clientX: Math.ceil(rect.left) });
   await delay(CHANGES_DELAY);
   await expect(elThumb.getAttribute("aria-valuenow")).toBe("0");
-  // got to end
+  // go to end
   userEvent.click(elRail, { clientX: Math.floor(rect.right) });
   await delay(CHANGES_DELAY);
   await expect(elThumb.getAttribute("aria-valuenow")).toBe("100");
-  // got to middle
+  // go to middle
   userEvent.click(elRail, { clientX: Math.floor(rect.left + rect.width / 2) });
   await delay(CHANGES_DELAY);
   await expect(elThumb.getAttribute("aria-valuenow")).toBe("50");
