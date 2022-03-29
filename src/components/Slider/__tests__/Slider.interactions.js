08bef5ca4a8772eed20a0f8550ac677e810a203b
Nik Savchenko <niksa@monday.com>
2022-03-29 14:53:03 +0300

    test(Slider): fix/improve delays during interaction tests
     - change delay to waiting for
     - remove "visual" delay to constant
     - according to review feedback

--- src/components/Slider/__tests__/Slider.interactions.js
+++ src/components/Slider/__tests__/Slider.interactions.js
@@ -1,26 +1,26 @@
 import { expect } from "@storybook/jest";
 import { within, userEvent } from "@storybook/testing-library";
-import { delay, interactionSuite, resetFocus } from "../../../__tests__/interactions-helper";
+import { delay, interactionSuite, resetFocus, waitForElementVisible } from "../../../__tests__/interactions-helper";
+
+const CHANGES_DELAY = 1;
 
 // Decrease/Increase value by mouse click on Track/Rail of Slider
 const changeSliderValueByClickingOnTrackTest = async canvas => {
-  // prepare: take sizes of slider
+  // prepare: take sizes of slider and waiting for render
   const elRail = canvas.getByTestId("monday-slider-show-value-s__rail");
   const rect = elRail.getBoundingClientRect();
-  // waiting for render
-  await delay(300);
-  const elThumb = within(elRail).getByRole("slider");
+  const elThumb = await waitForElementVisible(() => within(elRail).getByRole("slider"));
   // go to start
   userEvent.click(elRail, { clientX: Math.ceil(rect.left) });
-  await delay(300);
+  await delay(CHANGES_DELAY);
   await expect(elThumb.getAttribute("aria-valuenow")).toBe("0");
   // got to end
   userEvent.click(elRail, { clientX: Math.floor(rect.right) });
-  await delay(300);
+  await delay(CHANGES_DELAY);
   await expect(elThumb.getAttribute("aria-valuenow")).toBe("100");
   // got to middle
   userEvent.click(elRail, { clientX: Math.floor(rect.left + rect.width / 2) });
-  await delay(300);
+  await delay(CHANGES_DELAY);
   await expect(elThumb.getAttribute("aria-valuenow")).toBe("50");
 };
 
