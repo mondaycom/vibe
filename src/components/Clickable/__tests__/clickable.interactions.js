a51fb96f274ada942f685bdd49bec419e428968b
Hadas Farhi <hadasfa@monday.com>
2022-03-29 14:54:53 +0300

    fix

--- src/components/Clickable/__tests__/clickable.interactions.js
+++ src/components/Clickable/__tests__/clickable.interactions.js
@@ -1,14 +1,13 @@
 import { expect } from "@storybook/jest";
 import {
   getByLabelText,
-  testFunctionWrapper,
   pressNavigationKey,
   interactionSuite,
   resetFocus
 } from "../../../__tests__/interactions-helper";
 import { NAVIGATIONS_COMMANDS } from "utils/test-utils";
 
-export const states_onClickTabFocusElementTest = testFunctionWrapper(async canvas => {
+async function states_onClickTabFocusElementTest(canvas) {
   const CLICKABLE_LABEL = "clickable button";
   const CLICKABLE_DISABLED_LABEL = "disabled clickable button";
   const clickableElement = await getByLabelText(canvas, CLICKABLE_LABEL);
@@ -17,8 +16,11 @@ export const states_onClickTabFocusElementTest = testFunctionWrapper(async canva
   expect(document.activeElement).toEqual(clickableElement);
   await pressNavigationKey(NAVIGATIONS_COMMANDS.TAB);
   expect(document.activeElement).not.toEqual(disabledClickableElement);
-});
+}
 
 export const statesPlaySuite = interactionSuite({
-  tests: [states_onClickTabFocusElementTest]
+  tests: [states_onClickTabFocusElementTest],
+  afterEach: async () => {
+    await resetFocus();
+  }
 });
