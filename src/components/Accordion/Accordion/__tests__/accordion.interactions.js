6eee92e875150bba530b5d4d7e0451767c5d0b92
Nik Savchenko <niksa@monday.com>
2022-03-31 14:20:47 +0300

    test(accordion): add interaction tests for multi-active mode
     - add interactions tests
     - connect tests to storybook
     - minor refactoring (split to helper/check functions)

--- src/components/Accordion/Accordion/__tests__/accordion.interactions.js
+++ src/components/Accordion/Accordion/__tests__/accordion.interactions.js
@@ -4,7 +4,7 @@ import { delay, interactionSuite, resetFocus } from "../../../../__tests__/inter
 
 const CHANGES_DELAY = 1;
 
-function getAccordionHeadingBtText(canvas, title) {
+function getAccordionHeadingByText(canvas, title) {
   return canvas.getByText(title)?.closest("button");
 }
 
@@ -14,9 +14,17 @@ function getOpenedAccordionItem(canvas) {
   return { elPanel, elHeading };
 }
 
+function getOpenedAccordionItems(canvas) {
+  const elPanels = canvas.queryAllByRole("region");
+  const elHeadings = elPanels.map(elPanel => {
+    return within(elPanel.parentNode).getByRole("button");
+  });
+  return { elPanels, elHeadings };
+}
+
 async function openAndCheckAccordionItem(canvas, title) {
   const before = getOpenedAccordionItem(canvas);
-  const elHeading = getAccordionHeadingBtText(canvas, title);
+  const elHeading = getAccordionHeadingByText(canvas, title);
   userEvent.click(elHeading);
   const elPanel = canvas.getByRole("region");
   await expect(elHeading.getAttribute("aria-expanded")).toBe("true");
@@ -24,13 +32,29 @@ async function openAndCheckAccordionItem(canvas, title) {
   await expect(elHeading.getAttribute("aria-controls")).toBe(elPanel.id);
 }
 
+async function closeAndCheckMultiAccordionItem(canvas, shouldBeOpened) {
+  const before = getOpenedAccordionItems(canvas);
+  userEvent.click(before.elHeadings[0]);
+  const after = getOpenedAccordionItems(canvas);
+  await expect(after.elPanels.length).toBe(shouldBeOpened);
+  await expect(before.elHeadings[0].getAttribute("aria-expanded")).toBe("false");
+}
+
+async function openAndCheckMultiAccordionItem(canvas, title, shouldBeOpened) {
+  const elHeading = getAccordionHeadingByText(canvas, title);
+  userEvent.click(elHeading);
+  const after = getOpenedAccordionItems(canvas);
+  await expect(after.elPanels.length).toBe(shouldBeOpened);
+  await expect(after.elHeadings[0].getAttribute("aria-expanded")).toBe("true");
+  await expect(after.elHeadings[0].getAttribute("aria-controls")).toBe(after.elPanels[0].id);
+}
+
 const openAlreadyActiveSingleActiveTests = async canvas => {
-  let before, after;
   await delay(CHANGES_DELAY);
   // try to click on already selected Accordion Item heading
-  before = getOpenedAccordionItem(canvas);
+  const before = getOpenedAccordionItem(canvas);
   userEvent.click(before.elHeading);
-  after = getOpenedAccordionItem(canvas);
+  const after = getOpenedAccordionItem(canvas);
   // what was opened should be still opened
   await expect(before.elPanel.id).toBe(after.elPanel.id);
   // panel and heading aria controls are the same
@@ -46,8 +70,33 @@ const openCloseAccordionSingleActiveTests = async canvas => {
   await openAndCheckAccordionItem(canvas, "Setting");
 };
 
+const closeAlreadyActiveMultiActiveTests = async canvas => {
+  await delay(CHANGES_DELAY);
+  // close already opened Accordion Item (one from two)
+  await closeAndCheckMultiAccordionItem(canvas, 1);
+  // close last opened Accordion Item
+  await delay(CHANGES_DELAY);
+  await closeAndCheckMultiAccordionItem(canvas, 0);
+};
+
+const openAccordionItemsMultiActiveTests = async canvas => {
+  // open Accordion Item - Settings
+  await delay(CHANGES_DELAY);
+  await openAndCheckMultiAccordionItem(canvas, "Setting", 1);
+  // open one more Accordion Item - Integration
+  await delay(CHANGES_DELAY);
+  await openAndCheckMultiAccordionItem(canvas, "Integration", 2);
+};
+
 export const accordionSingleActivePlaySuite = interactionSuite({
   tests: [openAlreadyActiveSingleActiveTests, openCloseAccordionSingleActiveTests],
+  afterEach: async () => {
+    await resetFocus();
+  }
+});
+
+export const accordionMultiActivePlaySuite = interactionSuite({
+  tests: [closeAlreadyActiveMultiActiveTests, openAccordionItemsMultiActiveTests],
   afterEach: async () => {
     await resetFocus();
   }
