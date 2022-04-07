697ad8dc43920770f2059211635dd8b2a7cc9c7d
3dyonic <3dyonic@gmail.com>
2022-04-07 17:12:37 +0300

    #  Welcome page updates: re structure information box, contributor components * Sort SCSS import to be last. * Separate information-box title to a separate component * Use information-box-title in contributor.jsx * Add CSS to information-box-title.scss, remove it from information-box.scss

---
+++ src/storybook/components/information-box-title/information-box-title.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { BEMClass } from "helpers/bem-helper";
+import "./information-box-title.scss";
+
+const BASE_CSS_CLASS = "monday-storybook-information-box-title";
+const bemHelper = BEMClass(BASE_CSS_CLASS);
+
+export const InformationBoxTitle = ({ children }) => {
+  return typeof children === "string" ? (
+    <h4 className={bemHelper({ component: "component" })}>{children}</h4>
+  ) : (
+    children
+  );
+};
+
+InformationBoxTitle.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
+};
+
+InformationBoxTitle.defaultProps = {
+  component: ""
+};
