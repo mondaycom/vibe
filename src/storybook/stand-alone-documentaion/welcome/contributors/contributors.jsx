4d4e79cc749c2865fb2e76a200a75ef0da4093b5
yonatan lev ari <43612427+3dyonic@users.noreply.github.com>
2022-04-10 11:34:37 +0300

    Feature/yonatanari/wellcome page updates (#622)
    * # wip: Welcome page updates
    * Add contact option to maintainers
    * Add contributors to other-contributors-list.jsx
    * Correct ClassName for Maintainers Backgrounds
    * Add Yonatan to maintainers.
    * Remove Rotem image.
    
    * # wip: Welcome page updates, Update other contributors.
    * Add keys to contributors.
    
    * # 📝 Welcome page updates: Update contributors. Update Other contributors
    * Change Nick to Nik in assets
    * Revise "Information box", Add element injection option for title prop
    * Revise Contributors, Separate Data from Markup
    * Revise Other Contributors, Separate Data from Markup, Add designers
    
    * #  Welcome page updates: re structure information box, contributor components
    * Sort SCSS import to be last.
    * Separate information-box title to a separate component
    * Use information-box-title in contributor.jsx
    * Add CSS to information-box-title.scss, remove it from information-box.scss
    
    * #  Welcome page updates: re structure information box, contributor components
    * Sort SCSS import to be last.
    * Separate information-box title to a separate component
    * Use information-box-title in contributor.jsx
    * Add CSS to information-box-title.scss, remove it from information-box.scss
    
    * #  Welcome page updates: re structure information box, contributor components
    * Revised information-box-title , remove logic
    * Revised information-box , use information-box-title with type of condition.

--- src/storybook/stand-alone-documentaion/welcome/contributors/contributors.jsx
+++ src/storybook/stand-alone-documentaion/welcome/contributors/contributors.jsx
@@ -1,50 +1,70 @@
 import { Contributor } from "../contributor/contributor";
-import { Evgeniy, Hadas, Orr, Sahar, Moshe, Nick, Omri } from "../assets";
+import { Evgeniy, Hadas, Orr, Sahar, Moshe, Nik, Omri, Yonatan } from "../assets";
 import "./contributors.scss";
+
+const CONTRIBUTERS = [
+  {
+    imgSrc: Orr,
+    fullName: "Orr Gottlieb",
+    email: "orr@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-1"
+  },
+  {
+    imgSrc: Evgeniy,
+    fullName: "Evgeniy Kazenic",
+    email: "evgeniy@monday.com",
+    title: "Designer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-2"
+  },
+  {
+    imgSrc: Hadas,
+    fullName: "Hadas Farhi",
+    email: "hadasfa@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-3"
+  },
+  {
+    imgSrc: Moshe,
+    fullName: "Moshe Zemah",
+    email: "moshe@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-4"
+  },
+  {
+    imgSrc: Sahar,
+    fullName: "Sahar Brodbeker",
+    email: "saharb@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-5"
+  },
+  {
+    imgSrc: Nik,
+    fullName: "Nik Savchenko",
+    email: "niksa@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-4"
+  },
+  {
+    imgSrc: Omri,
+    fullName: "Omri Lavi",
+    email: "omrila@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-2"
+  },
+  {
+    imgSrc: Yonatan,
+    fullName: "Yonatan Lev Ari",
+    email: "yonatanari@monday.com",
+    title: "Developer",
+    className: "monday-storybook-welcome-contributors_contributor--bg-pattern-3"
+  }
+];
 
 export const Contributors = () => (
   <div className="monday-storybook-welcome-contributors">
-    <Contributor
-      imgSrc={Orr}
-      fullName="Orr Gottlieb"
-      title="Tech Lead"
-      className="monday-storybook-welcome-contributors_contributor--orr"
-    />
-    <Contributor
-      imgSrc={Evgeniy}
-      fullName="Evgeniy Kazenic"
-      title="Product Designer Lead"
-      className="monday-storybook-welcome-contributors_contributor--evgeniy"
-    />
-    <Contributor
-      imgSrc={Hadas}
-      fullName="Hadas Farhi"
-      title="Full Stack Developer"
-      className="monday-storybook-welcome-contributors_contributor--hadas"
-    />
-    <Contributor
-      imgSrc={Moshe}
-      fullName="Moshe Zemah"
-      title="Senior Tech Lead"
-      className="monday-storybook-welcome-contributors_contributor--moshe"
-    />
-    <Contributor
-      imgSrc={Sahar}
-      fullName="Sahar Brodbeker"
-      title="Tech Lead"
-      className="monday-storybook-welcome-contributors_contributor--sahar"
-    />
-    <Contributor
-      imgSrc={Nick}
-      fullName="Nik Savchenko"
-      title="Tech Lead"
-      className="monday-storybook-welcome-contributors_contributor--nick"
-    />
-    <Contributor
-      imgSrc={Omri}
-      fullName="Omri Lavi"
-      title="Senior Full Stack Engineer"
-      className="monday-storybook-welcome-contributors_contributor--evgeniy"
-    />
+    {CONTRIBUTERS.map(({ imgSrc, fullName, email, title, className }) => (
+      <Contributor key={email} imgSrc={imgSrc} fullName={fullName} email={email} title={title} className={className} />
+    ))}
   </div>
 );
