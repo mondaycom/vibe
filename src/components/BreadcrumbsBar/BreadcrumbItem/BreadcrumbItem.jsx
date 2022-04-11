b567c5375bd9e574e34fde9adc5f2ae4803d15c3
nofar baranes <nofarba@monday.com>
2022-04-11 14:27:35 +0300

    Support on disabled tabIndex

--- src/components/BreadcrumbsBar/BreadcrumbItem/BreadcrumbItem.jsx
+++ src/components/BreadcrumbsBar/BreadcrumbItem/BreadcrumbItem.jsx
@@ -57,6 +57,7 @@ const BreadcrumbItem = ({
           text={text}
           icon={icon}
           isCurrent={isCurrent}
+          disabled={isDisabled || disabled}
         />
       </li>
     </Tooltip>
