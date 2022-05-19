637d91576137c897ec58a2bbc59797af8df61c52
3dyonic <3dyonic@gmail.com>
2022-05-19 16:32:24 +0300

    # wip,Box component Initial Scaffold: ongoing

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -2,23 +2,41 @@ import React, {useRef, forwardRef} from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import useMergeRefs from "hooks/useMergeRefs";
+import {BOX_BACKGROUND_COLORS} from "./BoxConstants"
 import styles from "./Box.module.scss";
 
-const Box = forwardRef(({className, id}, ref) => {
-    const componentRef = useRef(null);
-    const mergedRef = useMergeRefs({refs: [ref, componentRef]});
+const Box = forwardRef(
+    ({
+         className,
+         id,
+         rounded,
+         bgColor
+     },
+     ref,
+    ) => {
+        const componentRef = useRef(null);
+        const mergedRef = useMergeRefs({refs: [ref, componentRef]});
 
-    return (
-        <div ref={mergedRef}
-             className={cx(styles.box,  styles.textPrimaryColor, styles.pSpacingXl)}
-             id={id}>test</div>
-    );
-});
+        console.log({rounded});
+        return (
+            <div ref={mergedRef}
+                 className={cx(
+                     styles.box,
+                     rounded,
+                     bgColor
+                 )}
+                 id={id}>test</div>
+        );
+    });
 
 Box.propTypes = {
     /**
      * class name to be add to the wrapper
      */
+    bgColor: PropTypes.oneOf(
+        [Box.bgColor.GREY_BACKGROUND]
+    ),
+    rounded: PropTypes.string,
     className: PropTypes.string,
     /**
      * id to be add to the wrapper
@@ -27,6 +45,8 @@ Box.propTypes = {
 };
 Box.defaultProps = {
     className: "",
+    bgColor: undefined,
+    rounded: undefined,
     id: undefined
 };
 
---
+++ src/components/Box/BoxConstants.js
@@ -0,0 +1,7 @@
+import styles from "./Box.module.scss";
+
+export const BOX_BACKGROUND_COLORS = Object.freeze(
+    {
+        GREY_BACKGROUND: styles.bgGreyBackgroundColor,
+    }
+)
