474a6ba8ae5997238971dfb79bb3b681153e6f09
3dyonic <3dyonic@gmail.com>
2022-05-22 18:47:36 +0300

    # wip,Box component props scaffold, ongoing.

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -2,41 +2,140 @@ import React, {useRef, forwardRef} from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import useMergeRefs from "hooks/useMergeRefs";
-import {BOX_BACKGROUND_COLORS} from "./BoxConstants"
+import {
+    BACKGROUND_COLORS,
+    COLORS,
+    ROUNDED,
+    ROUNDED_TOP,
+    ROUNDED_END,
+    ROUNDED_BOTTOM,
+    ROUNDED_START,
+    SHADOW,
+} from "./BoxConstants"
 import styles from "./Box.module.scss";
 
 const Box = forwardRef(
     ({
          className,
          id,
+         color,
+         bgColor,
          rounded,
-         bgColor
+         roundedTop,
+         roundedEnd,
+         roundedBottom,
+         roundedStart,
+         shadow,
      },
      ref,
     ) => {
         const componentRef = useRef(null);
         const mergedRef = useMergeRefs({refs: [ref, componentRef]});
 
-        console.log({rounded});
         return (
             <div ref={mergedRef}
                  className={cx(
                      styles.box,
+                     color,
+                     bgColor,
                      rounded,
-                     bgColor
+                     roundedTop,
+                     roundedEnd,
+                     roundedBottom,
+                     roundedStart,
+                     shadow
                  )}
                  id={id}>test</div>
         );
     });
+
+Box.bgColor = BACKGROUND_COLORS;
+Box.color = COLORS;
+Box.rounded = ROUNDED;
+Box.roundedTop = ROUNDED_TOP;
+Box.roundedEnd = ROUNDED_END;
+Box.roundedBottom = ROUNDED_BOTTOM;
+Box.roundedStart = ROUNDED_START;
+Box.shadow = SHADOW;
 
 Box.propTypes = {
     /**
      * class name to be add to the wrapper
      */
     bgColor: PropTypes.oneOf(
-        [Box.bgColor.GREY_BACKGROUND]
+        [
+            Box.bgColor.PRIMARY_BACKGROUND_COLOR,
+            Box.bgColor.PRIMARY_HOVER_BACKGROUND_COLOR,
+            Box.bgColor.SECONDARY_BACKGROUND_COLOR,
+            Box.bgColor.GREY_BACKGROUND_COLOR,
+            Box.bgColor.ALL_GREY_BACKGROUND_COLOR,
+            Box.bgColor.INVERTED_COLOR_BACKGROUND,
+            Box.bgColor.DISABLED_BACKGROUND_COLOR,
+        ]
     ),
-    rounded: PropTypes.string,
+    color: PropTypes.oneOf(
+        [
+            Box.color.PRIMARY_COLOR,
+            Box.color.PRIMARY_HOVER_COLOR,
+            Box.color.PRIMARY_TEXT_COLOR,
+            Box.color.PRIMARY_TECT_COLOR_ON_PRIMARY,
+            Box.color.PRIMARY_COLOR_ON_INVERTED,
+            Box.color.TEXT_COLOR_ON_PRIMARY,
+            Box.color.TEXT_COLOR_ON_INVERTED,
+            Box.color.SECONDARY_TEXT_COLOR,
+            Box.color.PLACEHOLDER_COLOR,
+            Box.color.ICON_COLOR,
+            Box.color.LINK_COLOR,
+            Box.color.DISABLED_TEXT_COLOR,
+        ]
+    ),
+    rounded: PropTypes.oneOf(
+        [
+            Box.rounded.NONE,
+            Box.rounded.SMALL,
+            Box.rounded.MEDIUM,
+            Box.rounded.BIG,
+        ]
+    ),
+    roundedTop: PropTypes.oneOf(
+        [
+            Box.roundedTop.NONE,
+            Box.roundedTop.SMALL,
+            Box.roundedTop.MEDIUM,
+            Box.roundedTop.BIG,
+        ]
+    ),
+    roundedEnd: PropTypes.oneOf(
+        [
+            Box.roundedEnd.NONE,
+            Box.roundedEnd.SMALL,
+            Box.roundedEnd.MEDIUM,
+            Box.roundedEnd.BIG,
+        ]
+    ),
+    roundedBottom: PropTypes.oneOf(
+        [
+            Box.roundedBottom.NONE,
+            Box.roundedBottom.SMALL,
+            Box.roundedBottom.MEDIUM,
+            Box.roundedBottom.BIG,
+        ]
+    ),
+    roundedStart: PropTypes.oneOf(
+        [
+            Box.roundedStart.NONE,
+            Box.roundedStart.SMALL,
+            Box.roundedStart.MEDIUM,
+            Box.roundedStart.BIG,
+        ]
+    ),
+    shadow: PropTypes.oneOf(
+        [
+            Box.shadow.SMALL,
+            Box.shadow.MEDIUM,
+            Box.shadow.LARGE,
+        ]
+    ),
     className: PropTypes.string,
     /**
      * id to be add to the wrapper
@@ -45,8 +144,14 @@ Box.propTypes = {
 };
 Box.defaultProps = {
     className: "",
+    color: undefined,
     bgColor: undefined,
     rounded: undefined,
+    roundedTop: undefined,
+    roundedEnd: undefined,
+    roundedBottom: undefined,
+    roundedStart: undefined,
+    shadow: undefined,
     id: undefined
 };
 
--- src/components/Box/BoxConstants.js
+++ src/components/Box/BoxConstants.js
@@ -1,7 +1,81 @@
 import styles from "./Box.module.scss";
 
-export const BOX_BACKGROUND_COLORS = Object.freeze(
+export const BACKGROUND_COLORS = Object.freeze(
     {
-        GREY_BACKGROUND: styles.bgGreyBackgroundColor,
+        PRIMARY_BACKGROUND_COLOR: styles.bgPrimaryBackgroundColor,
+        PRIMARY_HOVER_BACKGROUND_COLOR: styles.bgPrimaryBackgroundHoverColor,
+        SECONDARY_BACKGROUND_COLOR: styles.bgSecondaryBackgroundColor,
+        GREY_BACKGROUND_COLOR: styles.bgGreyBackgroundColor,
+        ALL_GREY_BACKGROUND_COLOR: styles.bgAllgreyBackgroundColor,
+        INVERTED_COLOR_BACKGROUND: styles.bgInvertedColorBackground,
+        DISABLED_BACKGROUND_COLOR: styles.bgDisabledBackgroundColor,
+    }
+)
+
+export const COLORS = Object.freeze(
+        PRIMARY_COLOR: styles.textPrimaryColor,
+        PRIMARY_HOVER_COLOR: styles.textPrimaryHoverColor,
+        PRIMARY_SELECTED_COLOR: styles.textPrimarySelectedColor,
+        PRIMARY_TEXT_COLOR: styles.textPrimaryTextColor,
+        TEXT_COLOR_ON_PRIMARY: styles.textTextColorOnInverted,
+        TEXT_COLOR_ON_INVERTED: styles.textSecondaryTextColor,
+        SECONDARY_TEXT_COLOR: styles.textSecondaryTextColor,
+        PLACEHOLDER_COLOR: styles.textPlaceholderColor,
+        ICON_COLOR: styles.textIconColor,
+        LINK_COLOR: styles.textLinkColor,
+        DISABLED_TEXT_COLOR: styles.textDisabledTextColor,
+    }
+)
+
+export const ROUNDED = Object.freeze(
+    {
+        NONE: styles.rounded0,
+        SMALL: styles.roundedSmall,
+        MEDIUM: styles.roundedMedium,
+        BIG: styles.roundedBig,
+    }
+)
+
+export const ROUNDED_TOP = Object.freeze(
+    {
+        NONE: styles.roundedTop0,
+        SMALL: styles.roundedTopSmall,
+        MEDIUM: styles.roundedTopMedium,
+        BIG: styles.roundedTopBig,
+    }
+)
+
+export const ROUNDED_END = Object.freeze(
+    {
+        NONE: styles.roundedEnd0,
+        SMALL: styles.roundedEndSmall,
+        MEDIUM: styles.roundedEndMedium,
+        BIG: styles.roundedEndBig,
+    }
+)
+
+export const ROUNDED_BOTTOM = Object.freeze(
+    {
+            NONE: styles.roundedBottom0,
+            SMALL: styles.roundedBottomSmall,
+            MEDIUM: styles.roundedBottomMedium,
+            BIG: styles.roundedBottomBig,
+    }
+)
+
+export const ROUNDED_START = Object.freeze(
+    {
+            NONE: styles.roundedStart0,
+            SMALL: styles.roundedStartSmall,
+            MEDIUM: styles.roundedStartMedium,
+            BIG: styles.roundedStartBig,
+    }
+)
+
+export const SHADOW = Object.freeze(
+    {
+        SMALL: styles.shadowSmall,
+        MEDIUM: styles.shadowMedium,
+        LARGE: styles.shadowBig,
     }
 )
