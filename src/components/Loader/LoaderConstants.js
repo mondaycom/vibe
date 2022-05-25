ac930dcf49e1d3d283a4d21bf9efef38cbaa5efa
Sergey Roytman <sergeyro@monday.com>
2022-05-25 18:30:34 +0300

    Fix uncommited files

---
+++ src/components/Loader/LoaderConstants.js
@@ -0,0 +1,18 @@
+export const LOADER_COLORS = {
+  PRIMARY: "var(--primary-color)",
+  SECONDARY: "var(--secondary-text-color)",
+  ON_PRIMARY: "var(--text-color-on-primary)",
+  DARK: "var(--primary-text-color)"
+};
+
+export const LOADER_SIZES = {
+  XS: "xs",
+  SMALL: "small",
+  MEDIUM: "medium",
+  LARGE: "large"
+};
+
+export const LOADER_TYPES = {
+  CASUAL: "casual",
+  WITH_BACKGROUND: "with-background"
+};
