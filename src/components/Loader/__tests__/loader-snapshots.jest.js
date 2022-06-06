63bbe5d23d2172e5ca295805cf0d8561d3aaba26
Sergey Roytman <sergeyro@monday.com>
2022-06-06 12:19:25 +0300

    Storybook/sergeyro/expand loader (#686)
    * Loader storybook: color types
    
    * Loader storybook: color types - fix constants naming
    
    * Loader storybook: completed storybook
    
    * Loader snapshot tests
    
    * Fix uncommited files
    
    * Loader storybook: fixes after designer review
    
    * Loader: SASS modules
    
    * Loader: update snapshot tests after changes
    
    * Loader forward-ref test fix
    
    * Update snapshots for component which use loader
    
    * Loader: old classnames added back for backwardCompatability, size propTypes - number added
    
    * Loader snapshots updated
    
    * Loader dependent components snapshots-update
    
    * Remove Loader.type with hasBackground bool prop; reorder classes positions to be as before
    
    * Combobox snapshots-tests fix
    
    * Story styles according to BEM convention
    
    * Fix Github-lint check
    
    * Remove useless tests, reorder imports
    
    * Fix loader snapshot tests

--- src/components/Loader/__tests__/loader-snapshots.jest.js
+++ src/components/Loader/__tests__/loader-snapshots.jest.js
@@ -2,12 +2,65 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Loader from "../Loader";
 
-it("renders correctly with empty props", () => {
-  const tree = renderer.create(<Loader />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe("Loader renders correctly", () => {
+  it("with custom class name", () => {
+    const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} className="dummy-class-name" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it("renders correctly with custom class name", () => {
-  const tree = renderer.create(<Loader className="dummy-class-name" />).toJSON();
-  expect(tree).toMatchSnapshot();
+  describe("with size variants", () => {
+    it("with XS size", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.XS} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with SMALL size", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.SMALL} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with MEDIUM size", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with LARGE size", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.LARGE} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with custom size as number", () => {
+      const tree = renderer.create(<Loader size={100} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe("with color variants", () => {
+    it("with PRIMARY color", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} color={Loader.colors.PRIMARY} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with ON_PRIMARY color", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} color={Loader.colors.ON_PRIMARY} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with SECONDARY color", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} color={Loader.colors.SECONDARY} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it("with DARK color", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} color={Loader.colors.DARK} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe("with visual variants", () => {
+    it("with background", () => {
+      const tree = renderer.create(<Loader size={Loader.sizes.MEDIUM} hasBackground />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
 });
