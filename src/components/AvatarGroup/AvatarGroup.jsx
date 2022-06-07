9b0d2d226918148191b1c65260e49c5666380bb7
Sergey Roytman <sergeyro@monday.com>
2022-06-07 11:08:30 +0300

    Counter auto-tooltip

--- src/components/AvatarGroup/AvatarGroup.jsx
+++ src/components/AvatarGroup/AvatarGroup.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import Avatar from "../Avatar/Avatar";
 import Counter from "../Counter/Counter";
+import { Tooltip } from "components";
 import styles from "./AvatarGroup.module.scss";
 
 const AvatarGroup = ({ className, id, children, size, type, max, count }) => {
+  const counterTooltipText = useMemo(() => {
+    if (!children || !children.length) {
+      return null;
+    }
+
+    let text = "";
+    for (let i = max; i < children.length; ++i) {
+      if (children[i].props.tooltipComponent) {
+        text += children[i].props.tooltipComponent.props.content + "\n";
+      }
+    }
+    return text;
+  }, [children, max]);
+
   if (!children) {
     return null;
   }
@@ -14,28 +29,44 @@ const AvatarGroup = ({ className, id, children, size, type, max, count }) => {
     children = [children];
   }
 
+  const CounterTooltipContainer = ({ children }) => {
+    if (!counterTooltipText) {
+      return <>{children}</>;
+    }
+
+    return (
+      // TODO disable hide on timer in Tooltip
+      <Tooltip content={counterTooltipText} hideDelay={10000000}>
+        {children}
+      </Tooltip>
+    );
+  };
+
   return (
     <div className={cx(styles.container, className)} id={id}>
       {children.map((avatar, index) => {
-        if (index < max)
+        if (index < max) {
           return (
             <Avatar
               {...avatar.props}
               size={size || avatar.props.size}
-              key={`${id}-${index}`}
-              className={styles.avatarContainer}
               type={type || avatar.props.type}
+              key={`${id}-${index}`}
+              className={cx(styles.avatarContainer, avatar.props.className)}
             />
           );
+        }
       })}
       {(children.length > max || count) && (
-        // TODO pass size to the counter somehow
-        <Counter
-          color={Counter.colors.LIGHT}
-          count={count || children.length - max}
-          prefix="+"
-          size={Counter.sizes.LARGE}
-        />
+        // TODO pass size to the counter
+        <CounterTooltipContainer>
+          <Counter
+            color={Counter.colors.LIGHT}
+            count={count || children.length - max}
+            prefix="+"
+            size={Counter.sizes.LARGE}
+          />
+        </CounterTooltipContainer>
       )}
     </div>
   );
