732b2a2462d5675630130189ab49f817e5f16fef
Sergey Roytman <sergeyro@monday.com>
2022-06-19 22:53:00 +0300

    Refactoring

--- src/components/AvatarGroup/AvatarGroupCounter.jsx
+++ src/components/AvatarGroup/AvatarGroupCounter.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import Tooltip from "../Tooltip/Tooltip";
@@ -33,7 +33,7 @@ const AvatarGroupCounter = ({
   const focusNextPlaceholderRef = useRef(null);
   const counterContainerRef = useRef(null);
 
-  const counterComponent = useCallback(() => {
+  const counterComponent = useMemo(() => {
     return (
       <Counter
         color={counterColor}
