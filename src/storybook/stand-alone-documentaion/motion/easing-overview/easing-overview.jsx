77f9c7166db49df6e9c1e82e3f245c495d6a1bcf
3dyonic <3dyonic@gmail.com>
2022-06-20 21:10:59 +0300

    wip: complete easing section

--- src/storybook/stand-alone-documentaion/motion/easing-overview/easing-overview.jsx
+++ src/storybook/stand-alone-documentaion/motion/easing-overview/easing-overview.jsx
@@ -1,13 +1,13 @@
 import { TokenInformationBox } from "../token-Information-box/token-information-box";
 import { Flex } from "../../../../components";
-import styles from "./easing overview.module.scss";
+import styles from "./easing-overview.module.scss";
 
 export const EasingOverview = ({ videoSrc, title, description, tokenDescription }) => {
   const EasingOverviewVideoSrc = <video className={styles.video} src={videoSrc} controls />;
 
   return (
     <div className={styles.row}>
-      <Flex align={Flex.align.START} gap={Flex.gaps.LARGE}>
+      <Flex justify={Flex.justify.SPACE_BETWEEN} align={Flex.align.START} gap={Flex.gaps.LARGE}>
         <TokenInformationBox title={title} description={description} tokenDescription={tokenDescription} />
         {EasingOverviewVideoSrc}
       </Flex>
--- src/storybook/stand-alone-documentaion/motion/easing-overviews/easing-overviews.jsx
+++ src/storybook/stand-alone-documentaion/motion/easing-overviews/easing-overviews.jsx
@@ -8,28 +8,28 @@ import {
 
 const EASING = [
   {
-    videoSrc: EmphesizeElasticEasing,
-    title: "2Small motion - 250ms",
-    description: "System aletrs, notifications, atention and midiation.",
-    tokenDescription: "Token: motion-expressive-short"
-  },
-  {
     videoSrc: EnterDeceleratedEasing,
-    title: "3Small motion - 250ms",
-    description: "System aletrs, notifications, atention and midiation.",
-    tokenDescription: "Token: motion-expressive-short"
+    title: "Enter - Decelerated easing ",
+    description: "We use when object enter from out of screen to reveal extra information.",
+    tokenDescription: "Qubic bezier: (0,0,0.35,1)  \n Token: motion-timing-enter"
   },
   {
     videoSrc: ExitAcceleratedEasing,
-    title: "Small motion - 250ms",
-    description: "System aletrs, notifications, atention and midiation.",
-    tokenDescription: "Token: motion-expressive-short"
+    title: "Exit - Accelerated easing",
+    description: "We use when object leave the screen to hide element",
+    tokenDescription: "Qubic bezier: (0.4,0,1,1) \n Token: motion-timing-exit"
   },
   {
     videoSrc: TransitionStandardEasing,
-    title: "7Small motion - 250ms",
+    title: "Transition - Standard easing ",
+    description: "Transition between different states of the same element in screen",
+    tokenDescription: "Qubic bezier: (0.2,0,0.4,1) \n Token: motion-timing-transition"
+  },
+  {
+    videoSrc: EmphesizeElasticEasing,
+    title: "Emphesize - Elastic easing",
     description: "System aletrs, notifications, atention and midiation.",
-    tokenDescription: "Token: motion-expressive-short"
+    tokenDescription: "Qubic bezier: (0,0,0.2,1.4) \n (Duration must be over 200ms ) \n motion-timing-emphesize"
   }
 ];
 
