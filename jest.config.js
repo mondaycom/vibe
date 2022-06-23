02326fe231b5fa9609413a0c05a3797712cb3af0
eran-cohen <eranco@monday.com>
2022-06-23 12:16:09 +0300

    Jest snapshot css modules (#713)
    * fix css-modules classnames on snapshot tests
    
    * update snapshots
    
    * remove unneeded import
    
    * remove unneeded styleMock.js

--- jest.config.js
+++ jest.config.js
@@ -78,7 +78,7 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js"
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy"
   },
 
   modulePaths: ["<rootDir>/src"],
