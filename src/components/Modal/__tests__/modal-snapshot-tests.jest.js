db7d23e342f1d0177cedc114745e516f0e73eb56
Eran Cohen <eranco@monday.com>
2022-06-23 09:21:29 +0300

    added more snapshot tests

--- src/components/Modal/__tests__/modal-snapshot-tests.jest.js
+++ src/components/Modal/__tests__/modal-snapshot-tests.jest.js
@@ -1,6 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Modal from "../Modal";
+import { ModalContent, ModalFooter, ModalHeader } from "components";
 
 const baseProps = {
   id: "modal-id",
@@ -9,9 +10,49 @@ const baseProps = {
   onClose: () => {}
 };
 
+const content = <p>content</p>;
+const footerContent = <p>footer</p>;
+
 describe("Modal renders correctly", () => {
   it("with base props", () => {
-    const tree = renderer.create(<Modal {...baseProps} />).toJSON();
+    const tree = renderer.create(<Modal {...baseProps}>{content}</Modal>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with Header", () => {
+    const tree = renderer
+      .create(
+        <Modal {...baseProps}>
+          <ModalHeader title={"ModalHeader Title"} />
+          {content}
+        </Modal>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with Footer", () => {
+    const tree = renderer
+      .create(
+        <Modal {...baseProps}>
+          {content}
+          <ModalFooter>{footerContent}</ModalFooter>
+        </Modal>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("with all parts", () => {
+    const tree = renderer
+      .create(
+        <Modal {...baseProps}>
+          <ModalHeader title={"ModalHeader Title"} />
+          <ModalContent>{content}</ModalContent>
+          <ModalFooter>{footerContent}</ModalFooter>
+        </Modal>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
