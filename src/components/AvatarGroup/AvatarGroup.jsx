32ea4e9e461ec8b3c90ee258daff356c4e0fb3f5
Sergey Roytman <sergeyro@monday.com>
2022-06-26 11:14:29 +0300

    Revert "AvatarGroupCotainer refactor"
    This reverts commit 1cf7dbea9b7a40cbe8e44a98437c76c20c192383.

--- src/components/AvatarGroup/AvatarGroup.jsx
+++ src/components/AvatarGroup/AvatarGroup.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import cx from "classnames";
 import Avatar from "../Avatar/Avatar";
 import Counter from "../Counter/Counter";
-import Flex from "../Flex/Flex";
 import Tooltip from "../Tooltip/Tooltip";
 import AvatarGroupCounter from "./AvatarGroupCounter";
 import styles from "./AvatarGroup.module.scss";
@@ -37,14 +36,7 @@ const AvatarGroup = ({
   }
 
   return (
-    <Flex
-      id={id}
-      className={cx(styles.avatarGroupContainer, className)}
-      direction={Flex.directions.ROW}
-      align={Flex.align.START}
-      justify={Flex.justify.START}
-      wrap
-    >
+    <div className={cx(styles.avatarGroupContainer, className)} id={id}>
       {displayAvatars.map((avatar, index) => {
         return React.cloneElement(avatar, {
           key: index,
@@ -63,7 +55,7 @@ const AvatarGroup = ({
         size={size}
         type={type}
       />
-    </Flex>
+    </div>
   );
 };
 
