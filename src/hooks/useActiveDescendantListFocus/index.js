763360da37dc85ea7e98fa11095b946a3c5e34ea
Tom Bogin <tombo@monday.com>
2022-07-05 16:25:36 +0300

    lint fix

--- src/hooks/useActiveDescendantListFocus/index.js
+++ src/hooks/useActiveDescendantListFocus/index.js
@@ -59,6 +59,7 @@ function useActiveDescendantListFocus({
     prevVisualFocusItemId,
     visualFocusItemId,
     visualFocusItemIndex,
+    keepOptionSelected,
     itemsIds,
     previousItemIds
   ]);
