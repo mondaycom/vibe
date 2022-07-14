977376279df4d52d59790d1df4dfefad05b3963e
amit <amitci@monday.com>
2022-07-14 17:47:23 +0300

    Navigate in list with keyboard.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,28 +1,66 @@
-import React, { useRef, forwardRef, useMemo } from "react";
+import React, { useRef, forwardRef, useMemo, useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import useMergeRefs from "../../hooks/useMergeRefs";
 import { VirtualizedListItems } from "components/List/VirtualizedListItems/VirtualizedListItems";
 import "./List.scss";
+import { keyCodes } from "../../constants/KeyCodes";
 
 const List = forwardRef(
   ({ className, id, component, children, dense, ariaLabel, ariaDescribedBy, renderOnlyVisibleItems, style }, ref) => {
     const componentRef = useRef(null);
+    const [focusIndex, setFocusIndex] = useState(0);
     const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
     const Component = component;
+    const childrenRefs = useRef([]);
+    const changeFocus = focusIndex => {
+      setFocusIndex(focusIndex);
+      childrenRefs.current[focusIndex].focus();
+    };
+    const onKeyDown = useCallback(
+      event => {
+        const isUpKey = event.keyCode === keyCodes.UP_ARROW;
+        const isDownKey = event.keyCode === keyCodes.DOWN_ARROW;
+        if (isUpKey || isDownKey) {
+          if (isDownKey && focusIndex + 1 < childrenRefs.current.length) {
+            changeFocus(focusIndex + 1);
+          } else if (isUpKey && focusIndex > 0) {
+            changeFocus(focusIndex - 1);
+          }
+          event.preventDefault();
+        }
+      },
+      [focusIndex]
+    );
+    const childMapping = useCallback(
+      (child, index) => {
+        return {
+          ...child,
+          ref: ref => (childrenRefs.current[index] = ref),
+          props: {
+            ...child.props,
+            tabIndex: focusIndex === index ? "0" : "-1"
+          }
+        };
+      },
+      [focusIndex]
+    );
     const overrideChildren = useMemo(() => {
       let override = children;
       if (renderOnlyVisibleItems) {
         override = <VirtualizedListItems>{override}</VirtualizedListItems>;
+      } else {
+        override = override.map(childMapping);
       }
 
       return override;
-    }, [children, renderOnlyVisibleItems]);
+    }, [childMapping, children, renderOnlyVisibleItems]);
 
     return (
       <Component
         ref={mergedRef}
         style={style}
+        onKeyDown={!renderOnlyVisibleItems ? onKeyDown : undefined}
         className={cx("monday-style-list", className, {
           "monday-style-list--dense": dense,
           "monday-style-list-container": renderOnlyVisibleItems
@@ -30,6 +68,7 @@ const List = forwardRef(
         id={id}
         aria-label={ariaLabel}
         aria-describedby={ariaDescribedBy}
+        tabIndex="-1"
       >
         {overrideChildren}
       </Component>
