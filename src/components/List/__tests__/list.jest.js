81576a71f1186983c3d138d36935b42daed4f726
Amit Cirt <siamitcs0@gmail.com>
2022-07-20 14:09:45 +0300

    Add test for list without props. (#763)
    Co-authored-by: amit <amitci@monday.com>

--- src/components/List/__tests__/list.jest.js
+++ src/components/List/__tests__/list.jest.js
@@ -2,6 +2,11 @@ import React from "react";
 import renderer from "react-test-renderer";
 import List from "../List";
 
+it("renders correctly without props", () => {
+  const tree = renderer.create(<List />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 it("renders correctly with empty props", () => {
   const tree = renderer.create(<List>Something</List>).toJSON();
   expect(tree).toMatchSnapshot();
