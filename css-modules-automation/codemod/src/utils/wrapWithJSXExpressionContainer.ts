1d7de98a1946181949c4c097f460397d82a20a2a
Sergey Roytman <sergeyro@monday.com>
2022-07-23 13:26:44 +0300

    wrapWithJSXExpressionContainer types refactoring

--- css-modules-automation/codemod/src/utils/wrapWithJSXExpressionContainer.ts
+++ css-modules-automation/codemod/src/utils/wrapWithJSXExpressionContainer.ts
@@ -1,11 +1,17 @@
-import * as t from "@babel/types";
+import { JSXAttribute, JSXExpressionContainer } from "@babel/types";
 import { NodePath } from "@babel/traverse";
+import { types as t } from "@babel/core";
 
-export const wrapWithJSXExpressionContainer = (path: NodePath) => {
-  const literalNode = path.node as t.StringLiteral;
-  const node = literalNode.value as any as t.Node;
-  // @ts-ignore
-  const stringValue: string = node["value"];
-  const res = t.jsxExpressionContainer(t.stringLiteral(stringValue));
-  return res;
+/**
+ * Replace className="..." with className={"..."}
+ * @param path Path to parrent JSXAttribute nodde
+ */
+export const wrapWithJSXExpressionContainer = (path: NodePath<JSXAttribute>): JSXExpressionContainer | JSXAttribute => {
+  if (path.node.value?.type === "StringLiteral") {
+    const stringValue = path.node.value["value"];
+    const res = t.jsxExpressionContainer(t.stringLiteral(stringValue));
+    return res;
+  } else {
+    return path.node;
+  }
 };
