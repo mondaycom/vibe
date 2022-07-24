97ba70ad9b63f6605f01d5a4d63d3e363d3c930b
Sergey Roytman <sergeyro@monday.com>
2022-07-24 14:51:19 +0300

    fix - New test: anchor-list-item-multiple-classes

--- css-modules-automation/codemod/tests/anchor-list-item-multiple-classes.jsx
+++ css-modules-automation/codemod/tests/anchor-list-item-multiple-classes.jsx
@@ -1,18 +1,9 @@
 import PropTypes from "prop-types";
 import "./anchor-list-item-multiple-classes.scss";
+import cx from "classnames";
 
 export const AnchorListItem = ({ children, className }) => {
-  return (
-    <li
-      className={cx(
-        "monday-storybook-anchor-list-item-multiple-classes-1",
-        "monday-storybook-anchor-list-item-multiple-classes-2",
-        className
-      )}
-    >
-      {children}
-    </li>
-  );
+  return <li className={cx("class-1 class-2", "class-3", className)}>{children}</li>;
 };
 AnchorListItem.propTypes = {
   children: PropTypes.element
