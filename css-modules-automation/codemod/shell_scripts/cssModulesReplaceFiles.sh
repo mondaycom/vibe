b54226c9a04244d4617f4c3792b7f80eef02de61
Sergey Roytman <sergeyro@monday.com>
2022-07-25 19:19:17 +0300

    add shellScript - cssModulesReplaceFiles

--- css-modules-automation/codemod/shell_scripts/cssModulesReplaceFiles.sh
+++ css-modules-automation/codemod/shell_scripts/cssModulesReplaceFiles.sh
@@ -1,4 +1,20 @@
 #bin/zsh
-while read line; do
-  echo "$line"
-done < ./cssModulesReplaceFiles_Input.txt
+# Reads input file line by line and replace according .scss files with .module.scss files with same content
+INPUT_FILE="./cssModulesReplaceFiles_Input.txt"
+
+while read -r line; do
+  echo "line: $line"
+
+  fileDirPath=$(dirname -- "$line")
+  fileName=$(basename -- "$line")
+  fileName="${fileName%.*}"
+
+  cd "$fileDirPath" || (echo "Error such path as $fileDirPath doesn't exists! Paths to files should be absolute" && exit)
+
+  if test -f "$fileName.scss"; then
+      echo "$fileName.scss exists."
+      cp "$fileName.scss" "$fileName.module.scss"
+      rm "$fileName.scss"
+  fi
+
+done < "$INPUT_FILE"
