3b26ffe7ba984cb1ea9f3312f731be33edfb9f0f
Sergey Roytman <sergeyro@monday.com>
2022-07-31 11:40:13 +0300

    Replace BEM helper functions with full class name - support cases with variables inside

--- css-modules-automation/codemod/src/index.ts
+++ css-modules-automation/codemod/src/index.ts
@@ -1,4 +1,4 @@
-import { PluginObj, Visitor } from "@babel/core";
+import { PluginObj, template, Visitor } from "@babel/core";
 import * as t from "@babel/types";
 import { NodePath } from "@babel/traverse";
 import { defaults } from "lodash";
@@ -180,13 +180,34 @@ const bemHelperCallExpressionsVisitors: Visitor<State> = {
       const bemArguments = path.node.arguments;
       if (bemArguments.length === 1 && t.isObjectExpression(bemArguments[0])) {
         const properties = bemArguments[0].properties as t.ObjectProperty[];
-        const bemElement = properties.find(p => t.isIdentifier(p.key) && p.key.name === "element")
-          ?.value as t.StringLiteral;
-        const bemState = properties.find(p => t.isIdentifier(p.key) && p.key.name === "state")
-          ?.value as t.StringLiteral;
-        const bemClassName = bemHelper({ element: bemElement?.value, state: bemState?.value });
-        printWithCondition(false, "~~~ CallExpression, bemHelper, bemClassName", bemClassName);
-        path.replaceWith(t.stringLiteral(bemClassName));
+        const bemElement = properties.find(p => t.isIdentifier(p.key) && p.key.name === "element")?.value;
+        const bemState = properties.find(p => t.isIdentifier(p.key) && p.key.name === "state")?.value;
+
+        if ((!bemElement || bemElement.type === "StringLiteral") && (!bemState || bemState.type === "StringLiteral")) {
+          const bemClassName = bemHelper({ element: bemElement?.value, state: bemState?.value });
+          printWithCondition(false, "~~~ CallExpression, bemHelper, bemClassName", bemClassName);
+          path.replaceWith(t.stringLiteral(bemClassName));
+        } else if (
+          (!bemElement || bemElement.type === "StringLiteral" || bemElement.type === "Identifier") &&
+          (!bemState || bemState.type === "StringLiteral" || bemState.type === "Identifier")
+        ) {
+          // Either state either element is Identifier (variable)
+          let bemElementString = bemElement && bemElement.type === "StringLiteral" && bemElement.value;
+          let bemStateString = bemState && bemState.type === "StringLiteral" && bemState.value;
+          if (bemElement?.type === "Identifier") {
+            bemElementString = `\$\{${bemElement.name}\}`;
+          }
+          if (bemState?.type === "Identifier") {
+            bemStateString = `\$\{${bemState.name}\}`;
+          }
+          printWithCondition(false, "~~~ CallExpression, bemHelper, bemElementString", bemElementString);
+          printWithCondition(false, "~~~ CallExpression, bemHelper, bemStateString", bemStateString);
+
+          const bemClassName = bemHelper({ element: bemElementString, state: bemStateString });
+          printWithCondition(false, "~~~ CallExpression, bemHelper, bemClassName", bemClassName);
+
+          path.replaceWith(template(`\`${bemClassName}\``)() as any);
+        }
       }
     }
   }
