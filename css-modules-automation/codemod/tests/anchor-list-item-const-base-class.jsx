08e720686fcb4153e6dd2ada6ee4c552c9be2f6d
Sergey Roytman <sergeyro@monday.com>
2022-08-01 17:45:56 +0300

    Update test: achor-list-item-const-base-class

--- css-modules-automation/codemod/tests/anchor-list-item-const-base-class.jsx
+++ css-modules-automation/codemod/tests/anchor-list-item-const-base-class.jsx
@@ -4,7 +4,11 @@ import "./anchor-list-item-const-base-class.scss";
 const ANCHOR_LIST_ITEM_CSS_BASE_CLASS = "monday-storybook-anchor-list-item-const-base-class";
 
 export const AnchorListItem = ({ children }) => {
-  return <li className={`${ANCHOR_LIST_ITEM_CSS_BASE_CLASS}--disabled`}>{children}</li>;
+  return (
+    <li className={`${ANCHOR_LIST_ITEM_CSS_BASE_CLASS}--disabled`}>
+      <div className={`before--${ANCHOR_LIST_ITEM_CSS_BASE_CLASS}--after`}>{children}</div>
+    </li>
+  );
 };
 AnchorListItem.propTypes = {
   children: PropTypes.element
