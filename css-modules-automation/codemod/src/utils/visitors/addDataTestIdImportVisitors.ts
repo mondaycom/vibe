2729d61a10db1f08eedeb4e5642a61a1071d9692
Sergey Roytman <sergeyro@monday.com>
2022-08-15 17:06:05 +0300

    addDataTestIdImportVisitors - relative imports

--- css-modules-automation/codemod/src/utils/visitors/addDataTestIdImportVisitors.ts
+++ css-modules-automation/codemod/src/utils/visitors/addDataTestIdImportVisitors.ts
@@ -3,14 +3,7 @@ import { NodePath } from "@babel/traverse";
 import * as t from "@babel/types";
 import { Visitor } from "@babel/core";
 import { printWithCondition } from "../commonProcess/print";
-
-const dataTestIdImportDeclaration = t.importDeclaration(
-  [
-    t.importSpecifier(t.identifier("ELEMENT_TYPES"), t.identifier("ELEMENT_TYPES")),
-    t.importSpecifier(t.identifier("getTestId"), t.identifier("getTestId"))
-  ],
-  t.stringLiteral("src/utils/test-utils")
-);
+import * as p from "path";
 
 // 12: Adds `import { ELEMENT_TYPES, getTestId } from "src/utils/test-utils";` to the top of the imports
 export const addDataTestIdImportVisitors: Visitor<State> = {
@@ -20,13 +13,32 @@ export const addDataTestIdImportVisitors: Visitor<State> = {
       return;
     }
 
+    // @ts-ignore
+    const filename: string = path.hub.file.opts.filename;
+
+    const srcAbsolutePath = filename.slice(0, filename.indexOf("/components"));
+    const relativePathToTestUtils = p.relative(p.dirname(filename), `${srcAbsolutePath}/utils/test-utils`);
+    printWithCondition(false, "+++ addDataTestIdImport, filename, srcAbsolutePath", srcAbsolutePath);
+    printWithCondition(
+      false,
+      "+++ addDataTestIdImport, filename, relativePathToTestUtils",
+      filename,
+      relativePathToTestUtils
+    );
+
+    const dataTestIdImportDeclaration = t.importDeclaration(
+      [
+        t.importSpecifier(t.identifier("ELEMENT_TYPES"), t.identifier("ELEMENT_TYPES")),
+        t.importSpecifier(t.identifier("getTestId"), t.identifier("getTestId"))
+      ],
+      t.stringLiteral(relativePathToTestUtils)
+    );
+
     const newPaths = path.insertBefore(dataTestIdImportDeclaration);
     newPaths.forEach(p => p.skip());
     state.dataTestIdImported = true;
-    printWithCondition(false, "=== addDataTestIdImport, new dataTestId import inserted");
+    printWithCondition(false, "+++ addDataTestIdImport, new dataTestId import inserted");
 
-    // @ts-ignore
-    const filename = path.hub.file.opts.filename;
     // writeDownFileWithDataTestId(filename);
   }
 };
