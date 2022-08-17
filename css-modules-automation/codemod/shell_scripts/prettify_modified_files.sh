71708befbcad4d75028152624bb582d96643c3c8
Sergey Roytman <sergeyro@monday.com>
2022-08-18 02:27:06 +0300

    shell_scripts - check if file exists, before reading

--- css-modules-automation/codemod/shell_scripts/prettify_modified_files.sh
+++ css-modules-automation/codemod/shell_scripts/prettify_modified_files.sh
@@ -2,6 +2,11 @@
 
 INPUT_FILE="shell_scripts/files_to_prettify.txt"
 
+if ! test -f "$INPUT_FILE"; then
+  echo "prettify_modified_files.sh: file '$INPUT_FILE' doesn't exist"
+  exit 0;
+fi
+
 while read -r filePath; do
   # Prettify file
   prettier -w "$filePath"
