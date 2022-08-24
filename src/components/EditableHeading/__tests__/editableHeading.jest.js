9413b31bd58916b297274ab0cb4097e165ef26b1
Sergey Roytman <sergeyro@monday.com>
2022-08-24 22:47:01 +0300

    Update tests

--- src/components/EditableHeading/__tests__/editableHeading.jest.js
+++ src/components/EditableHeading/__tests__/editableHeading.jest.js
@@ -39,9 +39,9 @@ describe("EditableHeading", () => {
   });
 
   it("renders correctly with dataTestId prop", () => {
-    const { getByTestId } = render(<EditableHeading dataTestId="data-test-id" />);
+    const { getByTestId } = render(<EditableHeading data-testid={"data-testid"} />);
 
-    const editableHeadingElement = getByTestId("data-test-id");
+    const editableHeadingElement = getByTestId("data-testid");
 
     expect(editableHeadingElement).toBeInTheDocument();
   });
