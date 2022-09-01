1ac7e7eadff696079006f77ac6a920f4e37a0d05
Hadas Farhi <hadasfa@monday.com>
2022-09-01 15:23:13 +0300

    compile only files which converted to ts or used in already converted files

--- scripts/build-exports.js
+++ scripts/build-exports.js
@@ -4,6 +4,21 @@ const fs = require("fs");
 const path = require("path");
 const { publishedComponents, publishedTSComponents } = require("../webpack/published-components");
 
+function createFoldersIfNotExist() {
+  // if dist is not exist let's create it
+  const distDir = path.join(__dirname, `../dist/`);
+
+  if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+  }
+
+  // if dist/icons not exist let's create it
+  const iconsDir = path.join(distDir, `/icons`);
+  if (!fs.existsSync(iconsDir)) {
+    fs.mkdirSync(iconsDir);
+  }
+}
+
 function buildComponentExport(name, path) {
   return `export { default as ${name} } from "${path}";`;
 }
@@ -13,11 +28,19 @@ function convertExportsToFile(exportsArray, fileName) {
   fs.writeFileSync(path.join(__dirname, `../dist/${fileName}`), content, "utf8");
 }
 
-function buildComponentsEsmFile() {
-  const exports = Object.keys(publishedComponents).map(componentName =>
+function buildComponentsEsmFileByMap(componentsMap, fileName) {
+  const exports = Object.keys(componentsMap).map(componentName =>
     buildComponentExport(componentName, `./${componentName}`)
   );
-  convertExportsToFile(exports, "esm.js");
+  convertExportsToFile(exports, fileName);
+}
+
+function buildComponentsEsmFile() {
+  buildComponentsEsmFileByMap(publishedComponents, "esm.js");
+}
+
+function buildTSComponentsEsmFile() {
+  buildComponentsEsmFileByMap(publishedTSComponents, "ts-esm.js");
 }
 
 function buildIconsEsmFile() {
@@ -31,15 +54,18 @@ function buildIconsEsmFile() {
   fs.writeFileSync(path.join(__dirname, "../dist/icons/index.js"), iconsContent, "utf8");
 }
 
-/**function buildComponentsTypesIndexFile() {
+// eslint-disable-next-line no-unused-vars
+function buildComponentsTypesIndexFile() {
   const exports = Object.entries(publishedTSComponents).map(([name, path]) =>
     buildComponentExport(name, `./types/${path}`)
   );
   convertExportsToFile(exports, "types.d.ts");
-}**/
+}
 
+createFoldersIfNotExist();
 buildComponentsEsmFile();
+buildTSComponentsEsmFile();
 buildIconsEsmFile();
-// Right now we do not want to build types index file for not breaking micro-frontends d.ts files
-// (without index files there is no way to connect between the published components to their types)
-//buildComponentsTypesIndexFile();
+// we create new d.ts index file but still not give reference to it
+// until we finish to migrate all the components to typescript
+buildComponentsTypesIndexFile();
