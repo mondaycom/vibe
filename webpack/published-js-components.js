0a80beae5d8cb90748e7394a889042e251bf3336
Eran Cohen <eranco@monday.com>
2022-09-05 17:30:53 +0300

    fixed some styling issues + update from master

--- webpack/published-js-components.js
+++ webpack/published-js-components.js
@@ -79,6 +79,10 @@ const publishedJSComponents = {
   AvatarGroup: "components/AvatarGroup/AvatarGroup.jsx",
   IconButton: "components/IconButton/IconButton.jsx",
   Slider: "components/Slider/Slider.jsx",
+  Modal: "components/Modal/Modal.jsx",
+  ModalHeader: "components/ModalHeader/ModalHeader.jsx",
+  ModalFooter: "components/ModalFooter/ModalFooter.jsx",
+  ModalContent: "components/ModalContent/ModalContent.jsx",
   // Don't remove next line
   // plop_marker:published-hooks
   useKeyEvent: "hooks/useKeyEvent/index.js",
