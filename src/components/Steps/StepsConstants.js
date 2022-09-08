2efa5c82d72db4ebc0d3bb6ef20f3fc73dfb15a0
Sergey Roytman <sergeyro@monday.com>
2022-09-08 15:51:30 +0300

    Steps: fix global styles override

--- src/components/Steps/StepsConstants.js
+++ src/components/Steps/StepsConstants.js
@@ -4,7 +4,6 @@ export const NEXT_DESCRIPTION = "Next";
 export const BACK_DESCRIPTION = "Back";
 export const BACK_COMMAND_TEST_ID = "steps-backward-command";
 export const NEXT_COMMAND_TEST_ID = "steps-forward-command";
-export const STEPS_CSS_BASE_CLASS = "monday-style-steps";
 export const STEPS_TYPES = {
   NUMBERS: STEPS_NUMBERS_TYPE,
   GALLERY: STEPS_GALLERY_TYPE
