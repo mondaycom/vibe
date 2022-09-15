6dbb431e18f943b0589d45de712fa8090363e0c9
eran-cohen <eranco@monday.com>
2022-09-15 15:40:51 +0300

    animation changes (in progress)

--- src/components/Modal/useAnimationProps.js
+++ src/components/Modal/useAnimationProps.js
@@ -5,7 +5,7 @@ export default function useAnimationProps(triggerElement, instance) {
     (hideAnimation = false) => {
       let animationStart, animationEnd;
       if (triggerElement) {
-        const { top: sourceTop, left: sourceLeft } = triggerElement.getBoundingClientRect();
+        const { top: sourceTop, left: sourceLeft, right: sourceRight } = triggerElement.getBoundingClientRect();
         const {
           top: destinationTop,
           left: destinationLeft,
@@ -14,27 +14,35 @@ export default function useAnimationProps(triggerElement, instance) {
         } = instance?.$el.childNodes[1].getBoundingClientRect();
 
         animationStart = {
-          transform: `translate(${sourceLeft - destinationLeft - width / 2}px, ${
+          transform: `translate(${sourceRight - destinationLeft - width / 2}px, ${
             sourceTop - destinationTop - height / 2
-          }px) scale(0.2)`,
-          opacity: 0.2
+          }px) scale(0.6)`,
+          opacity: 0
         };
         animationEnd = { transform: `translate(0, 0) scale(1)`, opacity: 1 };
       } else {
         animationStart = {
-          transform: `scale(0.2)`,
-          opacity: 0.2
+          transform: `scale(0.70)`,
+          opacity: 0.4
         };
         animationEnd = { transform: `scale(1)`, opacity: 1 };
       }
 
-      return [
-        hideAnimation ? [animationEnd, animationStart] : [animationStart, animationEnd],
-        {
-          duration: hideAnimation ? 100 : 200,
-          easing: "ease-in"
-        }
-      ];
+      return hideAnimation
+        ? [
+            [animationEnd, animationStart],
+            {
+              duration: 100,
+              easing: "cubic-bezier(0.4, 0, 1, 1)"
+            }
+          ]
+        : [
+            [animationStart, animationEnd],
+            {
+              duration: 100,
+              easing: "cubic-bezier(0, 0, 0.35, 1)"
+            }
+          ];
     },
     [instance, triggerElement]
   );
