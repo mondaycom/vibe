6d894bbc23b1028e8f37317500b674cb6e0cbeeb
Aayush Bisen <aayushbisen@protonmail.com>
2022-10-04 16:20:51 +0530

    refactor: relative import of VibeComponentProps

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { backwardCompatibilityForProperties } from "../../helpers/backwardCompatibilityForProperties";
 import { DirectionType } from "./DividerConstants";
 import "./Divider.scss";
-import VibeComponentProps from "src/types/VibeComponentProps";
+import VibeComponentProps from "../../types/VibeComponentProps";
 
 interface DividerProps extends VibeComponentProps {
   direction?: DirectionType;
