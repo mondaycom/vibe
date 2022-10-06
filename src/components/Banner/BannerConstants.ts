5ef4dd1f6d12c71806fadbbb5ae5032c92becb41
Suvansh Rana <36293610+suvnshr@users.noreply.github.com>
2022-10-07 00:42:56 +0530

    refactor(Banner): Removed unused comments.

--- src/components/Banner/BannerConstants.ts
+++ src/components/Banner/BannerConstants.ts
@@ -1,10 +1,3 @@
-// export const IMAGE_POSITIONS = Object.freeze({
-//   LEFT: "left",
-//   RIGHT: "right",
-//   TOP: "top",
-//   BOTTOM: "bottom"
-// });
-
 export enum IMAGE_POSITIONS {
   LEFT = "left",
   RIGHT = "right",
