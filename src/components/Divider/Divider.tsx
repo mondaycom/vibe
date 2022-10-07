42586b510377610c5c4b1b0b3c87d6e196188395
Aayush Bisen <aayushbisen@protonmail.com>
2022-10-07 13:45:12 +0530

    refactor: migrate Divider in typescript (#916)
    * refactor: migrate Divider in typescript
    
    - refactor Divider and DividerConstants
    - refactor Divider jest test in typescript
    - changed Divider story as per documentation
    
    Related Issue #896
    
    * revert: retain proptypes & fix propNameArray value
    
    * refactor: relative import of VibeComponentProps

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,30 +1,31 @@
 import React from "react";
-import PropTypes from "prop-types";
 import cx from "classnames";
+import PropTypes from "prop-types";
 import { backwardCompatibilityForProperties } from "../../helpers/backwardCompatibilityForProperties";
-import { DIRECTIONS } from "./DividerConstants";
+import { DirectionType } from "./DividerConstants";
 import "./Divider.scss";
+import VibeComponentProps from "../../types/VibeComponentProps";
+
+interface DividerProps extends VibeComponentProps {
+  direction?: DirectionType;
+  classname?: string;
+}
 
 const Divider = ({
   // Backward compatibility for props naming
   classname,
-  className,
-  direction
-}) => {
+  className = undefined,
+  direction = DirectionType.HORIZONTAL
+} : DividerProps) => {
   const overrideClassName = backwardCompatibilityForProperties([className, classname]);
   return <div className={cx("monday-style-divider", overrideClassName, `monday-style-divider--${direction}`)} />;
 };
 
-Divider.directions = DIRECTIONS;
+Divider.directions = DirectionType
 
 Divider.propTypes = {
   className: PropTypes.string,
   direction: PropTypes.oneOf([Divider.directions.HORIZONTAL, Divider.directions.VERTICAL])
-};
-
-Divider.defaultProps = {
-  className: undefined,
-  direction: DIRECTIONS.HORIZONTAL
 };
 
 export default Divider;
