489cec79cf01f0d9f733f7ec9ec5c0f4987bea71
Sergey Roytman <sergeyro@monday.com>
2022-10-18 11:21:43 +0300

    AlertBannerButton: remove defaultProps

--- src/components/AlertBanner/AlertBannerButton/AlertBannerButton.tsx
+++ src/components/AlertBanner/AlertBannerButton/AlertBannerButton.tsx
@@ -1,13 +1,19 @@
 import cx from "classnames";
 import Button, { ButtonProps } from "../../Button/Button";
-
+import React from "react";
 import "./AlertBannerButton.scss";
 
 interface AlertBannerButtonProps extends ButtonProps {
   isDarkBackground?: boolean;
 }
 
-const AlertBannerButton = ({ marginLeft, isDarkBackground, ...buttonProps }: AlertBannerButtonProps) => {
+const AlertBannerButton = ({
+  marginLeft = false,
+  isDarkBackground = false,
+  ...buttonProps
+}: AlertBannerButtonProps) => {
+  const overrideButtonProps = { ...Button.defaultProps, ...buttonProps };
+
   const classNames = cx({
     "monday-style-alert-banner-button-margin-left": marginLeft,
     "monday-style-alert-banner-button-dark-background": isDarkBackground
@@ -16,7 +22,7 @@ const AlertBannerButton = ({ marginLeft, isDarkBackground, ...buttonProps }: Ale
   return (
     <div className={classNames}>
       <Button
-        {...buttonProps}
+        {...overrideButtonProps}
         size={Button.sizes.SMALL}
         className="monday-style-alert-banner-button"
         color={Button.colors.ON_PRIMARY_COLOR}
@@ -25,13 +31,8 @@ const AlertBannerButton = ({ marginLeft, isDarkBackground, ...buttonProps }: Ale
   );
 };
 
-AlertBannerButton.isAlertBannerItem = true;
-
-const { size: _sizeDefaultProp, ...linkDefaultPropTypes } = Button.defaultProps!;
-AlertBannerButton.defaultProps = {
-  ...linkDefaultPropTypes,
-  marginLeft: false,
-  isDarkBackground: false
-};
+Object.assign(AlertBannerButton, {
+  isAlertBannerItem: true
+});
 
 export default AlertBannerButton;
