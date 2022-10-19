c15006bfaa1e295daee8af20883d89018157d138
vishal-codes <itsvishal2417@gmail.com>
2022-10-19 19:51:43 +0530

    chore: Completed the requested changes
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

--- src/components/DialogContentContainer/DialogContentContainer.tsx
+++ src/components/DialogContentContainer/DialogContentContainer.tsx
@@ -1,58 +1,48 @@
-import { SIZES } from "../../constants/sizes";
 import React, { useRef, forwardRef } from "react";
 import cx from "classnames";
 import useMergeRefs from "../../hooks/useMergeRefs";
 import { BEMClass } from "../../helpers/bem-helper";
 import VibeComponentProps from "src/types/VibeComponentProps";
+import VibeComponent from "src/types/VibeComponent";
 import "./DialogContentContainer.scss";
+import { DialogSize, DialogType } from "./DialogContentContainerConstants";
 
 interface DialogContentContainerProps extends VibeComponentProps {
-  children: React.ReactNode;
-  className: string;
+  children?: React.ReactNode;
+  className?: string;
   ariaLabelledby?: string;
   ariaDescribedby?: string;
-  type: "modal" | "popover";
-  size: "small" | "medium" | "large";
-  style: React.CSSProperties;
+  type?: "modal" | "popover";
+  size?: "small" | "medium" | "large";
+  style?: React.CSSProperties;
 }
-
-const DIALOG_TYPES = {
-  MODAL: "modal",
-  POPOVER: "popover"
-};
-
-const DIALOG_SIZES = {
-  NONE: "none",
-  SMALL: SIZES.SMALL,
-  MEDIUM: SIZES.MEDIUM,
-  LARGE: SIZES.LARGE
-};
 
 const bemHelper = BEMClass("dialog-content-container");
 
-const DialogContentContainer: React.ForwardRefExoticComponent<DialogContentContainerProps> = forwardRef(
-  ({ className, ariaLabelledby, ariaDescribedby, type, size, children, style }, ref) => {
-    const componentRef = useRef(null);
-    const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
+const DialogContentContainer: VibeComponent<DialogContentContainerProps> & {
+  types?: typeof DialogType;
+  sizes?: typeof DialogSize;
+} = forwardRef(({ className, ariaLabelledby, ariaDescribedby, type, size, children, style }, ref) => {
+  const componentRef = useRef(null);
+  const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
 
-    return (
-      <div
-        role="dialog"
-        aria-labelledby={ariaLabelledby}
-        aria-describedby={ariaDescribedby}
-        ref={mergedRef}
-        style={style}
-        className={cx("dialog-content-container", className, bemHelper({ state: type }), bemHelper({ state: size }))}
-      >
-        {children}
-      </div>
-    );
-  }
-);
+  return (
+    <div
+      role="dialog"
+      aria-labelledby={ariaLabelledby}
+      aria-describedby={ariaDescribedby}
+      ref={mergedRef}
+      style={style}
+      className={cx("dialog-content-container", className, bemHelper({ state: type }), bemHelper({ state: size }))}
+    >
+      {children}
+    </div>
+  );
+});
 
 Object.assign(DialogContentContainer, {
-  types: DIALOG_TYPES,
-  sizes: DIALOG_SIZES
+  types: DialogType,
+  sizes: DialogSize
 });
 
 DialogContentContainer.defaultProps = {
--- src/components/DialogContentContainer/__tests__/dialogContentContainer.jest.tsx
+++ src/components/DialogContentContainer/__tests__/dialogContentContainer.jest.tsx
@@ -3,6 +3,6 @@ import renderer from "react-test-renderer";
 import DialogContentContainer from "../DialogContentContainer";
 
 it("renders correctly with empty props", () => {
-  const tree = renderer.create(<DialogContentContainer className="testClassName" type="popover" size="medium" style={{textAlign:'center'}}><p>Children text</p></DialogContentContainer>).toJSON();
+  const tree = renderer.create(<DialogContentContainer />).toJSON();
   expect(tree).toMatchSnapshot();
 });
