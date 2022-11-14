7895dbee3a27168540591ffac9a045c0889fc308
Hadas Farhi <hadasfa@monday.com>
2022-11-14 07:54:45 +0200

    fix issues

--- src/storybook/infra/vibe-storybook-addon/preset.js
+++
@@ -1,8 +0,0 @@
-function managerEntries(entry) {}
-module.exports = {
-  managerEntries: entry => {
-    return [...entry, require.resolve("@storybook/addon-interactions/register")];
-  }
-};
-
-module.exports = { managerEntries };
--- src/storybook/infra/vibe-storybook-preset-addon/preset.ts
+++
@@ -1,3 +0,0 @@
-export function managerEntries(entry: Array<any> = []) {
-  return [...entry, require.resolve("@storybook/addon-interactions/register")];
-}
