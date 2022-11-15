86500f1c4b41cca8d32e9a5c6f02ac22928ce9cd
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2022-11-15 14:34:11 +0200

    TS-Migration: Self review

--- types/window.d.ts
+++ types/window.d.ts
@@ -3,7 +3,6 @@ type DocumentMode = 5 | 7 | 8 | 9 | 10 | 11;
 export declare global {
   interface Window {
     MSInputMethodContext: unknown;
-    documentMode: DocumentMode;
   }
 
   interface Document {
