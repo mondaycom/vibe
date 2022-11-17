ffd6a6259117a25c6a8c7bd903479e015a505d3c
Sergey Roytman <sergeyro@monday.com>
2022-11-17 16:31:50 +0200

    TS-migration: VirtualizedList (#981)
    * TS-migration: VirtualizedList
    
    * Add @types/react-virtualized-auto-sizer package
    
    * Add dev dependency - @types/react-window
    
    * Fix PR notes

--- webpack/published-ts-components.js
+++ webpack/published-ts-components.js
@@ -58,6 +58,7 @@ const publishedTSComponents = {
   ModalContent: "components/ModalContent/ModalContent",
   Toast: "components/Toast/Toast",
   Box: "components/Box/Box",
+  VirtualizedList: "components/VirtualizedList/VirtualizedList",
   VirtualizedGrid: "components/VirtualizedGrid/VirtualizedGrid",
   LinearProgressBar: "components/ProgressBars/LinearProgressBar/LinearProgressBar",
   useKeyEvent: "hooks/useKeyEvent/index.ts",
