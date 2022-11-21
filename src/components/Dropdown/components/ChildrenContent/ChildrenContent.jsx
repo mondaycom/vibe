915ff8ff752d195d102228042dbe7ac41b83329f
Hadas Farhi <hadasfa@monday.com>
2022-11-21 15:47:12 +0200

    design review fixes

--- src/components/Dropdown/components/ChildrenContent/ChildrenContent.jsx
+++ src/components/Dropdown/components/ChildrenContent/ChildrenContent.jsx
@@ -12,14 +12,15 @@ export const ChildrenContent = ({ data, children, avatarSize = 18 }) => {
           customSize={avatarSize}
           src={data.leftAvatar}
           className={styles.avatar}
-          size={Avatar.sizes.SMALL}
           type={Avatar.types.IMG}
         />
       )}
       {data?.leftIcon && <Icon icon={data.leftIcon} clickable={false} />}
       {children}
       {data?.rightIcon && <Icon icon={data.rightIcon} clickable={false} />}
-      {data?.rightAvatar && <Avatar src={data.rightAvatar} className={styles.avatar} type={Avatar.types.IMG} />}
+      {data?.rightAvatar && (
+        <Avatar src={data.rightAvatar} className={styles.avatar} type={Avatar.types.IMG} customSize={avatarSize} />
+      )}
     </div>
   );
 };
--- src/components/Dropdown/components/singleValue/singleValue.jsx
+++ src/components/Dropdown/components/singleValue/singleValue.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
+import cx from "classnames";
 import { components } from "react-select";
 import { ChildrenContent } from "../ChildrenContent/ChildrenContent";
+import styles from "./singleValue.module.scss";
 
 const SingleValue = props => {
   const { Renderer, data, children } = props;
 
   const value = Renderer ? <Renderer {...data} /> : <ChildrenContent data={data}>{children}</ChildrenContent>;
   return (
-    <components.SingleValue {...props} className="dropdown-wrapper__single-value--reset">
+    <components.SingleValue
+      {...props}
+      className={cx(styles.singleValue, "dd", "dropdown-wrapper__single-value--reset")}
+    >
       {value}
     </components.SingleValue>
   );
