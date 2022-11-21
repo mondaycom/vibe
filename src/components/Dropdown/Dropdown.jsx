f5e45e753fdce25b95255df6fb4688441fdf82f4
Hadas Farhi <hadasfa@monday.com>
2022-11-21 10:15:49 +0200

    support icon and avatar in single state

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -12,7 +12,7 @@ import DropdownIndicatorComponent from "./components/DropdownIndicator/DropdownI
 import OptionComponent from "./components/option/option";
 import SingleValueComponent from "./components/singleValue/singleValue";
 import ClearIndicatorComponent from "./components/ClearIndicator/ClearIndicator";
-import ValueContainer from "./components/ValueContainer/ValueContainer";
+import MultiValueContainer from "./components/MultiValueContainer/MultiValueContainer";
 import { ADD_AUTO_HEIGHT_COMPONENTS, defaultCustomStyles, DROPDOWN_ID } from "./DropdownConstants";
 import generateBaseStyles, { customTheme } from "./Dropdown.styles";
 import Control from "./components/Control/Control";
@@ -153,7 +153,7 @@ const Dropdown = ({
   const Input = useCallback(props => <components.Input {...props} aria-label="Dropdown input" />, []);
 
   const SingleValue = useCallback(
-    props => <SingleValueComponent {...props} Renderer={finalValueRenderer} />,
+    props => <SingleValueComponent {...props} Renderer={finalValueRenderer} selectedOption={selectedOptions[0]} />,
     [finalValueRenderer]
   );
 
@@ -253,10 +253,10 @@ const Dropdown = ({
         Input,
         Option,
         Control,
-        ...(finalValueRenderer && { SingleValue }),
+        SingleValue,
         ...(multi && {
           MultiValue: NOOP, // We need it for react-select to behave nice with "multi"
-          ValueContainer
+          ValueContainer: MultiValueContainer
         }),
         ...(isVirtualized && { MenuList: WindowedMenuList })
       }}
--- src/components/Dropdown/components/singleValue/singleValue.jsx
+++ src/components/Dropdown/components/singleValue/singleValue.jsx
@@ -1,13 +1,31 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
 import { components } from "react-select";
+import styles from "./SingleValue.module.scss";
+import Avatar from "../../../Avatar/Avatar";
+import Icon from "../../../Icon/Icon";
 
 const SingleValue = props => {
-  const { Renderer, data } = props;
-  if (!Renderer) return null;
+  const { Renderer, data, children } = props;
+
+  const value = Renderer ? (
+    <Renderer {...data} />
+  ) : (
+    <div className={styles.valueContainer}>
+      {data?.leftAvatar && (
+        <Avatar src={data.leftAvatar} className={styles.avatar} size={Avatar.sizes.SMALL} type={Avatar.types.IMG} />
+      )}
+      {data?.leftIcon && <Icon icon={data.leftIcon} clickable={false} />}
+      {children}
+      {data?.rightIcon && <Icon icon={data.rightIcon} clickable={false} />}
+      {data?.rightAvatar && (
+        <Avatar src={data.rightAvatar} className={styles.avatar} size={Avatar.sizes.SMALL} type={Avatar.types.IMG} />
+      )}
+    </div>
+  );
   return (
     <components.SingleValue {...props} className="dropdown-wrapper__single-value--reset">
-      <Renderer {...data} />
+      {value}
     </components.SingleValue>
   );
 };
