e597b37070b34aded8d765973085c9d4dded7047
Hadas Farhi <hadasfa@monday.com>
2022-11-22 15:45:49 +0200

    Display avatar/icon in single value and in options for multi and single (#994)
    * support icon and avatar in single state
    
    * support draw icon and avatar on the options
    
    * fix lint
    
    * fix all tests
    
    * fix eslint issues
    
    * design review fixes
    
    * design review fixes
    
    * wip
    
    * support square avatars
    
    * fix eslint errors
    
    * cleanup

--- src/components/Dropdown/components/MultiValueContainer/MultiValueContainer.jsx
+++ src/components/Dropdown/components/MultiValueContainer/MultiValueContainer.jsx
@@ -7,7 +7,7 @@ import Counter from "../../../Counter/Counter";
 import Dialog from "../../../Dialog/Dialog";
 import DialogContentContainer from "../../../DialogContentContainer/DialogContentContainer";
 import Chips from "../../../Chips/Chips";
-import classes from "./ValueContainer.module.scss";
+import classes from "./MultiValueContainer.module.scss";
 import { DROPDOWN_CHIP_COLORS } from "../../dropdown-constants";
 
 export default function Container({ children, selectProps, ...otherProps }) {
