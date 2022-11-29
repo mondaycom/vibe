2445855658b2e3d71abf003e0ca445cfafcb3645
Hadas Farhi <hadasfa@monday.com>
2022-11-29 13:36:51 +0200

    fix import

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ import Loader from "../../components/Loader/Loader";
 import { BUTTON_ICON_SIZE, ButtonColor, ButtonInputType, ButtonType, getActualSize, Size } from "./ButtonConstants";
 import { getParentBackgroundColorNotTransparent, TRANSPARENT_COLOR } from "./helper/dom-helpers";
 import { getTestId } from "../../utils/test-utils";
-import { isIE11 } from "src/utils/user-agent-utils";
+import { isIE11 } from "../../utils/user-agent-utils";
 import { SubIcon, VibeComponent } from "../../types";
 import { ComponentDefaultTestId } from "../../interactions-tests";
 import "./Button.scss";
