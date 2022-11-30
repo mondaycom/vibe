92ce9676a5346109a30ca8485621456601e8807a
Orr Gottlieb <orr@monday.com>
2022-11-30 15:34:43 +0200

    fix lint error of redundant escape char

--- src/utils/user-agent-utils.ts
+++ src/utils/user-agent-utils.ts
@@ -1,5 +1,5 @@
 export function isFirefox() {
-  return !!/Firefox\/([0-9\.]+)(?:\s|$)/.exec(window.navigator.userAgent);
+  return !!/Firefox\/([0-9.]+)(?:\s|$)/.exec(window.navigator.userAgent);
 }
 
 export function isIE11() {
