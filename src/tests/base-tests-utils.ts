30b1ecf61e6d93b964d72a2cee0ecc6459971f55
Hadas Farhi <hadasfa@monday.com>
2022-12-04 10:29:08 +0200

    undo

--- src/tests/base-tests-utils.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentDefaultTestId as TestIds } from "./constants";
-export const ComponentDefaultTestId = TestIds;
-export const getTestId = (elementType: TestIds, id?: string | number) => {
-  return id ? `${elementType}_${id}` : elementType;
-};
