ec7b7a56fe3a79c6f10ef76de2f752cf73a72743
Sergey Roytman <sergeyro@monday.com>
2023-01-22 19:01:57 +0200

    isClickable prop deprecated

--- src/components/Chips/Chips.tsx
+++ src/components/Chips/Chips.tsx
@@ -14,6 +14,7 @@ import { SubIcon, VibeComponent, VibeComponentProps } from "../../types";
 import useHover from "../../hooks/useHover";
 import useFocus from "../../hooks/useFocus";
 import { ComponentDefaultTestId } from "../../tests/constants";
+import { backwardCompatibilityForProperties } from "../../helpers/backwardCompatibilityForProperties";
 import useClickableProps from "../../hooks/useClickableProps";
 import { BEMClass } from "../../helpers/bem-helper";
 import "../Clickable/Clickable.scss";
@@ -63,6 +64,10 @@ interface ChipsProps extends VibeComponentProps {
   /**
    * Should element be focusable & clickable - for backward compatability
    */
+  clickable?: boolean;
+  /**
+   * Backward compatibility for props naming - please use clickable instead
+   */
   isClickable?: boolean;
 }
 
@@ -89,13 +94,15 @@ const Chips: VibeComponent<ChipsProps, HTMLElement> & {
       onClick,
       noAnimation = false,
       ariaLabel,
-      isClickable = false,
+      isClickable,
+      clickable,
       dataTestId
     },
     ref
   ) => {
+    const overrideClickable = backwardCompatibilityForProperties([clickable, isClickable], false);
     const overrideDataTestId = dataTestId || getTestId(ComponentDefaultTestId.CHIP, id);
-    const hasClickableWrapper = isClickable && (!!onClick || !!onMouseDown);
+    const hasClickableWrapper = overrideClickable && (!!onClick || !!onMouseDown);
     const hasCloseButton = !readOnly && !disabled;
 
     const [hoverRef, isHovered] = useHover();
