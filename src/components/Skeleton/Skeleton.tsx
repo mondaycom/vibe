ccf4a5246e628834e40a72fcefd4fe28d6210ad4
Sergey Roytman <sergeyro@monday.com>
2023-01-30 11:07:05 +0200

    fullWidth prop added

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -20,17 +20,29 @@ interface SkeletonProps extends VibeComponentProps {
   width?: number;
   height?: number;
   wrapperClassName?: string;
+  /**
+   * Is wrapper width = 100%
+   */
+  fullWidth?: boolean;
 }
 
 const Skeleton: FC<SkeletonProps> & {
   types?: typeof SKELETON_TYPES;
   sizes?: typeof SKELETON_SIZES;
-} = ({ type = SKELETON_TYPES.RECTANGLE, size = SKELETON_SIZES.CUSTOM, className, width, height, wrapperClassName }) => {
+} = ({
+  type = SKELETON_TYPES.RECTANGLE,
+  size = SKELETON_SIZES.CUSTOM,
+  className,
+  wrapperClassName,
+  width,
+  height,
+  fullWidth
+}) => {
   const skeletonType = SKELETON_ALLOWED_TYPES.indexOf(type) > -1 ? type : SKELETON_TYPES.RECTANGLE;
   const typeDescription = skeletonType.toUpperCase() as keyof typeof SKELETON_ALLOWED_SIZES;
   const skeletonSize = SKELETON_ALLOWED_SIZES[typeDescription].indexOf(size) > -1 ? size : SKELETON_CUSTOM_SIZE;
   return (
-    <div className={cx(SKELETON_CSS_BASE_CLASS, wrapperClassName)}>
+    <div className={cx(SKELETON_CSS_BASE_CLASS, wrapperClassName, { "full-width": fullWidth })}>
       <div
         className={cx(
           bemHelper({ element: skeletonType }),
