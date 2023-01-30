0c565947637a183cee95e5d576a6ffa01d2d495d
Hadas Farhi <hadasfa@monday.com>
2023-01-30 12:04:04 +0200

    add withoutMargin prop

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -9,16 +9,24 @@ import "./Divider.scss";
 interface DividerProps extends VibeComponentProps {
   direction?: DirectionType;
   classname?: string;
+  withoutMargin?: boolean;
 }
 
 const Divider = ({
   // Backward compatibility for props naming
   classname,
   className = undefined,
+  withoutMargin = false,
   direction = DirectionType.HORIZONTAL
 }: DividerProps) => {
   const overrideClassName = backwardCompatibilityForProperties([className, classname]);
-  return <div className={cx("monday-style-divider", overrideClassName, `monday-style-divider--${direction}`)} />;
+  return (
+    <div
+      className={cx("monday-style-divider", overrideClassName, `monday-style-divider--${direction}`, {
+        [`monday-style-divider--with-margin`]: !withoutMargin
+      })}
+    />
+  );
 };
 
 Divider.directions = DirectionType;
