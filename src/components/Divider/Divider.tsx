309d2077ba2a0a673e1feba3c1b8f716cad6346b
Hadas Farhi <hadasfa@monday.com>
2023-02-01 11:01:09 +0200

    Add support for "without margin" prop in Divider component (#1082)
    * add withoutMargin prop
    
    * add withoutMargin prop
    
    * fix snapshots and story
    
    * fix snapshots and story
    
    * fix story
    
    * Fix change to not affect specificity

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import cx from "classnames";
-import PropTypes from "prop-types";
 import { backwardCompatibilityForProperties } from "../../helpers/backwardCompatibilityForProperties";
 import { DirectionType } from "./DividerConstants";
 import VibeComponentProps from "../../types/VibeComponentProps";
@@ -9,23 +8,26 @@ import "./Divider.scss";
 interface DividerProps extends VibeComponentProps {
   direction?: DirectionType;
   classname?: string;
+  withoutMargin?: boolean;
 }
 
 const Divider = ({
   // Backward compatibility for props naming
   classname,
   className = undefined,
+  withoutMargin = false,
   direction = DirectionType.HORIZONTAL
 }: DividerProps) => {
   const overrideClassName = backwardCompatibilityForProperties([className, classname]);
-  return <div className={cx("monday-style-divider", overrideClassName, `monday-style-divider--${direction}`)} />;
+  return (
+    <div
+      className={cx("monday-style-divider", overrideClassName, `monday-style-divider--${direction}`, {
+        [`monday-style-divider--without-margin`]: withoutMargin
+      })}
+    />
+  );
 };
 
 Divider.directions = DirectionType;
-
-Divider.propTypes = {
-  className: PropTypes.string,
-  direction: PropTypes.oneOf([Divider.directions.HORIZONTAL, Divider.directions.VERTICAL])
-};
 
 export default Divider;
