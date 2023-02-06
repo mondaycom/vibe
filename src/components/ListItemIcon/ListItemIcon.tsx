232fbc9bba46002b3606066deacedc3955787635
shiraWeiss <shirawe@monday.com>
2023-02-06 10:32:27 +0200

    feat(ListItemIcon): export icon size

--- src/components/ListItemIcon/ListItemIcon.tsx
+++ src/components/ListItemIcon/ListItemIcon.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef, useRef } from "react";
 import cx from "classnames";
 import useMergeRefs from "../../hooks/useMergeRefs";
 import Icon from "../Icon/Icon";
-import { ListItemIconMargin } from "./ListItemIconConstants";
+import { ListItemIconMargin, LIST_ITEM_ICON_SIZE } from "./ListItemIconConstants";
 import { SubIcon, VibeComponentProps } from "../../types";
 import "./ListItemIcon.scss";
 
@@ -26,7 +26,7 @@ const ListItemIcon: React.FC<ListItemIconProps> = forwardRef(
         id={id}
         aria-hidden="true"
       >
-        <Icon icon={icon} clickable={false} ignoreFocusStyle iconSize={20} />
+        <Icon icon={icon} clickable={false} ignoreFocusStyle iconSize={LIST_ITEM_ICON_SIZE} />
       </div>
     );
   }
