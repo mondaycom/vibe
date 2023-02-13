0d34014eb7306ad1778e4e9d2759b989675874a9
Hadas Farhi <hadasfa@monday.com>
2023-02-13 17:09:26 +0200

    fix tests

--- src/components/Modal/ModalButtons/__tests__/modal-buttons-tests.jest.js
+++ src/components/Modal/ModalButtons/__tests__/modal-buttons-tests.jest.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, screen } from "@testing-library/react";
 import ModalButtons from "../ModalButtons";
-import { ComponentDefaultTestId } from "../../../tests/constants";
-import { getTestId } from "../../../tests/test-ids-utils";
+import { ComponentDefaultTestId } from "../../../../tests/constants";
+import { getTestId } from "../../../../tests/test-ids-utils";
 
 const renderComponent = (props = {}) => {
   return render(<ModalButtons {...props} />);
@@ -16,16 +16,16 @@ describe("Modal buttons tests", () => {
   it("should trigger cancel callback when clicked", async () => {
     const mock = jest.fn();
     renderComponent({ onCancel: mock });
-    const cancel = getTestId(ComponentDefaultTestId.MODAL_BUTTONS, "cancel");
-    fireEvent.click(cancel);
+    const cancelTestId = getTestId(ComponentDefaultTestId.MODAL_BUTTONS, "cancel");
+    fireEvent.click(screen.getByTestId(cancelTestId));
     expect(mock.mock.calls.length).toBe(1);
   });
 
   it("should trigger confirm callback when clicked", async () => {
     const mock = jest.fn();
-    renderComponent({ onCancel: mock });
-    const confirm = getTestId(ComponentDefaultTestId.MODAL_BUTTONS, "confirm");
-    fireEvent.click(confirm);
+    renderComponent({ onConfirm: mock });
+    const confirmTestId = getTestId(ComponentDefaultTestId.MODAL_BUTTONS, "confirm");
+    fireEvent.click(screen.getByTestId(confirmTestId));
     expect(mock.mock.calls.length).toBe(1);
   });
 });
