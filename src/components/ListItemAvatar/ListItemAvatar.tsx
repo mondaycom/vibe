fc34497927c0ca6f3ca8540c38eafb53c7f2ff4d
Sergey Roytman <sergeyro@monday.com>
2023-02-16 16:00:27 +0200

    List and ListItem subcomponents props

--- src/components/ListItemAvatar/ListItemAvatar.tsx
+++ src/components/ListItemAvatar/ListItemAvatar.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef, useRef } from "react";
 import cx from "classnames";
 import useMergeRefs from "../../hooks/useMergeRefs";
-import { VibeComponentProps } from "../../types";
+import { VibeComponent, VibeComponentProps } from "../../types";
 import Avatar from "../Avatar/Avatar";
 import styles from "./ListItemAvatar.module.scss";
 
@@ -10,20 +10,22 @@ export interface ListItemAvatarProps extends VibeComponentProps {
   avatarClassName?: string;
 }
 
-const ListItemAvatar: React.FC<ListItemAvatarProps> = forwardRef(({ className, id, src, avatarClassName }, ref) => {
-  const componentRef = useRef(null);
-  const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
+const ListItemAvatar: VibeComponent<ListItemAvatarProps> = forwardRef(
+  ({ className, id, src, avatarClassName }, ref) => {
+    const componentRef = useRef(null);
+    const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
 
-  return (
-    <div ref={mergedRef} className={cx(styles.listItemAvatar, className)} id={id} aria-hidden="true">
-      <Avatar
-        src={src}
-        type={Avatar.types.IMG}
-        size={Avatar.sizes.SMALL}
-        className={cx(styles.avatar, avatarClassName)}
-      />
-    </div>
-  );
-});
+    return (
+      <div ref={mergedRef} className={cx(styles.listItemAvatar, className)} id={id} aria-hidden="true">
+        <Avatar
+          src={src}
+          type={Avatar.types.IMG}
+          size={Avatar.sizes.SMALL}
+          className={cx(styles.avatar, avatarClassName)}
+        />
+      </div>
+    );
+  }
+);
 
 export default ListItemAvatar;
