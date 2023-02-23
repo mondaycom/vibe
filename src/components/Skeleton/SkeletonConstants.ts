abffbc2dd61b2537951bd06b0516dff13e4b8bbe
Hadas Farhi <hadasfa@monday.com>
2023-02-23 15:17:01 +0200

    fixes

--- src/components/Skeleton/SkeletonConstants.ts
+++ src/components/Skeleton/SkeletonConstants.ts
@@ -1,31 +1,34 @@
 // Fixed names
-export const SKELETON_CIRCLE_TYPE = "circle";
-export const SKELETON_TEXT_TYPE = "text";
-export const SKELETON_RECTANGLE_TYPE = "rectangle";
 export const SKELETON_CUSTOM_SIZE = "custom";
 
-export const SKELETON_TYPES = {
-  CIRCLE: SKELETON_CIRCLE_TYPE,
-  RECTANGLE: SKELETON_RECTANGLE_TYPE,
-  TEXT: SKELETON_TEXT_TYPE
-};
+export enum SkeletonType {
+  CIRCLE = "circle",
+  RECTANGLE = "rectangle",
+  TEXT = "text"
+}
 
-export const SKELETON_ALLOWED_TYPES = Object.values(SKELETON_TYPES);
+export enum TextSkeletonSize {
+  H1 = "h1",
+  H2 = "h2",
+  H3 = "h3",
+  H4 = "h4",
+  H5 = "h5",
+  H6 = "h6",
+  P = "p",
+  SMALL = "small"
+}
+
+export enum ShapeSkeletonSize {
+  CUSTOM = "custom"
+}
 export const SKELETON_SIZES = {
-  CUSTOM: SKELETON_CUSTOM_SIZE,
+  CUSTOM: ShapeSkeletonSize.CUSTOM,
   CIRCLE: {},
   RECTANGLE: {},
-  TEXT: {
-    H1: "h1",
-    H2: "h2",
-    H3: "h3",
-    H4: "h4",
-    H5: "h5",
-    H6: "h6",
-    P: "p",
-    SMALL: "small"
-  }
+  TEXT: TextSkeletonSize
 };
+
+export type SkeletonSizeType = TextSkeletonSize | ShapeSkeletonSize;
 
 export const SKELETON_ALLOWED_SIZES = {
   CIRCLE: [SKELETON_CUSTOM_SIZE],
