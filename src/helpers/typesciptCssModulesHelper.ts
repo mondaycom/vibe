3ef74bba1331236ac922ea0fe3ca9ca58d630542
Sergey Roytman <sergeyro@monday.com>
2023-02-26 21:56:50 +0200

    Typescript helper - fix when key is not found

--- src/helpers/typesciptCssModulesHelper.ts
+++ src/helpers/typesciptCssModulesHelper.ts
@@ -1,8 +1,9 @@
 /**
- * Return style by key - solves to fix noImplicitAny errors when referencing modular styles from ts files via index accessor
+ * Return style by key - used to fix noImplicitAny errors when referencing modular styles from ts files via index accessor
  * @param styles modular styles object
  * @param key string classname
  */
-export function getStyle<StylesType>(styles: StylesType, key: string) {
+export function getStyle<StylesType>(styles: StylesType, key: string | undefined | null) {
+  if (!key) return "";
   return styles[key as keyof typeof styles];
 }
