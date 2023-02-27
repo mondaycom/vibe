5b51d29be62573cbb96dd708a93df3f59ef1a545
Orr Gottlieb <60314759+orrgottlieb@users.noreply.github.com>
2023-02-27 18:40:20 +0200

    Radio Button Docs (#1137)
    * description
    
    * fix lint

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -10,21 +10,35 @@ import Tooltip from "../Tooltip/Tooltip";
 import "./RadioButton.scss";
 
 interface RadioButtonProps extends VibeComponentProps {
+  /**  class to be added to wrapping component */
   className?: string;
+  /** deprecated */
   componentClassName?: string;
+  /** class to add to the  text/label */
   labelClassName?: string;
+  /** class to be added to the radiobutton */
   radioButtonClassName?: string;
+  /** text value */
   text?: string;
+  /** The input control's value. When specified in the HTML, this is the initial value, and from then on it can be altered or retrieved at any time using JavaScript to access the respective HTMLInputElement object's value property. The value attribute is always optional, though should be considered mandatory for checkbox, radio, and hidden.l */
   value?: string;
+  /** A string specifying a name for the input control. This name is submitted along with the control's value when the form data is submitted. */
   name?: string;
+  /** is disabled */
   disabled?: boolean;
+  /** why the input is disabled */
   disabledReason?: string;
+  /** default checked value*/
   defaultChecked?: boolean;
   children?: React.ReactNode;
+  /** callback function when value changed */
   onSelect?: (event: React.ChangeEvent<HTMLInputElement | null>) => void;
+  /** controlled parameter */
   checked?: boolean;
   retainChildClick?: boolean;
+  /** add tab index to the children */
   childrenTabIndex?: string;
+  /** disabled animation */
   noLabelAnimation?: boolean;
 }
 
