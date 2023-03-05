2b45199bd29200b278e62b9562114041ab93879d
Sergey Roytman <sergeyro@monday.com>
2023-03-05 23:38:31 +0200

    Remove wrapper from ChildrenContent.jsx + update snapshots

--- src/components/Dropdown/components/ChildrenContent/ChildrenContent.jsx
+++ src/components/Dropdown/components/ChildrenContent/ChildrenContent.jsx
@@ -19,7 +19,7 @@ export const ChildrenContent = ({ data, children, readOnly }) => {
         <Avatar withoutBorder square={data.square} src={data.leftAvatar} type={Avatar.types.IMG} customSize={18} />
       )}
       {data?.leftIcon && <Icon icon={data.leftIcon} clickable={false} />}
-      <div>{children}</div>
+      {children}
       {data?.rightIcon && <Icon icon={data.rightIcon} clickable={false} />}
       {data?.rightAvatar && (
         <Avatar withoutBorder square={data.square} src={data.rightAvatar} type={Avatar.types.IMG} customSize={18} />
