9ca8f825d2be65abed5ea12ac89d49d748a946ac
Sergey Roytman <sergeyro@monday.com>
2023-03-06 16:15:50 +0200

    Test: anchor-list-item-template-literal

---
+++ css-modules-automation/codemod/tests/anchor-list-item-template-literal.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import cx from "classnames";
+import "./anchor-list-item-template-literal.scss";
+
+export const AnchorListItem = ({ children }) => {
+  const type = "template-literal";
+  return <li className={cx(`monday-style-anchor-list-item-${type}`)}>{children}</li>;
+};
+AnchorListItem.propTypes = {
+  children: PropTypes.element
+};
+
+AnchorListItem.defaultProps = {
+  children: null
+};
