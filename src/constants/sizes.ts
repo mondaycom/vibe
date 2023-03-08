d5461f43cfa801cdc60b6d8aec1e98551cae9da3
Orr Gottlieb <60314759+orrgottlieb@users.noreply.github.com>
2023-03-08 16:26:06 +0200

    fix Sizes issue (#1153)

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -7,3 +7,11 @@ export const BASE_SIZES = {
 export const BASE_SIZES_WITH_NONE = { NONE: "none", ...BASE_SIZES };
 
 export const SIZES = { XXS: "xxs", XS: "xs", ...BASE_SIZES } as const;
+
+export enum Sizes {
+  XXS = "xxs",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large"
+}
