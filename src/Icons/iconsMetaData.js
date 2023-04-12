8b9f529b0d3d3161701eca3edd4d616d69527451
Filip Marszelewski <filipm@monday.com>
2023-04-12 10:35:23 +0200

    Content Directory icon (#190)

--- src/Icons/iconsMetaData.js
+++ src/Icons/iconsMetaData.js
@@ -5,6 +5,13 @@ export const iconsMetaData = [
   // DO NOT REMOVE THIS FOLLOWING LINE!
   // plop_marker:icon_metadata
   {
+      name: "ContentDirectory",
+      file: "ContentDirectory.svg",
+      description: "Content Directory icon",
+      tags: ""
+  },
+
+  {
       name: "Form",
       file: "Form.svg",
       description: "Form icon",
