3ba9f33efac0e2d9757de7981602cc32899d9f34
Shlomi Toussia-Cohen <shlomito@monday.com>
2023-04-16 09:26:18 +0300

    chore: use types correctly in color-utils

--- src/utils/colors-utils.ts
+++ src/utils/colors-utils.ts
@@ -1,13 +1,9 @@
-import { COLOR_STYLES, contentColors } from "./colors-vars-map";
+import { COLOR_STYLES, COLOR_STYLES_VALUES, contentColors } from "./colors-vars-map";
 
 const ColorUtils = {
   modes: COLOR_STYLES,
   contentColors,
-  getMondayColorAsStyle: (
-    color: string,
-    mode: "regular" | "hover" | "selected" = COLOR_STYLES.REGULAR,
-    withVar = true
-  ) => {
+  getMondayColorAsStyle: (color: string, mode: COLOR_STYLES_VALUES = COLOR_STYLES.REGULAR, withVar = true) => {
     return `${withVar ? "var(" : ""}--color-${color}${mode !== COLOR_STYLES.REGULAR ? `-${mode}` : ""}${
       withVar ? ")" : ""
     }`;
