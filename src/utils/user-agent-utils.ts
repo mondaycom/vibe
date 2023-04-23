6c3951cd4210d916228f4a29ba07599a1e1c8aac
Orr Gottlieb <orr@monday.com>
2023-04-23 14:25:56 +0300

    add window check to code [prerelease]

--- src/utils/user-agent-utils.ts
+++ src/utils/user-agent-utils.ts
@@ -1,7 +1,9 @@
 export function isFirefox() {
+  if (typeof window === "undefined") return false;
   return !!/Firefox\/([0-9.]+)(?:\s|$)/.exec(window.navigator.userAgent);
 }
 
 export function isIE11() {
+  if (typeof window === "undefined") return false;
   return !!window.MSInputMethodContext && !!document.documentMode;
 }
