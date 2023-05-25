9e79ef551f86af0403ca82c87671d3ca5beb9800
Ron Nachmnay <ronna@monday.com>
2023-05-25 12:20:00 +0300

    lint-fix

--- src/hooks/useDisableScroll/index.ts
+++ src/hooks/useDisableScroll/index.ts
@@ -1,33 +1,33 @@
 import { useCallback, useEffect } from "react";
 
 const useDisableScroll = (scrollableQuerySelector: string) => {
-    const _disableScroll = useCallback((e: Event) => {
-        e.preventDefault();
-        e.stopPropagation();
+  const _disableScroll = useCallback((e: Event) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-        return false;
-    }, []);
+    return false;
+  }, []);
 
-    const disableScroll = useCallback(() => {
-        document.querySelectorAll(scrollableQuerySelector).forEach((item: Element) => {
-            item.addEventListener("wheel", _disableScroll);
-        });
-    }, [_disableScroll, scrollableQuerySelector]);
+  const disableScroll = useCallback(() => {
+    document.querySelectorAll(scrollableQuerySelector).forEach((item: Element) => {
+      item.addEventListener("wheel", _disableScroll);
+    });
+  }, [_disableScroll, scrollableQuerySelector]);
 
-    const enableScroll = useCallback(() => {
-        document.querySelectorAll(scrollableQuerySelector).forEach((item: Element) => {
-            item.removeEventListener("wheel", _disableScroll);
-        });
-    }, [_disableScroll, scrollableQuerySelector]);
+  const enableScroll = useCallback(() => {
+    document.querySelectorAll(scrollableQuerySelector).forEach((item: Element) => {
+      item.removeEventListener("wheel", _disableScroll);
+    });
+  }, [_disableScroll, scrollableQuerySelector]);
 
-    useEffect(() => {
-        return disableScroll;
-    }, [disableScroll]);
+  useEffect(() => {
+    return disableScroll;
+  }, [disableScroll]);
 
-    return {
-        disableScroll,
-        enableScroll
-    };
+  return {
+    disableScroll,
+    enableScroll
+  };
 };
 
-export default useDisableScroll
+export default useDisableScroll;
