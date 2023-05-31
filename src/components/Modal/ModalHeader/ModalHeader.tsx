3b8580d741459c3446e7115b3ef016983da9fbb7
Sergey Roytman <sergeyro@monday.com>
2023-05-31 13:30:27 +0300

    Comment

--- src/components/Modal/ModalHeader/ModalHeader.tsx
+++ src/components/Modal/ModalHeader/ModalHeader.tsx
@@ -10,7 +10,7 @@ import styles from "./ModalHeader.module.scss";
 
 export interface ModalHeaderProps extends VibeComponentProps {
   /**
-   * Title of the modal - using string is a recommended standard
+   * Title of the modal - using string is a recommended standard, as it provides well-defined styles. Using it with JSX content should be according to design guidelines.
    */
   title: ElementContent;
   /**
