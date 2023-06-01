f83b2992048be5eae8070ac9b23facc2384bab77
Hadas Farhi <hadasfa@monday.com>
2023-06-01 16:40:50 +0300

    wip tests

--- src/components/Dialog/__tests__/Dialog.interactions.ts
+++ src/components/Dialog/__tests__/Dialog.interactions.ts
@@ -1,22 +1,31 @@
 import { resetFocus } from "../../../__tests__/interactions-helper";
 import { expect } from "@storybook/jest";
-import {BoundFunctions, Screen} from "@testing-library/react";
-import {CONTEXT_MENU_DIALOG, CLICK_OUTSIDE_DIALOG, HIDE_TRIGGERS_CONTAINER} from "./DialogDataTestIds";
-import { getByTestId, interactionSuite, pressNavigationKey } from "../../../tests/interactions-utils";
-import { ComponentDefaultTestId, NavigationCommand } from "../../../tests/constants";
-import { getTestId } from "../../../tests/test-ids-utils";
-import styles from "../__stories__/useActiveDescendantListFocus.module.scss";
-import {queries, userEvent, fireEvent, waitFor} from "@storybook/testing-library";
+import { BoundFunctions, Screen } from "@testing-library/react";
+import {
+  CONTEXT_MENU_DIALOG,
+  CLICK_OUTSIDE_DIALOG,
+  HIDE_TRIGGERS_CONTAINER,
+  CLICK_OUTSIDE_DIALOG_BUTTON
+} from "./DialogDataTestIds";
+import { getByTestId, interactionSuite } from "../../../tests/interactions-utils";
+import { queries, userEvent, fireEvent, waitFor } from "@storybook/testing-library";
+
+const isDialogHiddenAfterClickOutside = createTestIfDialogHiddenAfterTrigger(CLICK_OUTSIDE_DIALOG, () =>
+  userEvent.click(getDialogContainer())
+);
+const isDialogHiddenAfterContextMenu = createTestIfDialogHiddenAfterTrigger(CONTEXT_MENU_DIALOG, () =>
+  fireEvent.contextMenu(getDialogContainer())
+);
 
 export const closeTriggersInteractionSuite = interactionSuite({
-  tests: [
-      checkIfDialogHiddenAfterTrigger(CLICK_OUTSIDE_DIALOG,  (canvas) => fireEvent.click(getDialogContainer())),
-      checkIfDialogHiddenAfterTrigger(CONTEXT_MENU_DIALOG,  (canvas) => fireEvent.contextMenu(getDialogContainer()))],
-  beforeAll: async (canvas) => {
+  tests: [isDialogHiddenAfterClickOutside, isDialogHiddenAfterContextMenu],
+  beforeAll: async canvas => {
     // From some reason we have an issue with rendering the dialogs according to the container selector in the initial mount, after clicking
     // all dialog render in the right placements
     const container = getDialogContainer();
-    await userEvent.click(container)
+    fireEvent.click(container);
+    const clickOutsideButton = await getByTestId(canvas, CLICK_OUTSIDE_DIALOG_BUTTON);
+    waitFor(() => fireEvent.click(clickOutsideButton), { timeout: 1000 });
   },
   afterEach: async () => {
     await resetFocus();
@@ -32,14 +41,16 @@ async function getDialogElement(canvas: BoundFunctions<typeof queries>, dataTest
 }
 
 async function checkIfDialogHidden(dialogElement: HTMLElement) {
-  await waitFor(() => expect(dialogElement).not.toBeInTheDocument(), {timeout: 1000});
+  await waitFor(() => expect(dialogElement).not.toBeInTheDocument(), { timeout: 1000 });
 }
 
-function checkIfDialogHiddenAfterTrigger(dataTestId: string, triggerCallback: (canvas:Screen, element: HTMLElement) => void) {
+function createTestIfDialogHiddenAfterTrigger(
+  dataTestId: string,
+  triggerCallback: (canvas: Screen, element: HTMLElement) => void
+) {
   return async function (canvas: Screen) {
-    let dialog: HTMLElement;
-    dialog = await getDialogElement(canvas, dataTestId);
+    const dialog = await getDialogElement(canvas, dataTestId);
     triggerCallback(canvas, dialog);
-    await checkIfDialogHidden(dialog)
-  }
+    await checkIfDialogHidden(dialog);
+  };
 }
