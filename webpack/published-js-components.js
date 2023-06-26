52228e083cc86daf06b7eae98a6b93914542ecb0
Sergey Roytman <sergeyro@monday.com>
2023-06-26 17:18:15 +0300

    feat: Publish <useGridKeyboardNavigationContext> and <GridKeyboardNavigationContext> (#1372)

--- webpack/published-js-components.js
+++ webpack/published-js-components.js
@@ -3,6 +3,7 @@ const publishedJSComponents = {
   MultiStepIndicator: "components/MultiStepIndicator/MultiStepIndicator",
   Dropdown: "components/Dropdown/Dropdown",
   GridKeyboardNavigationContext: "components/GridKeyboardNavigationContext/GridKeyboardNavigationContext",
+  useGridKeyboardNavigationContext: "components/GridKeyboardNavigationContext/GridKeyboardNavigationContext",
   allIcons: "components/Icon/Icons"
 };
 // eslint-disable-next-line no-undef
