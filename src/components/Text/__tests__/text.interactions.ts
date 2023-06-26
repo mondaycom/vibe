e1743ae4166e0e52bdb9dc7f26b1853e5d57c4d6
Hadas Farhi <hadasfa@monday.com>
2023-06-26 11:37:11 +0300

    fix tests

--- src/components/Text/__tests__/text.interactions.ts
+++ src/components/Text/__tests__/text.interactions.ts
@@ -5,7 +5,7 @@ import { ONE_LINE_ELLIPSIS_TEST_ID } from "./text-tests-constants";
 import { interactionSuite } from "../../../tests/interactions-utils";
 
 async function isTooltipAppearOnHover(canvas: Screen) {
-  const getText = async () => await canvas.findByTestId(ONE_LINE_ELLIPSIS_TEST_ID);
+  const getText = () => canvas.findByTestId(ONE_LINE_ELLIPSIS_TEST_ID);
   await testHoverTooltipTrigger(canvas, getText);
 }
 
--- src/components/Tooltip/__tests__/tooltip.interactions.ts
+++ src/components/Tooltip/__tests__/tooltip.interactions.ts
@@ -6,30 +6,30 @@ import { ComponentDefaultTestId, getTestId } from "../../../tests/test-ids-utils
 export async function testHoverTooltipTrigger(
   canvas: Screen,
   getHoverableElement: () => Promise<HTMLElement>,
-  tooltipData: { id?: string; content?: string } = {}
+  tooltipData: { id?: string; content?: string; showDelay?: number } = {}
 ) {
-  // If we trying to get the element before it finishing it's first render we receive the instance without the tooltip wrapper and
-  // the userEvent.hover will not work (I did not find a way to implement this using react testing library or storybook interaction tests
-  await new Promise(resolve => {
-    setTimeout(resolve, 1000); // Wait for 1 second
-  });
-
-  const hoverableElement = await getHoverableElement();
-  const { id, content } = tooltipData;
-
-  userEvent.hover(hoverableElement);
-
-  // Waiting for tooltip appearance delay
+  /**
+   // If we try to access the text element before it finishes its initial render, we will receive the instance without the tooltip events.
+   // As a result, userEvent.hover will not work the first time. However, when we try to access the hoverable element for the second time, it will contain all the events
+   // and work as expected.
+  **/
   await waitFor(
-    () => {
+    async () => {
+      const hoverableElement = await getHoverableElement();
+      userEvent.hover(hoverableElement);
+      const { id, content, showDelay = 100 } = tooltipData;
+
+      // Waiting for tooltip appearance delay (100ms by default)
+      await new Promise(resolve => {
+        setTimeout(resolve, showDelay);
+      });
       const tooltipElement =
         content !== undefined
           ? canvas.getByText(content)
           : canvas.getByTestId(getTestId(ComponentDefaultTestId.TOOLTIP, id));
       expect(tooltipElement).toBeInTheDocument();
+      userEvent.unhover(hoverableElement);
     },
     { timeout: 1000 }
   );
-
-  userEvent.unhover(hoverableElement);
 }
