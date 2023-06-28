7078fa338edf17e18b8699cc78be12b765fd2e89
Sergey Roytman <sergeyro@monday.com>
2023-06-28 10:27:43 +0300

    fix: icons, interactionsTests, testIds types exports (#1369)
    * Remove unrelevant comments
    
    * fix: <Text> in published-ts-components.js
    
    * Refactoring
    
    * Fix icons, interactionsTests, testIds types export via typesVersions

--- scripts/build-types.js
+++ scripts/build-types.js
@@ -3,6 +3,4 @@
 const { createFoldersIfNotExist, buildComponentsTypesIndexFile } = require("./build-utils");
 createFoldersIfNotExist();
 
-// we create new d.ts index file but still not give reference to it in our package.json file
-// until we finish to migrate all the components to typescript
 buildComponentsTypesIndexFile();
