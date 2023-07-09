a66b0cfee9f5c15b8840da5ed6f317e2f3f5fc71
Sergey Roytman <sergeyro@monday.com>
2023-07-09 11:55:33 +0300

    Revert "refactor: support aria a11y in Tooltip component (#1391)"
    This reverts commit 64c0ac6a9cddc8bc95f11f852f8719416e147d73.

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -151,7 +151,7 @@ export default class Tooltip extends PureComponent<TooltipProps> {
   }
 
   renderTooltipContent() {
-    const { id, theme, content, paddingSize, className, style } = this.props;
+    const { theme, content, paddingSize, className, style } = this.props;
     if (!content) {
       // don't render empty tooltip
       return null;
@@ -170,9 +170,7 @@ export default class Tooltip extends PureComponent<TooltipProps> {
     }
     return (
       <div
-        id={id}
         style={style}
-        role="tooltip"
         className={cx(
           styles.tooltip,
           getStyle(styles, camelCase(theme)),
