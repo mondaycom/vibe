e2103e0648cc2e1f982d042cb955d22101d523f5
Sergey Roytman <sergeyro@monday.com>
2023-07-13 14:45:24 +0300

    Remove FunctionArguments

--- src/storybook/components/index.jsx
+++ src/storybook/components/index.jsx
@@ -2,7 +2,5 @@ import { ComponentName } from "./component-name/component-name";
 import { Tip } from "./tip/tip";
 import { RelatedComponentsDecorator } from "./related-components/related-components-decorator";
 import { ColorsDescription } from "./colors-description/colors-description";
-import { FunctionArgument } from "./function-arguments/function-argument";
-import { FunctionArguments } from "./function-arguments/function-arguments";
 
-export { Tip, ComponentName, RelatedComponentsDecorator, ColorsDescription, FunctionArgument, FunctionArguments };
+export { Tip, ComponentName, RelatedComponentsDecorator, ColorsDescription};
