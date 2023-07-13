2081cc54e66f4bacfea6693c6abdeaa35bbad2bd
Sergey Roytman <sergeyro@monday.com>
2023-07-13 14:59:42 +0300

    Fix ComingSoon export

--- src/storybook/components/index.jsx
+++ src/storybook/components/index.jsx
@@ -3,5 +3,6 @@ import { Tip } from "./tip/tip";
 import { AlphaWarning } from "./alpha-warning/alpha-warning";
 import { RelatedComponentsDecorator } from "./related-components/related-components-decorator";
 import { ColorsDescription } from "./colors-description/colors-description";
+import { ComingSoon } from "./coming-soon/coming-soon";
 
-export { AlphaWarning, Tip, ComponentName, RelatedComponentsDecorator, ColorsDescription };
+export { AlphaWarning, Tip, ComponentName, RelatedComponentsDecorator, ColorsDescription, ComingSoon };
