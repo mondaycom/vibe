0e8ef8276eb8d26758e2c2161e04fd7beb8ff7df
Sergey Roytman <sergeyro@monday.com>
2023-07-13 14:00:21 +0300

    Remove UsageGuidelines, Link, LinkComponent

--- src/storybook/components/index.jsx
+++ src/storybook/components/index.jsx
@@ -2,25 +2,20 @@ import { ComponentName } from "./component-name/component-name";
 import { Title } from "./title/title";
 import { Tip } from "./tip/tip";
 import { ComponentRules } from "./component-rules/component-rules";
-import { UsageGuidelines } from "./usage-guidelines/usage-guidelines";
 import { RelatedComponentsDecorator } from "./related-components/related-components-decorator";
 import { MultipleStoryElementsWrapper } from "./multiple-story-elements-wrapper/multiple-story-elements-wrapper";
-import { Link } from "./link/link";
 import { VisualDescription } from "./visual-description/visual-description";
 import { ColorsDescription } from "./colors-description/colors-description";
 import { FunctionArgument } from "./function-arguments/function-argument";
 import { FunctionArguments } from "./function-arguments/function-arguments";
 
-export { default as LinkComponent } from "./link-component/link-component";
 export {
   Title,
   Tip,
   ComponentRules,
   ComponentName,
-  UsageGuidelines,
   RelatedComponentsDecorator,
   MultipleStoryElementsWrapper,
-  Link,
   VisualDescription,
   ColorsDescription,
   FunctionArgument,
