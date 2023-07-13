7840262266abb2ed0fc413f98a536840e612a69d
Sergey Roytman <sergeyro@monday.com>
2023-07-13 12:38:34 +0300

    Remove SectionName, AnchorListItem, DocFooter, Paragraph

--- src/storybook/components/paragraph/paragraph.jsx
+++
@@ -1,8 +0,0 @@
-import cx from "classnames";
-import "./paragraph.scss";
-
-export const Paragraph = ({ children, className }) => (
-  <p className={cx("monday-storybook-paragraph", className)}>{children}</p>
-);
-
-export default Paragraph;
