206ce8b4898efb0d2d14f028c3d4304ea9e24bda
Sergey Roytman <sergeyro@monday.com>
2023-07-13 12:41:26 +0300

    Remove Frame, UnstyledList, UnstyledListItem

--- src/storybook/components/index.jsx
+++ src/storybook/components/index.jsx
@@ -7,11 +7,8 @@ import { RelatedComponentsDecorator } from "./related-components/related-compone
 import { StoryDescription } from "./story-description/story-description";
 import { MultipleStoryElementsWrapper } from "./multiple-story-elements-wrapper/multiple-story-elements-wrapper";
 import { Link } from "./link/link";
-import { Frame } from "./frame/frame";
 import { VisualDescription } from "./visual-description/visual-description";
 import { ColorsDescription } from "./colors-description/colors-description";
-import { UnstyledList } from "./unstyled-list/unstyled-list";
-import { UnstyledListItem } from "./unstyled-list-item/unstyled-list-item";
 import { FunctionArgument } from "./function-arguments/function-argument";
 import { FunctionArguments } from "./function-arguments/function-arguments";
 
@@ -25,11 +22,8 @@ export {
   RelatedComponentsDecorator,
   MultipleStoryElementsWrapper,
   Link,
-  Frame,
   VisualDescription,
   ColorsDescription,
-  UnstyledList,
-  UnstyledListItem,
   FunctionArgument,
   FunctionArguments,
   StoryDescription
