a19d69d338e0775c5f434e21c91176dd169526eb
Sergey Roytman <sergeyro@monday.com>
2023-07-13 10:49:29 +0300

    RelatedComponents - use from the package

--- src/storybook/components/related-components/related-components-decorator.jsx
+++ src/storybook/components/related-components/related-components-decorator.jsx
@@ -1,28 +1,9 @@
 import PropTypes from "prop-types";
 import { descriptionTypesMap } from "./component-description-map";
-import { useMemo } from "react";
-import { BEMClass } from "../../../helpers/bem-helper";
-import "./related-components.scss";
-
-const CSS_BASE_CLASS = "monday-storybook-related-components";
-const bemHelper = BEMClass(CSS_BASE_CLASS);
+import { RelatedComponents } from "monday-ui-storybook-blocks";
 
 export const RelatedComponentsDecorator = ({ componentsNames }) => {
-  // TODO not working - throwing React is not defined error
-  // return <RelatedComponents componentsNames={componentsNames} descriptionTypesMap={descriptionTypesMap} />;
-  const componentsDataElements = useMemo(
-    () =>
-      componentsNames.map((componentName, index) => {
-        const key = `${componentName}_${index}`;
-        return (
-          <section key={key} className={bemHelper({ element: "component-data" })}>
-            {descriptionTypesMap.get(componentName)}
-          </section>
-        );
-      }),
-    [componentsNames]
-  );
-  return <article className="monday-storybook-related-components">{componentsDataElements}</article>;
+  return <RelatedComponents componentsNames={componentsNames} descriptionTypesMap={descriptionTypesMap} />;
 };
 
 RelatedComponentsDecorator.propTypes = {
