e666a6b3c819fc2f81f087f8d4f17c9829ae8828
Hadas Farhi <hadasfa@monday.com>
2023-07-17 16:38:53 +0300

    fix

--- src/components/Tipseen/Tipseen.tsx
+++ src/components/Tipseen/Tipseen.tsx
@@ -142,7 +142,7 @@ const Tipseen: VibeComponent<TipseenProps> & {
           )}
           <TipseenTitle text={title} className={cx(styles.tipseenTitle, titleClassName)} />
         </div>
-        <Text color="onPrimary" size="small" className={cx(styles.tipseenContent)}>
+        <Text color="onPrimary" size="small" paragraph className={cx(styles.tipseenContent)}>
           {content}
         </Text>
       </div>
