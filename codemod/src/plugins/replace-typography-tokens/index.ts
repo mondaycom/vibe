68dbd5cd7afb2e403f0de654afbed36905ce7cc4
Hadas Farhi <hadasfa@monday.com>
2023-07-19 11:30:36 +0300

    wip

--- codemod/src/plugins/replace-typography-tokens/index.ts
+++ codemod/src/plugins/replace-typography-tokens/index.ts
@@ -12,6 +12,11 @@ const TYPOGRAPHY_TOKENS_INFO = [
   { from: "font-subtext", to: "vibe-text", params: ["2", "normal"], import: "~monday-ui-style/dist/mixins" }
 ];
 
+const FONT_SIZE_TO_MIXIN = new Map([
+  ["key1", "value1"],
+  ["key2", "value2"],
+  ["key3", "value3"]
+]);
 export const replaceTypographyTokensPlugin: Plugin = {
   postcssPlugin: "replace-typography-tokens",
   Once(root) {
@@ -21,3 +26,43 @@ export const replaceTypographyTokensPlugin: Plugin = {
     // If implementation use different tokens for font-size, font-family, line-height, font-weight,
   }
 };
+
+function replaceSubTokensWithMixin(root) {
+  root.walkRules(function (rule) {
+    // Iterate over the list of objects
+    options.forEach(function (obj) {
+      // Check if all mandatory properties exist in the rule
+      if (
+        obj.props.every(function (prop) {
+          return rule.some(function (decl) {
+            return decl.prop === prop;
+          });
+        })
+      ) {
+        // Check if the values match the specific constants
+        if (
+          obj.values.some(function (value) {
+            return rule.some(function (decl) {
+              return decl.value === value;
+            });
+          })
+        ) {
+          // Create a new rule
+          const newRule = postcss.rule({ selector: rule.selector });
+
+          // Move existing declarations to the new rule
+          rule.each(function (decl) {
+            newRule.append(decl.clone());
+            decl.remove();
+          });
+
+          // Add new declaration to the new rule
+          newRule.append({ prop: obj.newProp, value: obj.newValue });
+
+          // Insert the new rule after the original rule
+          rule.parent.insertAfter(rule, newRule);
+        }
+      }
+    });
+  });
+}
