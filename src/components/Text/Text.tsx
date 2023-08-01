7f4042b9dabbb2b50f99452f1ff6374f85457304
Hadas Farhi <hadasfa@monday.com>
2023-08-01 13:12:43 +0300

    Update src/components/Text/Text.tsx
    Co-authored-by: Yossi Saadi <yossisa@monday.com>

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -13,7 +13,7 @@ export interface TextProps extends TypographyProps {
 
 const Text: VibeComponent<TextProps, HTMLElement> = forwardRef(
   ({ className, size = "medium", weight = "normal", ellipsis, paragraph, ...typographyProps }, ref) => {
-    const overrideEllipsis = ellipsis === undefined ? !paragraph : ellipsis;
+    const overrideEllipsis = ellipsis ?? !paragraph;
     const overrideElement = paragraph ? "p" : "div";
     return (
       <Typography
