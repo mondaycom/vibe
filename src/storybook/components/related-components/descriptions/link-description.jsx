a90e9defdb74b9c46d5caf1ca0e6c014b7eb1cb0
Yossi Saadi <yossisa@monday.com>
2023-08-08 11:57:45 +0300

    feat(Indicator): add new Indicator component to component library (#1432)

---
+++ src/storybook/components/related-components/descriptions/link-description.jsx
@@ -0,0 +1,19 @@
+import { useMemo } from "react";
+import { RelatedComponent } from "vibe-storybook-components";
+import Link from "../../../../components/Link/Link";
+import { ExternalPage } from "../../../../components/Icon/Icons";
+
+export const LinkDescription = () => {
+  const component = useMemo(
+    () => <Link text="Read more" icon={ExternalPage} iconPosition={Link.iconPositions.START} />,
+    []
+  );
+  return (
+    <RelatedComponent
+      component={component}
+      href="/?path=/docs/navigation-link--overview"
+      title="Link"
+      description="Actionable text component with connection to another web pages."
+    />
+  );
+};
