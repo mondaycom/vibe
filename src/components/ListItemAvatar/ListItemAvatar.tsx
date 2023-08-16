64892561c227a0cad63ad289d61c31c6a5891f03
Sergey Roytman <sergeyro@monday.com>
2023-08-16 18:47:48 +0300

    ListItemAvatar - component prop

--- src/components/ListItemAvatar/ListItemAvatar.tsx
+++ src/components/ListItemAvatar/ListItemAvatar.tsx
@@ -1,29 +1,39 @@
-import React, { FC, forwardRef, useRef } from "react";
+import React, { forwardRef, useRef } from "react";
 import cx from "classnames";
 import useMergeRefs from "../../hooks/useMergeRefs";
-import { VibeComponentProps } from "../../types";
+import { VibeComponent, VibeComponentProps, withStaticProps } from "../../types";
 import Avatar from "../Avatar/Avatar";
+import { ListItemComponentType } from "../ListItem/ListItemConstants";
 import styles from "./ListItemAvatar.module.scss";
 
 export interface ListItemAvatarProps extends VibeComponentProps {
+  /**
+   * the ListItem component [li, div, a]
+   */
+  component?: ListItemComponentType;
   src?: string;
   avatarClassName?: string;
 }
 
-const ListItemAvatar: FC<ListItemAvatarProps> = forwardRef(({ className, id, src, avatarClassName }, ref) => {
-  const componentRef = useRef(null);
-  const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
+const ListItemAvatar: VibeComponent<ListItemAvatarProps> & { components?: typeof ListItemComponentType } = forwardRef(
+  ({ className, id, src, avatarClassName, component = ListItemAvatar.components.DIV }, ref) => {
+    const componentRef = useRef(null);
+    const mergedRef = useMergeRefs({ refs: [ref, componentRef] });
+    const Component = component;
 
-  return (
-    <div ref={mergedRef} className={cx(styles.listItemAvatar, className)} id={id} aria-hidden="true">
-      <Avatar
-        src={src}
-        type={Avatar.types.IMG}
-        size={Avatar.sizes.SMALL}
-        className={cx(styles.avatar, avatarClassName)}
-      />
-    </div>
-  );
+    return (
+      <Component ref={mergedRef} className={cx(styles.listItemAvatar, className)} id={id} aria-hidden="true">
+        <Avatar
+          src={src}
+          type={Avatar.types.IMG}
+          size={Avatar.sizes.SMALL}
+          className={cx(styles.avatar, avatarClassName)}
+        />
+      </Component>
+    );
+  }
+);
+
+export default withStaticProps(ListItemAvatar, {
+  components: ListItemComponentType
 });
-
-export default ListItemAvatar;
