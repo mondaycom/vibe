9bfbaf4dd097a4707854734c288606c2b0a41a24
Hadas Farhi <hadasfa@monday.com>
2023-08-22 14:15:17 +0300

    Fix documentation of text and heading

--- src/storybook/components/related-components/descriptions/heading-description.jsx
+++ src/storybook/components/related-components/descriptions/heading-description.jsx
@@ -1,12 +1,12 @@
 import { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
-import Heading from "../../../../components/LegacyHeading/LegacyHeading";
+import Heading from "../../../../components/Heading/Heading";
 
 export const HeadingDescription = () => {
   const component = useMemo(() => {
     return (
       <div>
-        <Heading value="Hello world" type={Heading.types.h3} />
+        <Heading type={Heading.types.H2}>Hello world</Heading>
       </div>
     );
   }, []);
