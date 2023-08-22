b52b053a0bb7b571724a8e179fc15a3142fab376
Sergey Roytman <sergeyro@monday.com>
2023-08-22 19:23:00 +0300

    docs: ExpandCollapse story

--- src/components/ExpandCollapse/__stories__/expandCollapse.stories.js
+++ src/components/ExpandCollapse/__stories__/expandCollapse.stories.js
@@ -1,42 +1,22 @@
 import React from "react";
-import { withPerformance } from "storybook-addon-performance";
 import ExpandCollapse from "../ExpandCollapse";
+import Text from "../../Text/Text";
 import Icon from "../../Icon/Icon";
 import Robot from "../../Icon/Icons/components/Robot";
-import "./expandCollapse.stories.scss";
+import { Heading } from "../../../next/next";
+import "./ExpandCollapse.stories.scss";
 
-function HeaderComponent() {
-  return <span>I can be anything</span>;
-}
+export const ExpandCollapseCustomHeadingComponent = () => {
+  return <Heading type={Heading.types.H3}>Any component you want</Heading>;
+};
 
-function H1Component() {
-  return <h1>Any component you want</h1>;
-}
-export const Sandbox = () => {
-  const width = 300;
-  const height = 200;
-
+export const expandCollapseTemplate = args => {
   return (
-    <div style={{ width, height }}>
-      <ExpandCollapse headerComponentRenderer={HeaderComponent}>
-        <h2>insert any component you want</h2>
-        <p>here is a robot for you</p>
-        <Icon iconType={Icon.type.SVG} icon={Robot} iconSize="52px" tabindex="-1" clickable={true} />
+    <div className="expandCollapse">
+      <ExpandCollapse {...args}>
+        <Text maxLines={2}>Insert any component that you want, here is a robot for you</Text>
+        <Icon iconType={Icon.type.SVG} icon={Robot} iconSize={40} clickable={false} />
       </ExpandCollapse>
     </div>
   );
-};
-
-export const OpenByDefault = () => (
-  <div>
-    <ExpandCollapse className="expandCollapse" defaultOpenState={true} headerComponentRenderer={H1Component}>
-      <Icon iconType={Icon.type.SVG} icon={Robot} iconSize="52px" tabindex="-1" clickable={true} />
-    </ExpandCollapse>
-  </div>
-);
-
-export default {
-  title: "Components|ExpandCollapse",
-  component: ExpandCollapse,
-  decorators: [withPerformance]
 };
