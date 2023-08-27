15c6fcd146dc699b5f363c686cf28e2f647b8c65
Gilad Artzi <giladar@monday.com>
2023-08-27 10:25:36 +0300

    TableVirtualizedBody - simplifying usage

--- src/components/Table/TableVirtualizedBody/TableVirtualizedBody.tsx
+++ src/components/Table/TableVirtualizedBody/TableVirtualizedBody.tsx
@@ -1,14 +1,22 @@
-import React, { ComponentProps, FC } from "react";
+import React, { ComponentProps, CSSProperties, FC, useCallback } from "react";
 import { VibeComponentProps } from "../../../types";
-import VirtualizedList from "../../VirtualizedList/VirtualizedList";
+import VirtualizedList, { VirtualizedListItem } from "../../VirtualizedList/VirtualizedList";
 import TableBody from "../TableBody/TableBody";
 
 export interface ITableVirtualizedBodyProps extends VibeComponentProps {
   items: ComponentProps<typeof VirtualizedList>["items"];
-  itemRenderer: ComponentProps<typeof VirtualizedList>["itemRenderer"];
+  rowRenderer: (item: VirtualizedListItem["value"]) => JSX.Element;
 }
 
-const TableVirtualizedBody: FC<ITableVirtualizedBodyProps> = ({ items, itemRenderer }) => {
+const TableVirtualizedBody: FC<ITableVirtualizedBodyProps> = ({ items, rowRenderer }) => {
+  const itemRenderer: ComponentProps<typeof VirtualizedList>["itemRenderer"] = useCallback(
+    (value, index: number, style: CSSProperties) => {
+      const element = rowRenderer(value);
+      return React.cloneElement(element, { style, key: index });
+    },
+    [rowRenderer]
+  );
+
   return (
     <TableBody>
       <VirtualizedList items={items} itemRenderer={itemRenderer} getItemHeight={() => 40} layout="vertical" />
