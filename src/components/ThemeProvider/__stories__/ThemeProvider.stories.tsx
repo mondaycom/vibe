f257cabf3e339b8dfab68202ed1914daf12b7382
Sergey Roytman <sergeyro@monday.com>
2023-09-05 13:13:11 +0300

    Refactoring

--- src/components/ThemeProvider/__stories__/ThemeProvider.stories.tsx
+++ src/components/ThemeProvider/__stories__/ThemeProvider.stories.tsx
@@ -13,7 +13,7 @@ export const ThemeProviderTemplateOverview = (args: JSX.IntrinsicAttributes & Th
   );
 };
 
-export const ThemeProviderStories = (_args: JSX.IntrinsicAttributes & ThemeProviderProps) => {
+export const ThemeProviderThemingScopeTemplate = (_args: JSX.IntrinsicAttributes & ThemeProviderProps) => {
   return (
     <>
       <ThemeProvider
