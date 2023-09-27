b879a9d3a24208bd224ce67a7e0306f170ef7f11
Sergey Roytman <sergeyro@monday.com>
2023-09-27 20:46:40 +0300

    Comment out test

--- src/components/ThemeProvider/__tests__/themeProvider-tests.jest.js
+++ src/components/ThemeProvider/__tests__/themeProvider-tests.jest.js
@@ -1,14 +1,40 @@
 // import React from "react";
-// import { fireEvent, render, cleanup } from "@testing-library/react";
-// import { act } from "@testing-library/react-hooks";
+// import { render, cleanup } from "@testing-library/react";
 // import ThemeProvider from "../ThemeProvider";
+// import Button from "../../Button/Button";
+// import { SystemTheme, ThemeColor } from "../ThemeProviderConstants";
+// import Label from "../../Label/Label";
+//
+// const THEME_NAME = "test-theme-name";
+// const BUTTON_TEST_ID = "button";
 
 describe("ThemeProvider tests", () => {
-  afterEach(() => {
-    // cleanup();
-  });
+  // afterEach(() => {
+  //   cleanup();
+  // });
 
   it("empty test", () => {
     expect(true).toBe(true);
   });
+
+  // it("basic colors overrides", () => {
+  //   const { getByTestId } = render(
+  //     <ThemeProvider
+  //       theme={{
+  //         name: THEME_NAME,
+  //         colors: {
+  //           [SystemTheme.LIGHT]: {
+  //             [ThemeColor.primaryColor]: "green"
+  //           }
+  //         }
+  //       }}
+  //     >
+  //       <Label color={Label.colors.PRIMARY} text="New" />
+  //     </ThemeProvider>
+  //   );
+  //   const button = getByTestId("text");
+  //   const buttonComputedStyle = window.getComputedStyle(button);
+  //   const buttonBackgroundColor = buttonComputedStyle.getPropertyValue("color");
+  //   expect(buttonBackgroundColor).toBe("green");
+  // });
 });
