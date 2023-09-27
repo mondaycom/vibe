7eafa1504c9b0b5aa5113f7b4d4998e88490e5c2
Sergey Roytman <sergeyro@monday.com>
2023-09-27 18:50:20 +0300

    themeProvider-snapshot-tests.jest.js

--- src/components/ThemeProvider/__tests__/themeProvider-snapshot-tests.jest.js
+++ src/components/ThemeProvider/__tests__/themeProvider-snapshot-tests.jest.js
@@ -2,13 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import ThemeProvider from "../ThemeProvider";
 
+const THEME_NAME = "test-theme-name";
+
 describe("ThemeProvider renders correctly", () => {
   it("with theme name", () => {
     const tree = renderer
       .create(
         <ThemeProvider
           theme={{
-            name: "test-theme-name"
+            name: THEME_NAME
           }}
         >
           <div />
