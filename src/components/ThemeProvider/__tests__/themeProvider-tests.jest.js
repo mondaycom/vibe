d605ac8e6f86c1a2e451dbd25c1221a3376a6337
Sergey Roytman <sergeyro@monday.com>
2023-09-28 09:53:13 +0300

    themeProvider-tests.jest.js

--- src/components/ThemeProvider/__tests__/themeProvider-tests.jest.js
+++ src/components/ThemeProvider/__tests__/themeProvider-tests.jest.js
@@ -1,40 +1,93 @@
-// import React from "react";
-// import { render, cleanup } from "@testing-library/react";
-// import ThemeProvider from "../ThemeProvider";
-// import Button from "../../Button/Button";
-// import { SystemTheme, ThemeColor } from "../ThemeProviderConstants";
-// import Label from "../../Label/Label";
-//
-// const THEME_NAME = "test-theme-name";
-// const BUTTON_TEST_ID = "button";
+import { generateThemeCssOverride } from "../ThemeProviderUtils";
+
+const THEME_NAME = "test-theme-name";
 
 describe("ThemeProvider tests", () => {
-  // afterEach(() => {
-  //   cleanup();
-  // });
-
-  it("empty test", () => {
+  it("theme with empty colors", () => {
     expect(true).toBe(true);
+    const theme = {
+      name: THEME_NAME,
+      colors: {}
+    };
+    const css = generateThemeCssOverride(theme);
+    expect(css).toBe("");
   });
 
-  // it("basic colors overrides", () => {
-  //   const { getByTestId } = render(
-  //     <ThemeProvider
-  //       theme={{
-  //         name: THEME_NAME,
-  //         colors: {
-  //           [SystemTheme.LIGHT]: {
-  //             [ThemeColor.primaryColor]: "green"
-  //           }
-  //         }
-  //       }}
-  //     >
-  //       <Label color={Label.colors.PRIMARY} text="New" />
-  //     </ThemeProvider>
-  //   );
-  //   const button = getByTestId("text");
-  //   const buttonComputedStyle = window.getComputedStyle(button);
-  //   const buttonBackgroundColor = buttonComputedStyle.getPropertyValue("color");
-  //   expect(buttonBackgroundColor).toBe("green");
-  // });
+  it("theme with only one override", () => {
+    expect(true).toBe(true);
+    const theme = {
+      name: THEME_NAME,
+      colors: {
+        light: {
+          "primary-color": "#d14900"
+        }
+      }
+    };
+    const css = generateThemeCssOverride(theme);
+    expect(css).toBe(".light-app-theme .test-theme-name {--primary-color: #d14900;}\n");
+  });
+
+  it("basic theme", () => {
+    expect(true).toBe(true);
+    const theme = {
+      name: THEME_NAME,
+      colors: {
+        light: {
+          "primary-color": "#d14900",
+          "primary-hover-color": "#ad4005",
+          "primary-selected-color": "#f8dccf",
+          "primary-selected-hover-color": "#f1d3c4"
+        },
+        dark: {
+          "primary-color": "#d14901",
+          "primary-hover-color": "#ad4006",
+          "primary-selected-color": "#6d2702",
+          "primary-selected-hover-color": "#491b03"
+        },
+        black: {
+          "primary-color": "#d14902",
+          "primary-hover-color": "#ad4007",
+          "primary-selected-color": "#6d2703",
+          "primary-selected-hover-color": "#491b04"
+        },
+        hacker: {
+          "primary-color": "#d14903",
+          "primary-hover-color": "#ad4008",
+          "primary-selected-color": "#6d2704",
+          "primary-selected-hover-color": "#491b05"
+        }
+      }
+    };
+    const css = generateThemeCssOverride(theme);
+    expect(css).toBe(
+      ".light-app-theme .test-theme-name {--primary-color: #d14900;--primary-hover-color: #ad4005;--primary-selected-color: #f8dccf;--primary-selected-hover-color: #f1d3c4;}\n" +
+        ".dark-app-theme .test-theme-name {--primary-color: #d14901;--primary-hover-color: #ad4006;--primary-selected-color: #6d2702;--primary-selected-hover-color: #491b03;}\n" +
+        ".black-app-theme .test-theme-name {--primary-color: #d14902;--primary-hover-color: #ad4007;--primary-selected-color: #6d2703;--primary-selected-hover-color: #491b04;}\n" +
+        ".hacker_theme-app-theme .test-theme-name {--primary-color: #d14903;--primary-hover-color: #ad4008;--primary-selected-color: #6d2704;--primary-selected-hover-color: #491b05;}\n"
+    );
+  });
+
+  it("theme with custom classes and variables", () => {
+    expect(true).toBe(true);
+    const theme = {
+      name: THEME_NAME,
+      colors: {
+        light: {
+          "primary-color": "#d14900",
+          "brand-colors": {
+            "brand-color": "#ad4005",
+            "custom-class": {
+              "custom-value-override": "#da1234"
+            }
+          }
+        }
+      }
+    };
+    const css = generateThemeCssOverride(theme);
+    expect(css).toBe(
+      ".light-app-theme .test-theme-name {--primary-color: #d14900;}\n" +
+        ".light-app-theme .test-theme-name .brand-colors {--brand-color: #ad4005;}\n" +
+        ".light-app-theme .test-theme-name .brand-colors .custom-class {--custom-value-override: #da1234;}\n"
+    );
+  });
 });
