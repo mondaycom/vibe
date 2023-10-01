c5972c0ea25b25a30b99066e3a4f898d28aba362
Sergey Roytman <sergeyro@monday.com>
2023-10-01 13:57:19 +0300

    Description comments

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -5,6 +5,9 @@ import { generateThemeCssOverride, shouldGenerateTheme } from "./ThemeProviderUt
 import { withStaticProps } from "../../types";
 
 export interface ThemeProviderProps {
+  /**
+   * The theme to apply, consists of name - name of css class that will be added to the children, which should be unique - and the object of colors overrides for each system theme.
+   */
   theme?: Theme;
   /**
    * The children to render with the theme
@@ -12,10 +15,10 @@ export interface ThemeProviderProps {
   children: ReactElement;
 }
 
-const ThemeProvider: FC<ThemeProviderProps> & { systemThemes?: typeof SystemTheme; colors?: typeof ThemeColor } = ({
-  theme,
-  children
-}) => {
+const ThemeProvider: FC<ThemeProviderProps> & {
+  systemThemes?: typeof SystemTheme;
+  colors?: typeof ThemeColor;
+} = ({ theme, children }) => {
   const [stylesLoaded, setStylesLoaded] = useState(false);
 
   useEffect(() => {
