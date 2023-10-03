fdf79ade949fad315349edd580db6002f751b827
jes14 <yujin.kim@chainstep.com>
2023-10-03 15:03:51 +0200

    feat: update label with VibeComponent

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -2,14 +2,15 @@ import { camelCase } from "lodash-es";
 import { ComponentDefaultTestId, getTestId } from "../../tests/test-ids-utils";
 import cx from "classnames";
 import { getStyle } from "../../helpers/typesciptCssModulesHelper";
-import React, { FC, useCallback, useMemo, useRef } from "react";
+import React, { forwardRef, useCallback, useMemo, useRef } from "react";
 import { backwardCompatibilityForProperties } from "../../helpers/backwardCompatibilityForProperties";
 import Text from "../Text/Text";
 import Leg from "./Leg";
 import { LabelColor, LabelKind } from "./LabelConstants";
-import { VibeComponentProps, withStaticProps } from "../../types";
+import { VibeComponent, VibeComponentProps, withStaticProps } from "../../types";
 import styles from "./Label.module.scss";
 import useClickableProps from "../../hooks/useClickableProps/useClickableProps";
+import useMergeRefs from "../../hooks/useMergeRefs";
 
 interface LabelProps extends VibeComponentProps {
   /**
@@ -28,75 +29,82 @@ interface LabelProps extends VibeComponentProps {
   onClick?: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
 }
 
-const Label: FC<LabelProps> & {
+const Label: VibeComponent<LabelProps> & {
   colors?: typeof LabelColor;
   kinds?: typeof LabelKind;
-} = ({
-  className,
-  wrapperClassName,
-  labelClassName,
-  kind = LabelKind.FILL,
-  color = LabelColor.PRIMARY,
-  text = "",
-  isAnimationDisabled = false,
-  isLegIncluded = false,
-  id,
-  "data-testid": dataTestId,
-  onClick
-}) => {
-  const overrideClassName = backwardCompatibilityForProperties([className, wrapperClassName]) as string;
-  const isClickable = Boolean(onClick);
-  const classNames = useMemo(
-    () =>
-      cx(
-        styles.label,
-        getStyle(styles, camelCase("kind" + "-" + kind)),
-        getStyle(styles, camelCase("color" + "-" + color)),
-        {
-          [styles.withAnimation]: !isAnimationDisabled,
-          [styles.withLeg]: isLegIncluded
-        },
-        labelClassName
-      ),
-    [kind, color, isAnimationDisabled, isLegIncluded, labelClassName]
-  );
-
-  const onClickCallback = useCallback(
-    (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
-      if (onClick) {
-        event.preventDefault();
-        onClick(event);
-      }
-    },
-    [onClick]
-  );
-
-  const labelRef = useRef<HTMLSpanElement>(null);
-  const clickableProps = useClickableProps(
+} = forwardRef<HTMLElement, LabelProps>(
+  (
     {
-      onClick: onClickCallback,
+      className,
+      wrapperClassName,
+      labelClassName,
+      kind = LabelKind.FILL,
+      color = LabelColor.PRIMARY,
+      text = "",
+      isAnimationDisabled = false,
+      isLegIncluded = false,
       id,
-      ariaHidden: false,
-      ariaHasPopup: false,
-      ariaExpanded: false
+      "data-testid": dataTestId,
+      onClick
     },
-    labelRef
-  );
+    ref
+  ) => {
+    const overrideClassName = backwardCompatibilityForProperties([className, wrapperClassName]) as string;
+    const isClickable = Boolean(onClick);
+    const mergedRef = useMergeRefs({ refs: [ref] });
 
-  return (
-    <span
-      {...(isClickable && clickableProps)}
-      className={cx({ [styles.clickable]: isClickable }, overrideClassName)}
-      data-testid={dataTestId || getTestId(ComponentDefaultTestId.LABEL, id)}
-      ref={labelRef}
-    >
-      <Text element="span" type={Text.types.TEXT2} className={classNames} color={Text.colors.ON_INVERTED}>
-        <span>{text}</span>
-        <span className={cx(styles.legWrapper)}>{isLegIncluded ? <Leg /> : null}</span>
-      </Text>
-    </span>
-  );
-};
+    const classNames = useMemo(
+      () =>
+        cx(
+          styles.label,
+          getStyle(styles, camelCase("kind" + "-" + kind)),
+          getStyle(styles, camelCase("color" + "-" + color)),
+          {
+            [styles.withAnimation]: !isAnimationDisabled,
+            [styles.withLeg]: isLegIncluded
+          },
+          labelClassName
+        ),
+      [kind, color, isAnimationDisabled, isLegIncluded, labelClassName]
+    );
+
+    const onClickCallback = useCallback(
+      (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+        if (onClick) {
+          event.preventDefault();
+          onClick(event);
+        }
+      },
+      [onClick]
+    );
+
+    const labelRef = useRef<HTMLSpanElement>(null);
+    const clickableProps = useClickableProps(
+      {
+        onClick: onClickCallback,
+        id,
+        ariaHidden: false,
+        ariaHasPopup: false,
+        ariaExpanded: false
+      },
+      labelRef
+    );
+
+    return (
+      <span
+        {...(isClickable && clickableProps)}
+        className={cx({ [styles.clickable]: isClickable }, overrideClassName)}
+        data-testid={dataTestId || getTestId(ComponentDefaultTestId.LABEL, id)}
+        ref={mergedRef}
+      >
+        <Text element="span" type={Text.types.TEXT2} className={classNames} color={Text.colors.ON_INVERTED}>
+          <span>{text}</span>
+          <span className={cx(styles.legWrapper)}>{isLegIncluded ? <Leg /> : null}</span>
+        </Text>
+      </span>
+    );
+  }
+);
 
 export default withStaticProps(Label, {
   colors: LabelColor,
