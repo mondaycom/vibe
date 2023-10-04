4b951310abbb096c8c6c989e24c06d4000442bcf
Naor Peled <thehecticbyte@gmail.com>
2023-10-04 19:47:44 -0400

    wip

--- src/components/Refable/Refable.tsx
+++ src/components/Refable/Refable.tsx
@@ -1,10 +1,13 @@
-import React, { HTMLProps, MutableRefObject, ReactElement } from 'react';
+import React, { HTMLProps, MutableRefObject, ReactElement } from "react";
 import { chainFunctions, chainRefFunctions } from "../../utils/function-utils";
 import { VibeComponentProps } from "../../types";
 
 export const Refable = React.forwardRef<
   ReactElement,
-  React.PropsWithChildren<unknown & HTMLProps<any> & VibeComponentProps> & { children: ReactElement | ReactElement[] | string }
+  React.PropsWithChildren<unknown & HTMLProps<any> & VibeComponentProps> & {
+    children: ReactElement | ReactElement[] | string;
+  }
+  //   @ts-expect-error React forwardRef type def doesn't seem to support multiple refs
 >(({ children, ...rest }, ref) => {
   return React.Children.map(children, child => {
     if (!React.isValidElement(child)) return null;
