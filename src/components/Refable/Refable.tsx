6b020361bced02f9e711d624fd4ef37bf50e2384
Naor Peled <thehecticbyte@gmail.com>
2023-10-04 19:41:49 -0400

    wip

--- src/components/Refable/Refable.tsx
+++ src/components/Refable/Refable.tsx
@@ -1,32 +1,35 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { MutableRefObject, ReactNode, RefAttributes } from "react";
+import React, { HTMLProps, MutableRefObject, ReactElement } from 'react';
 import { chainFunctions, chainRefFunctions } from "../../utils/function-utils";
+import { VibeComponentProps } from "../../types";
 
-export function Refable<Component, Props extends React.PropsWithChildren>() {
-  return React.forwardRef<Component, Props>(({ children, ...rest }, ref) => {
-    return React.Children.map(children, child => {
-      if (!React.isValidElement(child)) return null;
-      if (typeof child.type !== "string") {
-        return (
-          <span ref={ref as MutableRefObject<any>} {...rest}>
-            {React.cloneElement(child, { ...child.props })}
-          </span>
-        );
-      }
-      return React.cloneElement(child, {
-        ...rest,
-        ...child.props,
-        onClick: getChainedFunction("onClick", child.props, rest),
-        onBlur: getChainedFunction("onBlur", child.props, rest),
-        onMouseEnter: getChainedFunction("onMouseEnter", child.props, rest),
-        onMouseLeave: getChainedFunction("onMouseLeave", child.props, rest),
-        onMouseDown: getChainedFunction("onMouseDown", child.props, rest),
-        onFocus: getChainedFunction("onFocus", child.props, rest),
-        ref: chainRefFunctions([child.ref, ref as MutableRefObject<any>])
-      });
+export const Refable = React.forwardRef<
+  ReactElement,
+  React.PropsWithChildren<unknown & HTMLProps<any> & VibeComponentProps> & { children: ReactElement | ReactElement[] | string }
+>(({ children, ...rest }, ref) => {
+  return React.Children.map(children, child => {
+    if (!React.isValidElement(child)) return null;
+
+    if (typeof child.type !== "string") {
+      return (
+        <span ref={ref as MutableRefObject<any>} {...rest}>
+          {React.cloneElement(child, { ...child.props })}
+        </span>
+      ) as ReactElement;
+    }
+
+    return React.cloneElement(child, {
+      ...rest,
+      ...child.props,
+      onClick: getChainedFunction("onClick", child.props, rest),
+      onBlur: getChainedFunction("onBlur", child.props, rest),
+      onMouseEnter: getChainedFunction("onMouseEnter", child.props, rest),
+      onMouseLeave: getChainedFunction("onMouseLeave", child.props, rest),
+      onMouseDown: getChainedFunction("onMouseDown", child.props, rest),
+      onFocus: getChainedFunction("onFocus", child.props, rest),
+      ref: chainRefFunctions([child.ref, ref as MutableRefObject<any>])
     });
   });
-}
+});
 
 function getChainedFunction(
   name: keyof React.HTMLProps<unknown>,
