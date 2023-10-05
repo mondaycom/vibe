a6ef0497a7f6a0264d09608ceb0eb9735d126f2e
Naor Peled <thehecticbyte@gmail.com>
2023-10-04 20:14:55 -0400

    wip

--- src/components/Refable/__tests__/Refable.jest.tsx
+++ src/components/Refable/__tests__/Refable.jest.tsx
@@ -7,6 +7,22 @@ describe("Refable", () => {
     cleanup();
   });
 
+  it("should be able to render a string child", () => {
+    const onClickCallback = jest.fn();
+    const { getByTestId } = render(
+      <Refable data-testid="ref-component" onClick={onClickCallback}>
+        test
+      </Refable>
+    );
+
+    const component = getByTestId("ref-component");
+
+    expect(component).toHaveTextContent("test");
+
+    fireEvent.click(component);
+    expect(onClickCallback.mock.calls.length).toBe(1);
+  });
+
   it("onClick callback should be called after clicking the element", () => {
     const onClickCallback = jest.fn();
     const { getByTestId } = render(
