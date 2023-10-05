156a0f83f7dba7e524c1b2b3b40c1562d417f889
Tal Koren <talkor@gmail.com>
2023-10-05 15:31:01 +0300

    feat: Implement attention box, tip, alpha warning, deprecated warning components

--- packages/vibe-storybook-components/src/components/alpha-warning/alpha-warning.tsx
+++ packages/vibe-storybook-components/src/components/alpha-warning/alpha-warning.tsx
@@ -1,10 +1,11 @@
-// TODO uses attention box - not exported for now
-// import AttentionBox from "../../../components/AttentionBox/AttentionBox";
-// import Tip from "../tip/tip";
-//
-// export const AlphaWarning = () => (
-//   <Tip emoji="🚧" title="Alpha component" type={AttentionBox.types.WARNING}>
-//     This component is currently being developed and is ready for exploratory usage. Please note that there may be
-//     breaking changes in future minor version updates and use with caution.
-//   </Tip>
-// );
+import { FC } from 'react';
+import Tip from '../tip/tip';
+
+export const AlphaWarning: FC = () => (
+  <Tip emoji="🚧" title="Alpha component" type={Tip.types?.WARNING}>
+    This component is currently being developed and is ready for exploratory usage. Please note that there may be
+    breaking changes in future minor version updates and use with caution.
+  </Tip>
+);
+
+export default AlphaWarning;
