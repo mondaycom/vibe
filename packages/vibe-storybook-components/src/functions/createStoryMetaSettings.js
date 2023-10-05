7f912b824edc1074965d4b99ad08896fde2b3064
Sergey Roytman <sergeyro@monday.com>
2023-10-05 14:01:54 +0300

    feat: createMetastorySettings - string type for ContentElement controls, disable action controls (#30)

--- packages/vibe-storybook-components/src/functions/createStoryMetaSettings.js
+++ packages/vibe-storybook-components/src/functions/createStoryMetaSettings.js
@@ -109,12 +109,23 @@ export function createStoryMetaSettings({
 
   actionPropsArray?.forEach(actionProp => {
     if (typeof actionProp === 'string') {
-      argTypes[actionProp] = { action: actionProp };
+      argTypes[actionProp] = { action: actionProp, control: false };
     } else if (actionProp?.name && actionProp.linkedToPropValue) {
       // we assume that actionPropsArray is static. If it changes, things may break, since internally we call React.useState for the story decorator.
       decorators.push(createMappedActionToInputPropDecorator(actionProp.name, actionProp.linkedToPropValue));
     }
   });
+
+  // If prop type is ElementContent, set text control, otherwise it's displaying as object
+  const componentProps = component?.__docgenInfo?.props;
+  if (componentProps) {
+    Object.keys(componentProps)?.forEach(propName => {
+      const prop = componentProps[propName];
+      if (prop?.type?.name === 'ElementContent') {
+        argTypes[propName] = { control: { type: 'text' } };
+      }
+    });
+  }
 
   // Disable controls for specific props
   ignoreControlsPropNamesArray?.forEach(propName => {
