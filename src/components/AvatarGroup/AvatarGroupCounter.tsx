b6d7580fc4cf660bb79767f0a74b0e7bbd7e29f7
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-10-11 10:24:12 +0530

    update AvatarGroupCounter

--- src/components/AvatarGroup/AvatarGroupCounter.tsx
+++ src/components/AvatarGroup/AvatarGroupCounter.tsx
@@ -72,6 +72,7 @@ const AvatarGroupCounter: React.FC<AvatarGroupCounterProps> = ({
   const areAvatarsClickable = counterTooltipAvatars.some(a => a.props?.onClick);
   if (areAvatarsClickable) {
     return (
+      // @ts-ignore TODO ts-migration: remove this line & fix the issues when MenuButton is converted to TS
       <MenuButton
         component={counterComponent}
         zIndex={1}
