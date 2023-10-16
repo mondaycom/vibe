8cedada863d6560a571cd6a1c9ff55a6cd8aa436
Balaji K <balaji6292@gmail.com>
2023-10-16 21:49:06 +0530

    Migrated < InformationBox />, < InformationBoxTitle /> to TS (#75)

--- packages/vibe-storybook-components/src/components/information-box/information-box.tsx
+++ packages/vibe-storybook-components/src/components/information-box/information-box.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { FC } from 'react';
 import InformationBoxTitle from '../information-box-title/information-box-title';
 import styles from './information-box.module.scss';
+import { ElementContent } from '../../types';
 
-const InformationBox = ({ component, title, description, href }) => {
+type InformationBoxProps = {
+  component?: ElementContent;
+  title?: ElementContent;
+  description?: string;
+  href?: string;
+};
+
+const InformationBox: FC<InformationBoxProps> = ({ component = null, title = '', description = '', href }) => {
   const overrideTitle =
     typeof title === 'string' ? <InformationBoxTitle href={href}>{title}</InformationBoxTitle> : title;
 
@@ -14,18 +21,6 @@ const InformationBox = ({ component, title, description, href }) => {
       <section className={styles.description}>{description}</section>
     </section>
   );
-};
-
-InformationBox.propTypes = {
-  component: PropTypes.element,
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-  description: PropTypes.string,
-};
-
-InformationBox.defaultProps = {
-  component: null,
-  title: '',
-  description: '',
 };
 
 export default InformationBox;
