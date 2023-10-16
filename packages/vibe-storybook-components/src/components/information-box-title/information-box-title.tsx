43daf7bf18f762cf9180834e08b1f6baf76cacf2
Balaji K <balaji6292@gmail.com>
2023-10-16 21:49:06 +0530

    Migrated < InformationBox />, < InformationBoxTitle /> to TS (#75)

--- packages/vibe-storybook-components/src/components/information-box-title/information-box-title.tsx
+++ packages/vibe-storybook-components/src/components/information-box-title/information-box-title.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { FC } from 'react';
 import cx from 'classnames';
 import styles from './information-box-title.module.scss';
+import { ElementContent } from '../../types';
 
-const InformationBoxTitle = ({ children, href }) => {
+type InformationBoxTitleProps = {
+  children: ElementContent;
+  href?: string;
+};
+
+const InformationBoxTitle: FC<InformationBoxTitleProps> = ({ children, href }) => {
   const title = <h4 className={cx(styles.informationBoxTitle, { [styles.titleLink]: href })}>{children}</h4>;
 
   return href ? (
@@ -13,14 +18,6 @@ const InformationBoxTitle = ({ children, href }) => {
   ) : (
     title
   );
-};
-
-InformationBoxTitle.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-};
-
-InformationBoxTitle.defaultProps = {
-  component: '',
 };
 
 export default InformationBoxTitle;
