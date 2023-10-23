801e62b15de0e65a55fda00b844567f2c468bf55
Sergey Roytman <sergeyro@monday.com>
2023-10-23 12:39:33 +0300

    fix createStoryMettasettings fix types (#98)

--- packages/vibe-storybook-components/src/functions/createComponentTemplate.tsx
+++ packages/vibe-storybook-components/src/functions/createComponentTemplate.tsx
@@ -1,12 +1,12 @@
 import { ComponentType, ReactElement } from 'react';
 
-type Props = {
-  [key: string]: any;
+type ArgsType = {
+  [key: string]: unknown;
 };
 
 export function createComponentTemplate(ComponentClass: ComponentType) {
   // eslint-disable-next-line react/display-name
-  return (args: Props): ReactElement => <ComponentClass {...args} />;
+  return (args: ArgsType): ReactElement => <ComponentClass {...args} />;
 }
 
 export default createComponentTemplate;
