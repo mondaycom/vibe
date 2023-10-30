908ce326f82ca13d31e3bfec38458e12608c5616
Sergey Roytman <sergeyro@monday.com>
2023-10-30 11:58:02 +0200

    <Dropdown> <ChildrenContent> iconSize from 16 to 18

--- src/components/Dropdown/components/ChildrenContent/ChildrenContent.jsx
+++ src/components/Dropdown/components/ChildrenContent/ChildrenContent.jsx
@@ -24,9 +24,9 @@ export const ChildrenContent = ({ data, children, readOnly }) => {
       {data?.leftAvatar && (
         <Avatar withoutBorder square={data.square} src={data.leftAvatar} type={Avatar.types.IMG} customSize={18} />
       )}
-      {data?.leftIcon && <Icon icon={data.leftIcon} clickable={false} />}
+      {data?.leftIcon && <Icon iconSize={18} icon={data.leftIcon} clickable={false} />}
       {children}
-      {data?.rightIcon && <Icon icon={data.rightIcon} clickable={false} />}
+      {data?.rightIcon && <Icon iconSize={18} icon={data.rightIcon} clickable={false} />}
       {data?.rightAvatar && (
         <Avatar withoutBorder square={data.square} src={data.rightAvatar} type={Avatar.types.IMG} customSize={18} />
       )}
