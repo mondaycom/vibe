198d15b4256b758f73762c255affc5a99a4876e9
Sergey Roytman <sergeyro@monday.com>
2023-11-06 13:12:47 +0200

    <Combobox> person picker Avatar size from 20 to 22

--- src/components/Combobox/__stories__/OptionRenderer.jsx
+++ src/components/Combobox/__stories__/OptionRenderer.jsx
@@ -3,7 +3,7 @@ import Avatar from "../../Avatar/Avatar";
 export const optionRenderer = ({ src, id, label, position }) => {
   return (
     <div className="person-picker-mock-option">
-      <Avatar customSize={20} src={src} type={Avatar.types.IMG} key={id} />
+      <Avatar customSize={22} src={src} type={Avatar.types.IMG} key={id} />
       <span className="person-picker-mock-name">
         {label}
         <span>{position}</span>
