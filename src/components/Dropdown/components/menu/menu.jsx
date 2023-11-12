a8f18e76627300b9e78ccce9e9971eeb92d204a3
Sergey Roytman <sergeyro@monday.com>
2023-11-12 16:26:12 +0200

    fix: <DropdownMenu> disable animation when menuPosition = fixed

--- src/components/Dropdown/components/menu/menu.jsx
+++ src/components/Dropdown/components/menu/menu.jsx
@@ -5,8 +5,11 @@ import styles from "./menu.module.scss";
 
 const Menu = ({ children, Renderer, selectProps, dropdownMenuWrapperClassName, ...props }) => {
   const rendererProps = { children, selectProps, ...props };
-  // Temporary fix for menu animation is above the select when using menuPortalTarget
-  const withoutAnimation = !!selectProps?.menuPortalTarget;
+  // Temporary fix, which disables animation :
+  // - when using menuPortalTarget or popupsContainerSelector there are issues that animation goes above the select preventing from tapping it (pr #1543)
+  // - when using menuPosition="fixed" (e.g. inside of Modal) not whole list is visible for scroll cause of it (pr #1738)
+  const withoutAnimation =
+    !!selectProps?.menuPortalTarget || !!selectProps.popupsContainerSelector || selectProps?.menuPosition === "fixed";
   return (
     <components.Menu
       {...props}
