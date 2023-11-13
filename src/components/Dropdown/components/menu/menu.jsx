fb9ad7199b7c7c9d4fbbe53f60ba602af7eb7f22
Sergey Roytman <sergeyro@monday.com>
2023-11-13 15:17:10 +0200

    Disable menu animation in Dropdown

--- src/components/Dropdown/components/menu/menu.jsx
+++ src/components/Dropdown/components/menu/menu.jsx
@@ -7,11 +7,15 @@ const Menu = ({ children, Renderer, selectProps, dropdownMenuWrapperClassName, .
   const rendererProps = { children, selectProps, ...props };
   const withFixedPosition =
     selectProps?.selectProps?.insideOverflowContainer || selectProps?.selectProps?.insideOverflowWithTransformContainer;
+
   // Temporary fix, which disables animation :
   // - when using menuPortalTarget or popupsContainerSelector there are issues that animation goes above the select preventing from tapping it (pr #1543)
   // - when using menuPosition="fixed" (e.g. inside of Modal) not whole list is visible for scroll cause of it (pr #1738)
-  const withoutAnimation =
-    !!selectProps?.menuPortalTarget || !!selectProps.popupsContainerSelector || selectProps?.menuPosition === "fixed";
+  // const withoutAnimation =
+  //   !!selectProps?.menuPortalTarget || !!selectProps.popupsContainerSelector || selectProps?.menuPosition === Dropdown.menuPositions.FIXED;
+  // Always disable animation for now - messes up in too many places and isn't really noticeable
+  const withoutAnimation = true;
+
   return (
     <components.Menu
       {...props}
