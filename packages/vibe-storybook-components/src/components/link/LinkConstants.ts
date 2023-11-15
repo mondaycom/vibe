c26f39c68738aa8aa1a5233e883dc4b2a2f9f291
Sergey Roytman <sergeyro@monday.com>
2023-11-15 17:45:58 +0200

    feat: <Link> add target prop (#124)

--- packages/vibe-storybook-components/src/components/link/LinkConstants.ts
+++ packages/vibe-storybook-components/src/components/link/LinkConstants.ts
@@ -2,3 +2,10 @@ export enum LinkSize {
   SMALL = 'small',
   MEDIUM = 'medium',
 }
+
+export enum LinkTarget {
+  NEW_WINDOW = '_blank',
+  SELF = '_self',
+  PARENT = '_parent',
+  TOP = '_top',
+}
