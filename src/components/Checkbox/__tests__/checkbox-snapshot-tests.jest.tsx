403be5dc41e46af7b8d70f30dcf033f701b61cb3
Tal Koren <talko@monday.com>
2023-11-15 10:13:01 +0200

    test: use a fixed id

--- src/components/Checkbox/__tests__/checkbox-snapshot-tests.jest.tsx
+++ src/components/Checkbox/__tests__/checkbox-snapshot-tests.jest.tsx
@@ -5,53 +5,53 @@ import Checkbox from "../Checkbox";
 describe("Checkbox renders correctly", () => {
   it("with props", () => {
     const tree = renderer
-      .create(<Checkbox className="dummy-class-name" label="Option 1" name="checkbox" value="option1" />)
+      .create(<Checkbox className="dummy-class-name" label="Option 1" name="checkbox" value="option1" id="test" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with empty props", () => {
-    const tree = renderer.create(<Checkbox />).toJSON();
+    const tree = renderer.create(<Checkbox id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("when checked", () => {
-    const tree = renderer.create(<Checkbox checked />).toJSON();
+    const tree = renderer.create(<Checkbox checked id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("when disabled", () => {
-    const tree = renderer.create(<Checkbox disabled />).toJSON();
+    const tree = renderer.create(<Checkbox disabled id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with name", () => {
-    const tree = renderer.create(<Checkbox name="checkbox" />).toJSON();
+    const tree = renderer.create(<Checkbox name="checkbox" id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with value", () => {
-    const tree = renderer.create(<Checkbox value="checkboxValue" />).toJSON();
+    const tree = renderer.create(<Checkbox value="checkboxValue" id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("when default checked", () => {
-    const tree = renderer.create(<Checkbox defaultChecked />).toJSON();
+    const tree = renderer.create(<Checkbox defaultChecked id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("when interminate", () => {
-    const tree = renderer.create(<Checkbox indeterminate />).toJSON();
+    const tree = renderer.create(<Checkbox indeterminate id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("with ariaLabelledBy", () => {
-    const tree = renderer.create(<Checkbox ariaLabelledBy="aria" />).toJSON();
+    const tree = renderer.create(<Checkbox ariaLabelledBy="aria" id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("without label", () => {
-    const tree = renderer.create(<Checkbox label={false} />).toJSON();
+    const tree = renderer.create(<Checkbox label={false} id="test" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
