fe23a2ef22e005222d31bae98b3caba9b7a8afd8
Sergey Roytman <sergeyro@monday.com>
2023-11-15 14:18:53 +0200

    Fix notes: use Text for bold, role=presentation

--- src/storybook/stand-alone-documentaion/catalog/EmptyState/Catalog.stories.EmptyState.tsx
+++ src/storybook/stand-alone-documentaion/catalog/EmptyState/Catalog.stories.EmptyState.tsx
@@ -6,13 +6,16 @@ import styles from "./Catalog.stories.EmptyState.module.scss";
 export const CatalogEmptyState = () => {
   return (
     <div className={styles.emptyStateContainer} aria-labelledby="empty-state-id">
-      <img style={{ width: "290px" }} src={emptyStateExample} alt="" />
+      <img style={{ width: "290px" }} src={emptyStateExample} alt="" role="presentation" />
       <Heading type={Heading.types.H2} id="empty-state-id" className={styles.emptyStateHeading}>
         We haven&apos;t found this component in the catalog
       </Heading>
       <Text element="span" type={Text.types.TEXT1} style={{ width: "50%", textAlign: "center" }} ellipsis={false}>
         Not all the components have been added to the Catalog already, please, also check the{" "}
-        <b>search in the left panel</b>.
+        <Text element="span" type={Text.types.TEXT1} weight={Text.weights.BOLD}>
+          search in the left panel
+        </Text>
+        .
       </Text>
     </div>
   );
