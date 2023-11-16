ca0b62fff7aaa25a666a08ac74dafc0ee2cd04cc
Sergey Roytman <sergeyro@monday.com>
2023-11-16 17:28:49 +0200

    Remove onClick

--- src/components/Table/TableRow/TableRow.tsx
+++ src/components/Table/TableRow/TableRow.tsx
@@ -1,8 +1,7 @@
-import React, { forwardRef, useCallback, useRef } from "react";
+import React, { forwardRef, useRef } from "react";
 import { VibeComponent, VibeComponentProps } from "../../../types";
 import { ITableCellProps } from "../TableCell/TableCell";
-import { useKeyEvent, useMergeRefs } from "../../../hooks";
-import { SELECTION_KEYS } from "../../../constants";
+import { useMergeRefs } from "../../../hooks";
 import { getTestId } from "../../../tests/test-ids-utils";
 import { ComponentDefaultTestId } from "../../../tests/constants";
 import styles from "./TableRow.module.scss";
@@ -12,29 +11,14 @@ export interface ITableRowProps extends VibeComponentProps {
    * Does the row have a highlighted style
    */
   highlighted?: boolean;
-  onClick?: (event: React.MouseEvent | React.KeyboardEvent) => void;
   children?: React.ReactElement<ITableCellProps> | React.ReactElement<ITableCellProps>[];
   style?: React.CSSProperties;
 }
 
 const TableRow: VibeComponent<ITableRowProps, HTMLDivElement> = forwardRef(
-  ({ highlighted, onClick, children, style, id, "data-testid": dataTestId }, ref) => {
+  ({ highlighted, children, style, id, "data-testid": dataTestId }, ref) => {
     const componentRef = useRef(null);
     const mergedRef = useMergeRefs({ refs: [componentRef, ref] });
-    useKeyEvent({
-      keys: SELECTION_KEYS,
-      ref: componentRef,
-      callback: onClick,
-      preventDefault: true
-    });
-
-    const onClickCallback = useCallback(
-      (event: React.MouseEvent) => {
-        event.preventDefault();
-        onClick?.(event);
-      },
-      [onClick]
-    );
 
     return (
       // eslint-disable-next-line jsx-a11y/click-events-have-key-events
@@ -46,8 +30,6 @@ const TableRow: VibeComponent<ITableRowProps, HTMLDivElement> = forwardRef(
         aria-selected={highlighted || false}
         className={styles.tableRow}
         style={style}
-        tabIndex={onClick ? 0 : -1}
-        onClick={onClickCallback}
       >
         {children}
       </div>
