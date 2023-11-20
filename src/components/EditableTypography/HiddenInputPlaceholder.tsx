1283349167467dd69920ddc263b39da34a1a8fe7
Tal Koren <talko@monday.com>
2023-11-20 09:40:44 +0200

    feat: forward ref

--- src/components/EditableTypography/HiddenInputPlaceholder.tsx
+++ src/components/EditableTypography/HiddenInputPlaceholder.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef, forwardRef } from "react";
 import cx from "classnames";
 import styles from "./HiddenInputPlaceholder.module.scss";
+import useMergeRefs from "../../hooks/useMergeRefs";
 
 interface HiddenInputPlaceholderProps {
   className?: string;
@@ -8,19 +9,22 @@ interface HiddenInputPlaceholderProps {
   onChange: (width: number) => void;
 }
 
-const HiddenInputPlaceholder: FC<HiddenInputPlaceholderProps> = ({ className, value, onChange }) => {
-  const valueRef = useRef(null);
+const HiddenInputPlaceholder = forwardRef<HTMLElement, HiddenInputPlaceholderProps>(
+  ({ className, value, onChange }, ref) => {
+    const valueRef = useRef(null);
+    const mergedRef = useMergeRefs({ refs: [ref, valueRef] });
 
-  useLayoutEffect(() => {
-    const { width } = valueRef.current.getBoundingClientRect();
-    onChange(width);
-  }, [onChange, value]);
+    useLayoutEffect(() => {
+      const { width } = valueRef.current.getBoundingClientRect();
+      onChange(width);
+    }, [onChange, value]);
 
-  return (
-    <span className={cx(styles.hiddenInputPlaceholder, className)} ref={valueRef} aria-hidden>
-      {value}
-    </span>
-  );
-};
+    return (
+      <div className={cx(styles.hiddenInputPlaceholder, className)} ref={mergedRef} aria-hidden>
+        {value}
+      </div>
+    );
+  }
+);
 
 export default HiddenInputPlaceholder;
