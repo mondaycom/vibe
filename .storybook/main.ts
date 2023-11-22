5f2513f36c22a0043fb1e5cb3ea6ef415ef51bdf
Sergey Roytman <sergeyro@monday.com>
2023-11-22 19:48:05 +0200

    main.ts - comment out webpack config + refactoring

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,69 +1,6 @@
-// const projectConfig = require("../webpack/storybook.config.js");
-//
-// const mergeConfigRules = (originalConfig, newConfigRules) => {
-//   return {
-//     ...originalConfig,
-//     module: { ...originalConfig.module, rules: [...originalConfig.module.rules, ...newConfigRules] },
-//     resolve: { ...originalConfig.resolve, alias: { ...originalConfig.resolve.alias, ...projectConfig.resolve.alias } }
-//   };
-// };
-//
-// const getProjectConfigRules = () => {
-//   return projectConfig.module.rules;
-// };
-//
-// const getDocsConfigRules = () => {
-//   return [];
-// };
-//
-// const removeCssLoader = config => {
-//   return { ...config, module: { ...config.module, rules: config.module.rules.filter(r => !r.test.test(".css")) } };
-// };
-//
-// const buildConfig = config => {
-//   config = removeCssLoader(config); //remove storybook default css loader configuration
-//   config = mergeConfigRules(config, getProjectConfigRules());
-//   config = mergeConfigRules(config, getDocsConfigRules());
-//   return config;
-// };
-//
-// const getAddons = () => {
-//   const addons = [
-//     "@storybook/addon-controls",
-//     "storybook-addon-themes",
-//     "@storybook/addon-a11y",
-//     "storybook-addon-performance/register",
-//     "@storybook/addon-docs",
-//     "@storybook/addon-toolbars",
-//     "@storybook/addon-actions"
-//   ];
-//
-//   if (process.env.NODE_ENV !== "production") {
-//     addons.push("@storybook/addon-interactions");
-//   }
-//
-//   return addons;
-// };
-//
-// module.exports = {
-//   stories: ["../src/**/*.stories.mdx"],
-//   webpackFinal: async config => {
-//     return buildConfig(config);
-//   },
-//   features: {
-//     interactionsDebugger: true
-//   },
-//   addons: getAddons(),
-//   framework: {
-//     name: "@storybook/react-webpack5",
-//     options: {}
-//   }
-// };
-
 import type { StorybookConfig } from "@storybook/react-webpack5";
-// import projectConfig from "../webpack/storybook.config.js";
 // const projectConfig = require("../webpack/storybook.config.js");
-
+//
 // const mergeConfigRules = (originalConfig: any, newConfigRules: any) => {
 //   return {
 //     ...originalConfig,
@@ -81,12 +18,9 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 // };
 //
 // const removeCssLoader = (config: any) => {
-//   console.log("### config.module.rules", config.module.rules);
 //   return {
 //     ...config,
-//     // TODO r.test is undefined
-//     // module: { ...config.module, rules: config.module.rules.filter((r: any) => !r?.test?.test(".css")) }
-//     module: { ...config.module, rules: config.module.rules.filter(r => !r?.test?.test(".css")) }
+//     module: { ...config.module, rules: config.module.rules.filter((r: any) => !r?.test?.test(".css")) }
 //   };
 // };
 //
@@ -95,6 +29,25 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 //   config = mergeConfigRules(config, getProjectConfigRules());
 //   config = mergeConfigRules(config, getDocsConfigRules());
 //   return config;
+// };
+
+// Old addons
+// const getAddons = () => {
+//   const addons = [
+//     "@storybook/addon-controls",
+//     "storybook-addon-themes",
+//     "@storybook/addon-a11y",
+//     "storybook-addon-performance/register",
+//     "@storybook/addon-docs",
+//     "@storybook/addon-toolbars",
+//     "@storybook/addon-actions"
+//   ];
+//
+//   if (process.env.NODE_ENV !== "production") {
+//     addons.push("@storybook/addon-interactions");
+//   }
+//
+//   return addons;
 // };
 
 const config: StorybookConfig = {
@@ -118,6 +71,7 @@ const config: StorybookConfig = {
   },
   features: {
     legacyMdx1: true // 👈 Enables MDX v1 support
+    // interactionsDebugger: true // from storybook 6 - not sure it'll work here, also probably requires "@storybook/addon-interactions
   }
 };
 export default config;
