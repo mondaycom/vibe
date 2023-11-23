1595b5387f52383f5b614f0500c730d7cded1477
Sergey Roytman <sergeyro@monday.com>
2023-11-23 17:30:18 +0200

    Refactor storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,31 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 import { storybookAddonStylingWebpackOptions } from "./addon-styling-webpack-options";
 
-// Old addons
-// const getAddons = () => {
-//   const addons = [
-//     "@storybook/addon-controls",
-//     "storybook-addon-themes",
-//     "@storybook/addon-a11y",
-//     "storybook-addon-performance/register",
-//     "@storybook/addon-docs",
-//     "@storybook/addon-toolbars",
-//     "@storybook/addon-actions"
-//   ];
-//
-//   if (process.env.NODE_ENV !== "production") {
-//     addons.push("@storybook/addon-interactions");
-//   }
-//
-//   return addons;
-// };
-
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx"],
-  addons: [
+const getAddons = () => {
+  const addons = [
     "@storybook/addon-links",
+    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/addon-themes",
     "@storybook/preset-scss",
     "@storybook/addon-styling-webpack",
@@ -33,13 +13,24 @@ const config: StorybookConfig = {
       name: "@storybook/addon-styling-webpack",
       options: storybookAddonStylingWebpackOptions
     }
-  ],
+  ];
+
+  if (process.env.NODE_ENV !== "production") {
+    addons.push("@storybook/addon-interactions");
+  }
+
+  return addons;
+};
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx"],
+  addons: getAddons(),
   framework: {
     name: "@storybook/react-webpack5",
     options: {}
   },
   docs: {
-    autodocs: true
+    autodocs: false
   },
   features: {
     legacyMdx1: true // 👈 Enables MDX v1 support
