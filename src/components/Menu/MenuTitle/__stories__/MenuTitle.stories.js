a91cbf14dd4185e140a196048d907f98470b6bcc
Sergey Roytman <sergeyro@monday.com>
2023-11-24 21:04:43 +0200

    MDX2 - <Menu>, <MenuDivider>, <MenuGridItem>, <MenuItem>, <MenuItemButton>, <MenuTitle>

--- src/components/Menu/MenuTitle/__stories__/MenuTitle.stories.js
+++ src/components/Menu/MenuTitle/__stories__/MenuTitle.stories.js
@@ -1,18 +1,43 @@
 import React from "react";
+import { createComponentTemplate } from "vibe-storybook-components";
+import { createStoryMetaSettingsDecorator } from "../../../../storybook/functions/createStoryMetaSettingsDecorator";
 import MenuTitle from "../MenuTitle";
 import Menu from "../../Menu/Menu";
 import MenuItem from "../../MenuItem/MenuItem";
-import { createComponentTemplate } from "vibe-storybook-components";
+
+const metaSettings = createStoryMetaSettingsDecorator({
+  component: MenuTitle,
+  enumPropNamesArray: ["captionPosition"]
+});
+
+export default {
+  title: "Navigation/Menu/MenuTitle",
+  component: MenuTitle,
+  argTypes: metaSettings.argTypes,
+  decorators: metaSettings.decorators
+};
 
 export const menuTitleTemplate = createComponentTemplate(MenuTitle);
 
-export const menuTitleCaptionPositionsTemplate = () => [
-  <Menu key="top">
-    <MenuTitle caption="Top caption" captionPosition={MenuTitle.positions.top} />
-    <MenuItem title="Menu item" />
-  </Menu>,
-  <Menu key="center">
-    <MenuTitle caption="Center caption" captionPosition={MenuTitle.positions.CENTER} />
-    <MenuItem title="Menu item" />
-  </Menu>
-];
+export const Overview = {
+  render: menuTitleTemplate.bind({}),
+  name: "Overview",
+
+  args: {
+    caption: "Menu title"
+  }
+};
+
+export const CaptionPlacements = {
+  render: () => [
+    <Menu key="top">
+      <MenuTitle caption="Top caption" captionPosition={MenuTitle.positions.top} />
+      <MenuItem title="Menu item" />
+    </Menu>,
+    <Menu key="center">
+      <MenuTitle caption="Center caption" captionPosition={MenuTitle.positions.CENTER} />
+      <MenuItem title="Menu item" />
+    </Menu>
+  ],
+  name: "Caption placements"
+};
