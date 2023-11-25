a3b0866e702de023975eb5ce8a1d14ff81975230
Sergey Roytman <sergeyro@monday.com>
2023-11-25 16:56:32 +0200

    MDX2 - <Button>, <ListItem>

---
+++ src/components/Button/__stories__/button.stories.helpers.js
@@ -0,0 +1,11 @@
+import { Link, Tip } from "vibe-storybook-components";
+
+export const TipIconButton = () => (
+  <Tip>
+    If you need to use icon as button with no text, check out
+    <Link href="/?path=/docs/buttons-iconbutton--overview" size={Link.sizes.SMALL}>
+      IconButton
+    </Link>
+    component
+  </Tip>
+);
