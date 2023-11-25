666f326d07ceecd811834c0f6acf87eff417b9ad
Sergey Roytman <sergeyro@monday.com>
2023-11-25 16:08:29 +0200

    main.ts - disable legacyMdx1

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -23,7 +23,7 @@ const getAddons = () => {
 };
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.js"],
   addons: getAddons(),
   framework: {
     name: "@storybook/react-webpack5",
@@ -33,8 +33,7 @@ const config: StorybookConfig = {
     autodocs: false
   },
   features: {
-    legacyMdx1: true // 👈 Enables MDX v1 support
-    // interactionsDebugger: true // from storybook 6 - not sure it'll work here, also probably requires "@storybook/addon-interactions
+    // interactionsDebugger: true // TODO from storybook 6 - not sure it'll work here, need to test
   }
 };
 export default config;
