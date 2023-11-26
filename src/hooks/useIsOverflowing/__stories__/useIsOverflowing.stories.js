7615ed7c009e06ba67ddc44f5ceb01c98a8b3aea
Sergey Roytman <sergeyro@monday.com>
2023-11-26 20:54:14 +0200

    useIsOverflowing.mdx - TODO

--- src/hooks/useIsOverflowing/__stories__/useIsOverflowing.stories.js
+++ src/hooks/useIsOverflowing/__stories__/useIsOverflowing.stories.js
@@ -1,41 +1,49 @@
-import { useRef, useState } from "@storybook/addons";
-import useIsOverflowing from "../useIsOverflowing";
-import { EditableInput, Flex, Tooltip } from "../../../components";
-import styles from "./useIsOverflowing.stories.module.scss";
+// import { useRef, useState } from "@storybook/addons";
+// import useIsOverflowing from "../useIsOverflowing";
+// import { EditableInput, Flex, Tooltip } from "../../../components";
+// import styles from "./useIsOverflowing.stories.module.scss";
+
+import { Tip } from "vibe-storybook-components";
 
 export default {
   title: "Hooks/useIsOverflowing"
 };
 
+// TODO storybook 7 migration - story is broken - hook usage is not supported
 export const Overview = {
   render: () => {
-    const ComponentWithOverflow = ({ text }) => {
-      const ref = useRef(null);
-
-      const isOverflowing = useIsOverflowing({
-        ref
-      });
-
-      return (
-        <Flex direction={Flex.directions.COLUMN} gap={Flex.gaps.MEDIUM} align={Flex.align.START}>
-          <div>Is overflowing:{isOverflowing.toString()}</div>
-          <Tooltip content={isOverflowing ? text : undefined}>
-            <div ref={ref} className={styles.textContainer}>
-              {text}
-            </div>
-          </Tooltip>
-        </Flex>
-      );
-    };
-
-    const [text, setText] = useState("");
-
     return (
-      <Flex direction={Flex.directions.COLUMN} gap={Flex.gaps.SMALL} align={Flex.align.START}>
-        <EditableInput onChange={setText} placeholder="Type text here" className={styles.input} />
-        <ComponentWithOverflow text={text} />
-      </Flex>
+      <Tip emoji={"🔨"} title={"Story is under maintenance"} type={Tip.types.WARNING}>
+        We will fix the story as soon as possible! Sorry for the inconvenience.
+      </Tip>
     );
+    // const ComponentWithOverflow = ({ text }) => {
+    //   const ref = useRef(null);
+    //
+    //   const isOverflowing = useIsOverflowing({
+    //     ref
+    //   });
+    //
+    //   return (
+    //     <Flex direction={Flex.directions.COLUMN} gap={Flex.gaps.MEDIUM} align={Flex.align.START}>
+    //       <div>Is overflowing: {isOverflowing.toString()}</div>
+    //       <Tooltip content={isOverflowing ? text : undefined}>
+    //         <div ref={ref} className={styles.textContainer}>
+    //           {text}
+    //         </div>
+    //       </Tooltip>
+    //     </Flex>
+    //   );
+    // };
+    //
+    // const [text, setText] = useState("");
+    //
+    // return (
+    //   <Flex direction={Flex.directions.COLUMN} gap={Flex.gaps.SMALL} align={Flex.align.START}>
+    //     <EditableInput onChange={setText} placeholder="Type text here" className={styles.input} />
+    //     <ComponentWithOverflow text={text} />
+    //   </Flex>
+    // );
   },
 
   name: "Overview"
