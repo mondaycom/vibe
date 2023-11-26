83ea1d4ca817a6d048ab03f40172216a3b7c65a4
Sergey Roytman <sergeyro@monday.com>
2023-11-26 10:43:28 +0200

    fix: <Frame> make className, noBorder, noGutter props not required (#129)

--- packages/vibe-storybook-components/src/components/frame/frame.tsx
+++ packages/vibe-storybook-components/src/components/frame/frame.tsx
@@ -1,16 +1,16 @@
 import { FC } from 'react';
 import cx from 'classnames';
-import styles from './frame.module.scss';
 import { ElementContent } from '../../types';
+import styles from './frame.module.scss';
 
 type FrameProps = {
   children: ElementContent;
-  className: string;
-  noGutter: boolean;
-  noBorder: boolean;
+  className?: string;
+  noGutter?: boolean;
+  noBorder?: boolean;
 };
 
-const Frame: FC<FrameProps> = ({ children, className, noGutter, noBorder }) => (
+const Frame: FC<FrameProps> = ({ children, className, noGutter = false, noBorder = false }) => (
   <div
     className={cx(styles.frame, className, {
       [styles.noGutter]: noGutter,
