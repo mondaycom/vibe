5ea59666140e1b43ab8351e52f770b84ab8b007f
Sergey Roytman <sergeyro@monday.com>
2023-11-30 00:19:16 +0200

    Revert "useSwitch.stories.js - temp fix for the hook issue"
    This reverts commit 3481e4917ac5486709cc4c3efc16bdeca48c0ab4.

--- src/hooks/useSwitch/__stories__/useSwitch.stories.js
+++ src/hooks/useSwitch/__stories__/useSwitch.stories.js
@@ -1,32 +1,6 @@
-import { Toggle, Flex } from "../../../components";
-import { useCallback, useEffect, useState } from "@storybook/addons";
-
-// TODO storybook 7 migration: temp solution for the hooks issue - replace with the import in the future
 // import useSwitch from "../index";
-function useSwitch({ isChecked, defaultChecked, onChange, isDisabled } = {}) {
-  // if isChecked is empty, set defaultChecked value (default false value)
-  const overrideCheckedInitial = isChecked ?? !!defaultChecked;
-  const [overrideChecked, setOverrideChecked] = useState(overrideCheckedInitial);
-
-  const overrideOnChange = useCallback(() => {
-    if (isDisabled) {
-      return;
-    }
-    const newChecked = !overrideChecked;
-    if (isChecked === undefined) {
-      setOverrideChecked(newChecked);
-    }
-    onChange && onChange(newChecked);
-  }, [isChecked, isDisabled, onChange, overrideChecked]);
-
-  useEffect(() => {
-    if (isChecked !== undefined) {
-      setOverrideChecked(isChecked);
-    }
-  }, [isChecked]);
-
-  return { isChecked: overrideChecked, onChange: overrideOnChange };
-}
+// import { Toggle, Flex } from "../../../components";
+import { Tip } from "vibe-storybook-components";
 
 export default {
   title: "Hooks/useSwitch"
@@ -35,14 +9,19 @@ export default {
 // TODO Storybook 7 migration: story is under maintenance - hooks issue
 export const Overview = {
   render: () => {
-    const { isChecked, onChange } = useSwitch();
-
     return (
-      <Flex direction={Flex.directions.COLUMN} align={Flex.align.START} gap={Flex.gaps.MEDIUM}>
-        <Toggle onChange={onChange} isSelected={isChecked} />
-        <code>isChecked: {isChecked.toString()}</code>
-      </Flex>
+      <Tip emoji={"🔨"} title={"Story is under maintenance"} type={Tip.types.WARNING}>
+        We will fix the story as soon as possible! Sorry for the inconvenience.
+      </Tip>
     );
+    // const { isChecked, onChange } = useSwitch();
+    //
+    // return (
+    //   <Flex direction={Flex.directions.COLUMN} align={Flex.align.START} gap={Flex.gaps.MEDIUM}>
+    //     <Toggle onChange={onChange} isSelected={isChecked} />
+    //     <code>isChecked: {isChecked.toString()}</code>
+    //   </Flex>
+    // );
   },
 
   name: "Overview"
@@ -50,16 +29,21 @@ export const Overview = {
 
 export const Disabled = {
   render: () => {
-    const { isChecked, onChange } = useSwitch({
-      isDisabled: true
-    });
-
     return (
-      <Flex direction={Flex.directions.COLUMN} align={Flex.align.START} gap={Flex.gaps.MEDIUM}>
-        <Toggle onChange={onChange} isSelected={isChecked} />
-        <code>isChecked: {isChecked.toString()}</code>
-      </Flex>
+      <Tip emoji={"🔨"} title={"Story is under maintenance"} type={Tip.types.WARNING}>
+        We will fix the story as soon as possible! Sorry for the inconvenience.
+      </Tip>
     );
+    // const { isChecked, onChange } = useSwitch({
+    //   isDisabled: true
+    // });
+    //
+    // return (
+    //   <Flex direction={Flex.directions.COLUMN} align={Flex.align.START} gap={Flex.gaps.MEDIUM}>
+    //     <Toggle onChange={onChange} isSelected={isChecked} />
+    //     <code>isChecked: {isChecked.toString()}</code>
+    //   </Flex>
+    // );
   },
 
   name: "Disabled"
@@ -67,19 +51,24 @@ export const Disabled = {
 
 export const Default = {
   render: () => {
-    const defaultChecked = true;
-
-    const { isChecked, onChange } = useSwitch({
-      defaultChecked
-    });
-
     return (
-      <Flex direction={Flex.directions.COLUMN} align={Flex.align.START} gap={Flex.gaps.MEDIUM}>
-        <Toggle onChange={onChange} isSelected={isChecked} />
-        <code>isChecked {isChecked.toString()}</code>
-        <code>defaultChecked: {defaultChecked.toString()}</code>
-      </Flex>
+      <Tip emoji={"🔨"} title={"Story is under maintenance"} type={Tip.types.WARNING}>
+        We will fix the story as soon as possible! Sorry for the inconvenience.
+      </Tip>
     );
+    // const defaultChecked = true;
+    //
+    // const { isChecked, onChange } = useSwitch({
+    //   defaultChecked
+    // });
+    //
+    // return (
+    //   <Flex direction={Flex.directions.COLUMN} align={Flex.align.START} gap={Flex.gaps.MEDIUM}>
+    //     <Toggle onChange={onChange} isSelected={isChecked} />
+    //     <code>isChecked {isChecked.toString()}</code>
+    //     <code>defaultChecked: {defaultChecked.toString()}</code>
+    //   </Flex>
+    // );
   },
 
   name: "Default"
