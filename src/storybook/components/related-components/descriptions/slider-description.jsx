ab05f73fa69877813b7a031b7e895fc8d052b7e7
Dhoni77 <aldrinmarz7@gmail.com>
2023-12-14 11:41:10 +0530

    feat: add Slider description component

---
+++ src/storybook/components/related-components/descriptions/slider-description.jsx
@@ -0,0 +1,15 @@
+import { useMemo } from "react";
+import { RelatedComponent } from "vibe-storybook-components";
+import Slider from "../../../../components/Slider/Slider";
+export const SliderDescription = () => {
+  const component = useMemo(() => <Slider />, []);
+
+  return (
+    <RelatedComponent
+      component={component}
+      title="Slider"
+      href="/?path=/docs/inputs-slider--docs"
+      description="Displays a slider bar with range selection"
+    />
+  );
+};
