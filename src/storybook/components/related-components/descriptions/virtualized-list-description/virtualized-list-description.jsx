894867587e8a95f3e4ed4a231456a57846fa81d9
Sergey Roytman <sergeyro@monday.com>
2023-12-17 14:36:55 +0200

    docs: virtualized-list-description - fix import (#1825)

--- src/storybook/components/related-components/descriptions/virtualized-list-description/virtualized-list-description.jsx
+++ src/storybook/components/related-components/descriptions/virtualized-list-description/virtualized-list-description.jsx
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
 import VirtualizedList from "../../../../../components/VirtualizedList/VirtualizedList";
-import { generateItems } from "../../../../../components/VirtualizedGrid/__stories__/VirtualizedGrid.stories.helpers";
+import { generateItems } from "../../../../../components/VirtualizedList/__stories__/virtualizedList.stories.helpers";
 import styles from "./virtualizedList.module.scss";
 
 export const VirtualizedListDescription = () => {
