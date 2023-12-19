722dfbfdcb9e569b6aeb856378f1ff984153856d
Yossi Saadi <yossisa@monday.com>
2023-12-19 20:33:02 +0200

    docs: add playground addon for vibe's storybook

---
+++ src/storybook/stand-alone-documentaion/playground/Playground.stories.ts
@@ -0,0 +1,8 @@
+import { withPlaygroundRenderer } from "storybook-addon-playground";
+
+export default {
+  title: "Playground",
+  decorators: [withPlaygroundRenderer]
+};
+
+export const Playground = {};
