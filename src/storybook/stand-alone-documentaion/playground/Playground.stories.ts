6b9809ea08ed0e9e36f4dbfac9194e5e0104d7c1
Shlomi Toussia-Cohen <shlomito@monday.com>
2023-12-21 00:05:21 +0200

    chore: minor

--- src/storybook/stand-alone-documentaion/playground/Playground.stories.ts
+++ src/storybook/stand-alone-documentaion/playground/Playground.stories.ts
@@ -8,14 +8,20 @@ export default {
 export const Playground = {
   args: {
     code: {
-      jsx: `
-<div style={{display: 'flex', flexDirection: 'column', gap: '12px'}}>
+      jsx: `<div style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
   <div>
     <VibeNext.Heading>Online Playground</VibeNext.Heading>
-    <VibeNext.Heading type="h3" color="secondary">Prototype with actual components.</VibeNext.Heading>
+    <VibeNext.Heading type="h3" color="secondary">
+      Prototype with actual components.
+    </VibeNext.Heading>
   </div>
-  <div>  
-    <Text>Can't see the code editor?<br/>Click on the <VibeIcons.Settings/> button on the left panel and select the <b>"Change Addons Orientation"</b> </Text>
+  <div>
+    <Text>
+      Can't see the code editor?
+      <br />
+      Click on the <VibeIcons.Settings /> button on the left panel and select
+      the <b>"Change Addons Orientation"</b>{" "}
+    </Text>
   </div>
   <div>
     <Button onClick={() => alert("isn't that nice?")}>Click me</Button>
