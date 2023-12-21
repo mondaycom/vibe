af7412913cb09cb92ea6f5f0cbc4ebd5f38d8db3
Aldrin <53973174+Dhoni77@users.noreply.github.com>
2023-12-21 18:02:47 +0530

    docs(box): add box description on catalog page (#1840)
    Co-authored-by: Sergey Roytman <sergeyro@monday.com>

---
+++ src/storybook/components/related-components/descriptions/box-description.jsx
@@ -0,0 +1,29 @@
+import { useMemo } from "react";
+import { RelatedComponent } from "vibe-storybook-components";
+import Box from "../../../../components/Box/Box";
+
+export const BoxDescription = () => {
+  const component = useMemo(
+    () => (
+      <div
+        style={{
+          width: "80%",
+          textAlign: "center"
+        }}
+      >
+        <Box border={Box.borders.DEFAULT} rounded={Box.roundeds.MEDIUM}>
+          Box
+        </Box>
+      </div>
+    ),
+    []
+  );
+  return (
+    <RelatedComponent
+      component={component}
+      title="Box"
+      href="/?path=/docs/layout-box--docs"
+      description="Box component serves as a wrapper component."
+    />
+  );
+};
