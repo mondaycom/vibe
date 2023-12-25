8b7c4640a9492d69fc959cf44ff3e412b8cd0d49
Praveen Shinde <shindepraveen002@gmail.com>
2023-12-25 19:20:03 +0530

    feat: made the steps button clickable

--- src/storybook/components/related-components/descriptions/steps-description.jsx
+++ src/storybook/components/related-components/descriptions/steps-description.jsx
@@ -1,12 +1,29 @@
-import { useMemo } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
 import Steps from "../../../../components/Steps/Steps";
 
 export const StepsDescription = () => {
+
+  const [activeStepIndex, setActiveStepIndex] = useState(2);
+
+  const stepPrev = useCallback(() => {
+    setActiveStepIndex(prevState => prevState - 1);
+  }, []);
+
+  const stepNext = useCallback(() => {
+    setActiveStepIndex(prevState => prevState + 1);
+  }, []);
+
+  const onChangeActiveStep = useCallback((_e, stepIndex) => {
+    setActiveStepIndex(stepIndex);
+  }, []);
+
   const component = useMemo(() => {
+
     const style = {
       marginLeft: "-10px"
     };
+
     const steps = [
       <div key="step-1" />,
       <div key="step-2" />,
@@ -14,12 +31,25 @@ export const StepsDescription = () => {
       <div key="step-4" />,
       <div key="step-5" />
     ];
+
     return (
       <div style={style}>
-        <Steps steps={steps} activeStepIndex={2} />
+        <Steps
+          steps={steps}
+          activeStepIndex={activeStepIndex}
+          onChangeActiveStep={onChangeActiveStep}
+          backButtonProps={{
+            onClick: stepPrev
+          }}
+          nextButtonProps={{
+            onClick: stepNext
+          }}
+        />
       </div>
     );
-  }, []);
+
+  }, [activeStepIndex, stepPrev, stepNext, onChangeActiveStep]);
+
   return (
     <RelatedComponent
       component={component}
@@ -28,4 +58,5 @@ export const StepsDescription = () => {
       description="Steps display progress through a sequence of logical and numbered steps. They may also be used for navigation."
     />
   );
+
 };
