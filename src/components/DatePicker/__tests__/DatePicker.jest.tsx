1c05be90ca6c9b77e7eb804a0ece53fa22e35be1
Tal Koren <talko@monday.com>
2023-12-31 18:51:50 +0200

    test(DatePicker): make tests more deterministic

--- src/components/DatePicker/__tests__/DatePicker.jest.tsx
+++ src/components/DatePicker/__tests__/DatePicker.jest.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { act, fireEvent, render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import DatePicker from "../DatePicker";
 import moment, { Moment } from "moment";
 import { RangeDate } from "../types";
@@ -18,14 +18,17 @@ export function getNextWeekFirstDayElement(pivotElement: HTMLElement) {
 }
 
 describe("DatePicker", () => {
+  let dateNowSpy: jest.SpyInstance;
+
   beforeEach(() => {
-    // @ts-ignore
-    jest.useFakeTimers("modern");
+    dateNowSpy = jest.spyOn(Date, "now").mockImplementation(() => new Date("2023-05-01").getTime());
   });
+
   afterEach(() => {
-    jest.useRealTimers();
+    dateNowSpy.mockRestore();
   });
-  it.skip("Should call onPickDate date clicked value", () => {
+
+  it("should call onPickDate date clicked value", () => {
     const onSaveMock = jest.fn();
     const { container } = render(
       <DatePicker
@@ -36,13 +39,11 @@ describe("DatePicker", () => {
     );
 
     const element = container.querySelector(".CalendarDay");
-    act(() => {
-      fireEvent.click(element);
-    });
-    expect(onSaveMock.mock.calls.length).toBe(1);
+    fireEvent.click(element);
+    expect(onSaveMock).toHaveBeenCalledTimes(1);
   });
 
-  it.skip("Should call onPickDate with today date", () => {
+  it("should call onPickDate with today's date", () => {
     let selectedDate: Moment;
     const { container } = render(
       <DatePicker
@@ -53,18 +54,15 @@ describe("DatePicker", () => {
     );
     const today = moment();
     const element = container.querySelector(".CalendarDay__today");
-    act(() => {
-      fireEvent.click(element);
-    });
-    expect(selectedDate).toBeDefined();
-    if (selectedDate) {
-      expect(today.format(DATE_FORMAT)).toBe(selectedDate.format(DATE_FORMAT));
-    }
+
+    fireEvent.click(element);
+
+    expect(today.format(DATE_FORMAT)).toBe(selectedDate.format(DATE_FORMAT));
   });
 
-  it("Should call onPickDate with range date", () => {
+  it("should call onPickDate with range date", () => {
     const selectedRange: RangeDate = { startDate: null, endDate: null };
-    const { container, getByLabelText } = render(
+    const { container } = render(
       <DatePicker
         range
         onPickDate={(range: RangeDate) => {
@@ -73,51 +71,32 @@ describe("DatePicker", () => {
         }}
       />
     );
+
     const today = moment();
     const tomorrow = moment().add(1, "days");
-
     const todayElement = container.querySelector(".CalendarDay__today") as HTMLElement;
-    let tomorrowElement = todayElement.nextElementSibling;
+    const tomorrowElement = todayElement.nextElementSibling;
 
-    act(() => {
-      fireEvent.click(todayElement);
-      jest.advanceTimersByTime(500);
-      // First day next month if today is last day of the month
-      if (
-        today.startOf("day").isSame(moment().endOf("month").startOf("day")) &&
-        tomorrowElement &&
-        !tomorrowElement.className
-      ) {
-        const nextMonthNavigationButton = getByLabelText("Move forward to switch to the next month.");
-        fireEvent.click(nextMonthNavigationButton);
-        jest.advanceTimersByTime(1000);
-        fireEvent.click(nextMonthNavigationButton);
-        jest.advanceTimersByTime(1000);
-        tomorrowElement = getFirstDayOfMonthElement(container);
-      }
+    fireEvent.click(todayElement);
+    fireEvent.click(tomorrowElement);
 
-      // First day next week if today is last day of the week
-      if (todayElement.className.includes("CalendarDay__lastDayOfWeek_") && !tomorrowElement) {
-        tomorrowElement = getNextWeekFirstDayElement(todayElement);
-      }
-      fireEvent.click(tomorrowElement);
-    });
     expect(today.format(DATE_FORMAT)).toBe(selectedRange.startDate.format(DATE_FORMAT));
     expect(tomorrow.format(DATE_FORMAT)).toBe(selectedRange.endDate.format(DATE_FORMAT));
   });
 
-  it.skip("Should render 2 month", () => {
+  it("should render 2 months", () => {
     const { container } = render(<DatePicker range numberOfMonths={2} />);
     const monthsElements = container.getElementsByClassName("CalendarMonth");
+
     expect(monthsElements.length).toBe(4);
   });
 
-  it.skip("Should open an year selection dropdown", () => {
+  it("Should open an year selection dropdown", () => {
     const { container } = render(<DatePicker data-testid="date-picker" />);
     const toggleButtonElement = container.querySelector("button[data-testid='date-picker-year-toggle']");
-    act(() => {
-      fireEvent.click(toggleButtonElement);
-    });
+
+    fireEvent.click(toggleButtonElement);
+
     const yearSelectionElement = container.querySelector("div[data-testid='date-picker-year-picker']");
     expect(yearSelectionElement).not.toBe(null);
   });
