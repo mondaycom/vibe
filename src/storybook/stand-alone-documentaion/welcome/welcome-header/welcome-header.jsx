c02ad2fad64e69c2c31fcea38df8c6614245a126
Craig Aucutt <craig@caidentity.com>
2024-02-07 12:13:49 -0800

    IPLT-5319 - Init storybook styling updates

--- src/storybook/stand-alone-documentaion/welcome/welcome-header/welcome-header.jsx
+++ src/storybook/stand-alone-documentaion/welcome/welcome-header/welcome-header.jsx
@@ -1,12 +1,13 @@
 import { ComponentNameDecorator } from "../../../components";
 import "./welcome-header.scss";
 
-const BASE_CLASS = "monday-storybook-welcome-header";
+const BASE_CLASS = "origami-storybook-welcome-header";
 export const WelcomeHeader = () => (
   <ComponentNameDecorator className={BASE_CLASS}>
-    <span className={`${BASE_CLASS}_text`}>
-      Vibe Design System
-      <br /> by monday.com
+    <span className={`${BASE_CLASS}_head`}>Origami Design System</span>
+    <span className={`${BASE_CLASS}_subhead`}>
+      Here you can find our design guidelines, component documentation, and resources for buildings apps with Ikigai
+      Orgami Design System.
     </span>
   </ComponentNameDecorator>
 );
