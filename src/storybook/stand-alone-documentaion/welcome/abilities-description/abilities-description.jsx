0ece84fb824bd1dca103de67a2694a8199402f9c
Craig Aucutt <craig@caidentity.com>
2024-02-08 12:27:13 -0800

    iplt-5319 - intro cover updates

--- src/storybook/stand-alone-documentaion/welcome/abilities-description/abilities-description.jsx
+++ src/storybook/stand-alone-documentaion/welcome/abilities-description/abilities-description.jsx
@@ -3,13 +3,13 @@ import { components, foundations, gettingStarted } from "../assets";
 import "./abilities-description.scss";
 
 export const AbilitiesDescription = () => (
-  <div className="monday-storybook-abilities-description">
+  <div className="origami-storybook-abilities-description">
     <AbilityDescription
       title="Getting started"
       imageSrc={gettingStarted}
       linkHref="https://github.com/mondaycom/monday-ui-react-core#readme"
     >
-      Instructions and welcome to the monday.com OS design system
+      Instructions on how to use the Ikigai design system
     </AbilityDescription>
     <AbilityDescription title="Foundations" imageSrc={foundations} linkHref="/?path=/docs/foundations-colors--docs">
       All information about colors, typography, spacing, and icons
