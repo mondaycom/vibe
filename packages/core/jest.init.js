34969a0d3b3e6176e4b5298e816454cafb5a0af5
Sahar Brodbeker <saharonb@gmail.com>
2024-02-12 15:29:22 +0200

    feat(core): prettier

--- packages/core/jest.init.js
+++ packages/core/jest.init.js
@@ -25,7 +25,7 @@ if (TESTING_STORYBOOK) {
     return { CSSTransition: FakeCSSTransition, Transition: FakeTransition, SwitchTransition: FakeTransition };
   });
   jest.mock("consolidated-events", () => {
-    return { addEventListener: jest.fn()};
+    return { addEventListener: jest.fn() };
   });
 }
 
