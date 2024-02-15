0e26c4a5539eb1751713bb9506553823bf23a46c
Yossi Saadi <yossisa@monday.com>
2024-02-15 18:34:28 +0200

    refactor(Button): remove comment

--- packages/core/src/components/Button/Button.tsx
+++ packages/core/src/components/Button/Button.tsx
@@ -196,7 +196,7 @@ const Button: VibeComponent<ButtonProps, unknown> & {
       },
       [onMouseDown, disabled, loading, success]
     );
-    // comment 7
+
     const classNames = useMemo(() => {
       const calculatedColor = success ? ButtonColor.POSITIVE : color;
       return cx(
