93fbe593ea2d463bbb02ecaea32860ca575a49f9
Tal Koren <talko@monday.com>
2024-03-05 18:21:18 +0200

    refactor: make LayerProvider leaner

--- packages/core/src/components/LayerProvider/LayerProvider.tsx
+++ packages/core/src/components/LayerProvider/LayerProvider.tsx
@@ -1,34 +1,13 @@
-import React, { FC, useRef, useEffect, Children } from "react";
+import React, { FC } from "react";
 import LayerContext from "./LayerContext";
 
 interface LayerProviderType {
   children: JSX.Element | JSX.Element[];
-  layerRef?: React.RefObject<HTMLElement> | null;
+  layerRef: React.RefObject<HTMLElement> | null;
 }
 
 const LayerProvider: FC<LayerProviderType> = ({ children, layerRef }) => {
-  const childrenRef = useRef();
-
-  useEffect(() => {
-    if (!childrenRef.current && !layerRef) {
-      console.warn(
-        "LayerProvider: The child component does not forward its ref. Use forwardRef, or alternatively use the `layerRef` prop to pass a ref to the child component."
-      );
-    }
-  }, [layerRef]);
-
-  if (layerRef?.current) {
-    return <LayerContext.Provider value={{ layerRef }}>{children}</LayerContext.Provider>;
-  }
-
-  const child = children && Children.only(children);
-
-  if (!child) {
-    console.warn("LayerProvider: The child component must be a single element");
-  }
-
-  const childrenWithRef = React.cloneElement(child, { ref: childrenRef });
-  return <LayerContext.Provider value={{ layerRef: childrenRef }}>{childrenWithRef}</LayerContext.Provider>;
+  return <LayerContext.Provider value={{ layerRef }}>{children}</LayerContext.Provider>;
 };
 
 export default LayerProvider;
