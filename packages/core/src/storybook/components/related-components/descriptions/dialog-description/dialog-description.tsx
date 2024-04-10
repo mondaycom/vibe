c058d194f71e141ec094d7406badb0c249ca7787
Tal Koren <talko@monday.com>
2024-04-10 11:14:18 +0100

    docs: fix related components link

--- packages/core/src/storybook/components/related-components/descriptions/dialog-description/dialog-description.tsx
+++ packages/core/src/storybook/components/related-components/descriptions/dialog-description/dialog-description.tsx
@@ -43,7 +43,7 @@ export const DialogDescription = () => {
     <RelatedComponent
       component={component}
       title="Dialog"
-      href="/?path=/docs/components-dialog-dialog--docs"
+      href="/?path=/docs/popover-dialog--docs"
       description="The dialog component's purpose is to position a popup component nearby another element, such as any kind of a button."
     />
   );
