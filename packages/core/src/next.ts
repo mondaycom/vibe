29d2917296e03f7f76928f4ec54351e8f002f171
Yossi Saadi <yossisa@monday.com>
2024-04-14 14:56:04 +0300

    chore(Search): export old Search from LegacySearch path, export new Search from Search /next path

--- packages/core/src/next.ts
+++ packages/core/src/next.ts
@@ -2,5 +2,6 @@ export * from "./components/Heading/Heading";
 import Heading from "./components/Heading/Heading";
 export * from "./components/EditableHeading/EditableHeading";
 import EditableHeading from "./components/EditableHeading/EditableHeading";
+import Search from "./components/Search/Search";
 
-export { Heading, EditableHeading };
+export { Heading, EditableHeading, Search };
