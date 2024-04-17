04186f36140de64af765241876f9e776c68f66fd
Yossi Saadi <yossisa@monday.com>
2024-04-17 14:31:00 +0300

    docs(Search): jsdoc for props

--- packages/core/src/components/Search/Search.types.ts
+++ packages/core/src/components/Search/Search.types.ts
@@ -7,24 +7,81 @@ import MenuButton from "../MenuButton/MenuButton";
 type AdditionalActionRender = ReactElement<typeof IconButton | typeof MenuButton>;
 
 export interface SearchProps extends VibeComponentProps {
+  /**
+   * Name of the icon used for the search button.
+   */
   searchIconName?: SubIcon;
+  /**
+   * Name of the icon used for the clear button.
+   */
   clearIconName?: SubIcon;
+  /**
+   * Label for the clear icon button, for accessibility purposes.
+   */
   clearIconLabel?: string;
+  /**
+   * Render additional action within the right section of search component.
+   */
   additionalActionRender?: AdditionalActionRender;
+  /**
+   * The value of the search input.
+   */
   value?: HTMLInputElement["value"];
+  /**
+   * Placeholder text for the search input when it's empty.
+   */
   placeholder?: HTMLInputElement["placeholder"];
+  /**
+   * Size of the input element. Will influence also padding and font size.
+   */
   size?: InputSize;
+  /**
+   * If true, the search input is disabled and cannot be interacted with.
+   */
   disabled?: HTMLInputElement["disabled"];
+  /**
+   * Shows a loading indicator in the search component, typically used while fetching results.
+   */
   loading?: boolean;
+  /**
+   * If true, the search input is automatically focused when the component mounts.
+   */
   autoFocus?: HTMLInputElement["autofocus"];
+  /**
+   * Configures the browser's autocomplete feature for the search input.
+   */
   autoComplete?: HTMLInputElement["autocomplete"];
+  /**
+   * Aria-label for the search input, important for accessibility.
+   */
   inputAriaLabel?: AriaAttributes["aria-label"];
+  /**
+   * Rate at which search input changes are debounced.
+   */
   debounceRate?: number;
+  /**
+   * The id of the container element where search results are to be displayed.
+   */
   searchResultsContainerId?: string;
+  /**
+   * ARIA property that identifies the currently active item within the search results.
+   */
   activeDescendant?: AriaAttributes["aria-activedescendant"];
+  /**
+   * Callback function that is called whenever the value of the search input changes.
+   */
   onChange?: (value: string) => void;
+  /**
+   * Callback function that is called when the search input loses focus.
+   */
   onBlur?: (event: FocusEvent) => void;
+  /**
+   * Callback function that is called when the search input gains focus.
+   */
   onFocus?: (event: FocusEvent) => void;
+  /**
+   * Additional className to apply to the search component's wrapper element.
+   */
   wrapperClassName?: string;
 }
 
