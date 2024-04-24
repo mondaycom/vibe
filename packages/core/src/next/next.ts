5c9518d3bbfaaf49c414a8ebb4438b675fae0f2e
Tal Koren <talko@monday.com>
2024-04-24 16:03:52 +0300

    fix: export Search from next

--- packages/core/src/next/next.ts
+++ packages/core/src/next/next.ts
@@ -1,2 +1,3 @@
 export { default as Heading } from "../components/Heading/Heading";
 export { default as EditableHeading } from "../components/EditableHeading/EditableHeading";
+export { default as Search } from "../components/Search/Search";
