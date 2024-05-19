d0d2b3ede4b4d2c9592b8d5a99d7a15afb75a99c
Yossi Saadi <yossisa@monday.com>
2024-05-19 11:02:49 +0300

    fix(Dropdown): remove duplicates

--- packages/core/src/components/Dropdown/DropdownConstants.ts
+++ packages/core/src/components/Dropdown/DropdownConstants.ts
@@ -2,10 +2,6 @@ export const defaultCustomStyles = (baseStyles: string) => baseStyles;
 
 export const ADD_AUTO_HEIGHT_COMPONENTS = ["container", "control", "valueContainer"];
 
-export const DROPDOWN_MENU_ARIA_LABEL = "Dropdown menu";
-
-export const DROPDOWN_MENU_ID = "dropdown-menu-list-id";
-
 export const DROPDOWN_ID = "dropdown-menu-id";
 export const DROPDOWN_MENU_ID = "dropdown-menu-list-id";
 export const DROPDOWN_MENU_ARIA_LABEL = "Dropdown menu";
