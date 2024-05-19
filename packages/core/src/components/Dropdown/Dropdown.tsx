a7a51ada6400b045c831978e3d5d419c35e0a8b1
Yossi Saadi <yossisa@monday.com>
2024-05-19 11:49:11 +0300

    fix(Dropdown): migrate to TS continue (#2109)
    Co-authored-by: mila2999 <mila.savonik@gmail.com>
    Co-authored-by: Tal Koren <talko@monday.com>

--- packages/core/src/components/Dropdown/Dropdown.tsx
+++ packages/core/src/components/Dropdown/Dropdown.tsx
@@ -1,17 +1,9 @@
 /* eslint-disable react/require-default-props,react/forbid-prop-types */
 import { ComponentDefaultTestId, getTestId } from "../../tests/test-ids-utils";
 import cx from "classnames";
-import { BaseSizes, SIZES, SIZES_VALUES } from "../../constants/sizes";
+import { SIZES, SIZES_VALUES } from "../../constants";
 import React, { forwardRef, useCallback, useMemo, useRef, useState } from "react";
-import Select, {
-  InputProps,
-  MenuProps,
-  OptionProps,
-  SingleValueProps,
-  components,
-  createFilter,
-  OptionTypeBase
-} from "react-select";
+import Select, { InputProps, components, createFilter, ActionMeta } from "react-select";
 import AsyncSelect from "react-select/async";
 import { noop as NOOP } from "lodash-es";
 import { WindowedMenuList } from "react-windowed-select";
@@ -26,362 +18,109 @@ import {
   defaultCustomStyles,
   DROPDOWN_CHIP_COLORS,
   DROPDOWN_ID,
-  DROPDOWN_MENU_ID,
   DROPDOWN_MENU_ARIA_LABEL,
+  DROPDOWN_MENU_ID,
   DROPDOWN_MENU_PLACEMENT,
-  DROPDOWN_MENU_POSITION,
-  DropdownOption
+  DROPDOWN_MENU_POSITION
 } from "./DropdownConstants";
 import generateBaseStyles, { customTheme } from "./Dropdown.styles";
 import Control from "./components/Control/Control";
 import menuStyles from "./components/menu/menu.module.scss";
 import styles from "./Dropdown.module.scss";
-import { VibeComponent, VibeComponentProps } from "../../types";
+import {
+  DropdownOption,
+  DropdownState,
+  CustomMenuProps,
+  CustomOptionProps,
+  CustomSingleValueProps,
+  DropdownComponentProps
+} from "./Dropdown.types";
+import { VibeComponent, withStaticProps } from "../../types";
 
-interface CustomSingleValueProps extends SingleValueProps<DropdownOption> {
-  Renderer: React.ComponentType;
-  readOnly: boolean;
-  selectedOption: DropdownOption;
-}
-
-interface CustomMenuProps extends MenuProps<OptionTypeBase, boolean> {
-  dropdownMenuWrapperClassName: string;
-}
-
-interface CustomOptionProps extends OptionProps<OptionTypeBase, boolean> {
-  optionWrapperClassName?: string;
-}
-
-type SelectEvent = {
-  action: "select-option" | "clear";
-  option?: DropdownOption;
-};
-
-type DropdownState = {
-  isDisabled: boolean;
-  selectProps: { withReadOnlyStyle: boolean; readOnly: boolean };
-};
-
-export interface DropdownComponentProps extends VibeComponentProps {
-  /**
-   * Custom style
-   */
-  className?: string;
-  /** ClassName to be added to dropdown option wrapper (dropdown-wrapper__option--reset) */
-  optionWrapperClassName?: string;
-  /** ClassName to be added to dropdown single value wrapper (dropdown-wrapper__single-value--reset) */
-  singleValueWrapperClassName?: string;
-  /** ClassName to be added to dropdown menu wrapper (dropdown-menu-wrapper) */
-  dropdownMenuWrapperClassName?: string;
-  /**
-   * Placeholder to show when no value was selected
-   */
-  placeholder?: string;
-  /**
-   * If set to true, dropdown will be disabled
-   */
-  disabled?: boolean;
-  /**
-   * If set to true, dropdown won't be editable
-   */
-  readOnly?: boolean;
-  /**
-   * Called when menu is opened
-   */
-  onMenuOpen?: (event: React.MouseEvent) => void;
-  /**
-   * Called when menu is closed
-   */
-  onMenuClose?: (event: React.MouseEvent) => void;
-  /**
-   * Called when key is pressed in the dropdown
-   */
-  onKeyDown?: (event: React.MouseEvent) => void;
-  /**
-   * Called when focused
-   */
-  onFocus?: (event: React.MouseEvent) => void;
-  /**
-   * Called when blurred
-   */
-  onBlur?: (event: React.MouseEvent) => void;
-  /**
-   * Called when selected value has changed
-   */
-  onChange?: (
-    option: DropdownOption | DropdownOption[],
-    event: SelectEvent | React.MouseEvent | React.KeyboardEvent
-  ) => void;
-  /**
-   * Called when the dropdown's input changes.
-   */
-  onInputChange?: (event: React.KeyboardEvent) => void;
-  /**
-   * If true, search in options will be enabled
-   */
-  searchable?: boolean;
-  /**
-   * The dropdown options
-   */
-  options?: DropdownOption[];
-  /**
-   * Text to display when there are no options
-   */
-  noOptionsMessage?: (() => string) | typeof NOOP;
-  /**
-   * If set to true, the menu will open when focused
-   */
-  openMenuOnFocus?: boolean;
-  /**
-   * If set to true, the menu will open when clicked
-   */
-  openMenuOnClick?: boolean;
-  /**
-   * If set to true, clear button will be added
-   */
-  clearable?: boolean;
-  /**
-   * custom option render function
-   */
-  optionRenderer?: (option: DropdownOption) => JSX.Element;
-  /**
-   * custom value render function
-   */
-  valueRenderer?: (...args: unknown[]) => unknown;
-  ValueRenderer?: (...args: unknown[]) => unknown;
-  /**
-   * custom menu render function
-   */
-  menuRenderer?: (...args: unknown[]) => unknown;
-  /**
-   * Default placement of the Dropdown menu in relation to its control. Use "auto" to flip the menu when there isn't enough space below the control.
-   */
-  menuPlacement?: DROPDOWN_MENU_PLACEMENT;
-
-  //  enum() PropTypes.oneOf(Object.values(DROPDOWN_MENU_PLACEMENT)),
-  /**
-   * The CSS position value of the menu, when "fixed" extra layout management might be required
-   * Fixed position can be used to solve the issue of positioning Dropdown inside overflow container like Modal or Dialog
-   */
-  menuPosition?: DROPDOWN_MENU_POSITION;
-  /**
-   * If set to true, the dropdown will be in Right to Left mode
-   */
-  rtl?: boolean;
-  /**
-   * Set default selected value
-   */
-  defaultValue?: DropdownOption[];
-  /**
-   * The component's value.
-   * When passed, makes this a [controlled](https://reactjs.org/docs/forms.html#controlled-components) component.
-   */
-  value?: DropdownOption[] | DropdownOption;
-  /**
-   * Select menu size from `Dropdown.size` - Dropdown.sizes.LARGE | Dropdown.sizes.MEDIUM | Dropdown.sizes.SMALL
-   */
-  size?: BaseSizes;
-  /**
-   * If provided Dropdown will work in async mode. Can be either promise or callback
-   */
-  asyncOptions?: (inputValue: string) => Promise<DropdownOption[]>;
-  /**
-   * If set to true, fetched async options will be cached
-   */
-  cacheOptions?: boolean;
-  /**
-   * If set, `asyncOptions` will be invoked with its value on mount and the resolved results will be loaded
-   */
-  defaultOptions?: boolean | Record<string, string>[];
-  /**
-   * If set to true, the menu will use virtualization. Virtualized async works only with
-   */
-  isVirtualized?: boolean;
-  /**
-   * Whether the menu should use a portal, and where it should attach
-   */
-  menuPortalTarget?: HTMLElement;
-  /**
-   * Custom function to override existing styles (similar to `react-select`'s `style` prop), for example: `base => ({...base, color: 'red'})`, where `base` is the component's default styles
-   */
-  extraStyles?: (...args: unknown[]) => unknown;
-  /**
-   * Maximum height of the menu before scrolling
-   */
-  maxMenuHeight?: number;
-  /**
-   * Tab index for keyboard navigation purposes
-   */
-  tabIndex?: number | string;
-  /**
-   * ID for the select container
-   */
-  id?: typeof DROPDOWN_ID;
-  /**
-   * ID for the menu component
-   */
-  menuId?: typeof DROPDOWN_MENU_ID;
-  /**
-   * focusAuto when component mount
-   */
-  menuAriaLabel?: typeof DROPDOWN_MENU_ARIA_LABEL,
-  autoFocus?: boolean;
-  /**
-   * If set to true, the dropdown will be in multi-select mode.
-   * When in multi-select mode, the selected value will be an array,
-   * and it will be displayed as our [`<Chips>`](/?path=/docs/components-chips--sandbox) component.
-   */
-  multi?: boolean;
-  /**
-   * If set to true together with `multi`, it will make the dropdown expand to multiple lines when new values are selected.
-   */
-  multiline?: boolean;
-  /**
-  Pass closeMenuOnSelect to close the multi choose any time an options is chosen.
-  */
-  closeMenuOnSelect?: boolean;
-  // Won't be needed once we upgrade to react-select ^5.5 https://github.com/JedWatson/react-select/issues/4088#issuecomment-1276835389
-  /**
-   * If menu should be closed on scroll - helpful for some tricky use cases
-   * @default false, but true when insideOverflowContainer or insideOverflowWithTransformContainer are true
-   */
-  closeMenuOnScroll?: boolean;
-  /**
-   * callback to be called when `multiline` is `true` and the option is removed
-   */
-  onOptionRemove?: (...args: unknown[]) => unknown;
-  /**
-  Pass Ref for reference of the actual dropdown component
-  */
-  ref?: React.ForwardedRef<HTMLDivElement> | string;
-  /**
-  The options set by default will be set as mandatory and the user will not be able to cancel their selection
-  */
-  withMandatoryDefaultOptions?: boolean;
-  /**
-   * Override the built-in logic to detect whether an option is selected.
-   */
-  isOptionSelected?: (option: DropdownOption, selectValue: DropdownOption["value"]) => boolean;
-  /**
-   * For display the drop down menu in overflow hidden/scroll container.
-   */
-  insideOverflowContainer?: boolean;
-  /**
-   * For display the drop down menu in overflow hidden/scroll container which contains transform css function usage.
-   */
-  insideOverflowWithTransformContainer?: boolean;
-  /**
-   * When content is passed, the dropdown will include a tooltip on the dropdown's value.
-   */
-  tooltipContent?: string;
-  /**
-   * Display the drop down with loading state.
-   */
-  isLoading?: boolean;
-  /**
-   * Overrides the built-in logic of loading message design
-   */
-  loadingMessage?: (obj: { inputValue: string }) => React.ReactNode;
-  /**
-   * aria-label attribute for dropdown
-   */
-  ariaLabel?: string;
-  /**
-   * Overrides the built-in logic of tab selecting value (default: true)
-   */
-  tabSelectsValue?: boolean;
-  /**
-   * Overrides the build-in search filter logic - https://react-select.com/advanced#custom-filter-logic
-   * createFilter function is available at Dropdown.createFilter
-   */
-  filterOption?: (option: unknown, inputValue: string) => boolean;
-
-  withReadOnlyStyle?: boolean;
-  OptionRenderer?: (option: DropdownOption) => JSX.Element;
-  menuIsOpen?: boolean;
-  onOptionSelect?: (...args: unknown[]) => void;
-  onClear?: (...args: unknown[]) => void;
-  popupsContainerSelector?: string;
-  selectProps?: Record<string, string>;
-}
-
-const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardRef(
+const Dropdown: VibeComponent<DropdownComponentProps, HTMLElement> & {
+  size?: typeof SIZES;
+  sizes?: typeof SIZES;
+  chipColors?: typeof DROPDOWN_CHIP_COLORS;
+  menuPlacements?: typeof DROPDOWN_MENU_PLACEMENT;
+  menuPositions?: typeof DROPDOWN_MENU_POSITION;
+  createFilter?: typeof createFilter;
+} = forwardRef(
   (
     {
       className,
       optionWrapperClassName,
       singleValueWrapperClassName,
       dropdownMenuWrapperClassName,
-      placeholder,
-      disabled,
-      readOnly,
+      placeholder = "",
+      disabled = false,
+      readOnly = false,
       withReadOnlyStyle,
-      onMenuOpen,
-      onMenuClose,
-      onFocus,
-      onBlur,
-      onChange: customOnChange,
-      searchable,
-      options,
+      onMenuOpen = NOOP,
+      onMenuClose = NOOP,
+      onFocus = NOOP,
+      onBlur = NOOP,
+      onChange: customOnChange = NOOP,
+      searchable = true,
+      options = [],
       defaultValue,
       value: customValue,
       noOptionsMessage,
       openMenuOnFocus,
       openMenuOnClick,
-      clearable,
+      clearable = true,
       OptionRenderer,
       optionRenderer,
       ValueRenderer,
       valueRenderer,
       menuRenderer,
-      menuPlacement,
+      menuPlacement = Dropdown.menuPlacements.BOTTOM,
       rtl,
-      size,
+      size = Dropdown.sizes.MEDIUM,
       asyncOptions,
       cacheOptions,
       defaultOptions,
       isVirtualized,
       menuPortalTarget,
-      extraStyles,
+      extraStyles = defaultCustomStyles,
       maxMenuHeight,
       menuIsOpen,
-      tabIndex,
-      id,
-      menuId,
-      menuAriaLabel,
-      autoFocus,
+      tabIndex = "0",
+      id = DROPDOWN_ID,
+      menuId = DROPDOWN_MENU_ID,
+      menuAriaLabel = DROPDOWN_MENU_ARIA_LABEL,
+      autoFocus = false,
       multi = false,
       multiline = false,
       onOptionRemove: customOnOptionRemove,
       onOptionSelect,
       onClear,
-      onInputChange,
+      onInputChange = NOOP,
       closeMenuOnSelect = !multi,
-      closeMenuOnScroll: customCloseMenuOnScroll,
-      withMandatoryDefaultOptions,
+      closeMenuOnScroll: customCloseMenuOnScroll = false,
+      withMandatoryDefaultOptions = false,
       isOptionSelected,
-      insideOverflowContainer,
-      insideOverflowWithTransformContainer,
-      tooltipContent,
-      onKeyDown,
-      isLoading,
+      insideOverflowContainer = false,
+      insideOverflowWithTransformContainer = false,
+      tooltipContent = "",
+      onKeyDown = NOOP,
+      isLoading = false,
       loadingMessage,
       ariaLabel,
       tabSelectsValue = true,
       popupsContainerSelector,
       filterOption,
-      menuPosition,
+      menuPosition = Dropdown.menuPositions.ABSOLUTE,
       "data-testid": dataTestId
-    },
-    ref
+    }: DropdownComponentProps,
+    ref: React.ForwardedRef<HTMLElement>
   ) => {
     const controlRef = useRef();
     const overrideMenuPortalTarget =
       menuPortalTarget || (popupsContainerSelector && document.querySelector(popupsContainerSelector));
-    const overrideDefaultValue: DropdownOption | DropdownOption[] = useMemo(() => {
+    const overrideDefaultValue = useMemo(() => {
       if (defaultValue) {
         return Array.isArray(defaultValue)
-          ? defaultValue.map(df => ({ ...df, isMandatory: true }))
+          ? (defaultValue as DropdownOption[]).map(df => ({ ...df, isMandatory: true }))
           : { ...(defaultValue as DropdownOption), isMandatory: true };
       }
 
@@ -395,12 +134,9 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
     const selectedOptions = customValue ?? selected;
     const selectedOptionsMap = useMemo(() => {
       if (Array.isArray(selectedOptions)) {
-        return selectedOptions.reduce(
-          (acc, option) => ({ ...acc, [option.value as DropdownOption["label"]]: option }),
-          {} as DropdownOption
-        );
+        return selectedOptions.reduce((acc, option) => ({ ...acc, [option.value]: option }), {});
       }
-      return {} as DropdownOption;
+      return {};
     }, [selectedOptions]);
 
     const overrideAriaLabel = useMemo(() => {
@@ -429,7 +165,7 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
       const customStyles = extraStyles(baseStyles);
 
       // Lastly, we create a style groups object that makes sure we run each custom group with our basic overrides.
-      const mergedStyles = Object.entries(customStyles).reduce((accumulator, [stylesGroup, stylesFn]) => {
+      const mergedStyles: any = Object.entries(customStyles).reduce((accumulator, [stylesGroup, stylesFn]) => {
         return {
           ...accumulator,
           [stylesGroup]: (defaultStyles: BaseStyles, state: DropdownState) => {
@@ -443,7 +179,7 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
 
       if (multi) {
         if (multiline) {
-          Object.values(ADD_AUTO_HEIGHT_COMPONENTS).forEach(component => {
+          ADD_AUTO_HEIGHT_COMPONENTS.forEach((component: string) => {
             const original = mergedStyles[component];
             mergedStyles[component] = (provided: BaseStyles, state: DropdownState) => ({
               ...original(provided, state),
@@ -500,7 +236,7 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
           {...props}
           readOnly={readOnly}
           Renderer={finalValueRenderer}
-          selectedOption={(selectedOptions as DropdownOption[])[0]}
+          selectedOption={selectedOptions[0]}
           singleValueWrapperClassName={singleValueWrapperClassName}
         />
       ),
@@ -517,7 +253,7 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
     const onOptionRemove = useMemo(() => {
       return function (optionValue: number, e: React.MouseEvent | React.KeyboardEvent) {
         if (customOnOptionRemove) {
-          customOnOptionRemove((selectedOptionsMap as unknown as DropdownOption[])[optionValue]);
+          customOnOptionRemove(selectedOptionsMap[optionValue]);
         }
         const newSelectedOptions = Array.isArray(selectedOptions)
           ? selectedOptions.filter(option => option.value !== optionValue)
@@ -551,21 +287,21 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
         popupsContainerSelector
       ]
     );
-    const onChange = (option: DropdownOption, event: SelectEvent) => {
+    const onChange = (option: DropdownOption | DropdownOption[], meta: ActionMeta<DropdownOption>) => {
       if (customOnChange) {
-        customOnChange(option, event);
+        customOnChange(option, meta);
       }
 
-      switch (event.action) {
+      switch (meta.action) {
         case "select-option": {
-          const selectedOption = multi ? event.option : option;
+          const selectedOption = multi ? meta.option : option;
 
           if (onOptionSelect) {
             onOptionSelect(selectedOption);
           }
 
           if (!isControlled) {
-            setSelected([...(selectedOptions as DropdownOption[]), selectedOption]);
+            setSelected([...selectedOptions, selectedOption]);
           }
           break;
         }
@@ -670,7 +406,7 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
         data-testid={dataTestId || getTestId(ComponentDefaultTestId.DROPDOWN, id)}
         autoFocus={autoFocus}
         closeMenuOnSelect={closeMenuOnSelect}
-        ref={ref as React.RefObject<any>}
+        ref={ref as React.Ref<any>}
         withMandatoryDefaultOptions={withMandatoryDefaultOptions}
         isOptionSelected={isOptionSelected}
         isLoading={isLoading}
@@ -684,7 +420,7 @@ const Dropdown: VibeComponent<DropdownComponentProps, HTMLDivElement> = forwardR
   }
 );
 
-Object.assign(Dropdown, {
+export default withStaticProps(Dropdown, {
   // TODO Deprecate Dropdown.size in the next major version - use Dropdown.sizes instead
   size: SIZES,
   sizes: SIZES,
@@ -693,47 +429,3 @@ Object.assign(Dropdown, {
   menuPositions: DROPDOWN_MENU_POSITION,
   createFilter: createFilter
 });
-
-Dropdown.defaultProps = {
-  className: "",
-  optionWrapperClassName: undefined,
-  dropdownMenuWrapperClassName: undefined,
-  singleValueWrapperClassName: undefined,
-  placeholder: "",
-  onMenuOpen: NOOP,
-  onMenuClose: NOOP,
-  onKeyDown: NOOP,
-  onFocus: NOOP,
-  onBlur: NOOP,
-  onChange: NOOP,
-  onInputChange: NOOP,
-  searchable: true,
-  options: [],
-  menuPlacement: DROPDOWN_MENU_PLACEMENT.BOTTOM,
-  menuPosition: DROPDOWN_MENU_POSITION.ABSOLUTE,
-  noOptionsMessage: NOOP,
-  clearable: true,
-  size: BaseSizes.MEDIUM,
-  extraStyles: defaultCustomStyles,
-  tabIndex: 0,
-  onOptionRemove: undefined,
-  id: DROPDOWN_ID,
-  menuId: DROPDOWN_MENU_ID,
-  menuAriaLabel: DROPDOWN_MENU_ARIA_LABEL,
-  autoFocus: false,
-  closeMenuOnSelect: undefined,
-  closeMenuOnScroll: false,
-  ref: undefined,
-  withMandatoryDefaultOptions: false,
-  insideOverflowContainer: false,
-  insideOverflowWithTransformContainer: false,
-  tooltipContent: "",
-  disabled: false,
-  readOnly: false,
-  isLoading: false,
-  loadingMessage: undefined,
-  ariaLabel: undefined,
-  filterOption: undefined
-};
-
-export default Dropdown;
---
+++ packages/core/src/components/Dropdown/DropdownConstants.ts
@@ -0,0 +1,24 @@
+export const defaultCustomStyles = (baseStyles: string) => baseStyles;
+
+export const ADD_AUTO_HEIGHT_COMPONENTS = ["container", "control", "valueContainer"];
+
+export const DROPDOWN_ID = "dropdown-menu-id";
+export const DROPDOWN_MENU_ID = "dropdown-menu-list-id";
+export const DROPDOWN_MENU_ARIA_LABEL = "Dropdown menu";
+
+export const DROPDOWN_CHIP_COLORS = {
+  PRIMARY: "PRIMARY",
+  NEGATIVE: "NEGATIVE",
+  POSITIVE: "POSITIVE"
+};
+
+export const DROPDOWN_MENU_POSITION = {
+  ABSOLUTE: "absolute",
+  FIXED: "fixed"
+};
+
+export const DROPDOWN_MENU_PLACEMENT = {
+  TOP: "top",
+  BOTTOM: "bottom",
+  AUTO: "auto"
+};
