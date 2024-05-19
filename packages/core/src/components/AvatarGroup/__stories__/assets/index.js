f3201b7e51d00cec88086feb1e167dc38fb182a5
Tal Koren <talko@monday.com>
2024-05-19 09:58:21 +0300

    docs: revise AvatarGroup stories

--- packages/core/src/components/AvatarGroup/__stories__/assets/index.js
+++ packages/core/src/components/AvatarGroup/__stories__/assets/index.js
@@ -1,5 +1,6 @@
 import person1 from "./person1.png";
 import person2 from "./person2.png";
 import person3 from "./person3.png";
+import person4 from "./person4.png";
 
-export { person1, person2, person3 };
+export { person1, person2, person3, person4 };
