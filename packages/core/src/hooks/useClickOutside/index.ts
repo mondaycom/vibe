67f28876c1db0765e3a443bc533ba09eef748fac
Omer Nave <omerna@monday.com>
2024-05-22 14:30:06 +0300

    Update index.ts

--- packages/core/src/hooks/useClickOutside/index.ts
+++ packages/core/src/hooks/useClickOutside/index.ts
@@ -5,16 +5,24 @@ import { GenericEventCallback } from "../../types/events";
 export default function useClickOutside({
   ref,
   callback,
+  ignoreClasses,
   eventName = "click"
 }: {
   ref: RefObject<HTMLElement>;
   callback: GenericEventCallback;
+  ignoreClasses?: string[];
   eventName?: keyof HTMLElementEventMap | string;
 }) {
   const onClickOutsideListener = useCallback(
     (event: MouseEvent) => {
       if (!ref || !ref.current || ref.current.contains(event.target as Node)) {
         return;
+      }
+
+      if (ignoreClasses && event.target instanceof HTMLElement) {
+        if (event.target.closest(ignoreClasses.join(','))) {
+          return;
+        }
       }
 
       callback(event);
