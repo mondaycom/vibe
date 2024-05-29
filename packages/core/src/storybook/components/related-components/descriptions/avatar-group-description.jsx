cf65f6ba005107247cc2cf45d618d95ae6103c68
Tal Koren <talko@monday.com>
2024-05-29 10:06:34 +0300

    feat(AvatarGroup): add disabled state (#2126)

--- packages/core/src/storybook/components/related-components/descriptions/avatar-group-description.jsx
+++ packages/core/src/storybook/components/related-components/descriptions/avatar-group-description.jsx
@@ -1,18 +1,20 @@
 import { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
-import { person1, person2, person3 } from "../../../../components/Avatar/__stories__/assets";
+import { person1, person2, person3, person4 } from "../../../../components/Avatar/__stories__/assets";
 import { AvatarGroup, Avatar } from "../../../../components";
 
 export const AvatarGroupDescription = () => {
   const component = useMemo(
     () => (
       <AvatarGroup size={Avatar.sizes.LARGE} max={3}>
-        <Avatar type={Avatar.types.IMG} src={person1} ariaLabel="Hadas Fahri" />
-        <Avatar type={Avatar.types.IMG} src={person2} ariaLabel="Sergey Roytman" />
-        <Avatar type={Avatar.types.IMG} src={person3} ariaLabel="Yossi Saadi" />
-        <Avatar type={Avatar.types.IMG} src={person1} ariaLabel="Hadas Fahri" />
-        <Avatar type={Avatar.types.IMG} src={person2} ariaLabel="Sergey Roytman" />
-        <Avatar type={Avatar.types.IMG} src={person3} ariaLabel="Yossi Saadi" />
+        <Avatar type={Avatar.types.IMG} src={person1} ariaLabel="Julia Martinez" />
+        <Avatar type={Avatar.types.IMG} src={person2} ariaLabel="Sophia Johnson" />
+        <Avatar type={Avatar.types.IMG} src={person3} ariaLabel="Marco DiAngelo" />
+        <Avatar type={Avatar.types.IMG} src={person4} ariaLabel="Liam Caldwell" />
+        <Avatar type={Avatar.types.IMG} src={person1} ariaLabel="Julia Martinez" />
+        <Avatar type={Avatar.types.IMG} src={person2} ariaLabel="Sophia Johnson" />
+        <Avatar type={Avatar.types.IMG} src={person3} ariaLabel="Marco DiAngelo" />
+        <Avatar type={Avatar.types.IMG} src={person4} ariaLabel="Liam Caldwell" />
       </AvatarGroup>
     ),
     []
