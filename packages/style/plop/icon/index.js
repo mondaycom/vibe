617e558b7f2b88d0f03d0e4d5b50545b7295b6c4
Rivka Ungar <rivkaun@monday.com>
2024-06-06 15:12:15 +0300

    Added the build and lint to plop

--- packages/style/plop/icon/index.js
+++ packages/style/plop/icon/index.js
@@ -1,3 +1,4 @@
+const { exec } = require("child_process");
 module.exports = plop => {
   plop.setGenerator("Icon", {
     description: "Adding a new icon",
@@ -23,13 +24,37 @@ module.exports = plop => {
         message: "Enter tags for the icon (separate with ',')"
       }
     ],
-    actions: [
-      {
-        type: "modify",
-        path: "src/Icons/iconsMetaData.js",
-        pattern: /(\/\/ plop_marker:icon_metadata)/g,
-        templateFile: "plop/icon/icon-metadata.txt"
-      }
-    ]
+    actions: () => {
+      return [
+        {
+          type: "modify",
+          path: "src/Icons/iconsMetaData.js",
+          templateFile: "plop/icon/icon-metadata.txt",
+          pattern: /(\/\/ plop_marker:icon_metadata)/g
+        },
+        () =>
+          new Promise((resolve, reject) => {
+            console.log("Adding icon...");
+            exec("cd ../core && npm run lint", (lintErr, lintStdout) => {
+              if (lintErr) {
+                console.error(lintErr);
+                reject(lintErr);
+                return;
+              }
+              console.log(lintStdout);
+
+              exec("cd ../core && yarn build:react-icons", (buildErr, buildStdout) => {
+                if (buildErr) {
+                  console.error(buildErr);
+                  reject(buildErr);
+                  return;
+                }
+                console.log(buildStdout);
+                resolve("Icon added and icons built successfully!");
+              });
+            });
+          })
+      ];
+    }
   });
 };
