521bc8f0557364805643a7735dddf4cd7179a886
Yossi Saadi <yossisa@monday.com>
2024-06-23 16:26:10 +0300

    chore(codemods): infra for changing component name

--- packages/codemods/src/utils/component-tag-utils.ts
+++ packages/codemods/src/utils/component-tag-utils.ts
@@ -1,7 +1,13 @@
-import { Collection, JSXOpeningElement } from "jscodeshift";
+import { ASTPath, Collection, JSXOpeningElement } from "jscodeshift";
 
 export function findComponentElements(root: Collection, componentName: string): Collection<JSXOpeningElement> {
   return root.find(JSXOpeningElement, {
     name: { type: "JSXIdentifier", name: componentName }
   });
+}
+
+export function renameComponentIdentifierName(path: ASTPath<JSXOpeningElement>, oldName: string, newName: string) {
+  if (path.node.name.type === "JSXIdentifier" && path.node.name.name === oldName) {
+    path.node.name.name = newName;
+  }
 }
