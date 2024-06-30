649f2914c7590c2bb4f5b0b60c7f846f5b3a8893
Yossi Saadi <yossisa@monday.com>
2024-06-30 16:19:54 +0300

    feat(Table): add table root ref for menu button positioning

--- packages/core/src/components/Table/Table/TableRoot.tsx
+++ packages/core/src/components/Table/Table/TableRoot.tsx
@@ -3,6 +3,7 @@ import { ITableProps } from "./Table";
 import cx from "classnames";
 import { useTable } from "../context/TableContext/TableContext";
 import styles from "./TableRoot.module.scss";
+import useMergeRef from "../../../hooks/useMergeRef";
 
 type TableRootProps = Pick<ITableProps, "id" | "className" | "data-testid" | "style" | "children">;
 
@@ -12,10 +13,13 @@ const TableRoot = forwardRef(
     ref: React.ForwardedRef<HTMLDivElement>
   ) => {
     const { isVirtualized, scrollLeft, onTableRootScroll } = useTable();
+    const { tableRootRef, isVirtualized, scrollLeft, onTableRootScroll } = useTable();
+    const mergedRef = useMergeRef(tableRootRef, ref);
+
 
     return (
       <div
-        ref={ref}
+        ref={mergedRef}
         id={id}
         className={cx(className, { [styles.virtualized]: isVirtualized, [styles.hasScroll]: scrollLeft > 0 })}
         data-testid={dataTestId}
