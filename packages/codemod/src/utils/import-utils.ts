633aa6bf0809961ee93356021a5f3935da9e8fd6
Yossi Saadi <yossisa@monday.com>
2024-07-07 12:12:47 +0300

    chore(codemod): util to extract the actual name from a specified import (alias or direct import name)

--- packages/codemod/src/utils/import-utils.ts
+++ packages/codemod/src/utils/import-utils.ts
@@ -1,4 +1,4 @@
-import { Collection, ImportDeclaration } from "jscodeshift";
+import { ASTPath, Collection, ImportDeclaration, ImportSpecifier, JSCodeshift } from "jscodeshift";
 import { CORE_IMPORT_PATH, CORE_NEXT_IMPORT_PATH } from "../consts";
 import { VibeCorePath } from "../../types";
 
@@ -16,4 +16,31 @@ export function getCoreImportsForFile(root: Collection): Collection<ImportDeclar
 
 export function getCoreNextImportsForFile(root: Collection): Collection<ImportDeclaration> {
   return getImports(root, CORE_NEXT_IMPORT_PATH);
+}
+
+function extractSpecifiersFromImport(j: JSCodeshift, path: ASTPath<ImportDeclaration>): ImportSpecifier[] {
+  const specifiers: ImportSpecifier[] = [];
+  j(path)
+    .find(ImportSpecifier)
+    .forEach(namedImport => {
+      specifiers.push(namedImport.node);
+    });
+  return specifiers;
+}
+
+export function getComponentNameOrAliasFromImports(
+  j: JSCodeshift,
+  paths: Collection<ImportDeclaration>,
+  componentName: string
+): string | null {
+  let actualName = null;
+  paths.forEach(path => {
+    const componentImportSpecifier = j(extractSpecifiersFromImport(j, path))
+      .filter(spec => spec.node.imported.name === componentName)
+      .at(0);
+    if (componentImportSpecifier.length) {
+      actualName = componentImportSpecifier.get().node.local.name;
+    }
+  });
+  return actualName;
 }
