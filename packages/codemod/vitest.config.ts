61c509f343789c00ae30677c4dd5a09b8a47c2c5
Yossi Saadi <yossisa@monday.com>
2024-07-07 11:21:17 +0300

    chore: init 'codemod' - a new package to store Vibe's codemods

---
+++ packages/codemod/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    setupFiles: ["test/setup.ts"],
+    include: ["**/__tests__/**/*.test.ts"],
+    clearMocks: true,
+    typecheck: {
+      enabled: true
+    }
+  }
+});
