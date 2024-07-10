9fb3a556f0f404a88d5c5a3af0c972d01f6d3873
Yossi Saadi <yossisa@monday.com>
2024-07-10 13:26:52 +0300

    docs(CanvasWrapper): enable dark theme for stories in "docs" view mode

--- packages/core/src/storybook/components/canvas-wrapper/CanvasWrapper.tsx
+++ packages/core/src/storybook/components/canvas-wrapper/CanvasWrapper.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { Canvas, useOf } from "@storybook/blocks";
 import { ComponentProps, FC, useMemo, useState } from "react";
 import styles from "./CanvasWrapper.module.scss";
+import cx from "classnames";
 
-type CanvasWrapper = ComponentProps<typeof Canvas>;
+interface CanvasWrapper extends ComponentProps<typeof Canvas> {
+  theme?: "light" | "dark" | "black";
+}
 
-const CanvasWrapper: FC<CanvasWrapper> = ({ of }) => {
+const themeToGlobalClassName = {
+  light: "light-app-theme",
+  dark: "dark-app-theme",
+  black: "black-app-theme"
+};
+
+const CanvasWrapper: FC<CanvasWrapper> = ({ of, theme, className, ...rest }) => {
   const [open, setOpen] = useState<boolean>(false);
 
   // resolve Storybook story module to get the story id and parameters of current rendered story
@@ -28,7 +37,8 @@ const CanvasWrapper: FC<CanvasWrapper> = ({ of }) => {
         of={of}
         sourceState={sourceState || (liveEditEnabledForStory ? "none" : "hidden")}
         additionalActions={liveEditEnabledForStory ? [toggleCodeAction] : []}
-        className={styles.canvas}
+        className={cx(styles.canvas, themeToGlobalClassName[theme], className)}
+        {...rest}
       />
       {liveEditEnabledForStory && <div id={story.id} className={styles.editorContainer} data-editor-open={open} />}
     </>
