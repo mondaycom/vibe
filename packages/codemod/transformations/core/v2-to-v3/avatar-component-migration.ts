490dd85d4809a86f68e8af17e42b56cb85bff187
Yossi Saadi <yossisa@monday.com>
2024-07-14 17:08:58 +0300

    chore(Avatar): use migratePropsNames

--- packages/codemod/transformations/core/v2-to-v3/avatar-component-migration.ts
+++ packages/codemod/transformations/core/v2-to-v3/avatar-component-migration.ts
@@ -2,9 +2,7 @@ import {
   findComponentElements,
   getComponentNameOrAliasFromImports,
   getCoreImportsForFile,
-  isPropExists,
-  removeProp,
-  updatePropName,
+  migratePropsNames,
   wrap
 } from "../../../src/utils";
 import { TransformationContext } from "../../../types";
@@ -13,7 +11,7 @@ import { TransformationContext } from "../../../types";
  * 1. Update the 'isSquare' prop to 'square'
  * 2. Update the 'isDisabled' prop to 'disabled'
  */
-function transform({ j, root }: TransformationContext) {
+function transform({ j, root, filePath }: TransformationContext) {
   if (!getCoreImportsForFile(root).length) return;
   const imports = getCoreImportsForFile(root);
   const componentName = getComponentNameOrAliasFromImports(j, imports, "Avatar");
@@ -22,20 +20,8 @@ function transform({ j, root }: TransformationContext) {
   const elements = findComponentElements(root, componentName);
   if (!elements.length) return;
 
-  elements.forEach(path => {
-    const hasIsSquare = isPropExists(j, path, "isSquare");
-    const hasSquare = isPropExists(j, path, "square");
-    if (hasIsSquare && hasSquare) {
-      removeProp(j, path, "isSquare");
-    }
-
-    const hasIsDisabled = isPropExists(j, path, "isDisabled");
-    const hasDisabled = isPropExists(j, path, "disabled");
-    if (hasIsDisabled && hasDisabled) {
-      removeProp(j, path, "isDisabled");
-    }
-
-    updatePropName(path, { isSquare: "square", isDisabled: "disabled" });
+  elements.forEach(elementPath => {
+    migratePropsNames(j, elementPath, filePath, componentName, { isSquare: "square", isDisabled: "disabled" });
   });
 }
 
