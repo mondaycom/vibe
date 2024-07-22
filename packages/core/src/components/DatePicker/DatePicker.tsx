ead721faa72aea1d57a1c167b720fcd3ec79ff9d
Moshe Zemah <moshe@monday.com>
2024-07-22 17:27:02 +0300

    Fix style linter

--- packages/core/src/components/DatePicker/DatePicker.tsx
+++ packages/core/src/components/DatePicker/DatePicker.tsx
@@ -14,6 +14,7 @@ import { getTestId } from "../../tests/test-ids-utils";
 import { ComponentDefaultTestId } from "../../tests/constants";
 import { NOOP } from "../../utils/function-utils";
 import styles from "./DatePicker.module.scss";
+// Make sure to update when upgrading react-dates
 import "./external_datepicker.scss";
 
 export interface DatePickerProps extends VibeComponentProps {
