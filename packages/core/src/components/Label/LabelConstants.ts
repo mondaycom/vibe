0b5c7f955a35fc6f2e3780b2220ac0b44a027958
Rivka Ungar <rivkaun@monday.com>
2024-07-25 16:03:56 +0300

    chore(Label): deprecate enums

--- packages/core/src/components/Label/LabelConstants.ts
+++ packages/core/src/components/Label/LabelConstants.ts
@@ -1,8 +1,14 @@
+/**
+ * @deprecated
+ */
 export enum LabelKind {
   FILL = "fill",
   LINE = "line"
 }
 
+/**
+ * @deprecated
+ */
 export enum LabelColor {
   PRIMARY = "primary",
   DARK = "dark",
