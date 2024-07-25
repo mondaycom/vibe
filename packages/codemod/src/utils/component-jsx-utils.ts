b80fd91efd1239d4e5542e728006f943baadd4e8
Yossi Saadi <yossisa@monday.com>
2024-07-25 19:03:29 +0300

    feat(codemod): util for updating a component name in the jsx element

--- packages/codemod/src/utils/component-jsx-utils.ts
+++ packages/codemod/src/utils/component-jsx-utils.ts
@@ -6,3 +6,14 @@ import { ASTPath, Collection, JSCodeshift, JSXElement } from "jscodeshift";
 export function findComponentElements(root: Collection, componentName: string): Collection<JSXElement> {
   return root.findJSXElements(componentName);
 }
+
+/**
+ * Updates the name of a JSX element.
+ */
+export function updateComponentName(j: JSCodeshift, elementPath: ASTPath<JSXElement>, newName: string) {
+  const elements = [elementPath.node.openingElement, elementPath.node.closingElement];
+  elements.forEach(element => {
+    if (!element?.name) return;
+    element.name = j.jsxIdentifier(newName);
+  });
+}
