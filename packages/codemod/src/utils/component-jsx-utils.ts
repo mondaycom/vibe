4f9719104608820a4ee88000c53314dc4221cba7
Yossi Saadi <yossisa@monday.com>
2024-07-25 19:02:41 +0300

    refactor(codemod): use jscodeshift native util for finding JSX elements

--- packages/codemod/src/utils/component-jsx-utils.ts
+++ packages/codemod/src/utils/component-jsx-utils.ts
@@ -1,8 +1,8 @@
-import { Collection, JSXElement } from "jscodeshift";
+import { ASTPath, Collection, JSCodeshift, JSXElement } from "jscodeshift";
 
 /**
  * Finds all JSX elements that match a component name.
  */
 export function findComponentElements(root: Collection, componentName: string): Collection<JSXElement> {
-  return root.find(JSXElement, { openingElement: { name: { name: componentName } } });
+  return root.findJSXElements(componentName);
 }
