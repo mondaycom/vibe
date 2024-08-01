c0d5959c5252a26a95daabd2e16733393a57d7b1
Rivka Ungar <rivkaun@monday.com>
2024-08-01 10:54:16 +0300

    Add prop for aria label

--- packages/core/src/components/DatePicker/DatePickerHeader/DatePickerHeader.tsx
+++ packages/core/src/components/DatePicker/DatePickerHeader/DatePickerHeader.tsx
@@ -12,6 +12,7 @@ export interface DatePickerHeaderProps {
   onToggleMonthYearPicker: () => void;
   hideNavigationKeys: boolean;
   "data-testid"?: string;
+  ariaLabel: string;
 }
 
 const DatePickerHeader = (props: DatePickerHeaderProps) => {
@@ -20,7 +21,8 @@ const DatePickerHeader = (props: DatePickerHeaderProps) => {
     isMonthYearSelection,
     onToggleMonthYearPicker,
     hideNavigationKeys,
-    "data-testid": dateTestId
+    "data-testid": dateTestId,
+    ariaLabel = "Toggle select year"
   } = props;
 
   const localedDated = moment(currentDate.valueOf());
@@ -35,7 +37,7 @@ const DatePickerHeader = (props: DatePickerHeaderProps) => {
           data-testid={`${dateTestId}-year-toggle`}
           type="button"
           className={styles.button}
-          aria-label="Toggle select year"
+          aria-label={ariaLabel}
           onClick={onToggleMonthYearPicker}
         >
           <div className={styles.buttonContent}>
