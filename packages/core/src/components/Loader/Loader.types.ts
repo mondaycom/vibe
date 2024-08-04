6f93d361a3b0ce3dbb720756abde98d6656e3dce
Rivka Ungar <rivkaun@monday.com>
2024-08-04 11:26:40 +0300

    re-export loader

--- packages/core/src/components/Loader/Loader.types.ts
+++ packages/core/src/components/Loader/Loader.types.ts
@@ -1,5 +1,5 @@
 export type LoaderColors = "primary" | "secondary" | "onPrimary" | "dark";
 
-type LoaderSizes = "xs" | "small" | "medium" | "large";
+export type LoaderSizes = "xs" | "small" | "medium" | "large";
 
 export type LoaderSize = LoaderSizes | number;
