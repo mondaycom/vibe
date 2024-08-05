3abe93ba8fdeaf119f6dc5bd69264a090a4033c5
Tal Koren <talko@monday.com>
2024-08-05 15:52:01 +0300

    feat: drop CommonJS support (#2226)

--- packages/core/scripts/build-storybook-exports.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const { createFoldersIfNotExist, buildStorybookComponentsIndexFile } = require("./build-utils");
-
-createFoldersIfNotExist();
-
-buildStorybookComponentsIndexFile();
