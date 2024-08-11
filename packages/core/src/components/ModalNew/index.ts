f934075ca16c91d4cd8bdbc24373c0032f9027f4
Yossi Saadi <yossisa@monday.com>
2024-08-11 18:50:46 +0300

    feat(ModalContent): export modal content component from next

--- packages/core/src/components/ModalNew/index.ts
+++ packages/core/src/components/ModalNew/index.ts
@@ -1,2 +1,3 @@
 export { default as ModalFooterBasic } from "./ModalFooter/ModalFooterBasic/ModalFooterBasic";
 export { default as ModalFooterWizard } from "./ModalFooter/ModalFooterWizard/ModalFooterWizard";
+export { default as ModalContent } from "./ModalContent/ModalContent";
