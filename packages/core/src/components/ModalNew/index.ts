9be0201350433b193838544526932255af9554e2
Yossi Saadi <yossisa@monday.com>
2024-08-11 23:51:58 +0300

    feat(ModalHeader): export modal header component from next

--- packages/core/src/components/ModalNew/index.ts
+++ packages/core/src/components/ModalNew/index.ts
@@ -1,3 +1,4 @@
+export { default as ModalHeader } from "./ModalHeader/ModalHeader";
 export { default as ModalFooterBasic } from "./ModalFooter/ModalFooterBasic/ModalFooterBasic";
 export { default as ModalFooterWizard } from "./ModalFooter/ModalFooterWizard/ModalFooterWizard";
 export { default as ModalContent } from "./ModalContent/ModalContent";
