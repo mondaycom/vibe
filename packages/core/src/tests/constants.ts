778a160dfd9aeb8d1bd138c32dacfdddd6a499fc
Yossi Saadi <yossisa@monday.com>
2024-08-11 18:28:58 +0300

    feat: add data-testids for modal next footer

--- packages/core/src/tests/constants.ts
+++ packages/core/src/tests/constants.ts
@@ -103,6 +103,8 @@ export enum ComponentDefaultTestId {
   MODAL_CONTENT = "modal-content",
   MODAL_HEADER = "modal-header",
   MODAL_FOOTER_BUTTONS = "modal-footer-buttons",
+  MODAL_NEXT_FOOTER_BASIC = "modal-footer",
+  MODAL_NEXT_FOOTER_WIZARD = "modal-footer-wizard",
   FORMATTED_NUMBER = "formatted-number",
   HIDDEN_TEXT = "hidden-text",
   DIALOG_CONTENT_CONTAINER = "dialog-content-container",
