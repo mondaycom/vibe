b093e8c3995d4bb707b27aea75160be724008223
Tal Koren <talko@monday.com>
2024-08-21 20:28:31 +0300

    docs: fix typography example in the catalog

--- packages/core/src/storybook/components/related-components/descriptions/typography-description/typography-description.jsx
+++ packages/core/src/storybook/components/related-components/descriptions/typography-description/typography-description.jsx
@@ -1,18 +1,24 @@
 import React, { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
-import { Heading } from "../../../../../components";
+import { Flex, Heading } from "../../../../../components";
 
 export const TypographyDescription = () => {
   const component = useMemo(() => {
-    const style = { display: "flex", justifyContent: "center", alignItems: "center" };
     return (
-      <div style={style}>
-        <Heading type="h1" ellipsis={false} value="H1" size="small" />
-        <Heading type="h2" ellipsis={false} value="H2" />
-        <Heading type="h3" ellipsis={false} value="H3" />
-      </div>
+      <Flex gap="small">
+        <Heading type="h1" ellipsis={false} size="small">
+          H1
+        </Heading>
+        <Heading type="h2" ellipsis={false}>
+          H2
+        </Heading>
+        <Heading type="h3" ellipsis={false}>
+          H3
+        </Heading>
+      </Flex>
     );
   }, []);
+
   return (
     <RelatedComponent
       component={component}
