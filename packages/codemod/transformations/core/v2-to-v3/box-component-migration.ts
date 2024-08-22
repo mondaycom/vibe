441ebbe98bdbe7903dd99c778fa9f6d3fc0cb9d1
Rivka Ungar <rivkaun@monday.com>
2024-08-22 14:39:15 +0300

    chore(Box): codemod for migration of border value

---
+++ packages/codemod/transformations/core/v2-to-v3/box-component-migration.ts
@@ -0,0 +1,26 @@
+import {
+  wrap,
+  getCoreImportsForFile,
+  getComponentNameOrAliasFromImports,
+  findComponentElements,
+  updatePropValues
+} from "../../../src/utils";
+import { TransformationContext } from "../../../types";
+
+/**
+ * 1. "Border" prop update 'Box.borders.DEFAULT' to true
+ */
+function transform({ j, root }: TransformationContext) {
+  const imports = getCoreImportsForFile(root);
+  const componentName = getComponentNameOrAliasFromImports(j, imports, "Box");
+  if (!componentName) return;
+
+  const elements = findComponentElements(root, componentName);
+  if (!elements.length) return;
+
+  elements.forEach(elementPath => {
+    updatePropValues(j, elementPath, "border", { "Box.borders.DEFAULT": true });
+  });
+}
+
+export default wrap(transform);
