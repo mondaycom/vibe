5a21832b8584a494e4306a65d86b78c63e866c61
Tal Koren <talko@monday.com>
2024-08-28 15:27:09 +0300

    chore: codemods for entry points

--- packages/codemod/transformations/core/v2-to-v3/__tests__/packages-rename-migration.test.ts
+++ packages/codemod/transformations/core/v2-to-v3/__tests__/packages-rename-migration.test.ts
@@ -5,9 +5,33 @@ describe("Packages rename migration", () => {
   defineInlineTest(
     transform,
     {},
-    `import { Button } from "monday-ui-react-core";`,
-    `import { Button } from "@vibe/core";`,
+    `import { Button, Label } from "monday-ui-react-core";`,
+    `import { Button, Label } from "@vibe/core";`,
     "should rename 'monday-ui-react-core' to '@vibe/core'"
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `import { BUTTON_TEST_ID } from "monday-ui-react-core/testIds";`,
+    `import { BUTTON_TEST_ID } from "@vibe/core/testIds";`,
+    "should rename 'monday-ui-react-core/testIds' to '@vibe/core/testIds'"
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `import { getWithin } from "monday-ui-react-core/interactionsTests";`,
+    `import { getWithin } from "@vibe/core/interactionsTests";`,
+    "should rename 'monday-ui-react-core/interactionsTests' to '@vibe/core/interactionsTests'"
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `import "monday-ui-react-core/tokens";`,
+    `import "@vibe/core/tokens";`,
+    "should rename 'monday-ui-react-core/tokens' to '@vibe/core/tokens'"
   );
 
   defineInlineTest(
--- packages/codemod/transformations/core/v2-to-v3/packages-rename-migration.ts
+++ packages/codemod/transformations/core/v2-to-v3/packages-rename-migration.ts
@@ -1,4 +1,12 @@
-import { CORE_ICONS_IMPORT_PATH, CORE_IMPORT_PATH, NEW_CORE_IMPORT_PATH, NEW_ICONS_IMPORT_PATH } from "src/consts";
+import {
+  CORE_ICONS_IMPORT_PATH,
+  CORE_IMPORT_PATH,
+  CORE_INTERACTIONS_IMPORT_PATH,
+  CORE_TESTIDS_IMPORT_PATH,
+  CORE_TOKENS_IMPORT_PATH,
+  NEW_CORE_IMPORT_PATH,
+  NEW_ICONS_IMPORT_PATH
+} from "src/consts";
 import { wrap, renameImportPath, getImports } from "../../../src/utils";
 import { TransformationContext } from "../../../types";
 
@@ -17,6 +25,24 @@ function transform({ root }: TransformationContext) {
 
   iconImports.forEach(importPath => {
     renameImportPath(importPath, NEW_ICONS_IMPORT_PATH);
+  });
+
+  const interactionsImports = getImports(root, CORE_INTERACTIONS_IMPORT_PATH);
+
+  interactionsImports.forEach(importPath => {
+    renameImportPath(importPath, `${NEW_CORE_IMPORT_PATH}/interactionsTests`);
+  });
+
+  const testIdsImports = getImports(root, CORE_TESTIDS_IMPORT_PATH);
+
+  testIdsImports.forEach(importPath => {
+    renameImportPath(importPath, `${NEW_CORE_IMPORT_PATH}/testIds`);
+  });
+
+  const tokensImports = getImports(root, CORE_TOKENS_IMPORT_PATH);
+
+  tokensImports.forEach(importPath => {
+    renameImportPath(importPath, `${NEW_CORE_IMPORT_PATH}/tokens`);
   });
 }
 
