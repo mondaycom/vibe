28e03ce19f963160e4c637888c073b852cb9e70f
Tal Koren <talko@monday.com>
2024-08-29 13:35:14 +0300

    test: add test

--- packages/codemod/transformations/core/v2-to-v3/__tests__/type-imports-migration.test.ts
+++ packages/codemod/transformations/core/v2-to-v3/__tests__/type-imports-migration.test.ts
@@ -17,4 +17,32 @@ describe("Rename type imports migration", () => {
     `import { VibeComponentProps } from "monday-ui-react-core";`,
     "should not modify the import path if it's already 'monday-ui-react-core'"
   );
+
+  defineInlineTest(
+    transform,
+    {},
+    `import { SomeOtherType } from "another-library";`,
+    `import { SomeOtherType } from "another-library";`,
+    "should not change unrelated imports"
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `import { VibeComponentProps } from "monday-ui-react-core/types";
+     import { AnotherType } from "monday-ui-react-core/types";`,
+    `import { VibeComponentProps } from "monday-ui-react-core";
+     import { AnotherType } from "monday-ui-react-core";`,
+    "should change multiple imports from 'monday-ui-react-core/types' to 'monday-ui-react-core'"
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `import { VibeComponentProps } from "monday-ui-react-core/types";
+     import { AnotherType } from "another-library";`,
+    `import { VibeComponentProps } from "monday-ui-react-core";
+     import { AnotherType } from "another-library";`,
+    "should change the 'monday-ui-react-core/types' import and leave other imports unchanged"
+  );
 });
