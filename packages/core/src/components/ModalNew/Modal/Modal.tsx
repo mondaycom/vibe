7819eea5d516a950f14fb8681c3a2d670268434e
Yossi Saadi <yossisa@monday.com>
2024-09-05 09:44:46 +0300

    feat(Modal): modal component with basic functionality

---
+++ packages/core/src/components/ModalNew/Modal/Modal.tsx
@@ -0,0 +1,50 @@
+import React, { forwardRef } from "react";
+import cx from "classnames";
+import { getTestId } from "../../../tests/test-ids-utils";
+import { ComponentDefaultTestId } from "../../../tests/constants";
+import styles from "./Modal.module.scss";
+import { ModalProps } from "./Modal.types";
+import ModalTopActions from "../ModalTopActions/ModalTopActions";
+
+const Modal = forwardRef(
+  (
+    {
+      show,
+      size = "medium",
+      renderHeaderAction,
+      closeButtonTheme,
+      closeButtonAriaLabel,
+      onClose,
+      children,
+      className,
+      id,
+      "data-testid": dataTestId
+    }: ModalProps,
+    ref: React.ForwardedRef<HTMLDivElement>
+  ) => {
+    return (
+      <div id="overlay" className={styles.overlay}>
+        <div
+          ref={ref}
+          className={cx(styles.modal, className)}
+          id={id}
+          data-testid={dataTestId || getTestId(ComponentDefaultTestId.MODAL, id)}
+          role="dialog"
+          aria-modal
+          aria-labelledby="TODO" // TODO
+          aria-describedby="TODO" // TODO
+        >
+          <ModalTopActions
+            renderAction={renderHeaderAction}
+            color={closeButtonTheme}
+            closeButtonAriaLabel={closeButtonAriaLabel}
+            onClose={onClose}
+          />
+          {children}
+        </div>
+      </div>
+    );
+  }
+);
+
+export default Modal;
