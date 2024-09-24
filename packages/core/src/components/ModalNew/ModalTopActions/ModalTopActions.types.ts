3e6faca0b16ee92f6f3f802bc94fadf74f608047
Yossi Saadi <yossisa@monday.com>
2024-09-24 14:35:18 +0300

    feat(ModalTopActions): top actions component for internal use (#2415)

---
+++ packages/core/src/components/ModalNew/ModalTopActions/ModalTopActions.types.ts
@@ -0,0 +1,23 @@
+import React from "react";
+import MenuButton from "../../MenuButton/MenuButton";
+import IconButton from "../../IconButton/IconButton";
+import { ButtonColor } from "../../Button/ButtonConstants";
+
+export type ModalTopActionsColor = "light" | "dark";
+export type ModalTopActionsButtonColor =
+  | ButtonColor.PRIMARY
+  | ButtonColor.ON_PRIMARY_COLOR
+  | ButtonColor.ON_INVERTED_BACKGROUND;
+
+export interface ModalTopActionsProps {
+  /**
+   * action can be passed either as a function or direct
+   * it allows passing back to consumer the color he chose, so he won't have to define it twice
+   */
+  renderAction?:
+    | React.ReactElement<typeof MenuButton | typeof IconButton>
+    | ((color?: ModalTopActionsButtonColor) => React.ReactElement<typeof MenuButton | typeof IconButton>);
+  color?: ModalTopActionsColor;
+  closeButtonAriaLabel?: string;
+  onClose?: React.MouseEventHandler<HTMLDivElement>;
+}
