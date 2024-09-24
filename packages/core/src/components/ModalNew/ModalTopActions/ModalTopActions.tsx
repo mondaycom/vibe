4999a844b17fa140ab382af14d164274ca016e70
Yossi Saadi <yossisa@monday.com>
2024-09-24 14:35:18 +0300

    feat(ModalTopActions): top actions component for internal use (#2415)

---
+++ packages/core/src/components/ModalNew/ModalTopActions/ModalTopActions.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./ModalTopActions.module.scss";
+import { ModalTopActionsButtonColor, ModalTopActionsColor, ModalTopActionsProps } from "./ModalTopActions.types";
+import Flex from "../../Flex/Flex";
+import IconButton from "../../IconButton/IconButton";
+import { CloseSmall } from "../../Icon/Icons";
+import { ButtonColor } from "../../Button/ButtonConstants";
+
+const colorToButtonColor: Record<ModalTopActionsColor, ModalTopActionsButtonColor> = {
+  dark: ButtonColor.ON_INVERTED_BACKGROUND,
+  light: ButtonColor.ON_PRIMARY_COLOR
+};
+
+const ModalTopActions = ({ renderAction, color, closeButtonAriaLabel, onClose }: ModalTopActionsProps) => {
+  const buttonColor = colorToButtonColor[color] || ButtonColor.PRIMARY;
+
+  return (
+    <Flex className={styles.actions}>
+      {typeof renderAction === "function" ? renderAction(buttonColor) : renderAction}
+      <IconButton
+        icon={CloseSmall}
+        onClick={onClose}
+        size={IconButton.sizes.SMALL}
+        kind={IconButton.kinds.TERTIARY}
+        color={buttonColor}
+        ariaLabel={closeButtonAriaLabel}
+      />
+    </Flex>
+  );
+};
+
+export default ModalTopActions;
