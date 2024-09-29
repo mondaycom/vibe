efcbb3305647db46a163d02b7a5de77e5e2d00e6
Rivka Ungar <rivkaun@monday.com>
2024-09-29 16:32:08 +0300

    restore style package to remove in another pr

--- packages/style/stylelint-config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  customSyntax: require.resolve("postcss-scss"),
-  plugins: ["./rules/use-defined-css-var-when-available/index.js"],
-  rules: {
-    "monday-ui-style/use-defined-css-var-when-available": true
-  }
-};
