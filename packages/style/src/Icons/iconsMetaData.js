e66dfe04544defb688e0dce0efbaf34c51711cad
Rivka Ungar <rivkaun@monday.com>
2024-09-29 14:36:03 +0300

    fix typo

--- packages/style/src/Icons/iconsMetaData.js
+++ packages/style/src/Icons/iconsMetaData.js
@@ -5,10 +5,10 @@ export const iconsMetaData = [
   // DO NOT REMOVE THIS FOLLOWING LINE!
   // plop_marker:icon_metadata
   {
-      name: "Shuffle",
-      file: "Shuffle.svg",
-      description: "Use to describe shuffle if items.",
-      tags: "Arrows, Random, Question, Form"
+    name: "Shuffle",
+    file: "Shuffle.svg",
+    description: "Use to describe shuffle of items.",
+    tags: "Arrows, Random, Question, Form"
   },
 
   {
