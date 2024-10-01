23b5b1ea221fd6ac8d26e646a19f37f8d38d3d54
Yossi Saadi <yossisa@monday.com>
2024-10-01 18:14:25 +0300

    feat(Modal): track content scrolled state for a shared state between Modal layouts

--- packages/core/src/components/ModalNew/context/ModalContext.tsx
+++ packages/core/src/components/ModalNew/context/ModalContext.tsx
@@ -1,10 +1,21 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useMemo, useState } from "react";
 import { ModalContextProps, ModalProviderProps } from "./ModalContext.types";
 
 const ModalContext = createContext<ModalContextProps | undefined>(undefined);
 
 export const ModalProvider = ({ value, children }: ModalProviderProps) => {
-  return <ModalContext.Provider value={value}>{children}</ModalContext.Provider>;
+  const [contentScrolled, setContentScrolled] = useState<boolean>(false);
+
+  const contextValue = useMemo<ModalContextProps>(
+    () => ({
+      ...value,
+      contentScrolled,
+      setContentScrolled: (newContentScrolled: boolean) => setContentScrolled(newContentScrolled)
+    }),
+    [contentScrolled, value]
+  );
+
+  return <ModalContext.Provider value={contextValue}>{children}</ModalContext.Provider>;
 };
 
 export const useModal = (): ModalContextProps => {
--- packages/core/src/components/ModalNew/context/ModalContext.types.ts
+++ packages/core/src/components/ModalNew/context/ModalContext.types.ts
@@ -4,6 +4,8 @@ export type ModalProviderValue = {
   modalId: string;
   setTitleId: (id: string) => void;
   setDescriptionId: (id: string) => void;
+  contentScrolled?: boolean;
+  setContentScrolled: (scrolled: boolean) => void;
 };
 
 export type ModalContextProps = ModalProviderValue;
