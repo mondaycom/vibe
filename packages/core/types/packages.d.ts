709e6d7fdd6ac743369b4cd9f2407e4a32828f66
Tal Koren <talko@monday.com>
2024-10-09 16:04:33 +0300

    chore(Dropdown): adjust imports

--- packages/core/types/packages.d.ts
+++ packages/core/types/packages.d.ts
@@ -1,4 +1,4 @@
 // Packages
 declare module "react-windowed-select";
-declare module "react-select/async/dist/react-select.cjs.js";
-declare module "react-select/base/dist/react-select.cjs.js";
+declare module "react-select/async/dist/react-select.esm.js";
+declare module "react-select/base/dist/react-select.esm.js";
