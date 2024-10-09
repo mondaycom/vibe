f816e8a81c7f93c65ab07d649e5592b15727389a
Moshe Zemah <moshe@monday.com>
2024-10-09 20:52:41 +0300

    Fix ssr issues [prerelease]

--- packages/core/src/components/Modal/Modal.tsx
+++ packages/core/src/components/Modal/Modal.tsx
@@ -13,6 +13,7 @@ import { getTestId } from "../../tests/test-ids-utils";
 import { ComponentDefaultTestId } from "../../tests/constants";
 import styles from "./Modal.module.scss";
 import { useWarnDeprecated } from "../../utils/warn-deprecated";
+import { isClient } from "../../utils/ssr-utils";
 
 export interface ModalProps {
   /**
@@ -170,40 +171,42 @@ const Modal: FC<ModalProps> & { width?: typeof ModalWidth } = ({
 
   const customWidth = width !== ModalWidth.DEFAULT && width !== ModalWidth.FULL_WIDTH;
 
-  const dialog = ReactDOM.createPortal(
-    <div
-      {...attr.container}
-      className={cx(styles.container, classNames.container)}
-      data-testid={dataTestId || getTestId(ComponentDefaultTestId.MODAL, id)}
-      style={{ "--monday-modal-z-index": zIndex }}
-    >
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+  const dialog =
+    isClient() &&
+    ReactDOM.createPortal(
       <div
-        onClick={closeIfNotAlertType}
-        className={cx(styles.overlay, classNames.overlay)}
-        data-testid={ComponentDefaultTestId.MODAL_OVERLAY}
-      />
-      <div
-        {...attr.dialog}
-        className={cx(styles.dialog, classNames.modal, {
-          [styles.default]: width === ModalWidth.DEFAULT,
-          [styles.full]: width === ModalWidth.FULL_WIDTH,
-          [styles.spacing]: contentSpacing
-        })}
-        style={{ width: customWidth ? width : null }}
+        {...attr.container}
+        className={cx(styles.container, classNames.container)}
+        data-testid={dataTestId || getTestId(ComponentDefaultTestId.MODAL, id)}
+        style={{ "--monday-modal-z-index": zIndex }}
       >
-        {header}
-        {content}
-        {footer}
-      </div>
-    </div>,
-    document.body
-  );
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+        <div
+          onClick={closeIfNotAlertType}
+          className={cx(styles.overlay, classNames.overlay)}
+          data-testid={ComponentDefaultTestId.MODAL_OVERLAY}
+        />
+        <div
+          {...attr.dialog}
+          className={cx(styles.dialog, classNames.modal, {
+            [styles.default]: width === ModalWidth.DEFAULT,
+            [styles.full]: width === ModalWidth.FULL_WIDTH,
+            [styles.spacing]: contentSpacing
+          })}
+          style={{ width: customWidth ? width : null }}
+        >
+          {header}
+          {content}
+          {footer}
+        </div>
+      </div>,
+      document.body
+    );
 
   if (unmountOnClose && !shouldShow) {
     return null;
   }
-  return ReactDOM.createPortal(dialog, document.body);
+  return isClient() ? ReactDOM.createPortal(dialog, document.body) : null;
 };
 
 export default withStaticProps(Modal, {
