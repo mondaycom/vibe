fe98f40c5a6e92b2bbac53851e392ac0313b7abe
Yossi Saadi <yossisa@monday.com>
2024-10-09 10:51:55 +0300

    feat(ModalHeader): set titleId and descriptionId for Modal (#2446)

--- packages/core/src/components/ModalNew/ModalHeader/ModalHeader.tsx
+++ packages/core/src/components/ModalNew/ModalHeader/ModalHeader.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect } from "react";
 import cx from "classnames";
 import { getTestId } from "../../../tests/test-ids-utils";
 import { ComponentDefaultTestId } from "../../../tests/constants";
@@ -8,12 +8,24 @@ import Flex from "../../Flex/Flex";
 import Heading from "../../Heading/Heading";
 import Text from "../../Text/Text";
 import Icon from "../../Icon/Icon";
+import { useModal } from "../context/ModalContext";
 
 const ModalHeader = forwardRef(
   (
     { title, description, descriptionIcon, className, id, "data-testid": dataTestId }: ModalHeaderProps,
     ref: React.ForwardedRef<HTMLDivElement>
   ) => {
+    const { modalId, setDescriptionId, setTitleId } = useModal();
+    const titleId = id ? `${modalId}_${id}_label` : `${modalId}_label`;
+    const descriptionId = id ? `${modalId}_${id}_desc` : `${modalId}_desc`;
+
+    useEffect(() => {
+      if (!modalId) return;
+      setTitleId(titleId);
+      if (!description) return;
+      setDescriptionId(descriptionId);
+    }, [modalId, setTitleId, titleId, description, setDescriptionId, descriptionId]);
+
     return (
       <Flex
         gap={Flex.gaps.XS}
@@ -24,11 +36,17 @@ const ModalHeader = forwardRef(
         id={id}
         data-testid={dataTestId || getTestId(ComponentDefaultTestId.MODAL_NEXT_HEADER, id)}
       >
-        <Heading type={Heading.types.H2} weight={Heading.weights.MEDIUM} maxLines={2} className={styles.title}>
+        <Heading
+          id={titleId}
+          type={Heading.types.H2}
+          weight={Heading.weights.MEDIUM}
+          maxLines={2}
+          className={styles.title}
+        >
           {title}
         </Heading>
         {description && (
-          <Flex>
+          <Flex id={descriptionId}>
             {descriptionIcon && (
               <Icon
                 icon={typeof descriptionIcon === "object" ? descriptionIcon.name : descriptionIcon}
--- packages/core/src/components/ModalNew/ModalHeader/__tests__/ModalHeader.test.tsx
+++ packages/core/src/components/ModalNew/ModalHeader/__tests__/ModalHeader.test.tsx
@@ -2,11 +2,27 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ModalHeader from "../ModalHeader";
 import { Text as TextIcon } from "../../../Icon/Icons";
+import { useModal } from "../../context/ModalContext";
+
+jest.mock("../../context/ModalContext", () => ({
+  useModal: jest.fn()
+}));
+const useModalMocked = jest.mocked(useModal);
 
 describe("ModalHeader", () => {
   const title = "Test Modal Header";
   const simpleDescription = "This is a description";
   const descriptionIcon = TextIcon;
+
+  const useModalMockedReturnedValue = {
+    modalId: "modal-id",
+    setTitleId: jest.fn(),
+    setDescriptionId: jest.fn()
+  };
+
+  beforeEach(() => {
+    useModalMocked.mockReturnValue(useModalMockedReturnedValue);
+  });
 
   it("renders the title correctly", () => {
     const { getByText } = render(<ModalHeader title={title} />);
@@ -55,5 +71,43 @@ describe("ModalHeader", () => {
 
     const icon = getByTestId("icon");
     expect(icon).toHaveClass(descriptionIconObject.className);
+  });
+
+  it("sets the titleId and descriptionId in the context when rendered", () => {
+    const { getByText } = render(<ModalHeader title={title} description={simpleDescription} />);
+
+    expect(useModalMockedReturnedValue.setTitleId).toHaveBeenCalledWith("modal-id_label");
+    expect(useModalMockedReturnedValue.setDescriptionId).toHaveBeenCalledWith("modal-id_desc");
+    expect(getByText(title)).toBeInTheDocument();
+    expect(getByText(simpleDescription)).toBeInTheDocument();
+  });
+
+  it("does not set descriptionId if no description is provided", () => {
+    render(<ModalHeader title={title} />);
+
+    expect(useModalMockedReturnedValue.setTitleId).toHaveBeenCalledWith("modal-id_label");
+    expect(useModalMockedReturnedValue.setDescriptionId).not.toHaveBeenCalled();
+  });
+
+  it("renders the title with the correct id", () => {
+    const { getByText } = render(<ModalHeader title={title} />);
+
+    const titleElement = getByText(title);
+    expect(titleElement).toHaveAttribute("id", "modal-id_label");
+  });
+
+  it("renders the description container with the correct id when provided", () => {
+    const { getByText } = render(<ModalHeader title={title} description={simpleDescription} />);
+
+    const descriptionElement = getByText(simpleDescription);
+    expect(descriptionElement.parentElement).toHaveAttribute("id", "modal-id_desc");
+  });
+
+  it("calls setTitleId and setDescriptionId with a custom id if provided", () => {
+    const customId = "custom-header-id";
+    render(<ModalHeader title={title} id={customId} description={simpleDescription} />);
+
+    expect(useModalMockedReturnedValue.setTitleId).toHaveBeenCalledWith("modal-id_custom-header-id_label");
+    expect(useModalMockedReturnedValue.setDescriptionId).toHaveBeenCalledWith("modal-id_custom-header-id_desc");
   });
 });
