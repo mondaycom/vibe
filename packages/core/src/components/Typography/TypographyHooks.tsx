0f2c618ad89bf96a36421c1ca23eb6701aa0c0e1
Moshe Zemah <moshe@monday.com>
2024-10-10 13:01:01 +0300

    fix: Fix ellipsis clamp lines for SSR [prerelease] (#2484)

--- packages/core/src/components/Typography/TypographyHooks.tsx
+++ packages/core/src/components/Typography/TypographyHooks.tsx
@@ -1,22 +1,20 @@
-import { MutableRefObject } from "react";
+import { MutableRefObject, useMemo } from "react";
 import { ElementContent } from "../../types";
 import useIsOverflowing from "../../hooks/useIsOverflowing/useIsOverflowing";
 import { TooltipProps } from "../Tooltip/Tooltip";
 import styles from "./Typography.module.scss";
 
-export function useEllipsisClass(ref: MutableRefObject<HTMLElement>, ellipsis: boolean, maxLines: number) {
-  let ellipsisClass;
-  const overrideRef = (node: HTMLElement) => {
-    node?.style.setProperty("--text-clamp-lines", maxLines.toString());
-    ref.current = node;
-  };
-
+export function useEllipsisClass(ellipsis: boolean, maxLines = 1) {
+  let ellipsisClass: string;
   // If component contains ellipsis return the fit ellipsis class
   if (ellipsis) {
     ellipsisClass = maxLines > 1 ? styles.multiLineEllipsis : styles.singleLineEllipsis;
   }
 
-  return { ref: overrideRef, class: ellipsisClass };
+  const result = useMemo(() => {
+    return { class: ellipsisClass, style: { "--text-clamp-lines": maxLines.toString() } };
+  }, [ellipsisClass, maxLines]);
+  return result;
 }
 
 export function useTooltipProps(
