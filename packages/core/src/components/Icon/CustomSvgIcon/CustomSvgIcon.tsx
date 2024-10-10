ee1eefddfe0cb552573401c64fc620fef0a04e3e
Moshe Zemah <moshe@monday.com>
2024-10-10 15:46:59 +0300

    Add placeholder for custom svg icon for SSR [prerelease] (#2485)

--- packages/core/src/components/Icon/CustomSvgIcon/CustomSvgIcon.tsx
+++ packages/core/src/components/Icon/CustomSvgIcon/CustomSvgIcon.tsx
@@ -4,6 +4,7 @@ import useIconScreenReaderAccessProps from "../../../hooks/useIconScreenReaderAc
 import VibeComponentProps from "../../../types/VibeComponentProps";
 import { ComponentDefaultTestId } from "../../../tests/constants";
 import { getTestId } from "../../../tests/test-ids-utils";
+import { useIsMounted } from "../../../hooks/ssr/useIsMounted";
 
 function modifySvgCode(svg: string, color = "currentColor") {
   return svg.replace(/fill=".*?"/g, `fill="${color}"`);
@@ -41,6 +42,8 @@ const CustomSvgIcon: FunctionComponent<CustomSvgIconProps> = ({
     isDecorationOnly: ariaHidden
   });
 
+  const isMounted = useIsMounted();
+
   const svgProcessor = useCallback(
     (svg: string) => {
       if (replaceToCurrentColor) return modifySvgCode(svg, "currentColor");
@@ -55,6 +58,10 @@ const CustomSvgIcon: FunctionComponent<CustomSvgIconProps> = ({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const SVGComponent = SVG as React.FC<any>;
 
+  if (!isMounted) {
+    // placeholder for server side rendering
+    return <div className={className} id={id}></div>;
+  }
   return (
     <SVGComponent
       innerRef={ref}
--- packages/core/src/utils/ssr-utils.ts
+++ packages/core/src/utils/ssr-utils.ts
@@ -1,1 +1,2 @@
 export const isClient = () => typeof window !== "undefined";
+export const isServer = () => !isClient();
