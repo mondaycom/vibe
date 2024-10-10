94417975b0de8fa4c81078631f66c7249f66d957
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2024-10-10 17:33:33 +0200

    fix: add missing generic type to useHover hook usage

--- packages/core/src/hooks/useHover/__stories__/useHover.stories.tsx
+++ packages/core/src/hooks/useHover/__stories__/useHover.stories.tsx
@@ -10,7 +10,7 @@ export default {
 
 export const Overview = {
   render: () => {
-    const [hoverRef, isHovered] = useHover();
+    const [hoverRef, isHovered] = useHover<HTMLDivElement>();
 
     return (
       <div
