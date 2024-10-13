2c6f6be79888691ca8960d3f412ec484d9a28753
Vlad Boisa <60569670+vladboisa@users.noreply.github.com>
2024-10-13 18:36:22 +0000

    feat(doc): convert VirtualizedGrid to TSX
    Converted stories and stories.helpers from JS to TSX, and add ItempType

--- packages/core/src/components/VirtualizedGrid/__stories__/VirtualizedGrid.stories.helpers.tsx
+++ packages/core/src/components/VirtualizedGrid/__stories__/VirtualizedGrid.stories.helpers.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { ComponentType } from "react";
+import { GridChildComponentProps } from "react-window";
 
-export const generateItems = (height = 30, width = "100%", itemsCount) => {
-  const items = [];
+type ItemType = {
+  value: string;
+  height: number;
+  width: number;
+  id: string;
+};
+
+export const generateItems = (height = 30, width = 100, itemsCount: number): ItemType[] => {
+  const items: ItemType[] = [];
   for (let i = 0; i < itemsCount; i++) {
-    items.push({ value: `Item ${i}`, height, width, id: i });
+    items.push({ value: `Item ${i}`, height, width, id: `${i}` });
   }
   return items;
 };
 
-export const itemRenderer = (item, index, style) => {
+export const itemRenderer = (
+  item: ItemType,
+  index: number,
+  style: React.CSSProperties
+): JSX.Element | ComponentType<GridChildComponentProps<ItemType>> => {
   if (item) {
     const backgroundColor = index % 2 === 0 ? "#e1e1e1" : "#f8f8f0";
     return (
