1b60fe73737fa247b4dcbe06365845a1636f6297
NS <noyshlomo2004@gmail.com>
2024-10-13 17:10:11 +0300

    fix: replace inline strings with Vibe's Text component in DialogContentContainer

--- packages/core/src/storybook/components/related-components/descriptions/dialog-content-container.jsx
+++ packages/core/src/storybook/components/related-components/descriptions/dialog-content-container.jsx
@@ -1,13 +1,14 @@
 import React, { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
 import DialogContentContainer from "../../../../components/DialogContentContainer/DialogContentContainer";
+import Text from "../../../../components/Text/Text"; 
 
 export const DialogContentContainerDescription = () => {
   const component = useMemo(() => {
     return (
       <div style={{ width: "200px" }}>
         <DialogContentContainer>
-          <p>A content section within an elevated dialog content container</p>
+          <Text ellipsis={false}>A content section within an elevated dialog content container</Text>
         </DialogContentContainer>
       </div>
     );
