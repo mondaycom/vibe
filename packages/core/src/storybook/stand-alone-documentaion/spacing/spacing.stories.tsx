caefb692e752855ecd0e6cbf9409ff7645541b3b
Vaibhav Jangid <118388839+vaibhavj2111@users.noreply.github.com>
2024-10-15 01:24:04 +0530

    Changes Done
    Resolved all the changes mentioned in the comments. Very silly errors on my part :dizzy_face:

--- packages/core/src/storybook/stand-alone-documentaion/spacing/spacing.stories.tsx
+++ packages/core/src/storybook/stand-alone-documentaion/spacing/spacing.stories.tsx
@@ -1,13 +1,13 @@
 import { SpacingSizes } from "./spacing-sizes/spacing-sizes";
-import { StoryObj , Meta } from "@storybook/blocks";
+import { StoryObj, Meta } from "@storybook/react";
 
 const meta: Meta = {
-    title: "Foundations/Spacing"
-  };
-  export default meta;
-  
-  type Story = StoryObj;
-  
-  export const Sizes: Story = {
-    render: SpacingSizes
-  };
+  title: "Foundations/Spacing"
+};
+export default meta;
+
+type Story = StoryObj;
+
+export const Sizes: Story = {
+  render: SpacingSizes
+};
