7b3090dbd5a091ff3ce7a9620a303ac95487dcad
Piyush Tilokani <2021.piyush.tilokani@ves.ac.in>
2024-10-19 01:18:45 +0530

    use existing type VirtualizedListItem instead of creating a new type

--- packages/core/src/components/VirtualizedList/__stories__/VirtualizedList.stories.helpers.tsx
+++ packages/core/src/components/VirtualizedList/__stories__/VirtualizedList.stories.helpers.tsx
@@ -2,30 +2,21 @@
 import React from "react";
 import { Tip } from "vibe-storybook-components";
 
-interface Item {
-  value: string;
-  size: number;
-  id: number;
-  height: number;
-}
-
-export const generateItems = (
-  defaultItemSize: number = 30,
-  itemsCount: number,
-  layout: string
-): Item[] => {
-  const items: Item[] = [];
+export const generateItems = (defaultItemSize: number = 30, itemsCount: number, layout: string) => {
+  const items: { value: string; size: number; id: number; height: number }[] = [];
   const isVertical = layout !== "horizontal";
+
   for (let i = 0; i < itemsCount; i++) {
     const size = (i > 0 && i % 15) === 0 ? defaultItemSize * 2 : defaultItemSize;
     items.push({ value: `Item ${i}`, size, id: i, height: isVertical ? size : 30 });
   }
+
   return items;
 };
 
 export const TipItemRenderer: React.FC = () => (
   <Tip title="Are your list items not rendered correctly?">
-    Please make sure you inject the style parameter of the <code>itemRenderer</code> function to the item{" "}
-    {"element's"} wrapper style.
+    Please make sure you inject the style parameter of the <code>itemRenderer</code> function to the item {"element's"}
+    wrapper style.
   </Tip>
 );
