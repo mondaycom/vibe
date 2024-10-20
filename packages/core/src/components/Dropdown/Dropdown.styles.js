bca2c681e622fbd7a6f8291df1c413cc8fc2083f
Malav Shah <mdshah9574@gmail.com>
2024-10-20 18:03:33 +0530

    fix: Dropdown font broken (#2551)

--- packages/core/src/components/Dropdown/Dropdown.styles.js
+++ packages/core/src/components/Dropdown/Dropdown.styles.js
@@ -39,7 +39,11 @@ const getColor = () => {
   return { color, backgroundColor };
 };
 
-const getFont = size => ({ fontSize: getSingleValueTextSize(size), lineHeight: getSingleValueTextSize(size) });
+const getFont = size => ({
+  fontSize: getSingleValueTextSize(size),
+  lineHeight: getSingleValueTextSize(size),
+  fontFamily: getCSSVar("font-family")
+});
 
 const disabledContainerStyle = isDisabled => {
   if (!isDisabled) return {};
