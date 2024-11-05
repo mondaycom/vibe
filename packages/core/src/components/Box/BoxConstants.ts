f044d9b00c6a604ed7abbf881e486cc2611fa7c1
Rivka Ungar <rivkaun@monday.com>
2024-11-05 18:55:32 +0200

    fix(Box): fix enums

--- packages/core/src/components/Box/BoxConstants.ts
+++ packages/core/src/components/Box/BoxConstants.ts
@@ -24,208 +24,208 @@ export enum BorderColor {
  * @deprecated
  */
 export enum Rounded {
-  SMALL = "roundedSmall",
-  MEDIUM = "roundedMedium",
-  BIG = "roundedBig"
+  SMALL = "small",
+  MEDIUM = "medium",
+  BIG = "big"
 }
 
 /**
  * @deprecated
  */
 export enum Shadow {
-  XS = "shadowXs",
-  SMALL = "shadowSmall",
-  MEDIUM = "shadowMedium",
-  LARGE = "shadowLarge"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large"
 }
 
 /**
  * @deprecated
  */
 export enum Margin {
-  AUTO = "mAuto",
-  XS = "mXs",
-  SMALL = "mSmall",
-  MEDIUM = "mMedium",
-  LARGE = "mLarge",
-  XL = "mXl",
-  XXL = "mXxl",
-  XXXL = "mXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum MarginX {
-  AUTO = "mxAuto",
-  XS = "mxXs",
-  SMALL = "mxSmall",
-  MEDIUM = "mxMedium",
-  LARGE = "mxLarge",
-  XL = "mxXl",
-  XXL = "mxXxl",
-  XXXL = "mxXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum MarginY {
-  AUTO = "myAuto",
-  XS = "myXs",
-  SMALL = "mySmall",
-  MEDIUM = "myMedium",
-  LARGE = "myLarge",
-  XL = "myXl",
-  XXL = "myXxl",
-  XXXL = "myXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum MarginTop {
-  AUTO = "mtAuto",
-  XS = "mtXs",
-  SMALL = "mtSmall",
-  MEDIUM = "mtMedium",
-  LARGE = "mtLarge",
-  XL = "mtXl",
-  XXL = "mtXxl",
-  XXXL = "mtXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum MarginEnd {
-  AUTO = "meAuto",
-  XS = "meXs",
-  SMALL = "meSmall",
-  MEDIUM = "meMedium",
-  LARGE = "meLarge",
-  XL = "meXl",
-  XXL = "meXxl",
-  XXXL = "meXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum MarginBottom {
-  AUTO = "mbAuto",
-  XS = "mbXs",
-  SMALL = "mbSmall",
-  MEDIUM = "mbMedium",
-  LARGE = "mbLarge",
-  XL = "mbXl",
-  XXL = "mbXxl",
-  XXXL = "mbXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum MarginStart {
-  AUTO = "msAuto",
-  XS = "msXs",
-  SMALL = "msSmall",
-  MEDIUM = "msMedium",
-  LARGE = "msLarge",
-  XL = "msXl",
-  XXL = "msXxl",
-  XXXL = "msXxxl"
+  AUTO = "auto",
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum Padding {
-  XS = "pXs",
-  SMALL = "pSmall",
-  MEDIUM = "pMedium",
-  LARGE = "pLarge",
-  XL = "pXl",
-  XXL = "pXxl",
-  XXXL = "pXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum PaddingX {
-  XS = "pxXs",
-  SMALL = "pxSmall",
-  MEDIUM = "pxMedium",
-  LARGE = "pxLarge",
-  XL = "pxXl",
-  XXL = "pxXxl",
-  XXXL = "pxXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum PaddingY {
-  XS = "pyXs",
-  SMALL = "pySmall",
-  MEDIUM = "pyMedium",
-  LARGE = "pyLarge",
-  XL = "pyXl",
-  XXL = "pyXxl",
-  XXXL = "pyXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum PaddingTop {
-  XS = "ptXs",
-  SMALL = "ptSmall",
-  MEDIUM = "ptMedium",
-  LARGE = "ptLarge",
-  XL = "ptXl",
-  XXL = "ptXxl",
-  XXXL = "ptXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum PaddingEnd {
-  XS = "peXs",
-  SMALL = "peSmall",
-  MEDIUM = "peMedium",
-  LARGE = "peLarge",
-  XL = "peXl",
-  XXL = "peXxl",
-  XXXL = "peXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum PaddingBottom {
-  XS = "pbXs",
-  SMALL = "pbSmall",
-  MEDIUM = "pbMedium",
-  LARGE = "pbLarge",
-  XL = "pbXl",
-  XXL = "pbXxl",
-  XXXL = "pbXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
  * @deprecated
  */
 export enum PaddingStart {
-  XS = "psXs",
-  SMALL = "psSmall",
-  MEDIUM = "psMedium",
-  LARGE = "psLarge",
-  XL = "psXl",
-  XXL = "psXxl",
-  XXXL = "psXxxl"
+  XS = "xs",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+  XL = "xl",
+  XXL = "xxl",
+  XXXL = "xxxl"
 }
 
 /**
