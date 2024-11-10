d5b1ffe025deebaf0a6bcfc521e862e98690a610
Rivka Ungar <rivkaun@monday.com>
2024-11-10 13:56:44 +0200

    rebuild icons

--- packages/core/src/components/Icon/Icons/components/CollapseRound.tsx
+++ packages/core/src/components/Icon/Icons/components/CollapseRound.tsx
@@ -5,10 +5,10 @@ export interface CollapseRoundProps extends React.SVGAttributes<SVGElement> {
 size?: string | number;
 }
 const CollapseRound: React.FC<CollapseRoundProps> = ({size, ...props}) => (
-  <svg viewBox="0 0 20 21" fill="currentColor" width={ size || "20" } height={ size || "21" } {...props}>
-    <path d="M4.343 4.843a8 8 0 1 1 11.314 11.314A8 8 0 0 1 4.343 4.843ZM10 4.006a6.494 6.494 0 1 0 0 12.988 6.494 6.494 0 0 0 0-12.988Z" fillRule="evenodd" clipRule="evenodd"
+  <svg viewBox="0 0 20 20" fill="currentColor" width={ size || "20" } height={ size || "20" } {...props}>
+    <path d="M4.343 4.343a8 8 0 1 1 11.314 11.314A8 8 0 0 1 4.343 4.343ZM10 3.506a6.494 6.494 0 1 0 0 12.988 6.494 6.494 0 0 0 0-12.988Z" fillRule="evenodd" clipRule="evenodd"
     />
-    <path d="M12.821 6.48a.753.753 0 0 1-.049 1.063l-2.147 1.958a.885.885 0 0 1-1.221.002l-2.16-1.958a.753.753 0 0 1 1.011-1.116l1.757 1.593 1.745-1.591a.753.753 0 0 1 1.064.049Zm-5.629 8.038a.753.753 0 0 1 .052-1.063l2.16-1.958a.885.885 0 0 1 1.221.001l2.147 1.959a.753.753 0 1 1-1.015 1.112l-1.744-1.592-1.758 1.594a.753.753 0 0 1-1.063-.053Z"
+    <path d="M12.821 5.98a.753.753 0 0 1-.049 1.063l-2.147 1.958a.885.885 0 0 1-1.221.002l-2.16-1.958a.753.753 0 0 1 1.011-1.116l1.757 1.593 1.745-1.591a.753.753 0 0 1 1.064.049Zm-5.629 8.038a.753.753 0 0 1 .052-1.063l2.16-1.958a.885.885 0 0 1 1.221.001l2.147 1.959a.753.753 0 1 1-1.015 1.112l-1.744-1.592-1.758 1.594a.753.753 0 0 1-1.063-.053Z"
       fillRule="evenodd" clipRule="evenodd" />
   </svg>
 );
