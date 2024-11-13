717f8c4428a3114cbaa885ee61159102019669c8
Yossi Saadi <yossisa@monday.com>
2024-11-13 14:37:48 +0200

    fix(TransitionView): handle parent without definite height

--- packages/core/src/components/TransitionView/TransitionView.tsx
+++ packages/core/src/components/TransitionView/TransitionView.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import cx from "classnames";
 import { AnimatePresence } from "framer-motion";
 import { TransitionViewProps } from "./TransitionView.types";
@@ -9,18 +9,33 @@ import SlideTransition from "../SlideTransition/SlideTransition";
 
 const TransitionView = forwardRef(
   (
-    { activeStep, direction, id, className, "data-testid": dataTestId, children }: TransitionViewProps,
+    { activeStep, direction, height, id, className, "data-testid": dataTestId, children }: TransitionViewProps,
     ref: React.ForwardedRef<HTMLDivElement>
   ) => {
+    const slideTransitionRef = useRef<HTMLDivElement>(null);
+    const [slideHeight, setSlideHeight] = useState<number>(height);
+
+    useEffect(() => {
+      if (!slideTransitionRef.current) return;
+      setSlideHeight(slideTransitionRef.current.scrollHeight);
+    }, [height, slideTransitionRef]);
+
     return (
       <div
         id={id}
         className={cx(styles.slideshow, className)}
         data-testid={dataTestId || getTestId(ComponentDefaultTestId.TRANSITION_VIEW, id)}
         ref={ref}
+        style={{ height: height ?? slideHeight }}
       >
         <AnimatePresence initial={false} custom={direction}>
-          <SlideTransition key={activeStep} direction={direction}>
+          <SlideTransition
+            key={activeStep}
+            direction={direction}
+            // it must be "auto" on initial load to consider scrollable content in slideHeight calculation
+            style={{ height: height || slideHeight > 0 ? "100%" : "auto" }}
+            ref={slideTransitionRef}
+          >
             {children[activeStep]}
           </SlideTransition>
         </AnimatePresence>
