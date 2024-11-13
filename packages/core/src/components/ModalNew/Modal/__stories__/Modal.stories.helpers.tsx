a2dba1ad53575cb8f7cec193f7d282a2d99922f8
Yossi Saadi <yossisa@monday.com>
2024-11-13 11:30:44 +0200

    docs(Modal): wrapping preview container logic in a decorator to not be seen on story's code

--- packages/core/src/components/ModalNew/Modal/__stories__/Modal.stories.helpers.tsx
+++ packages/core/src/components/ModalNew/Modal/__stories__/Modal.stories.helpers.tsx
@@ -1,34 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../../Button/Button";
 import { StorybookLink, Tip } from "vibe-storybook-components";
 import { useAfterFirstRender } from "../../../../hooks";
+import cx from "classnames";
+import styles from "./Modal.stories.module.scss";
+import { createPortal } from "react-dom";
 
 export const OpenedModalPreview = ({
   onOpenModalClick,
-  large = false,
+  isDocsView,
+  large,
   children: modal
 }: {
   onOpenModalClick: () => void;
+  isDocsView?: boolean;
   large?: boolean;
   children: React.ReactNode;
 }) => {
   const isAfterFirstRender = useAfterFirstRender();
   return (
     <div
-      style={{ paddingInlineStart: 32, paddingBlockStart: 40, height: large ? 540 : 360 }}
+      className={cx(styles.preview, { [styles.large]: large })}
       // workaround to prevent modal from autofocusing on page load
       // autofocus would work as expected when modal closes and reopens
-      {...(!isAfterFirstRender.current && { "data-no-autofocus": true })}
+      {...(!isAfterFirstRender.current && isDocsView && { "data-no-autofocus": true })}
     >
       <Button onClick={onOpenModalClick}>Open Modal</Button>
-      {modal}
+      {isDocsView ? modal : createPortal(modal, document.body)}
     </div>
   );
 };
 
-export const useRemoveModalScrollLock = (show: boolean) => {
+export const useRemoveModalScrollLock = (show: boolean, isDocsView?: boolean) => {
   useEffect(() => {
-    if (show && document.body.attributes.getNamedItem("data-scroll-locked")) {
+    if (show && document.body.attributes.getNamedItem("data-scroll-locked") && isDocsView) {
       document.body.attributes.removeNamedItem("data-scroll-locked");
       document.documentElement.addEventListener(
         "wheel",
@@ -41,6 +46,21 @@ export const useRemoveModalScrollLock = (show: boolean) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps -- this is intended to run once, on mount
   }, []);
 };
+
+export function OpenedModalPreviewDecorator(
+  Story: React.FunctionComponent<{ show: boolean; setShow: (show: boolean) => void }>,
+  { large, isDocsView }: { large?: boolean; isDocsView: boolean }
+) {
+  const [show, setShow] = useState(true);
+  useRemoveModalScrollLock(show, isDocsView); // internal hook, for documentation purposes, to enable scroll on first load
+
+  return (
+    // internal component, for documentation purposes, to open modal inside a container
+    <OpenedModalPreview large={large} onOpenModalClick={() => setShow(true)} isDocsView={isDocsView}>
+      <Story show={show} setShow={setShow} />
+    </OpenedModalPreview>
+  );
+}
 
 export const ModalTip = () => (
   <Tip>
