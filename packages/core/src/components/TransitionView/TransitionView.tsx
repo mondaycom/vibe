356255e21d3dbdd0eccab4a410e86411e62e35e5
Yossi Saadi <yossisa@monday.com>
2024-11-14 09:04:04 +0200

    fix(TransitionView): handle parent without definite height (#2584)

--- packages/core/src/components/TransitionView/TransitionView.tsx
+++ packages/core/src/components/TransitionView/TransitionView.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import cx from "classnames";
 import { AnimatePresence } from "framer-motion";
 import { TransitionViewProps } from "./TransitionView.types";
@@ -9,18 +9,34 @@ import SlideTransition from "../SlideTransition/SlideTransition";
 
 const TransitionView = forwardRef(
   (
-    { activeStep, direction, id, className, "data-testid": dataTestId, children }: TransitionViewProps,
+    { activeStep, direction, height, id, className, "data-testid": dataTestId, children }: TransitionViewProps,
     ref: React.ForwardedRef<HTMLDivElement>
   ) => {
+    const slideTransitionRef = useRef<HTMLDivElement>(null);
+    const [contentHeight, setContentHeight] = useState<number>(height);
+    const slideTransitionHeight = height || contentHeight > 0 ? "100%" : "auto";
+
+    useEffect(() => {
+      if (!slideTransitionRef.current) return;
+      setContentHeight(slideTransitionRef.current.scrollHeight);
+    }, [height, slideTransitionRef]);
+
     return (
       <div
         id={id}
         className={cx(styles.slideshow, className)}
         data-testid={dataTestId || getTestId(ComponentDefaultTestId.TRANSITION_VIEW, id)}
         ref={ref}
+        style={{ height: height ?? contentHeight }}
       >
         <AnimatePresence initial={false} custom={direction}>
-          <SlideTransition key={activeStep} direction={direction}>
+          <SlideTransition
+            key={activeStep}
+            direction={direction}
+            // it must be "auto" on initial load to consider scrollable content in contentHeight calculation
+            style={{ height: slideTransitionHeight }}
+            ref={slideTransitionRef}
+          >
             {children[activeStep]}
           </SlideTransition>
         </AnimatePresence>
--- packages/core/src/components/TransitionView/TransitionView.types.ts
+++ packages/core/src/components/TransitionView/TransitionView.types.ts
@@ -5,6 +5,7 @@ import { SlideDirection } from "../SlideTransition/SlideTransition.types";
 export interface TransitionViewProps extends VibeComponentProps {
   activeStep: number;
   direction: TransitionDirection;
+  height?: number;
   children: React.ReactNode[];
 }
 
