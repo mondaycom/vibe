705021997dcf51f597645839dd069a0eb777cca0
Tal Koren <talko@monday.com>
2024-11-17 12:19:15 +0200

    refactor: use getCoreNextImportsForFile

--- packages/codemod/transformations/core/v2-to-v3/next-imports-migration.ts
+++ packages/codemod/transformations/core/v2-to-v3/next-imports-migration.ts
@@ -1,12 +1,12 @@
 import { CORE_IMPORT_PATH } from "../../../src/consts";
-import { wrap, renameImportPath, getImports } from "../../../src/utils";
+import { wrap, renameImportPath, getCoreNextImportsForFile } from "../../../src/utils";
 import { TransformationContext } from "../../../types";
 
 /**
  * 1. Changes imports from 'monday-ui-react-core/next' to 'monday-ui-react-core'
  */
 function transform({ root }: TransformationContext) {
-  const imports = getImports(root, `${CORE_IMPORT_PATH}/next`);
+  const imports = getCoreNextImportsForFile(root);
 
   imports.forEach(importPath => {
     renameImportPath(importPath, CORE_IMPORT_PATH);
