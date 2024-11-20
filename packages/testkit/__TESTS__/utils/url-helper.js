c307f823ebc8e4fcd2d3cf0c3e41f54fc40b6b64
Yossi Saadi <yossisa@monday.com>
2024-11-20 16:11:58 +0200

    refactor: rename __TESTS__ folder to __tests__, change tests file from .spec.js to .test.js

--- packages/testkit/__TESTS__/utils/url-helper.js
+++ packages/testkit/__TESTS__/utils/url-helper.js
@@ -5,4 +5,3 @@ export const dropdownStory = "/?path=/story/inputs-dropdown--overview";
 export const menuButtonStory = "/?path=/story/buttons-menubutton--overview";
 export const textAreaStory = "/?path=/story/inputs-textarea--overview";
 export const textfieldStory = "/?path=/story/inputs-textfield--overview";
-
