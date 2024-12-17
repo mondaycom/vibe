68c727d13c9a951f8e792334850bd91aac411c42
Yossi Saadi <yossisa@monday.com>
2024-12-17 14:42:23 +0200

    feat(StatusTag): allow New tag

--- packages/storybook-blocks/src/components/status-tag/status-tag.tsx
+++ packages/storybook-blocks/src/components/status-tag/status-tag.tsx
@@ -2,13 +2,14 @@ import { FC } from 'react';
 import styles from './status-tag.module.scss';
 import cx from 'classnames';
 
-export type StatusTagType = 'alpha' | 'beta' | 'deprecated';
+export type StatusTagType = 'alpha' | 'beta' | 'new' | 'deprecated';
 
 interface StatusTagProps {
   type: StatusTagType;
 }
 
 const StatusTag: FC<StatusTagProps> = ({ type }) => {
+  console.log('type', type);
   return <label className={cx(styles.statusTag, styles[type])}>{type}</label>;
 };
 
