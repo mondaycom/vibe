bec20c350f626ec6be761b2d6b11bfb984b8c4c9
Dor Dahuki <dorda@monday.com>
2025-01-21 12:23:40 +0200

    Add widthTolerance prop to useIsOverflowing

--- packages/core/src/hooks/useIsOverflowing/useIsOverflowing.ts
+++ packages/core/src/hooks/useIsOverflowing/useIsOverflowing.ts
@@ -1,11 +1,11 @@
 import { RefObject, useCallback, useState } from "react";
 import useResizeObserver from "../useResizeObserver";
 
-function checkOverflow(element: HTMLElement, ignoreHeightOverflow: boolean, tolerance = 0) {
+function checkOverflow(element: HTMLElement, ignoreHeightOverflow: boolean, tolerance = 0, widthTolerance = 0) {
   if (!element) {
     return false;
   }
-  const isOverflowingWidth = element.clientWidth < element.scrollWidth;
+  const isOverflowingWidth = element.clientWidth + widthTolerance < element.scrollWidth;
   const isOverflowingHeight = !ignoreHeightOverflow && element.clientHeight + tolerance < element.scrollHeight;
   return isOverflowingWidth || isOverflowingHeight;
 }
@@ -13,22 +13,24 @@ function checkOverflow(element: HTMLElement, ignoreHeightOverflow: boolean, tole
 export default function useIsOverflowing({
   ref,
   ignoreHeightOverflow = false,
-  tolerance
+  tolerance,
+  widthTolerance
 }: {
   ref: RefObject<HTMLElement>;
   ignoreHeightOverflow?: boolean;
   tolerance?: number;
+  widthTolerance?: number;
 }) {
   const [isOverflowing, setIsOverflowing] = useState<boolean>(() =>
-    checkOverflow(ref?.current, ignoreHeightOverflow, tolerance)
+    checkOverflow(ref?.current, ignoreHeightOverflow, tolerance, widthTolerance)
   );
   const callback = useCallback(() => {
     const element = ref?.current;
     if (!element) return;
 
-    const newOverflowState = checkOverflow(element, ignoreHeightOverflow, tolerance);
+    const newOverflowState = checkOverflow(element, ignoreHeightOverflow, tolerance, widthTolerance);
     setIsOverflowing(prevState => (prevState !== newOverflowState ? newOverflowState : prevState));
-  }, [ignoreHeightOverflow, ref, tolerance]);
+  }, [ignoreHeightOverflow, ref, tolerance, widthTolerance]);
 
   useResizeObserver({
     ref,
