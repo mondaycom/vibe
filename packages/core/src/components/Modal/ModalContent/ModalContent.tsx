1ed3acdc5914a0a8b0d1df08af661f8419f155f4
Yossi Saadi <yossisa@monday.com>
2025-01-26 13:24:46 +0200

    feat(Modal): pass autoFocus value to ModalContent to prevent autofocus on content

--- packages/core/src/components/Modal/ModalContent/ModalContent.tsx
+++ packages/core/src/components/Modal/ModalContent/ModalContent.tsx
@@ -2,19 +2,21 @@ import React, { forwardRef } from "react";
 import { getTestId } from "../../../tests/test-ids-utils";
 import { ComponentDefaultTestId } from "../../../tests/constants";
 import { ModalContentProps } from "./ModalContent.types";
+import { useModal } from "../context/ModalContext";
 
 const ModalContent = forwardRef(
   (
     { children, className, id, "data-testid": dataTestId }: ModalContentProps,
     ref: React.ForwardedRef<HTMLDivElement>
   ) => {
+    const { autoFocus } = useModal();
     return (
       <div
         ref={ref}
         className={className}
         id={id}
         data-testid={dataTestId || getTestId(ComponentDefaultTestId.MODAL_NEXT_CONTENT, id)}
-        data-autofocus-inside={true}
+        data-autofocus-inside={autoFocus}
       >
         {children}
       </div>
