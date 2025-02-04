ec933458ff1f1abbb4de1286ba03e3dec05814e5
tomer gillmore <tomergi@monday.com>
2025-02-04 12:44:34 +0200

    fix lint

--- packages/core/src/components/Tabs/Tab/Tab.tsx
+++ packages/core/src/components/Tabs/Tab/Tab.tsx
@@ -1,5 +1,5 @@
 import cx from "classnames";
-import React, { FC, forwardRef, ReactElement, useMemo, useRef } from "react";
+import React, { FC, forwardRef, ReactElement, useRef } from "react";
 import { noop as NOOP } from "lodash-es";
 import useMergeRef from "../../../hooks/useMergeRef";
 import { getStyle } from "../../../helpers/typesciptCssModulesHelper";
