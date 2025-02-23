5fec6c6dc57bd5d392576f45f418106f1851b32b
Itay Mastor <itaym@monday.com>
2025-02-23 14:54:48 +0200

    Only fix when isSafari

--- packages/core/src/utils/user-agent-utils.ts
+++ packages/core/src/utils/user-agent-utils.ts
@@ -2,6 +2,11 @@ export function isFirefox() {
   return !!/Firefox\/([0-9.]+)(?:\s|$)/.exec(window.navigator.userAgent);
 }
 
+export function isSafari() {
+  // NOTE - Chrome userAgent contains Safari, so we need to exclude it (see: https://stackoverflow.com/a/12799309)
+  return !!/^(?!.*Chrome).*Safari\/([0-9.]+)(?:\s|$)/.exec(window.navigator.userAgent);
+}
+
 export function isIE11() {
   return !!window.MSInputMethodContext && !!document.documentMode;
 }
