0e191a99bc99109d93cac6a9f89a2ccf28656dca
Tal Koren <talko@monday.com>
2025-03-17 16:16:35 +0200

    docs: align Editable components in the catalog

--- packages/core/src/storybook/components/related-components/descriptions/editable-heading-description.tsx
+++ packages/core/src/storybook/components/related-components/descriptions/editable-heading-description.tsx
@@ -1,13 +1,14 @@
 import React, { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
 import EditableHeading from "../../../../components/EditableHeading/EditableHeading";
+import Flex from "../../../../components/Flex/Flex";
 
 export const EditableHeadingDescription = () => {
   const component = useMemo(() => {
     return (
-      <div>
+      <Flex justify="center" style={{ width: "100%" }}>
         <EditableHeading value="Hello world" />
-      </div>
+      </Flex>
     );
   }, []);
   return (
