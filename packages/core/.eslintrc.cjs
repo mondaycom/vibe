ecadd77ef6c3c7eb5fe04eb81311c6ac617fa2f0
Yossi Saadi <yossisa@monday.com>
2025-04-22 11:00:54 +0300

    build: allow dev deps in scripts, add scripts dir to tsconfig

--- packages/core/.eslintrc.cjs
+++ packages/core/.eslintrc.cjs
@@ -51,7 +51,7 @@ const commonRules = {
   "arrow-parens": "off",
   "lodash/import-scope": [2, "member"],
   "implicit-arrow-linebreak": "off",
-  "import/no-extraneous-dependencies": ["error", { devDependencies: true }]
+  "import/no-extraneous-dependencies": ["error", { devDependencies: ["**/scripts/**/*.ts"] }]
 };
 const commonPlugins = ["import", "lodash", "react", "jsx-a11y", "json", "markdown", "jest"];
 const commonExtends = ["plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:prettier/recommended"];
