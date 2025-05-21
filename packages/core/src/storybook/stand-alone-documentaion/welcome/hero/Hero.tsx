9e976fa7ec3a5dcf670b5cb06465ee1476180404
Tal Koren <talko@monday.com>
2025-05-21 14:27:27 +0300

    docs: home page hero section

---
+++ packages/core/src/storybook/stand-alone-documentaion/welcome/hero/Hero.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./Hero.module.scss";
+import HeartIcon from "./icons/HeartIcon";
+import ArrowIcon from "./icons/ArrowIcon";
+import FigmaIcon from "./icons/FigmaIcon";
+import GitHubIcon from "./icons/GitHubIcon";
+import ResourceButton from "./ResourceButton";
+import Flex from "../../../../components/Flex/Flex";
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles.header}>
+        <Flex align="center" justify="center" gap="medium">
+          Vibe design <HeartIcon /> system
+        </Flex>
+        <Flex align="center" justify="center" gap="large">
+          by <ArrowIcon /> monday.com
+        </Flex>
+      </h1>
+      <p className={styles.subheader}>
+        Foundations, components, patterns and processes are what experiences are made of.
+      </p>
+      <Flex gap="small">
+        <ResourceButton
+          text="Figma"
+          icon={<FigmaIcon />}
+          href="https://www.figma.com/file/pTpO3Wobs5cQwRsabt21Nd/monday.com-UI-kit?node-id=14811%3A20455"
+        />
+        <ResourceButton text="GitHub" icon={<GitHubIcon />} href="https://github.com/mondaycom/vibe" />
+      </Flex>
+    </div>
+  );
+}
---
+++ packages/core/src/storybook/stand-alone-documentaion/welcome/hero/icons/ArrowIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ArrowIcon() {
+  return (
+    <svg width="146" height="89" viewBox="0 0 146 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M142.191 52.8681L107.627 41.1405M142.191 52.8681L114.507 85.239M142.191 52.8681C50.1209 94.2659 34.4833 12.7837 63.7408 4.73466C92.9982 -3.31434 96.5293 100.303 4.0957 57.5865"
+        stroke="#FF5A85"
+        strokeWidth="7.14286"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
