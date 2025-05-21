726f05a1591b1c53d5dc30fdcead8b0ff250151d
Tal Koren <talko@monday.com>
2025-05-21 14:21:33 +0300

    docs: add classname

--- packages/core/src/storybook/stand-alone-documentaion/welcome/Title.tsx
+++ packages/core/src/storybook/stand-alone-documentaion/welcome/Title.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Title.module.scss";
+import cx from "classnames";
 
-export default function Title({ children }: { children: React.ReactNode }) {
-  return <h3 className={styles.title}>{children}</h3>;
+export default function Title({ children, className }: { children: React.ReactNode; className?: string }) {
+  return <h3 className={cx(styles.title, className)}>{children}</h3>;
 }
