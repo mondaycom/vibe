fe4beef28d7597300fda02cec9c1c0ddf1b1b421
Yossi Saadi <yossisa@monday.com>
2025-06-22 17:16:35 +0300

    docs(Link): update story structure and typings for improved clarity

--- packages/core/src/components/Link/__stories__/Link.stories.tsx
+++ packages/core/src/components/Link/__stories__/Link.stories.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Link from "../Link";
+import { Meta, StoryObj } from "@storybook/react";
+import Link, { LinkProps } from "../Link";
 import { createStoryMetaSettingsDecorator } from "../../../storybook";
 import { ExternalPage, Info, Link as IconLink } from "@vibe/icons";
-import { createComponentTemplate } from "vibe-storybook-components";
 import "./Link.stories.scss";
 
 const metaSettings = createStoryMetaSettingsDecorator({
@@ -15,18 +15,12 @@ export default {
   component: Link,
   argTypes: metaSettings.argTypes,
   decorators: metaSettings.decorators
-};
+} satisfies Meta<typeof Link>;
 
-const linkTemplate = createComponentTemplate(Link);
+type Story = StoryObj<typeof Link>;
 
-export const Overview = {
-  render: linkTemplate.bind({}),
-  name: "Overview",
-
-  args: {
-    text: "Read more",
-    href: "https://www.monday.com"
-  },
+export const Overview: Story = {
+  render: (args: LinkProps) => <Link text="Read more" href="https://www.monday.com" {...args} />,
   parameters: {
     docs: {
       liveEdit: {
@@ -36,12 +30,11 @@ export const Overview = {
   }
 };
 
-export const States = {
-  render: () => <Link text="Default" href="https://www.monday.com" />,
-  name: "States"
+export const States: Story = {
+  render: () => <Link text="Default" href="https://www.monday.com" />
 };
 
-export const RightToLeft = {
+export const RightToLeft: Story = {
   render: () => (
     <>
       <Link text="اقرأ أكثر" href="https://www.monday.com" icon={IconLink} />
@@ -57,7 +50,7 @@ export const RightToLeft = {
   }
 };
 
-export const WithIcons = {
+export const WithIcons: Story = {
   render: () => (
     <>
       <Link text="Read more" href="https://www.monday.com" icon={ExternalPage} />
@@ -73,7 +66,7 @@ export const WithIcons = {
   }
 };
 
-export const ReferenceLink = {
+export const ReferenceLink: Story = {
   render: () => (
     <div>
       {`Lorem Ipsum has been the industry's `}
@@ -83,7 +76,7 @@ export const ReferenceLink = {
   )
 };
 
-export const ShorteningTexts = {
+export const ShorteningTexts: Story = {
   render: () => (
     <div>
       {`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
