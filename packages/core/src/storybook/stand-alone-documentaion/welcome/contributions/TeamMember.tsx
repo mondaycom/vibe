d9001811a1b617dffb4c9e9e3fb89572a0bdb317
Tal Koren <talko@monday.com>
2025-06-29 12:12:03 +0300

    docs: fix team members images

--- packages/core/src/storybook/stand-alone-documentaion/welcome/contributions/TeamMember.tsx
+++ packages/core/src/storybook/stand-alone-documentaion/welcome/contributions/TeamMember.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cx from "classnames";
 import styles from "./TeamMember.module.scss";
 import Text from "../../../../components/Text/Text";
 import Flex from "../../../../components/Flex/Flex";
@@ -8,20 +9,21 @@ export interface TeamMemberProps {
   name: string;
   title: string;
   linkedinUrl: string;
+  className?: string;
 }
 
-export const TeamMember = ({ image, name, title, linkedinUrl }: TeamMemberProps) => {
+export const TeamMember = ({ image, name, title, linkedinUrl, className }: TeamMemberProps) => {
   return (
-    <Flex className={styles.teamMember} direction="column" gap="large" align="start">
-      <img src={image} alt={name} className={styles.image} />
-      <Flex className={styles.content} direction="column">
-        <a href={linkedinUrl} className={styles.link} target="_blank" rel="noreferrer">
+    <a href={linkedinUrl} className={cx(styles.teamMember, className)} target="_blank" rel="noreferrer">
+      <Flex direction="column" gap="large" align="start">
+        <img src={image} alt={name} className={styles.image} />
+        <Flex className={styles.content} direction="column">
           <Text type="text1" weight="medium" className={styles.name}>
             {name}
           </Text>
-        </a>
-        <Text className={styles.title}>{title}</Text>
+          <Text className={styles.title}>{title}</Text>
+        </Flex>
       </Flex>
-    </Flex>
+    </a>
   );
 };
