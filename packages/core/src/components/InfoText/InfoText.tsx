a2f737c1ca2722015da7ff413c5b75ba72669949
Yossi Saadi <yossisa@monday.com>
2025-07-02 11:33:03 +0300

    feat(InfoText): add InfoText component with encapsulation of styling and accessibility features

---
+++ packages/core/src/components/InfoText/InfoText.tsx
@@ -0,0 +1,33 @@
+import React, { forwardRef } from "react";
+import cx from "classnames";
+import { InfoTextProps } from "./InfoText.types";
+import Text from "../Text/Text";
+import styles from "./InfoText.module.scss";
+
+const InfoText = forwardRef(
+  ({ id, text, error, success, disabled, readOnly }: InfoTextProps, ref: React.ForwardedRef<HTMLDivElement>) => {
+    if (!text) {
+      return null;
+    }
+
+    return (
+      <Text
+        id={id}
+        ref={ref}
+        className={cx(styles.infoText, {
+          [styles.error]: error,
+          [styles.success]: success,
+          [styles.disabled]: disabled,
+          [styles.readOnly]: readOnly
+        })}
+        type={Text.types.TEXT2}
+      >
+        {text}
+      </Text>
+    );
+  }
+);
+
+InfoText.displayName = "InfoText";
+
+export default InfoText;
