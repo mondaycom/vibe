1ead36eef79277a0b3bb614d2653c5fea297c8c6
Yossi Saadi <yossisa@monday.com>
2025-07-08 01:36:50 +0300

    docs: use new AttentionBox in component catalog

--- packages/core/src/storybook/components/related-components/descriptions/attention-box-description.jsx
+++ packages/core/src/storybook/components/related-components/descriptions/attention-box-description.jsx
@@ -1,14 +1,11 @@
 import React, { useMemo } from "react";
 import { RelatedComponent } from "vibe-storybook-components";
-import AttentionBox from "../../../../components/LegacyAttentionBox/LegacyAttentionBox";
+import AttentionBox from "../../../../components/AttentionBox/AttentionBox";
 
 export const AttentionBoxDescription = () => {
   const component = useMemo(() => {
-    const style = {
-      width: "90%"
-    };
     return (
-      <div style={style}>
+      <div style={{ width: "90%" }}>
         <AttentionBox
           title="Attention box title"
           text="Studies show that 100% of people who celebrate birthdays, will die."
@@ -20,7 +17,7 @@ export const AttentionBoxDescription = () => {
     <RelatedComponent
       component={component}
       title="AttentionBox"
-      href="/?path=/docs/components-attentionbox--docs"
+      href="/?path=/docs/components-attentionbox-new--docs"
       description="Displays content classification."
     />
   );
