5f66f02f73cd45727ba1793182f9e3e345b7aa31
Or Halag <orhal@monday.com>
2025-07-20 11:25:10 +0300

    removed unit tests from branch

---
+++ packages/testkit/__tests__/checkbox.test.js
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+import { Checkbox } from "../components";
+import { checkboxStory } from "./utils/url-helper";
+
+test.describe.fixme("menuButton Class with Storybook", () => {
+  let checkbox;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(checkboxStory);
+    const frame = page.frameLocator("[id='storybook-preview-iframe']");
+    const checkboxLocator = frame.locator('[data-testid="checkbox-checkbox"]');
+    checkbox = new Checkbox(page, checkboxLocator, "Test checkbox button");
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  test("set checkbox", async () => {
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    if (await checkbox.isChecked()) {
+      await checkbox.setChecked(false);
+      // eslint-disable-next-line playwright/prefer-web-first-assertions, playwright/no-conditional-expect
+      expect(await checkbox.isChecked()).toBe(false);
+    } else {
+      await checkbox.setChecked(true);
+      // eslint-disable-next-line playwright/prefer-web-first-assertions, playwright/no-conditional-expect
+      expect(await checkbox.isChecked()).toBe(true);
+    }
+  });
+});
---
+++ packages/testkit/__tests__/utils/url-helper.js
@@ -0,0 +1,7 @@
+export const buttonStory = "/?path=/story/components-button--overview";
+export const buttonGroupStory = "/?path=/story/components-buttongroup--overview";
+export const checkboxStory = "/?path=/story/components-checkbox--overview";
+export const dropdownStory = "/?path=/story/components-dropdown--overview";
+export const menuButtonStory = "/?path=/story/components-menubutton--overview";
+export const textAreaStory = "/?path=/story/components-textarea--overview";
+export const textfieldStory = "/?path=/story/components-textfield--overview";
