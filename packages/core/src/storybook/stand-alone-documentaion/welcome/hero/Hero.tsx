f23a02a6171d3997bea4bf9fb8b8ace98b509000
Tal Koren <talko@monday.com>
2025-07-22 09:22:55 +0300

    docs: docs revamp (#2900)
    Co-authored-by: Rivka Ungar <rivkaun@monday.com>

---
+++ packages/core/src/storybook/stand-alone-documentaion/welcome/hero/Hero.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./Hero.module.scss";
+import FigmaIcon from "./icons/FigmaIcon";
+import GitHubIcon from "./icons/GitHubIcon";
+import ResourceButton from "./ResourceButton";
+import Flex from "../../../../components/Flex/Flex";
+import EntranceAnimation from "../EntranceAnimation";
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.content}>
+        <EntranceAnimation delay={0}>
+          <h1 className={styles.header}>
+            <Flex align="center" justify="center" gap="medium">
+              Vibe design system
+            </Flex>
+            <Flex align="center" justify="center" gap="medium">
+              by monday.com
+            </Flex>
+          </h1>
+        </EntranceAnimation>
+
+        <EntranceAnimation delay={0.1}>
+          <p className={styles.subheader}>Resources for building great monday.com experiences.</p>
+        </EntranceAnimation>
+
+        <EntranceAnimation delay={0.2}>
+          <Flex gap="small">
+            <ResourceButton
+              text="GitHub"
+              icon={<GitHubIcon />}
+              href="https://github.com/mondaycom/vibe"
+              tooltipContent="Become a contributor & join our GitHub community"
+            />
+            <ResourceButton
+              text="Figma"
+              icon={<FigmaIcon />}
+              href="https://www.figma.com/file/pTpO3Wobs5cQwRsabt21Nd/monday.com-UI-kit?node-id=14811%3A20455"
+              tooltipContent="Our Figma file is free to use"
+            />
+          </Flex>
+        </EntranceAnimation>
+      </div>
+    </div>
+  );
+}
