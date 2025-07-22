a0962e57a0e822df771d353bce05e48c84dbb13d
Rivka Ungar <rivkaun@monday.com>
2025-07-22 17:58:49 +0300

    docs(counter): cleanup stories

--- packages/core/src/components/Counter/__stories__/Counter.stories.tsx
+++ packages/core/src/components/Counter/__stories__/Counter.stories.tsx
@@ -1,46 +1,26 @@
 import React from "react";
 import { useCallback, useEffect, useState } from "react";
-import { createComponentTemplate } from "vibe-storybook-components";
 import Counter from "../Counter";
 import { createStoryMetaSettingsDecorator } from "../../../storybook";
 import Divider from "../../Divider/Divider";
 import { AddUpdate, Update, Notifications } from "@vibe/icons";
 import Icon from "../../Icon/Icon";
 import Avatar from "../../Avatar/Avatar";
-import "./Counter.stories.scss";
+import Flex from "../../Flex/Flex";
 
 const metaSettings = createStoryMetaSettingsDecorator({
   component: Counter
 });
 
-const counterTemplate = createComponentTemplate(Counter);
-
 export default {
   title: "Components/Counter",
   component: Counter,
-  argTypes: {
-    ...metaSettings.argTypes,
-    wrapperClassName: {
-      table: {
-        disable: true
-      }
-    },
-    "data-testid": {
-      table: {
-        disable: true
-      }
-    }
-  },
   decorators: metaSettings.decorators
 };
 
 export const Overview = {
-  render: counterTemplate.bind({}),
+  render: () => <Counter count={5} />,
   name: "Overview",
-
-  args: {
-    count: 5
-  },
   parameters: {
     docs: {
       liveEdit: {
@@ -52,115 +32,96 @@ export const Overview = {
 
 export const Sizes = {
   render: () => (
-    <>
-      <div className="storybook-counter_column">
+    <Flex gap={160}>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} size="xs" />
         XS
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} size="small" />
         Small
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} />
         Large
-      </div>
-    </>
+      </Flex>
+    </Flex>
   )
 };
 
 export const Colors = {
   render: () => (
-    <>
-      <div className="storybook-counter_column">
+    <Flex gap={160}>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} />
         Primary
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} color="negative" />
         Negative or notification
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} color="dark" />
         Dark
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} color="light" />
         Light
-      </div>
-    </>
+      </Flex>
+    </Flex>
   )
 };
 
 export const Outline = {
   render: () => (
-    <>
-      <div className="storybook-counter_column">
+    <Flex gap={160}>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} kind="line" />
         Primary
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} color="negative" kind="line" />
         Negative or notification
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} color="dark" kind="line" />
         Dark
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={5} color="light" kind="line" />
         Light
-      </div>
-    </>
+      </Flex>
+    </Flex>
   )
 };
 
 export const Limits = {
   render: () => (
-    <>
-      <div className="storybook-counter_column">
+    <Flex gap={160}>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={10} maxDigits={1} />
         One digit limit
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={100} maxDigits={2} />
         Two digits limit
-      </div>
-      <div className="storybook-counter_column">
+      </Flex>
+      <Flex direction="column" gap="large" style={{ width: "100px" }} align="start">
         <Counter count={1000} />
         Three digits limit
-      </div>
-    </>
+      </Flex>
+    </Flex>
   )
 };
 
 export const NotificationCounter = {
   render: () => {
-    const maxCount = 10;
-    const initialCount = 4;
-    const [count, setCount] = useState(4);
-
-    const changeCountCallback = useCallback(() => {
-      const newCount = count === maxCount ? initialCount : count + 1;
-      setCount(newCount);
-    }, [count, setCount]);
-
-    useEffect(() => {
-      setCount(initialCount);
-    }, [initialCount, setCount]);
-
-    useEffect(() => {
-      const interval = setInterval(changeCountCallback, 1000);
-
-      return () => {
-        clearInterval(interval);
-      };
-    }, [changeCountCallback]);
-
     return (
-      <div className="storybook-counter_position">
+      <div style={{ position: "relative" }}>
         <Avatar type="icon" icon={Notifications} backgroundColor="royal" />
-        <Counter count={count} maxDigits={1} color="negative" className="storybook-counter_counter-position-top" />
+        <div style={{ position: "absolute", top: "-5px", right: "-5px" }}>
+          <Counter count={5} maxDigits={1} color="negative" />
+        </div>
       </div>
     );
   },
@@ -175,36 +136,40 @@ export const NotificationCounter = {
 
 export const CounterOnInboxFilters = {
   render: () => (
-    <>
-      <div className="storybook-counter_wrapper">
+    <Flex gap={28}>
+      <Flex direction="column" gap="large" align="start">
         <span className="a">UX Writing & microcopy Re...</span>
         <span className="a"> Mobile Data- Iteration Plan...</span>
         <span className="a">Q Plans.</span>
-      </div>
-      <div className="storybook-counter_wrapper">
+      </Flex>
+      <Flex direction="column" gap="large" align="start">
         <Counter count={195} color="dark" />
         <Counter count={141} color="dark" />
         <Counter count={99} color="dark" />
-      </div>
-    </>
+      </Flex>
+    </Flex>
   )
 };
 
 export const CountTheNumberOfUpdates = {
   render: () => (
-    <div className="storybook-counter_icon-wrapper">
+    <Flex gap={12} direction="column" align="start">
       <Icon icon={AddUpdate} iconSize="36" />
       <Divider />
-      <div className="storybook-counter_position">
+      <div style={{ position: "relative" }}>
         <Icon icon={Update} iconSize="36" />
-        <Counter count={5} size="small" className="storybook-counter_counter-position-bot" />
+        <div style={{ position: "absolute", bottom: 0, right: -3 }}>
+          <Counter count={5} size="small" />
+        </div>
       </div>
       <Divider />
-      <div className="storybook-counter_position">
+      <div style={{ position: "relative" }}>
         <Icon icon={Update} iconSize="36" />
-        <Counter count={5} color="dark" size="small" className="storybook-counter_counter-position-bot" />
+        <div style={{ position: "absolute", bottom: 0, right: -3 }}>
+          <Counter count={5} color="dark" size="small" />
+        </div>
       </div>
-    </div>
+    </Flex>
   ),
   parameters: {
     docs: {
