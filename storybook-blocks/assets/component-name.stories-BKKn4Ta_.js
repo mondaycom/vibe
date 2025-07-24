51c8edd38cad80168cc17cd8037a01f024519423
talkor <talkor@users.noreply.github.com>
2025-07-24 06:13:20 +0000

    Deploying to gh-pages from @ mondaycom/vibe@8bddf7eb9b71935c3d4d3746dc2a4df114826d7a 🚀

---
+++ storybook-blocks/assets/component-name.stories-BKKn4Ta_.js
@@ -0,0 +1,5 @@
+import{C as t}from"./component-name-DlshIJwS.js";const a={component:t,title:"Components/ComponentName"},e={args:{children:"Component Name"}};var o,r,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Component Name'
+  }
+}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const m=["Overview"],p=Object.freeze(Object.defineProperty({__proto__:null,Overview:e,__namedExportsOrder:m,default:a},Symbol.toStringTag,{value:"Module"}));export{p as C,e as O};
