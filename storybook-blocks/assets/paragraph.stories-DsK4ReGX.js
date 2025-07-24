d28123ea2d2dee04d182c6c764a1f9c9594a0c83
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-07-24 15:31:46 +0000

    Deploying to gh-pages from @ mondaycom/vibe@a9c1cada7ab38494365a312769e3b347971929a0 🚀

---
+++ storybook-blocks/assets/paragraph.stories-DsK4ReGX.js
@@ -0,0 +1,5 @@
+import{P as s}from"./paragraph-y8K-Wwnt.js";const t={component:s,title:"Components/Paragraph"},r={args:{children:"This is a paragraph"}};var a,e,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'This is a paragraph'
+  }
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const n=["Overview"],c=Object.freeze(Object.defineProperty({__proto__:null,Overview:r,__namedExportsOrder:n,default:t},Symbol.toStringTag,{value:"Module"}));export{r as O,c as P};
---
+++ storybook-blocks/assets/react-16-DcpGWsab.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-B3l35gtK.js";import"./index-CeXuxf-f.js";var p=async(e,r)=>new Promise(o=>{n.render(e,r,()=>o(null))}),u=e=>{n.unmountComponentAtNode(e)};export{p as renderElement,u as unmountElement};
