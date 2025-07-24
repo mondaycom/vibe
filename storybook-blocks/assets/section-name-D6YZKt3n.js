a4c5506941b09e2be30ed359b737b458e0c9f6ee
talkor <talkor@users.noreply.github.com>
2025-07-24 10:24:03 +0000

    Deploying to gh-pages from @ mondaycom/vibe@309cfab751b8f687b58a1a6e3dcba5b7f1383f41 🚀

---
+++ storybook-blocks/assets/section-name-D6YZKt3n.js
@@ -0,0 +1,7 @@
+import{j as e}from"./jsx-runtime-B8889x5w.js";import{useMDXComponents as r}from"./index-ZXiX6yWz.js";import{M as i,C as s,a}from"./index-ClG681zV.js";import{S as c,O as m}from"./section-name.stories-COv32GTK.js";import"./index-CeXuxf-f.js";import"./iframe-BDhklINK.js";import"./index-B3l35gtK.js";import"./index-B0HCylqX.js";import"./index-DrFu-skq.js";import"./section-name-B8AuDv3Q.js";import"./index-De0AmtSO.js";function t(n){const o={code:"code",h1:"h1",h2:"h2",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(o.h1,{id:"sectionname",children:"SectionName"}),`
+`,e.jsx(o.h2,{id:"overview",children:"Overview"}),`
+`,e.jsxs(o.p,{children:["SectionName is a component that display the name of a section. This component can be used as a replacement for ",e.jsx(o.code,{children:"h2"})," in storybook ",e.jsx(o.code,{children:"preview"})," file."]}),`
+`,e.jsx(s,{of:m}),`
+`,e.jsx(o.h2,{id:"props",children:"Props"}),`
+`,e.jsx(a,{})]})}function w(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t(n)}export{w as default};
---
+++ storybook-blocks/assets/token-table-BmZdAz-d.js
@@ -0,0 +1,7 @@
+import{j as n}from"./jsx-runtime-B8889x5w.js";import{useMDXComponents as s}from"./index-ZXiX6yWz.js";import{M as r,C as i,a}from"./index-ClG681zV.js";import{T as m,O as p}from"./token-table.stories-ijvHOsJX.js";import"./index-CeXuxf-f.js";import"./iframe-BDhklINK.js";import"./index-B3l35gtK.js";import"./index-B0HCylqX.js";import"./index-DrFu-skq.js";function t(e){const o={h1:"h1",h2:"h2",p:"p",...s(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:m}),`
+`,n.jsx(o.h1,{id:"tokentable",children:"TokenTable"}),`
+`,n.jsx(o.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx(o.p,{children:"TokenTable component is used to display a table with rows and columns and is designed to display tokens values."}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(o.h2,{id:"props",children:"Props"}),`
+`,n.jsx(a,{})]})}function T(e={}){const{wrapper:o}={...s(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{T as default};
