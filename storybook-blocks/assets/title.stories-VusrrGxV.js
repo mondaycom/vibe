9755b48d618886c2b4a2c5444ca2a9ca30ddcfa1
talkor <talkor@users.noreply.github.com>
2025-07-27 09:33:33 +0000

    Deploying to gh-pages from @ mondaycom/vibe@6f0eb18d62701365ea366ef24382062c8f5cd380 🚀

---
+++ storybook-blocks/assets/title.stories-VusrrGxV.js
@@ -0,0 +1,5 @@
+import{T as s}from"./title-ByCKPGaa.js";const a={component:s,title:"Components/Title"},e={args:{children:"This is a story title"}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'This is a story title'
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const i=["Overview"],c=Object.freeze(Object.defineProperty({__proto__:null,Overview:e,__namedExportsOrder:i,default:a},Symbol.toStringTag,{value:"Module"}));export{e as O,c as T};
---
+++ storybook-blocks/assets/unstyled-list.stories-D9iBMUBr.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-B8889x5w.js";import{U as m,a as s}from"./unstyled-list-item-UBZBKk-g.js";const a={component:m,title:"Components/UnstyledList"},e={args:{children:t.jsxs(t.Fragment,{children:[t.jsx(s,{children:"Item one"},"1"),t.jsx(s,{children:"Item two"},"2")]})}};var n,r,o;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <UnstyledListItem key="1">Item one</UnstyledListItem>
+        <UnstyledListItem key="2">Item two</UnstyledListItem>
+      </>
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const i=["Overview"],c=Object.freeze(Object.defineProperty({__proto__:null,Overview:e,__namedExportsOrder:i,default:a},Symbol.toStringTag,{value:"Module"}));export{e as O,c as U};
