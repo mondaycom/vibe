9dd0c514307789192a235e0f837abfde84aec562
talkor <talkor@users.noreply.github.com>
2025-07-27 08:39:28 +0000

    Deploying to gh-pages from @ mondaycom/vibe@15a6764249f6347108a2926866e6649c4267bb59 🚀

---
+++ storybook-blocks/assets/link.stories-Brzesgpe.js
@@ -0,0 +1,7 @@
+import{L as n}from"./link-Dv_XKWiw.js";const t={component:n,title:"Components/Link"},e={args:{href:"/?path=/docs/welcome--docs",size:n.sizes.MEDIUM,children:"This is a link to welcome page"}};var o,s,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    href: '/?path=/docs/welcome--docs',
+    size: Link.sizes.MEDIUM,
+    children: 'This is a link to welcome page'
+  }
+}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const a=["Overview"],c=Object.freeze(Object.defineProperty({__proto__:null,Overview:e,__namedExportsOrder:a,default:t},Symbol.toStringTag,{value:"Module"}));export{c as L,e as O};
---
+++ storybook-blocks/assets/usage-guidelines.stories-CdecUc4M.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-B8889x5w.js";import{U as t}from"./usage-guidelines-Bcxv2eJy.js";import{L as o}from"./link-Dv_XKWiw.js";const a={component:t,title:"Components/UsageGuidelines"},s={args:{guidelines:["Usage guideline #1",e.jsx(e.Fragment,{children:e.jsx("b",{children:"Usage guideline #2 - with JSX element"})}),e.jsxs(e.Fragment,{children:["Usage guideline #3 -",e.jsx(o,{href:"/?path=/docs/welcome--docs",children:"with link"})]})]}};var i,r,n;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    guidelines: ['Usage guideline #1', <>
+        <b>Usage guideline #2 - with JSX element</b>
+      </>, <>
+        Usage guideline #3 -<Link href="/?path=/docs/welcome--docs">with link</Link>
+      </>]
+  }
+}`,...(n=(r=s.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const l=["Overview"],m=Object.freeze(Object.defineProperty({__proto__:null,Overview:s,__namedExportsOrder:l,default:a},Symbol.toStringTag,{value:"Module"}));export{s as O,m as U};
