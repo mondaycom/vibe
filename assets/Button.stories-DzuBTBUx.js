2c1624eb567dd4106834a9f726682212b541b908
talkor <talkor@users.noreply.github.com>
2025-08-03 14:23:24 +0000

    Deploying to gh-pages from @ mondaycom/vibe@baae40bd313ab6efb0b61eeae40d1fbcfcfa37c1 🚀

---
+++ assets/Button.stories-DzuBTBUx.js
@@ -0,0 +1,160 @@
+import{j as e}from"./jsx-runtime-DDzbWKUH.js";import{r as y}from"./index-Hemj67b4.js";import{c as ne}from"./createStoryMetaSettingsDecorator-Dxrg_MSB.js";import{B as r}from"./Button-DgBL92G0.js";import{T as g}from"./Text-BIudgOyK.js";import{r as oe}from"./createComponentTemplate-Y0VTmW_y.js";import{r as h}from"./Calendar-BX7mDXzx.js";import{r as v}from"./Check-B_icfwyn.js";import{i as S}from"./index-BA_MN9l1.js";import{r as b}from"./Remove-C7LZQ9Te.js";const j=ne({component:r,iconPropNamesArray:["leftIcon","rightIcon","successIcon"],actionPropsArray:["onClick"]}),te={title:"Components/Button",component:r,argTypes:j.argTypes,decorators:j.decorators},se=oe(r),o={render:se.bind({}),args:{children:"Button"},parameters:{docs:{liveEdit:{isEnabled:!1}}}},t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{children:"Primary"}),e.jsx(r,{kind:"secondary",children:"Secondary"}),e.jsx(r,{kind:"tertiary",children:"Tertiary"})]}),name:"Button kinds"},s={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{size:"large",children:"Large"}),e.jsx(r,{size:"medium",children:"Medium"}),e.jsx(r,{size:"small",children:"Small"})]})},a={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{disabled:!0,children:"Primary"}),e.jsx(r,{kind:"secondary",disabled:!0,children:"Secondary"}),e.jsx(r,{kind:"tertiary",disabled:!0,children:"Tertiary"})]})},c={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{children:"Regular"}),e.jsx(r,{active:!0,children:"Active"})]})},i={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{color:"positive",children:"Positive"}),e.jsx(r,{color:"negative",children:"Negative"})]}),name:"Positive and Negative"},d={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{rightIcon:h,children:"Right icon"}),e.jsx(r,{leftIcon:h,children:"Left icon"})]}),parameters:{docs:{liveEdit:{scope:{Calendar:h}}}}},l={render:()=>{const[B,n]=y.useState(!1),x=y.useCallback(()=>{n(!0)},[n]);return e.jsx(r,{loading:B,onClick:x,children:"Click here for loading"})},name:"Loading state"},m={render:()=>{const[B,n]=y.useState(!1),x=y.useCallback(()=>{n(!0)},[n]);return e.jsx(r,{success:B,onClick:x,successIcon:v,successText:"Success",children:"Click here for success"})},parameters:{docs:{liveEdit:{scope:{Check:v}}}},name:"Success state"},u={render:()=>e.jsx(e.Fragment,{children:e.jsxs("div",{style:{display:"flex",flexDirection:"column"},children:[e.jsx(g,{type:g.types.TEXT1,style:{marginBottom:"var(--sb-spacing-small)"},children:"Regular"}),e.jsxs("div",{style:{backgroundColor:"var(--sb-primary-color)",padding:"16px"},children:[e.jsx(r,{color:"on-primary-color",marginRight:!0,children:"Primary on color"}),e.jsx(r,{color:"on-primary-color",kind:"secondary",marginRight:!0,children:"Secondary on color"}),e.jsx(r,{color:"on-primary-color",kind:"tertiary",children:"Tertiary on color"})]}),e.jsx("br",{}),e.jsx(g,{type:g.types.TEXT1,style:{marginBottom:"var(--sb-spacing-small)"},children:"Disabled"}),e.jsxs("div",{style:{backgroundColor:"var(--sb-primary-color)",padding:"16px"},children:[e.jsx(r,{color:"on-primary-color",disabled:!0,marginRight:!0,children:"Primary on color"}),e.jsx(r,{color:"on-primary-color",disabled:!0,marginRight:!0,kind:"secondary",children:"Secondary on color"}),e.jsx(r,{color:"on-primary-color",disabled:!0,kind:"tertiary",children:"Tertiary on color"})]})]})}),name:"On color states"},p={render:()=>e.jsxs("div",{children:[e.jsx(r,{kind:"secondary",size:"small",ariaLabel:"decrease zoom level",rightFlat:!0,children:e.jsx(b,{})}),e.jsx(r,{kind:"secondary",size:"small",ariaLabel:"increase zoom level",leftFlat:!0,children:e.jsx(S,{})})]}),parameters:{docs:{liveEdit:{scope:{Remove:b,Add:S}}}},name:"Adjacent buttons"};var k,f,C;o.parameters={...o.parameters,docs:{...(k=o.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: buttonTemplate.bind({}),
+  args: {
+    children: "Button"
+  },
+  parameters: {
+    docs: {
+      liveEdit: {
+        isEnabled: false
+      }
+    }
+  }
+}`,...(C=(f=o.parameters)==null?void 0:f.docs)==null?void 0:C.source}}};var T,P,R;t.parameters={...t.parameters,docs:{...(T=t.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: () => <>
+      <Button>Primary</Button>
+      <Button kind="secondary">Secondary</Button>
+      <Button kind="tertiary">Tertiary</Button>
+    </>,
+  name: "Button kinds"
+}`,...(R=(P=t.parameters)==null?void 0:P.docs)==null?void 0:R.source}}};var z,E,L;s.parameters={...s.parameters,docs:{...(z=s.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: () => <>
+      <Button size="large">Large</Button>
+      <Button size="medium">Medium</Button>
+      <Button size="small">Small</Button>
+    </>
+}`,...(L=(E=s.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};var A,I,F;a.parameters={...a.parameters,docs:{...(A=a.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => <>
+      <Button disabled>Primary</Button>
+      <Button kind="secondary" disabled>
+        Secondary
+      </Button>
+      <Button kind="tertiary" disabled>
+        Tertiary
+      </Button>
+    </>
+}`,...(F=(I=a.parameters)==null?void 0:I.docs)==null?void 0:F.source}}};var O,D,N;c.parameters={...c.parameters,docs:{...(O=c.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: () => <>
+      <Button>Regular</Button>
+      <Button active>Active</Button>
+    </>
+}`,...(N=(D=c.parameters)==null?void 0:D.docs)==null?void 0:N.source}}};var _,M,X;i.parameters={...i.parameters,docs:{...(_=i.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: () => <>
+      <Button color="positive">Positive</Button>
+      <Button color="negative">Negative</Button>
+    </>,
+  name: "Positive and Negative"
+}`,...(X=(M=i.parameters)==null?void 0:M.docs)==null?void 0:X.source}}};var $,w,K;d.parameters={...d.parameters,docs:{...($=d.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  render: () => <>
+      <Button rightIcon={Calendar}>Right icon</Button>
+      <Button leftIcon={Calendar}>Left icon</Button>
+    </>,
+  parameters: {
+    docs: {
+      liveEdit: {
+        scope: {
+          Calendar
+        }
+      }
+    }
+  }
+}`,...(K=(w=d.parameters)==null?void 0:w.docs)==null?void 0:K.source}}};var q,G,H;l.parameters={...l.parameters,docs:{...(q=l.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: () => {
+    const [loading, setLoading] = useState(false);
+    const onClick = useCallback(() => {
+      setLoading(true);
+    }, [setLoading]);
+    return <Button loading={loading} onClick={onClick}>
+        Click here for loading
+      </Button>;
+  },
+  name: "Loading state"
+}`,...(H=(G=l.parameters)==null?void 0:G.docs)==null?void 0:H.source}}};var J,Q,U;m.parameters={...m.parameters,docs:{...(J=m.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  render: () => {
+    const [success, setSuccess] = useState(false);
+    const onClick = useCallback(() => {
+      setSuccess(true);
+    }, [setSuccess]);
+    return <Button success={success} onClick={onClick} successIcon={Check} successText="Success">
+        Click here for success
+      </Button>;
+  },
+  parameters: {
+    docs: {
+      liveEdit: {
+        scope: {
+          Check
+        }
+      }
+    }
+  },
+  name: "Success state"
+}`,...(U=(Q=m.parameters)==null?void 0:Q.docs)==null?void 0:U.source}}};var V,W,Y;u.parameters={...u.parameters,docs:{...(V=u.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: () => <>
+      <div style={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
+        <Text type={Text.types.TEXT1} style={{
+        marginBottom: "var(--sb-spacing-small)"
+      }}>
+          Regular
+        </Text>
+        <div style={{
+        backgroundColor: "var(--sb-primary-color)",
+        padding: "16px"
+      }}>
+          <Button color="on-primary-color" marginRight>
+            Primary on color
+          </Button>
+          <Button color="on-primary-color" kind="secondary" marginRight>
+            Secondary on color
+          </Button>
+          <Button color="on-primary-color" kind="tertiary">
+            Tertiary on color
+          </Button>
+        </div>
+        <br />
+        <Text type={Text.types.TEXT1} style={{
+        marginBottom: "var(--sb-spacing-small)"
+      }}>
+          Disabled
+        </Text>
+        <div style={{
+        backgroundColor: "var(--sb-primary-color)",
+        padding: "16px"
+      }}>
+          <Button color="on-primary-color" disabled marginRight>
+            Primary on color
+          </Button>
+          <Button color="on-primary-color" disabled marginRight kind="secondary">
+            Secondary on color
+          </Button>
+          <Button color="on-primary-color" disabled kind="tertiary">
+            Tertiary on color
+          </Button>
+        </div>
+      </div>
+    </>,
+  name: "On color states"
+}`,...(Y=(W=u.parameters)==null?void 0:W.docs)==null?void 0:Y.source}}};var Z,ee,re;p.parameters={...p.parameters,docs:{...(Z=p.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  render: () => <div>
+      <Button kind="secondary" size="small" ariaLabel="decrease zoom level" rightFlat>
+        <Remove />
+      </Button>
+      <Button kind="secondary" size="small" ariaLabel="increase zoom level" leftFlat>
+        <Add />
+      </Button>
+    </div>,
+  parameters: {
+    docs: {
+      liveEdit: {
+        scope: {
+          Remove,
+          Add
+        }
+      }
+    }
+  },
+  name: "Adjacent buttons"
+}`,...(re=(ee=p.parameters)==null?void 0:ee.docs)==null?void 0:re.source}}};const ae=["Overview","ButtonKinds","Sizes","Disabled","States","PositiveAndNegative","Icons","LoadingState","SuccessState","OnColorStates","AdjacentButtons"],xe=Object.freeze(Object.defineProperty({__proto__:null,AdjacentButtons:p,ButtonKinds:t,Disabled:a,Icons:d,LoadingState:l,OnColorStates:u,Overview:o,PositiveAndNegative:i,Sizes:s,States:c,SuccessState:m,__namedExportsOrder:ae,default:te},Symbol.toStringTag,{value:"Module"}));export{p as A,xe as B,a as D,d as I,l as L,o as O,i as P,s as S,t as a,c as b,m as c,u as d};
---
+++ assets/Icon-BpbYAVuW.js
@@ -0,0 +1,27 @@
+import{j as o}from"./jsx-runtime-DDzbWKUH.js";import{useMDXComponents as s}from"./index-CXa8ra7y.js";import{M as c}from"./index-B0i9nAPG.js";import{I as a,O as d,V as h,F as l,C as x,a as j,b as f}from"./Icon.stories-CzS2_akH.js";import"./index-Hemj67b4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DZ1QTQvZ.js";import"./index-C8TICx0s.js";import"./index-D63y3F3s.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./Icon-CDdqL_gb.js";import"./test-ids-utils-9ISiqDto.js";import"./index-BpvXyOxN.js";import"./useMergeRef-DQ4XPFxl.js";import"./useIsomorphicLayoutEffect-FdvxY73P.js";import"./ssr-utils-Do6V6AqB.js";import"./index-DLqmmtYJ.js";import"./index-8zVcErkr.js";import"./function-utils-6EQ2u6TA.js";import"./isArray-Dxzbedgu.js";import"./_baseGetTag-ENn7h-yl.js";import"./isObject-C3e4t58V.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./index-BA_MN9l1.js";import"./_tslib-Ct4JumdA.js";import"./Invite-CWwo6O26.js";import"./AddSmall-WZFkNTzJ.js";import"./Workspace-BIJf5qCK.js";import"./Calendar-BX7mDXzx.js";import"./Table-DKnXfo9h.js";import"./Check-B_icfwyn.js";import"./CloseSmall-CIab6kaf.js";import"./Settings-DOfpeGYp.js";import"./Drag-po37gvvh.js";import"./DropdownChevronDown-hX_A7Og1.js";import"./DropdownChevronRight-DGHOJF1-.js";import"./Wand-Cv7iNunW.js";import"./NewTab-CFmtLVc3.js";import"./Info-BB4o2YOm.js";import"./NavigationChevronRight-BM1UJa0v.js";import"./Remove-C7LZQ9Te.js";import"./ThumbsUp-DnZbk6gE.js";import"./Search-BxUBhs64.js";import"./SortDescending-B1UwxT6Q.js";import"./Upgrade-Jax58DJ-.js";import"./Search-DLSUN22t.js";import"./BaseInput-D7E1OPHx.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./index-CxHVRCQs.js";import"./debounce-D-uO1lYf.js";import"./toNumber-CBHRrcgy.js";import"./isSymbol-DWIQbemu.js";import"./IconButton-Du4Le5D5.js";import"./Tooltip-BjElzwP7.js";import"./index-NFRdg0hY.js";import"./index-8rJYV3Dp.js";import"./CSSTransition-usT7-fC7.js";import"./extends-B6xKY8K9.js";import"./camelCase-B8c2Dc7Y.js";import"./eq-DPGIUuQ3.js";import"./DialogConstants-DkeX1sw_.js";import"./Flex-BwTwg4EO.js";import"./Clickable-L_6z1d8K.js";import"./useClickableProps-BnhuEIMR.js";import"./sizes-BOsbvv4u.js";import"./Button-DgBL92G0.js";import"./Loader-DRZxb8MF.js";import"./createStoryMetaSettingsDecorator-Dxrg_MSB.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./createComponentTemplate-Y0VTmW_y.js";function m(t){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...s(),...t.components},{Canvas:r,PropsTable:e,UsageGuidelines:p}=i;return r||n("Canvas"),e||n("PropsTable"),p||n("UsageGuidelines"),o.jsxs(o.Fragment,{children:[o.jsx(c,{of:a}),`
+`,o.jsx(i.h1,{id:"icon",children:"Icon"}),`
+`,o.jsx(i.p,{children:"Icon component is our component to unify the supported icon types (Vibe Icons, FontIcon and Custom SVG Icons)"}),`
+`,o.jsx(r,{of:d}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { Icon } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(e,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(p,{guidelines:["Always wrap icons with Icon component","Try to always use icon alongside text or provide tooltip to accommodate the icon"]}),`
+`,o.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,o.jsx(i.h3,{id:"vibe-icon",children:"Vibe Icon"}),`
+`,o.jsx(r,{of:h}),`
+`,o.jsx(i.h3,{id:"fonticon",children:"FontIcon"}),`
+`,o.jsx(r,{of:l}),`
+`,o.jsx(i.h3,{id:"custom-svg",children:"Custom SVG"}),`
+`,o.jsx(r,{of:x}),`
+`,o.jsx(i.h2,{id:"states",children:"States"}),`
+`,o.jsx(i.h3,{id:"color",children:"Color"}),`
+`,o.jsx(i.p,{children:"As a default the icon will inherit the color of it's parent container"}),`
+`,o.jsx(r,{of:j}),`
+`,o.jsx(i.h2,{id:"icons-list",children:"Icons List"}),`
+`,o.jsxs(i.p,{children:["Icons are exported by name from ",o.jsx(i.code,{children:"@vibe/icons"}),":"]}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-javascript",children:`import { DoubleCheck } from "@vibe/icons";
+`})}),`
+`,o.jsx(r,{of:f})]})}function _o(t={}){const{wrapper:i}={...s(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(m,{...t})}):m(t)}function n(t,i){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{_o as default};
