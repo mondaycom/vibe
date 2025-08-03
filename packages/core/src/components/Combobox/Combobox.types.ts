a2ba240103d681b3a7508c8b03cd180e706f1569
Tal Koren <talko@monday.com>
2025-08-03 12:03:26 +0300

    types(Combobox): export types

--- packages/core/src/components/Combobox/Combobox.types.ts
+++ packages/core/src/components/Combobox/Combobox.types.ts
@@ -1,3 +1,12 @@
 export type ComboboxOptionIconType = "default" | "renderer";
 
 export type ComboboxSizes = "small" | "medium" | "large";
+
+export {
+  IComboboxCategoryMap as ComboboxCategoryMap,
+  IComboboxCategory as ComboboxCategory,
+  IComboboxItem as ComboboxItem,
+  IComboboxOption as ComboboxOption,
+  IComboboxOptionEvents as ComboboxOptionEvents,
+  IOptionItemRendererArgs as ComboboxOptionItemRendererArgs
+} from "./components/ComboboxConstants";
