20153f5505b70eef24d9405cdacb3acab2309e26
Tal Koren <talko@monday.com>
2025-08-03 16:14:22 +0300

    docs(storybook): use !dev tags for catalog and color stories (#3028)

--- packages/core/src/storybook/stand-alone-documentaion/spacing/spacing.stories.tsx
+++ packages/core/src/storybook/stand-alone-documentaion/spacing/spacing.stories.tsx
@@ -3,17 +3,18 @@ import { StoryObj, Meta } from "@storybook/react";
 import { SpacingUsageExamples } from "./spacing-usage-examples/spacing-usage-examples";
 
 const meta: Meta = {
-  title: "Foundations/Spacing",
-  tags: ["internal"]
+  title: "Foundations/Spacing"
 };
 export default meta;
 
 type Story = StoryObj;
 
 export const Sizes: Story = {
-  render: SpacingSizes
+  render: SpacingSizes,
+  tags: ["!dev"]
 };
 
 export const UsageExamples: Story = {
-  render: SpacingUsageExamples
+  render: SpacingUsageExamples,
+  tags: ["!dev"]
 };
