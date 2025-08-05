0d66d0a1604988177a002acda024fbdf45a479d1
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-08-05 07:54:07 +0000

    Deploying to gh-pages from @ mondaycom/vibe@ee0d8feef42dfbec7d099c9ebd93337677b82c34 🚀

---
+++ assets/AvatarGroup-x5h48OW_.js
@@ -0,0 +1,47 @@
+import{j as o}from"./jsx-runtime-DDzbWKUH.js";import{useMDXComponents as a}from"./index-CXa8ra7y.js";import{M as l}from"./index-BV0Q_tUf.js";import{A as d,a as c,C as h}from"./component-description-map-C2eqxUgk.js";import{A as x,O as u,S as j,C as f,H as v,D as g,M as C,a as b,G as y,V as w,b as M,L as k,c as A}from"./AvatarGroup.stories-Cu2LJ64y.js";import{r as D}from"./usage-guidelines-CjZwxEOd.js";import"./index-Hemj67b4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C9qllXDf.js";import"./index-DvcHSlZB.js";import"./index-D63y3F3s.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./SplitButtonMenu-CDWEDn-j.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./test-ids-utils-9ISiqDto.js";import"./index-BpvXyOxN.js";import"./DialogConstants-DkeX1sw_.js";import"./function-utils-6EQ2u6TA.js";import"./isArray-Dxzbedgu.js";import"./_baseGetTag-ENn7h-yl.js";import"./isObject-C3e4t58V.js";import"./Tooltip-Bnazf5Ct.js";import"./index-NFRdg0hY.js";import"./index-DLqmmtYJ.js";import"./ssr-utils-Do6V6AqB.js";import"./index-8zVcErkr.js";import"./index-8rJYV3Dp.js";import"./CSSTransition-usT7-fC7.js";import"./extends-B6xKY8K9.js";import"./camelCase-B8c2Dc7Y.js";import"./isSymbol-DWIQbemu.js";import"./eq-DPGIUuQ3.js";import"./Icon-CDdqL_gb.js";import"./useMergeRef-DQ4XPFxl.js";import"./useIsomorphicLayoutEffect-FdvxY73P.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-2Q04fxOc.js";import"./Clickable-L_6z1d8K.js";import"./useClickableProps-BnhuEIMR.js";import"./Button-CmXrS9k2.js";import"./Loader-DmaXbwBu.js";import"./sizes-BOsbvv4u.js";import"./DialogContentContainer-Cj9UqVh2.js";import"./DropdownChevronDown-hX_A7Og1.js";import"./_tslib-Ct4JumdA.js";import"./useMenuItemKeyboardEvents-DddXeYKp.js";import"./useFullKeyboardListeners-44yzp6oL.js";import"./index-CXrJR90Q.js";import"./MenuItem-D8TT7ZSr.js";import"./useIsOverflowing-DshK7x5b.js";import"./debounce-D-uO1lYf.js";import"./toNumber-CBHRrcgy.js";import"./Label-CdcHnQYx.js";import"./Text-C9ywLJgn.js";import"./TypographyConstants-tjBqaxE4.js";import"./colors-vars-map-DOupSotq.js";import"./Divider-Dxm0fdi1.js";import"./IconButton-BkBMs5zW.js";import"./AddSmall-WZFkNTzJ.js";import"./DropdownChevronRight-DGHOJF1-.js";import"./Check-B_icfwyn.js";import"./Workspace-BIJf5qCK.js";import"./information-box--odEzv8P.js";import"./withStaticProps-DibdfTK_.js";import"./link-ByEAMSgF.js";import"./ButtonGroup-DmqP3KI9.js";import"./Checkbox-D0Aarr6O.js";import"./Remove-C7LZQ9Te.js";import"./RadioButton-Xf3I4rpI.js";import"./Link-BU7s9Y2d.js";import"./Counter-D2I5rwEB.js";import"./index-BhJ9w1Ot.js";import"./SwitchTransition-BQCJMqAO.js";import"./Chips-IuW7Y5WG.js";import"./Avatar-BnB0-Ru2.js";import"./index-DxqC8EIR.js";import"./CloseSmall-CIab6kaf.js";import"./Toggle-Do4zt_kB.js";import"./index-DS1BdwZI.js";import"./Dropdown-CFISacFX.js";import"./inheritsLoose-C4PSNlvh.js";import"./memoize-one.esm-CcMeOnPo.js";import"./TabList-B5QRoTsw.js";import"./useGridKeyboardNavigation-Baegmm3H.js";import"./BreadcrumbItem-CTUSzApy.js";import"./NavigationChevronRight-BM1UJa0v.js";import"./Settings-DOfpeGYp.js";import"./Info-BB4o2YOm.js";import"./AttentionBox-DI3Wf5cK.js";import"./AlertBannerText-BWU-Gp7o.js";import"./Toast-Bdhw3VI4.js";import"./MultiStepIndicator-CfAN5Qck.js";import"./HiddenText-CdPxyfMy.js";import"./TipseenWizard-CRISuneU.js";import"./Steps-CHPTlMHt.js";import"./StepsGalleryHeader-DnRIkmMz.js";import"./range-Bu-eLZAw.js";import"./_isIterateeCall-6qSKvFMf.js";import"./isArrayLike-RI9Auh3I.js";import"./toFinite-CwoL-S44.js";import"./TextField-BiEMtZRQ.js";import"./index-CxHVRCQs.js";import"./FieldLabel-BFfWW7Ah.js";import"./NumberField-D6brpMJR.js";import"./BaseInput-D7E1OPHx.js";import"./Search-H_T6nYop.js";import"./Search-BxUBhs64.js";import"./Combobox-DnesvNbq.js";import"./index-Bs0C4iL5.js";import"./index-BMFTbhVI.js";import"./VirtualizedList-lphc29Jg.js";import"./index.esm-CsnITLvm.js";import"./assertThisInitialized-B9jnkVVz.js";import"./react-virtualized-auto-sizer.esm-CuRSY4Lt.js";import"./virtualized-service-D-n4jNAX.js";import"./person1-D9Wcho68.js";import"./person3-B60Gf7uf.js";import"./AvatarGroup-DcFh6wZJ.js";import"./MenuButton-BJPmICRc.js";import"./Skeleton-Czjp8IwX.js";import"./Heading-eaMKbVha.js";import"./LinearProgressBar-anUgb80z.js";import"./EditableHeading-Y-HtHf66.js";import"./EditableTypography-D46qXyxh.js";import"./EditableText-Ctn95W4T.js";import"./ListItem-Cg-4VdB_.js";import"./List-2MooNM_D.js";import"./Box-MunfHFlU.js";import"./AccordionItem-gofM14dv.js";import"./ExpandCollapse-DBl7JlQH.js";import"./VirtualizedList.stories.helpers-BcKCXt6n.js";import"./tip-BKixvd9t.js";import"./Flex-DaTSFknt.js";import"./ColorPicker-C8Y-N-Sk.js";import"./colors-utils-C1Q1nqOD.js";import"./identity-DKeuBCMA.js";import"./Badge-hYHeTroH.js";import"./Slider-9wewxVK6.js";import"./Table.stories.helpers-C9bhoa9l.js";import"./Calendar-BX7mDXzx.js";import"./TableBody-8mUH8JGX.js";import"./SortDescending-B1UwxT6Q.js";import"./VirtualizedGrid.stories.helpers-CLeVWqg3.js";import"./useGridKeyboardNavigationContext.stories.helpers-DUs4MvwO.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./MenuGridItem-mPiGjF5K.js";import"./story-description-B5F6CX2L.js";import"./createStoryMetaSettingsDecorator-DIIwAjh2.js";import"./index-CUPFiJE2.js";import"./Invite-CWwo6O26.js";import"./Table-DKnXfo9h.js";import"./Drag-po37gvvh.js";import"./Wand-Cv7iNunW.js";import"./NewTab-CFmtLVc3.js";import"./ThumbsUp-DnZbk6gE.js";import"./Upgrade-Jax58DJ-.js";function s(r){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...a(),...r.components},{Canvas:t,PropsTable:n,RelatedComponents:m,StorybookLink:p}=i;return t||e("Canvas"),n||e("PropsTable"),m||e("RelatedComponents"),p||e("StorybookLink"),o.jsxs(o.Fragment,{children:[o.jsx(l,{of:x}),`
+`,o.jsx(i.h1,{id:"avatargroup",children:"AvatarGroup"}),`
+`,o.jsx(i.p,{children:"Use this component if you need to stack avatars as a group."}),`
+`,o.jsx(t,{of:u}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { AvatarGroup } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(n,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(D,{guidelines:[o.jsxs(o.Fragment,{children:["Use ",o.jsx(p,{page:"Components/Tooltip",children:"tooltip"})," component while hovering on the counter when you need only to display the content"]}),o.jsxs(o.Fragment,{children:["If clickable and navigable list is required on counter, use"," ",o.jsx(p,{page:"Components/Menu/Menu",children:"Menu"})," component"]})]}),`
+`,o.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,o.jsx(i.h3,{id:"size",children:"Size"}),`
+`,o.jsx(i.p,{children:"Avatar Group appears in 4 sizes: XS, Small, Medium, and Large."}),`
+`,o.jsx(t,{of:j}),`
+`,o.jsx(i.h3,{id:"color-variants",children:"Color variants"}),`
+`,o.jsx(i.p,{children:"You can use Light or Dark counter color to maintain visual hierarchy."}),`
+`,o.jsx(t,{of:f}),`
+`,o.jsx(i.h3,{id:"clickable-vs-hover",children:"Clickable vs. Hover"}),`
+`,o.jsxs(i.p,{children:["If avatars are clickable, they will be displayed via ",o.jsx(p,{page:"Components/Menu/Menu",children:"Menu"}),` and user will be able to navigate each additional item.
+Otherwise, avatars will be displayed in a Tooltip with no item's navigation.`]}),`
+`,o.jsx(t,{of:v}),`
+`,o.jsx(i.h3,{id:"disabled",children:"Disabled"}),`
+`,o.jsx(i.p,{children:"Use when the avatar group is inactive in the specific context."}),`
+`,o.jsx(t,{of:g}),`
+`,o.jsx(i.h3,{id:"max-avatars-shown",children:"Max avatars shown"}),`
+`,o.jsx(i.p,{children:"Choose the ammount of avatars you want to show"}),`
+`,o.jsx(t,{of:C}),`
+`,o.jsx(i.h3,{id:"custom-counter",children:"Custom counter"}),`
+`,o.jsxs(i.p,{children:["You can pass ",o.jsx(i.code,{children:"counterProps"})," to specify counter params."]}),`
+`,o.jsx(t,{of:b}),`
+`,o.jsx(i.h3,{id:"grid-tooltip",children:"Grid tooltip"}),`
+`,o.jsx(i.p,{children:"When tooltip text for additional avatars is not passed, extra avatars will be displayed in a grid mode."}),`
+`,o.jsx(t,{of:y}),`
+`,o.jsx(i.h3,{id:"virtualized-list",children:"Virtualized list"}),`
+`,o.jsx(i.p,{children:"Should be used only to display large amount of avatars in default counter tooltip"}),`
+`,o.jsx(t,{of:w}),`
+`,o.jsx(i.h3,{id:"counter-custom-tooltip-content",children:"Counter custom tooltip content"}),`
+`,o.jsx(i.p,{children:"Counter tooltip props can be specified in order to render tooltip with custom content."}),`
+`,o.jsx(t,{of:M}),`
+`,o.jsx(i.h2,{id:"use-cases-and-examples",children:"Use cases and examples"}),`
+`,o.jsx(i.h3,{id:"last-seen-users",children:"Last seen users"}),`
+`,o.jsx(t,{of:k}),`
+`,o.jsx(i.h3,{id:"displaying-teams",children:"Displaying teams"}),`
+`,o.jsx(t,{of:A}),`
+`,o.jsx(i.h2,{id:"related-components",children:"Related components"}),`
+`,o.jsx(m,{componentsNames:[d,c,h]})]})}function At(r={}){const{wrapper:i}={...a(),...r.components};return i?o.jsx(i,{...r,children:o.jsx(s,{...r})}):s(r)}function e(r,i){throw new Error("Expected component `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{At as default};
---
+++ assets/Link.stories-BeUInNkz.js
@@ -0,0 +1,54 @@
+import{j as e}from"./jsx-runtime-DDzbWKUH.js";import{L as n}from"./Link-BU7s9Y2d.js";import{t as m}from"./Info-BB4o2YOm.js";import{c as d}from"./index-CUPFiJE2.js";import{c}from"./Workspace-BIJf5qCK.js";import{c as _}from"./createStoryMetaSettingsDecorator-DIIwAjh2.js";const p=_({component:n,iconPropNamesArray:["icon"]}),F={title:"Components/Link",component:n,argTypes:p.argTypes,decorators:p.decorators},t={render:T=>e.jsx(n,{text:"Read more",href:"https://www.monday.com",...T}),parameters:{docs:{liveEdit:{isEnabled:!1}}}},r={render:()=>e.jsx(n,{text:"Default",href:"https://www.monday.com"})},o={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(n,{text:"اقرأ أكثر",href:"https://www.monday.com",icon:d}),e.jsx(n,{text:"קרא עוד",href:"https://www.monday.com",iconPosition:"end",icon:m})]}),parameters:{docs:{liveEdit:{scope:{IconLink:d,Info:m}}}}},s={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(n,{text:"Read more",href:"https://www.monday.com",icon:c}),e.jsx(n,{text:"Read more",href:"https://www.monday.com",iconPosition:"end",icon:c})]}),parameters:{docs:{liveEdit:{scope:{ExternalPage:c}}}}},a={render:()=>e.jsxs("div",{children:["Lorem Ipsum has been the industry's ",e.jsx(n,{inlineText:!0,inheritFontSize:!0,text:"standard",href:"https://www.monday.com"})," dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]})},i={render:()=>e.jsxs("div",{children:[`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+      galley of type and scrambled it to make a type specimen book. `,e.jsx(n,{text:"Read more",href:"https://www.monday.com",inheritFontSize:!0,inlineText:!0})]})};var h,w,l;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: (args: LinkProps) => <Link text="Read more" href="https://www.monday.com" {...args} />,
+  parameters: {
+    docs: {
+      liveEdit: {
+        isEnabled: false
+      }
+    }
+  }
+}`,...(l=(w=t.parameters)==null?void 0:w.docs)==null?void 0:l.source}}};var x,u,y;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <Link text="Default" href="https://www.monday.com" />
+}`,...(y=(u=r.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var f,k,g;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: () => <>
+      <Link text="اقرأ أكثر" href="https://www.monday.com" icon={IconLink} />
+      <Link text="קרא עוד" href="https://www.monday.com" iconPosition="end" icon={Info} />
+    </>,
+  parameters: {
+    docs: {
+      liveEdit: {
+        scope: {
+          IconLink,
+          Info
+        }
+      }
+    }
+  }
+}`,...(g=(k=o.parameters)==null?void 0:k.docs)==null?void 0:g.source}}};var L,v,S;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: () => <>
+      <Link text="Read more" href="https://www.monday.com" icon={ExternalPage} />
+      <Link text="Read more" href="https://www.monday.com" iconPosition="end" icon={ExternalPage} />
+    </>,
+  parameters: {
+    docs: {
+      liveEdit: {
+        scope: {
+          ExternalPage
+        }
+      }
+    }
+  }
+}`,...(S=(v=s.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var b,j,E;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: () => <div>
+      {\`Lorem Ipsum has been the industry's \`}
+      <Link inlineText inheritFontSize text="standard" href="https://www.monday.com" />
+      {\` dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\`}
+    </div>
+}`,...(E=(j=a.parameters)==null?void 0:j.docs)==null?void 0:E.source}}};var R,I,P;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: () => <div>
+      {\`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+      galley of type and scrambled it to make a type specimen book. \`}
+      <Link text="Read more" href="https://www.monday.com" inheritFontSize inlineText />
+    </div>
+}`,...(P=(I=i.parameters)==null?void 0:I.docs)==null?void 0:P.source}}};const O=["Overview","States","RightToLeft","WithIcons","ReferenceLink","ShorteningTexts"],C=Object.freeze(Object.defineProperty({__proto__:null,Overview:t,ReferenceLink:a,RightToLeft:o,ShorteningTexts:i,States:r,WithIcons:s,__namedExportsOrder:O,default:F},Symbol.toStringTag,{value:"Module"}));export{C as L,t as O,o as R,r as S,s as W,a,i as b};
