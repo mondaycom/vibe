5203a897cde95ccb6a5826a5f1791957eef13116
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-08-05 11:01:32 +0000

    Deploying to gh-pages from @ mondaycom/vibe@ab90f9933d7637f79d96205ca4846db7a9cde116 🚀

---
+++ assets/Box-BOrQSiEt.js
@@ -0,0 +1,61 @@
+import{j as o}from"./jsx-runtime-DDzbWKUH.js";import{useMDXComponents as s}from"./index-CXa8ra7y.js";import{M as d}from"./index-BhtjH93-.js";import{F as a,x as c,d as h}from"./component-description-map-BEmR0Pvz.js";import{l}from"./tip-BKixvd9t.js";import{B as x,O as j,a as f,T as g,b as u,c as b,R,S as C,M as O,P as y,D}from"./Box.stories-h_K3mjWj.js";import{r as B}from"./usage-guidelines-CjZwxEOd.js";import"./index-Hemj67b4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BoMporuu.js";import"./index-B2vSB2uV.js";import"./index-D63y3F3s.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./SplitButtonMenu-DS37QJME.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./test-ids-utils-9ISiqDto.js";import"./index-BpvXyOxN.js";import"./DialogConstants-DkeX1sw_.js";import"./function-utils-6EQ2u6TA.js";import"./isArray-Dxzbedgu.js";import"./_baseGetTag-ENn7h-yl.js";import"./isObject-C3e4t58V.js";import"./Tooltip-JDdE3SYE.js";import"./index-NFRdg0hY.js";import"./index-DLqmmtYJ.js";import"./ssr-utils-Do6V6AqB.js";import"./index-8zVcErkr.js";import"./index-8rJYV3Dp.js";import"./CSSTransition-usT7-fC7.js";import"./extends-B6xKY8K9.js";import"./camelCase-B8c2Dc7Y.js";import"./isSymbol-DWIQbemu.js";import"./eq-DPGIUuQ3.js";import"./Icon-CDdqL_gb.js";import"./useMergeRef-DQ4XPFxl.js";import"./useIsomorphicLayoutEffect-FdvxY73P.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-BwTwg4EO.js";import"./Clickable-L_6z1d8K.js";import"./useClickableProps-BnhuEIMR.js";import"./Button-B3Hc6Y0x.js";import"./Loader-DRZxb8MF.js";import"./sizes-BOsbvv4u.js";import"./DialogContentContainer-D1nuw_bN.js";import"./DropdownChevronDown-hX_A7Og1.js";import"./_tslib-Ct4JumdA.js";import"./useMenuItemKeyboardEvents-DGvnAHeq.js";import"./useFullKeyboardListeners-44yzp6oL.js";import"./index-CXrJR90Q.js";import"./MenuItem-2Pbim4jm.js";import"./useIsOverflowing-DshK7x5b.js";import"./debounce-D-uO1lYf.js";import"./toNumber-CBHRrcgy.js";import"./Label-BLaEPVt8.js";import"./Text-C3O7wRxm.js";import"./TypographyConstants-DeCtmaAc.js";import"./colors-vars-map-DOupSotq.js";import"./Divider-Dxm0fdi1.js";import"./IconButton-Dy3MHaCm.js";import"./AddSmall-WZFkNTzJ.js";import"./DropdownChevronRight-DGHOJF1-.js";import"./Check-B_icfwyn.js";import"./Workspace-BIJf5qCK.js";import"./information-box--odEzv8P.js";import"./withStaticProps-DibdfTK_.js";import"./link-ByEAMSgF.js";import"./ButtonGroup-CsaZazCL.js";import"./Checkbox-DB2mXIJ1.js";import"./Remove-C7LZQ9Te.js";import"./RadioButton-DccIsBzK.js";import"./Link-BU7s9Y2d.js";import"./Counter-BwvcTAcf.js";import"./index-BhJ9w1Ot.js";import"./SwitchTransition-BQCJMqAO.js";import"./Chips-Dz6w7jaX.js";import"./Avatar-DBQ5wLiv.js";import"./index-DxqC8EIR.js";import"./CloseSmall-CIab6kaf.js";import"./Toggle-Bzp3cQsg.js";import"./index-DS1BdwZI.js";import"./Dropdown-DDoAVX10.js";import"./inheritsLoose-C4PSNlvh.js";import"./memoize-one.esm-CcMeOnPo.js";import"./TabList-Kj2wlOaA.js";import"./useGridKeyboardNavigation-Baegmm3H.js";import"./BreadcrumbItem-BqKF1BCI.js";import"./NavigationChevronRight-BM1UJa0v.js";import"./Settings-DOfpeGYp.js";import"./Info-BB4o2YOm.js";import"./AttentionBox-D8zovFDB.js";import"./AlertBannerText-B29Aa2FN.js";import"./Toast-ycAjgetD.js";import"./MultiStepIndicator-Cm2hmbat.js";import"./HiddenText-CdPxyfMy.js";import"./TipseenWizard-BYqFPJK7.js";import"./Steps-5zahcRsx.js";import"./StepsGalleryHeader-DnRIkmMz.js";import"./range-Bu-eLZAw.js";import"./_isIterateeCall-6qSKvFMf.js";import"./isArrayLike-RI9Auh3I.js";import"./toFinite-CwoL-S44.js";import"./TextField-6wOPUDse.js";import"./index-CxHVRCQs.js";import"./FieldLabel-BFfWW7Ah.js";import"./NumberField-BaXhrf0Z.js";import"./BaseInput-vlpA-ujk.js";import"./Search-D49GuSOG.js";import"./Search-BxUBhs64.js";import"./Combobox-CrO1iUwv.js";import"./index-Bs0C4iL5.js";import"./index-BMFTbhVI.js";import"./VirtualizedList-lphc29Jg.js";import"./index.esm-CsnITLvm.js";import"./assertThisInitialized-B9jnkVVz.js";import"./react-virtualized-auto-sizer.esm-CuRSY4Lt.js";import"./virtualized-service-D-n4jNAX.js";import"./person1-D9Wcho68.js";import"./person3-B60Gf7uf.js";import"./AvatarGroup-DptQlirn.js";import"./MenuButton-DIQKoL34.js";import"./Skeleton-DtQq_Edn.js";import"./Heading-D8MV7aea.js";import"./LinearProgressBar-CxsRLqgc.js";import"./EditableHeading-DWJx0k0a.js";import"./EditableTypography-apOKvnfD.js";import"./EditableText-DgCQi3hx.js";import"./ListItem-JwF-cIO1.js";import"./List-BCE0xzF0.js";import"./Box-BDmFEuj7.js";import"./AccordionItem-DeTzvEBm.js";import"./ExpandCollapse-mYPND1ML.js";import"./VirtualizedList.stories.helpers-BcKCXt6n.js";import"./ColorPicker-CkfiziQv.js";import"./colors-utils-C1Q1nqOD.js";import"./identity-DKeuBCMA.js";import"./Badge-5_yBXy3-.js";import"./Slider-_FkBcvd0.js";import"./Table.stories.helpers-CQCnErHA.js";import"./Calendar-BX7mDXzx.js";import"./TableBody-D-KoBv-6.js";import"./SortDescending-B1UwxT6Q.js";import"./VirtualizedGrid.stories.helpers-DHyN3xGN.js";import"./useGridKeyboardNavigationContext.stories.helpers-UzNSpi-M.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./MenuGridItem-BBXa5k1f.js";import"./Flex-DaTSFknt.js";import"./createStoryMetaSettingsDecorator-DIIwAjh2.js";import"./index-CUPFiJE2.js";import"./Invite-CWwo6O26.js";import"./Table-DKnXfo9h.js";import"./Drag-po37gvvh.js";import"./Wand-Cv7iNunW.js";import"./NewTab-CFmtLVc3.js";import"./ThumbsUp-DnZbk6gE.js";import"./Upgrade-Jax58DJ-.js";const E=()=>o.jsx(l,{title:"Benefits of the Box component",children:"Easily add Vibe styles without adding css"});function e(t){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",h4:"h4",p:"p",pre:"pre",...s(),...t.components},{Canvas:r,PropsTable:n,RelatedComponents:m}=i;return r||p("Canvas"),n||p("PropsTable"),m||p("RelatedComponents"),o.jsxs(o.Fragment,{children:[o.jsx(d,{of:x}),`
+`,o.jsx(i.h1,{id:"box",children:"Box"}),`
+`,o.jsx(i.p,{children:"Box component is used as a wrapper component."}),`
+`,o.jsxs(i.p,{children:["Its purpose is to help scaffold compositions while using Vibe's prop keys without writing new CSS. ",o.jsx("br",{}),`It can be used as a container for
+atom based compositions, it can accept all Vibe style related props and have a semantic html tag.`]}),`
+`,o.jsx(r,{of:j}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { Box } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(n,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(B,{guidelines:["Use as a styled container","Use to add spacing, borders, and rounded corners to areas of content","Use as an inner component for spacing or styles"]}),`
+`,o.jsx(E,{}),`
+`,o.jsx(i.h2,{id:"utility-props",children:"Utility props"}),`
+`,o.jsx(i.h3,{id:"backgrounds-colors",children:"BACKGROUNDS COLORS"}),`
+`,o.jsx(r,{of:f}),`
+`,o.jsx(i.h3,{id:"text-colors",children:"TEXT COLORS"}),`
+`,o.jsx(r,{of:g}),`
+`,o.jsx(i.h3,{id:"border",children:"BORDER"}),`
+`,o.jsx(r,{of:u}),`
+`,o.jsx(i.h3,{id:"border-color",children:"BORDER COLOR"}),`
+`,o.jsx(r,{of:b}),`
+`,o.jsx(i.h3,{id:"rounded-corners",children:"ROUNDED CORNERS"}),`
+`,o.jsx(i.h4,{id:"size-props",children:"Size props"}),`
+`,o.jsx(r,{of:R}),`
+`,o.jsx(i.h3,{id:"shadow",children:"SHADOW"}),`
+`,o.jsx(r,{of:C}),`
+`,o.jsx(i.h3,{id:"margin",children:"MARGIN"}),`
+`,o.jsx(i.h4,{id:"size-props-1",children:"Size props"}),`
+`,o.jsx(r,{of:O}),`
+`,o.jsx(i.h4,{id:"property-variations-per-each-size",children:"Property variations per each size:"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{children:`margin
+marginX (x axis: left, right)
+marginY (y axis: top, bottom)
+marginTop
+marginEnd
+marginBottom
+marginStart
+`})}),`
+`,o.jsx(i.h4,{id:"aligning-auto-and-none",children:"Aligning Auto and none"}),`
+`,o.jsx(i.p,{children:"each property variation can hold additional values for:"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{children:`AUTO
+NONE
+`})}),`
+`,o.jsx(i.h3,{id:"padding",children:"PADDING"}),`
+`,o.jsx(i.h4,{id:"size-props-2",children:"Size props"}),`
+`,o.jsx(r,{of:y}),`
+`,o.jsx(i.h4,{id:"property-variations-per-each-size-1",children:"Property variations per each size:"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{children:`padding
+paddingX (x axis: left, right)
+paddingY (y axis: top, bottom)
+paddingTop
+paddingEnd
+paddingBottom
+paddingStart
+`})}),`
+`,o.jsx(i.h3,{id:"component-disabled",children:"Component Disabled"}),`
+`,o.jsx(r,{of:D}),`
+`,o.jsx(i.h2,{id:"related-components",children:"Related components"}),`
+`,o.jsx(m,{componentsNames:[a,c,h]})]})}function Dr(t={}){const{wrapper:i}={...s(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(e,{...t})}):e(t)}function p(t,i){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{Dr as default};
---
+++ assets/dropdown-in-modals-olCbUSDh.js
@@ -0,0 +1,75 @@
+import{j as o}from"./jsx-runtime-DDzbWKUH.js";import{useMDXComponents as l}from"./index-CXa8ra7y.js";import{M as m,C as n}from"./index-BhtjH93-.js";import{l as a,D as h,d as c}from"./component-description-map-BEmR0Pvz.js";import{D as u,M as w,a as f,I as x,b as g}from"./dropdown-in-modals.stories-BhNyB8A7.js";import{l as t}from"./tip-BKixvd9t.js";import{n as s}from"./link-ByEAMSgF.js";import"./index-Hemj67b4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BoMporuu.js";import"./index-B2vSB2uV.js";import"./index-D63y3F3s.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./SplitButtonMenu-DS37QJME.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./test-ids-utils-9ISiqDto.js";import"./index-BpvXyOxN.js";import"./DialogConstants-DkeX1sw_.js";import"./function-utils-6EQ2u6TA.js";import"./isArray-Dxzbedgu.js";import"./_baseGetTag-ENn7h-yl.js";import"./isObject-C3e4t58V.js";import"./Tooltip-JDdE3SYE.js";import"./index-NFRdg0hY.js";import"./index-DLqmmtYJ.js";import"./ssr-utils-Do6V6AqB.js";import"./index-8zVcErkr.js";import"./index-8rJYV3Dp.js";import"./CSSTransition-usT7-fC7.js";import"./extends-B6xKY8K9.js";import"./camelCase-B8c2Dc7Y.js";import"./isSymbol-DWIQbemu.js";import"./eq-DPGIUuQ3.js";import"./Icon-CDdqL_gb.js";import"./useMergeRef-DQ4XPFxl.js";import"./useIsomorphicLayoutEffect-FdvxY73P.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-BwTwg4EO.js";import"./Clickable-L_6z1d8K.js";import"./useClickableProps-BnhuEIMR.js";import"./Button-B3Hc6Y0x.js";import"./Loader-DRZxb8MF.js";import"./sizes-BOsbvv4u.js";import"./DialogContentContainer-D1nuw_bN.js";import"./DropdownChevronDown-hX_A7Og1.js";import"./_tslib-Ct4JumdA.js";import"./useMenuItemKeyboardEvents-DGvnAHeq.js";import"./useFullKeyboardListeners-44yzp6oL.js";import"./index-CXrJR90Q.js";import"./MenuItem-2Pbim4jm.js";import"./useIsOverflowing-DshK7x5b.js";import"./debounce-D-uO1lYf.js";import"./toNumber-CBHRrcgy.js";import"./Label-BLaEPVt8.js";import"./Text-C3O7wRxm.js";import"./TypographyConstants-DeCtmaAc.js";import"./colors-vars-map-DOupSotq.js";import"./Divider-Dxm0fdi1.js";import"./IconButton-Dy3MHaCm.js";import"./AddSmall-WZFkNTzJ.js";import"./DropdownChevronRight-DGHOJF1-.js";import"./Check-B_icfwyn.js";import"./Workspace-BIJf5qCK.js";import"./information-box--odEzv8P.js";import"./withStaticProps-DibdfTK_.js";import"./ButtonGroup-CsaZazCL.js";import"./Checkbox-DB2mXIJ1.js";import"./Remove-C7LZQ9Te.js";import"./RadioButton-DccIsBzK.js";import"./Link-BU7s9Y2d.js";import"./Counter-BwvcTAcf.js";import"./index-BhJ9w1Ot.js";import"./SwitchTransition-BQCJMqAO.js";import"./Chips-Dz6w7jaX.js";import"./Avatar-DBQ5wLiv.js";import"./index-DxqC8EIR.js";import"./CloseSmall-CIab6kaf.js";import"./Toggle-Bzp3cQsg.js";import"./index-DS1BdwZI.js";import"./Dropdown-DDoAVX10.js";import"./inheritsLoose-C4PSNlvh.js";import"./memoize-one.esm-CcMeOnPo.js";import"./TabList-Kj2wlOaA.js";import"./useGridKeyboardNavigation-Baegmm3H.js";import"./BreadcrumbItem-BqKF1BCI.js";import"./NavigationChevronRight-BM1UJa0v.js";import"./Settings-DOfpeGYp.js";import"./Info-BB4o2YOm.js";import"./AttentionBox-D8zovFDB.js";import"./AlertBannerText-B29Aa2FN.js";import"./Toast-ycAjgetD.js";import"./MultiStepIndicator-Cm2hmbat.js";import"./HiddenText-CdPxyfMy.js";import"./TipseenWizard-BYqFPJK7.js";import"./Steps-5zahcRsx.js";import"./StepsGalleryHeader-DnRIkmMz.js";import"./range-Bu-eLZAw.js";import"./_isIterateeCall-6qSKvFMf.js";import"./isArrayLike-RI9Auh3I.js";import"./toFinite-CwoL-S44.js";import"./TextField-6wOPUDse.js";import"./index-CxHVRCQs.js";import"./FieldLabel-BFfWW7Ah.js";import"./NumberField-BaXhrf0Z.js";import"./BaseInput-vlpA-ujk.js";import"./Search-D49GuSOG.js";import"./Search-BxUBhs64.js";import"./Combobox-CrO1iUwv.js";import"./index-Bs0C4iL5.js";import"./index-BMFTbhVI.js";import"./VirtualizedList-lphc29Jg.js";import"./index.esm-CsnITLvm.js";import"./assertThisInitialized-B9jnkVVz.js";import"./react-virtualized-auto-sizer.esm-CuRSY4Lt.js";import"./virtualized-service-D-n4jNAX.js";import"./person1-D9Wcho68.js";import"./person3-B60Gf7uf.js";import"./AvatarGroup-DptQlirn.js";import"./MenuButton-DIQKoL34.js";import"./Skeleton-DtQq_Edn.js";import"./Heading-D8MV7aea.js";import"./LinearProgressBar-CxsRLqgc.js";import"./EditableHeading-DWJx0k0a.js";import"./EditableTypography-apOKvnfD.js";import"./EditableText-DgCQi3hx.js";import"./ListItem-JwF-cIO1.js";import"./List-BCE0xzF0.js";import"./Box-BDmFEuj7.js";import"./AccordionItem-DeTzvEBm.js";import"./ExpandCollapse-mYPND1ML.js";import"./VirtualizedList.stories.helpers-BcKCXt6n.js";import"./ColorPicker-CkfiziQv.js";import"./colors-utils-C1Q1nqOD.js";import"./identity-DKeuBCMA.js";import"./Badge-5_yBXy3-.js";import"./Slider-_FkBcvd0.js";import"./Table.stories.helpers-CQCnErHA.js";import"./Calendar-BX7mDXzx.js";import"./TableBody-D-KoBv-6.js";import"./SortDescending-B1UwxT6Q.js";import"./VirtualizedGrid.stories.helpers-DHyN3xGN.js";import"./useGridKeyboardNavigationContext.stories.helpers-UzNSpi-M.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./MenuGridItem-BBXa5k1f.js";import"./LegacyModal-ByMXCfNv.js";import"./LayerProvider-DZk5Zra8.js";import"./ModalExampleContent-CoIxsE2q.js";import"./Flex-DaTSFknt.js";const p=()=>o.jsx(t,{type:t.types.WARNING,emoji:"⚠️",title:"Known issue",children:`This solution have a serious issue - it doesn't properly support options-scrolling on popovers when lacking of screen space. 
+    We recommend checking using other options instead of this one.`});function d(i){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...l(),...i.components},{RelatedComponents:r}=e;return r||v("RelatedComponents"),o.jsxs(o.Fragment,{children:[o.jsx(m,{of:u}),`
+`,o.jsx(e.h1,{id:"display-dropdowns-inside-pop-overs",children:"Display dropdowns inside pop-overs"}),`
+`,o.jsx("p",{children:o.jsx(t,{title:"New Dropdown Available",emoji:"🚀",children:o.jsxs(e.p,{children:["Consider using the new ",o.jsx(s,{href:"/?path=/docs/components-dropdown-alpha--docs",children:"Dropdown component"}),` which
+handles pop-overs automatically without requiring these workarounds. It's built with better accessibility and
+performance.`]})})}),`
+`,o.jsx(e.h2,{id:"problem-description",children:"Problem description"}),`
+`,o.jsxs(e.p,{children:["Our dropdowns are implemented using the ",o.jsx(s,{href:"https://react-select.com/home",withoutSpacing:!0,children:"React Select"}),` library.
+One of the styling issues with React Select dropdowns is that the menu is not fully displayed when its container has `,o.jsx("code",{children:"overflow: hidden"}),", ",o.jsx("code",{children:"overflow: auto"}),", or ",o.jsx("code",{children:"overflow: scroll"}),`.
+As a result, when dropdown menus are placed inside a scrolling container or within a Vibe's dialog or modal, the dropdown menu appears damaged, as you can see in the following examples.`]}),`
+`,o.jsx(n,{of:w}),`
+`,o.jsx(e.p,{children:`To address this problem, we need to adjust the styling of the dropdown menu to ensure it appears correctly even within these specific container.
+We implemented few methods for solving this issue.`}),`
+`,o.jsx(e.h2,{id:"solution-1-menuposition--fixed",children:"Solution 1: menuPosition = 'fixed'"}),`
+`,o.jsxs(e.p,{children:["To handle straightforward scenarios of displaying dropdowns inside a container with relevant overflow styles, you can set ",o.jsx("code",{children:"menuPosition"})," prop to ",o.jsx("code",{children:"Dropdown.menuPositions.FIXED"}),`.
+This prop overrides the dropdown menu's default settings and ensures proper display within the container.
+`,o.jsx(e.strong,{children:"This solution will work for most cases single select dropdown inside Vibe's Modal or DialogContentContainer, it will not work properly for Vibe's Dialog."})]}),`
+`,o.jsx(n,{of:f}),`
+`,o.jsx(e.h2,{id:"solution-2-insideoverflowcontainer",children:"Solution 2: insideOverflowContainer"}),`
+`,o.jsxs(e.p,{children:["To handle straightforward scenarios of displaying dropdowns inside a container with relevant overflow styles, you can utilize our dropdown's ",o.jsx("code",{children:"insideOverflowContainer"}),` prop.
+This prop overrides the dropdown menu's default settings and ensures proper display within the container.
+`,o.jsx(e.strong,{children:"This solution will work for single select dropdown inside Vibe's Modal or DialogContentContainer cases, it will not work for Vibe's Dialog."})]}),`
+`,o.jsx(p,{}),`
+`,o.jsx(e.p,{children:`Please note that this solution is intended for the most basic use cases and may not function optimally if your
+overflow container has one parent or more with conflicting overflow definitions.`}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(e.h2,{id:"solution-3-insideoverflowwithtransformcontainer",children:"Solution 3: insideOverflowWithTransformContainer"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx("code",{children:"insideOverflowContainer"}),` prop will not function as expected for dropdowns displayed inside Vibe's dialog components.
+The reason is that Vibe dialogs use CSS transform to position the dialog next to the attached element.
+For this specific case, use the 'insideOverflowWithTransformContainer' prop to ensure proper display and functionality.`]}),`
+`,o.jsx(p,{}),`
+`,o.jsx(n,{of:g}),`
+`,o.jsx(e.h2,{id:"solution-4-popupscontainerselector",children:"Solution 4: popupsContainerSelector"}),`
+`,o.jsxs(e.p,{children:["For the most complex use cases, you can utilize our dropdown's ",o.jsx("code",{children:"popupsContainerSelector"}),` prop.
+This prop allows you to render all the dropdown's popovers in a different node in the DOM, as specified by the provided selector.
+This prop will solve all the issues described before and also allow you to control under which DOM element other dropdown containers will render.
+To implement this solution and ensure proper display of all dropdown popovers, you need to define a few styles for the container selector, as shown in the following code example:`]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-css",children:`.container {
+  top: 0; // Set the container and the beginning of the web page
+  right: 0; // Set the container and the beginning of the web page
+  position: "fixed"; // The container should not move when scrolling the web page
+  width: "100%"; // The container should cover the whole viewport
+  height: "100%"; // The container should cover the whole viewport
+  pointerevents: "none"; // The container should not block any event
+  zindex: 99999; // The container z index should be heigher from the z-index of the modal/dialog which contains the dropdown itself
+}
+.container * {
+  pointer-events: auto; // all container children should handle events as usual
+}
+`})}),`
+`,o.jsx(e.p,{children:`This solution can be useful for multiselection dropdown with single line state, which contains multiple popovers (one for the dropdown menu and one for the selections which hidden under the counter).
+If you have defined the dropdown's popovers container styles as required, you can use it as demonstrated in the following example:`}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`<div>
+  <div
+    id="popovers-container"
+    style={{
+      position: "fixed",
+      width: "100%",
+      pointerEvents: "none",
+      zIndex: 99999,
+      height: "100%",
+      top: 0,
+      right: 0
+    }}
+    className={styles.pointerEventsAutoToAllChildren}
+  />
+  <Button onClick={() => setShow(true)}>Open Modal</Button>
+  <Modal id="modal-with-multiple-choice" title="Modal with dropdown" show={show} onClose={closeModal}>
+    <ModalContent>
+      <Dropdown multi options={options} popupsContainerSelector="#popovers-container" />
+    </ModalContent>
+  </Modal>
+</div>
+`})}),`
+`,o.jsx(r,{componentsNames:[a,h,c]})]})}function mi(i={}){const{wrapper:e}={...l(),...i.components};return e?o.jsx(e,{...i,children:o.jsx(d,{...i})}):d(i)}function v(i,e){throw new Error("Expected component `"+i+"` to be defined: you likely forgot to import, pass, or provide it.")}export{mi as default};
