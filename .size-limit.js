0ee72fa5369c66a0eb270621cbfd40c769d714b0
Tal Koren <talko@monday.com>
2025-08-06 15:37:29 +0300

    ci: fix script

--- .size-limit.js
+++ .size-limit.js
@@ -1,6 +1,18 @@
 module.exports = [
   {
-    path: "packages/core/src/components/*/index.{ts,tsx}",
+    path: "packages/core/dist/src/components/Accordion/Accordion/Accordion.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/AlertBanner/AlertBanner.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/AttentionBox/AttentionBox.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Avatar/Avatar.js",
     webpack: false
   }
 ];
