af3c8fb7e12ad2c914acf7cda9b3fc2ba7276fc4
Tal Koren <talko@monday.com>
2025-08-06 16:02:29 +0300

    ci: fix script

--- .size-limit.js
+++ .size-limit.js
@@ -14,5 +14,253 @@ module.exports = [
   {
     path: "packages/core/dist/src/components/Avatar/Avatar.js",
     webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/AvatarGroup/AvatarGroup.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Badge/Badge.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/BaseInput/BaseInput.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Box/Box.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/BreadcrumbsBar/BreadcrumbsBar.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Button/Button.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/ButtonGroup/ButtonGroup.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Checkbox/Checkbox.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Chips/Chips.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Clickable/Clickable.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/ColorPicker/ColorPicker.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Combobox/Combobox.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Counter/Counter.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/DatePicker/DatePicker.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Dialog/Dialog.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/DialogContentContainer/DialogContentContainer.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Divider/Divider.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Dropdown/Dropdown.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/DropdownNew/DropdownNew.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/EditableHeading/EditableHeading.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/EditableText/EditableText.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/EmptyState/EmptyState.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/ExpandCollapse/ExpandCollapse.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Flex/Flex.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/FormattedNumber/FormattedNumber.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Heading/Heading.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/HiddenText/HiddenText.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Icon/Icon.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/IconButton/IconButton.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Label/Label.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/LegacyModal/LegacyModal.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Link/Link.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/List/List.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Loader/Loader.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Menu/Menu.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/MenuButton/MenuButton.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Modal/Modal.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/MultiStepIndicator/MultiStepIndicator.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/NumberField/NumberField.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/ProgressBars/ProgressBars/LinearProgressBar/LinearProgressBar.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/RadioButton/RadioButton.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Search/Search.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Skeleton/Skeleton.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Slider/Slider.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/SplitButton/SplitButton.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Steps/Steps.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Switch/Switch.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Table/Table.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Tabs/Tabs.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Text/Text.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/TextArea/TextArea.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/TextField/TextField.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/TextWithHighlight/TextWithHighlight.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/ThemeProvider/ThemeProvider.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Tipseen/Tipseen.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Toast/Toast.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Toggle/Toggle.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Tooltip/Tooltip.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/TransitionView/TransitionView.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/Typography/Typography.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/VirtualizedGrid/VirtualizedGrid.js",
+    webpack: false
+  },
+  {
+    path: "packages/core/dist/src/components/VirtualizedList/VirtualizedList.js",
+    webpack: false
   }
 ];
