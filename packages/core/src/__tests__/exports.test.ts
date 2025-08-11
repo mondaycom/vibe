b53e4fe1882be6c0ae62d016491b2ae0df8fc37a
Tal Koren <talko@monday.com>
2025-08-11 15:41:39 +0300

    test: add snapshot tests for hooks exports

--- packages/core/src/__tests__/exports.test.ts
+++ packages/core/src/__tests__/exports.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import * as components from "../components/index";
 import * as nextComponents from "../components/next";
+import * as hooks from "../hooks";
 
 describe("exports", () => {
   it("should export all components", () => {
@@ -9,5 +10,9 @@ describe("exports", () => {
 
   it("should export next all components", () => {
     expect(Object.keys(nextComponents).sort()).toMatchSnapshot();
+  });
+
+  it("should export all hooks", () => {
+    expect(Object.keys(hooks).sort()).toMatchSnapshot();
   });
 });
