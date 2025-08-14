dc85ac22790df54bba10c4a9f5d907d9d4cdab92
YossiSaadi <YossiSaadi@users.noreply.github.com>
2025-08-14 11:40:31 +0000

    Deploying to gh-pages from @ mondaycom/vibe@a18d7ef186a1e05951455c3bbaa8475459069f0c 🚀

---
+++ assets/Dropdown--mXs65AF.js
@@ -0,0 +1,72 @@
+import{j as o}from"./jsx-runtime-DDzbWKUH.js";import{useMDXComponents as c}from"./index-CXa8ra7y.js";import{M as w}from"./index-C7SVe1CG.js";import{g as x,S as u,M as j}from"./component-description-map-D4vwn7ph.js";import{D as f,O as g,S as D,a as b,R as v,b as y,M as C,A as M,c as S,d as I,e as O,f as z,g as T,h as R,i as k,V as W,j as L,k as P,T as U,l as A,m as N,n as V,o as _}from"./Dropdown.stories-D5mPclL6.js";import{D as l}from"./Dropdown-DHFzz-9F.js";import"./index-Hemj67b4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C42BGM1p.js";import"./index-BUyTjIoM.js";import"./index-D63y3F3s.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./SplitButtonMenu-BvJWIq-n.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./test-ids-utils-9ISiqDto.js";import"./index-BpvXyOxN.js";import"./DialogConstants-DkeX1sw_.js";import"./function-utils-6EQ2u6TA.js";import"./isArray-Dxzbedgu.js";import"./_baseGetTag-ENn7h-yl.js";import"./isObject-C3e4t58V.js";import"./Tooltip-Bnazf5Ct.js";import"./index-NFRdg0hY.js";import"./index-DLqmmtYJ.js";import"./ssr-utils-Do6V6AqB.js";import"./index-8zVcErkr.js";import"./index-8rJYV3Dp.js";import"./CSSTransition-usT7-fC7.js";import"./extends-B6xKY8K9.js";import"./camelCase-B8c2Dc7Y.js";import"./isSymbol-DWIQbemu.js";import"./eq-DPGIUuQ3.js";import"./Icon-CDdqL_gb.js";import"./useMergeRef-DQ4XPFxl.js";import"./useIsomorphicLayoutEffect-FdvxY73P.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-2Q04fxOc.js";import"./Clickable-L_6z1d8K.js";import"./useClickableProps-BnhuEIMR.js";import"./Button-CVjt-pk_.js";import"./Loader-DRZxb8MF.js";import"./sizes-BOsbvv4u.js";import"./DialogContentContainer-ChUwJ3df.js";import"./DropdownChevronDown-hX_A7Og1.js";import"./_tslib-Ct4JumdA.js";import"./useMenuItemKeyboardEvents-Di9oX_vA.js";import"./useFullKeyboardListeners-44yzp6oL.js";import"./index-CXrJR90Q.js";import"./MenuItem-BKxWkhOx.js";import"./useIsOverflowing-DshK7x5b.js";import"./debounce-D-uO1lYf.js";import"./toNumber-CBHRrcgy.js";import"./Label-Bcemx-h6.js";import"./Text-DTQ7koXM.js";import"./TypographyConstants-C-BYnuBK.js";import"./colors-vars-map-DOupSotq.js";import"./Divider-Dxm0fdi1.js";import"./IconButton-BPR1wrSl.js";import"./AddSmall-WZFkNTzJ.js";import"./DropdownChevronRight-DGHOJF1-.js";import"./Check-B_icfwyn.js";import"./Workspace-BIJf5qCK.js";import"./information-box-C2DnEpI4.js";import"./withStaticProps-DibdfTK_.js";import"./link-Dw3-1D-q.js";import"./ButtonGroup-jYx3hw9S.js";import"./Checkbox-CM89_ANP.js";import"./Remove-C7LZQ9Te.js";import"./RadioButton-CdpM8cKT.js";import"./Link-BU7s9Y2d.js";import"./Counter-BYXgd8xQ.js";import"./index-BhJ9w1Ot.js";import"./SwitchTransition-BQCJMqAO.js";import"./Chips-DkPA05uo.js";import"./Avatar-CI3LDg4d.js";import"./index-DxqC8EIR.js";import"./CloseSmall-CIab6kaf.js";import"./Toggle-DMA5Wo8t.js";import"./index-DS1BdwZI.js";import"./TabList-B5QRoTsw.js";import"./useGridKeyboardNavigation-Baegmm3H.js";import"./BreadcrumbItem-CTUSzApy.js";import"./NavigationChevronRight-BM1UJa0v.js";import"./Settings-DOfpeGYp.js";import"./Info-BB4o2YOm.js";import"./AttentionBox-B9qZExDr.js";import"./AlertBannerText-CuPGlsoC.js";import"./Toast-BE8sZ0a7.js";import"./MultiStepIndicator-Cm2hmbat.js";import"./HiddenText-CdPxyfMy.js";import"./TipseenWizard-XztGHs03.js";import"./Steps-C2to6Sun.js";import"./StepsGalleryHeader-DnRIkmMz.js";import"./range-Bu-eLZAw.js";import"./_isIterateeCall-6qSKvFMf.js";import"./isArrayLike-RI9Auh3I.js";import"./toFinite-CwoL-S44.js";import"./TextField-D58unDdX.js";import"./index-CxHVRCQs.js";import"./FieldLabel-BFfWW7Ah.js";import"./NumberField-Clgoko9F.js";import"./BaseInput-D7E1OPHx.js";import"./Search-CijcnBzO.js";import"./Search-BxUBhs64.js";import"./Combobox-fFkMvsn1.js";import"./index-Bs0C4iL5.js";import"./index-BMFTbhVI.js";import"./VirtualizedList-lphc29Jg.js";import"./index.esm-CsnITLvm.js";import"./assertThisInitialized-B9jnkVVz.js";import"./memoize-one.esm-CcMeOnPo.js";import"./react-virtualized-auto-sizer.esm-CuRSY4Lt.js";import"./virtualized-service-D-n4jNAX.js";import"./person1-D9Wcho68.js";import"./person3-B60Gf7uf.js";import"./AvatarGroup-COi-eIcu.js";import"./MenuButton-CwnSGjXE.js";import"./Skeleton-bKf2F5b0.js";import"./Heading-DeiTilVX.js";import"./LinearProgressBar-DsjWtc7N.js";import"./EditableHeading-CaLXuR1p.js";import"./EditableTypography-CK4mvZPL.js";import"./EditableText-Cy3iCqIQ.js";import"./ListItem-BSxeiwAo.js";import"./List-_C2NE_FM.js";import"./Box-CI4IQ3Nw.js";import"./AccordionItem--fHjKi0_.js";import"./ExpandCollapse-DfEFg97U.js";import"./VirtualizedList.stories.helpers-BcKCXt6n.js";import"./tip-BKixvd9t.js";import"./Flex-DaTSFknt.js";import"./ColorPicker-D4PVElDY.js";import"./colors-utils-C1Q1nqOD.js";import"./identity-DKeuBCMA.js";import"./Badge-D5wor_Ci.js";import"./Slider-5lLOUvsI.js";import"./Table.stories.helpers-BN9xgAKI.js";import"./Calendar-BX7mDXzx.js";import"./TableBody-D6nSkuuA.js";import"./SortDescending-B1UwxT6Q.js";import"./VirtualizedGrid.stories.helpers-BS6e0yNq.js";import"./useGridKeyboardNavigationContext.stories.helpers-CaeasHwV.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./MenuGridItem-BGpux8KT.js";import"./index-JSOB3pIc.js";import"./interactions-utils-Dp5ZzG-x.js";import"./isObjectLike-D4qpXYnB.js";import"./lz-string-C2WXLmjK.js";import"./test-utils-CHPNerom.js";import"./uniq-Bz5wUG22.js";import"./index-BXtAqCP_.js";import"./ModalExampleContent-92nzTSEs.js";import"./storybook-link-Db43le0U.js";import"./_tslib-BMUU9Vyh.js";import"./index-cpXe7r3k.js";import"./story-description-B5F6CX2L.js";import"./index-CUPFiJE2.js";import"./Invite-CWwo6O26.js";import"./Table-DKnXfo9h.js";import"./Drag-po37gvvh.js";import"./Wand-Cv7iNunW.js";import"./NewTab-CFmtLVc3.js";import"./ThumbsUp-DnZbk6gE.js";import"./Upgrade-Jax58DJ-.js";import"./LegacyModal-CdPSWnYP.js";import"./LayerProvider-SG5FD3L9.js";import"./createStoryMetaSettingsDecorator-DIIwAjh2.js";import"./inheritsLoose-C4PSNlvh.js";function h(t){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...c(),...t.components},{Canvas:e,ComponentRules:n,Link:p,PropsTable:s,RelatedComponents:d,StorybookLink:m,UsageGuidelines:a}=i;return e||r("Canvas"),n||r("ComponentRules"),p||r("Link"),s||r("PropsTable"),d||r("RelatedComponents"),m||r("StorybookLink"),a||r("UsageGuidelines"),o.jsxs(o.Fragment,{children:[o.jsx(w,{of:f}),`
+`,o.jsx(i.h1,{id:"dropdown",children:"Dropdown"}),`
+`,o.jsx(i.p,{children:"Dropdown present a list of options from which a user can select one or several."}),`
+`,o.jsx(e,{of:g}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { Dropdown } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(s,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(a,{guidelines:["Dropdown menus are typically used when you have 5-8 items to choose from. They're used for navigation or command menus, where an action is initiated based on the selection.","Use a consistent size of form components on the same page. For example, if you are using a medium size dropdown also use the same size text inputs, buttons, and so on.","Avoid having multiple lines of text in a dropdown. If the text is too long for one line, add an ellipsis (…).","When the menu is open, each option in the menu should be the same height as the field.","When organizing dropdown menu items, sort the list in a logical order by putting the most selected option at the top."]}),`
+`,o.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,o.jsx(i.h3,{id:"sizes",children:"Sizes"}),`
+`,o.jsx(i.p,{children:"There are three sizes available: Small, Medium, and Large"}),`
+`,o.jsx(e,{of:D}),`
+`,o.jsx(i.h3,{id:"disabled",children:"Disabled"}),`
+`,o.jsx(e,{of:b}),`
+`,o.jsx(i.h3,{id:"readonly",children:"Readonly"}),`
+`,o.jsx(e,{of:v}),`
+`,o.jsx(i.h3,{id:"rtl",children:"RTL"}),`
+`,o.jsx(e,{of:y}),`
+`,o.jsx(i.h3,{id:"multi-line-states",children:"Multi line states"}),`
+`,o.jsx(i.p,{children:"The Dropdown component supports multiple options selection in two different state - single line and multiple lines."}),`
+`,o.jsx(e,{of:C}),`
+`,o.jsx(i.h3,{id:"async-options",children:"Async Options"}),`
+`,o.jsx(i.p,{children:"The Dropdown component supports async loading of options."}),`
+`,o.jsx(e,{of:M}),`
+`,o.jsx(i.h3,{id:"dropdown-with-avatar",children:"Dropdown with avatar"}),`
+`,o.jsx(e,{of:S}),`
+`,o.jsx(i.h3,{id:"dropdown-with-icon",children:"Dropdown with icon"}),`
+`,o.jsx(e,{of:I}),`
+`,o.jsx(i.h3,{id:"dropdown-with-chip-colors",children:"Dropdown with chip colors"}),`
+`,o.jsx(e,{of:O}),`
+`,o.jsx(i.h3,{id:"dropdown-with-tooltips-on-items",children:"Dropdown with tooltips on items"}),`
+`,o.jsx(e,{of:z}),`
+`,o.jsx(i.h2,{id:"dos-and-donts",children:"Do’s and Don’ts"}),`
+`,o.jsx(n,{rules:[{positive:{component:o.jsx(l,{placeholder:"Placeholder text here",options:[{value:1,label:"Option 1"},{value:2,label:"Option 2"},{value:3,label:"Option 3"}],className:"dropdown-stories-styles_rules-spacing"}),description:"Use the dropdown as a closed component. Users should normally be allowed only to click on the items; search is not recommended, though possible."},negative:{component:o.jsx(l,{menuIsOpen:!0,placeholder:"Placeholder text here",options:[{value:1,label:"Option 1"},{value:2,label:"Option 2"},{value:3,label:"Option 3"}],className:"dropdown-stories-styles_rules-spacing"}),description:o.jsxs(o.Fragment,{children:["Don't keep the dropdown component in open mode as permanent state. If this is a design requirement consider use ",o.jsx(m,{page:"Components/Combobox",children:"Combo box"})," instead."]})}}]}),`
+`,o.jsx(i.h2,{id:"use-cases-and-examples",children:"Use cases and examples"}),`
+`,o.jsx(i.h3,{id:"multiple-dropdown-with-renderers-as-options",children:"Multiple dropdown with renderers as options"}),`
+`,o.jsx(i.p,{children:"Inside the advanced filters, a user can select multiple people from the dropdown menu, and they will be shown as Chips."}),`
+`,o.jsx(e,{of:T}),`
+`,o.jsx(i.h3,{id:"searchable-dropdown",children:"Searchable Dropdown"}),`
+`,o.jsxs(i.p,{children:["In case of multiple options, you can use the ",o.jsx(i.code,{children:"onInputChange"})," prop to allow filtering options."]}),`
+`,o.jsx(e,{of:R}),`
+`,o.jsx(i.h3,{id:"dropdown-with-labels",children:"Dropdown with labels"}),`
+`,o.jsx(i.p,{children:"A dropdown menu can include labels."}),`
+`,o.jsx(e,{of:k}),`
+`,o.jsx(i.h3,{id:"dropdown-with-virtualized-items",children:"Dropdown with virtualized items"}),`
+`,o.jsxs(i.p,{children:["A dropdown with many items, can be virtualized using ",o.jsx(i.code,{children:"isVirtualized"}),"."]}),`
+`,o.jsx(e,{of:W}),`
+`,o.jsx(i.h3,{id:"dropdown-inside-a-form",children:"Dropdown inside a form"}),`
+`,o.jsx(i.p,{children:"A classic dropdown presents options a user needs to choose from."}),`
+`,o.jsx(e,{of:L}),`
+`,o.jsx(i.h3,{id:"dropdown-with-groups",children:"Dropdown with groups"}),`
+`,o.jsx(i.p,{children:"Dropdown menu can be organized into groups. It can be either by group title or divider."}),`
+`,o.jsx(e,{of:P}),`
+`,o.jsx(i.h3,{id:"dropdown-inside-popover",children:"Dropdown inside popover"}),`
+`,o.jsxs(i.p,{children:["Use ",o.jsx("code",{children:'menuPosition="fixed"'}),` prop attribute for displaying dropdown inside popover (like our Modal
+component) or container with overflow hidden content.`]}),`
+`,o.jsx(U,{}),`
+`,o.jsx(e,{of:A}),`
+`,o.jsx(i.h3,{id:"dropdown-with-loading",children:"Dropdown with loading"}),`
+`,o.jsx(i.p,{children:"If importing the dropdown options may take time, you can reflect this to the user using Dropdown isLoading flag with optional custom message."}),`
+`,o.jsx(e,{of:N}),`
+`,o.jsx(i.h3,{id:"dropdown-with-ref",children:"Dropdown with ref"}),`
+`,o.jsxs(i.p,{children:["If you need to programmatically control Dropdown state, use ",o.jsx(i.code,{children:"ref"})," prop. Pay attention that type of returned ref will be ",o.jsx(i.code,{children:"StateManager"})," from ",o.jsx(p,{withoutSpacing:!0,href:"https://react-select.com/home",children:o.jsx(i.code,{children:"react-select"})})," library."]}),`
+`,o.jsx(e,{of:V}),`
+`,o.jsx(i.h3,{id:"dropdown-value-selection",children:"Dropdown value selection"}),`
+`,o.jsxs(i.p,{children:["Control the value selection mechanism with the ",o.jsx("code",{children:"tabSelectsValue"})," (default: true) prop."]}),`
+`,o.jsx(e,{of:_}),`
+`,o.jsx(i.h2,{id:"related-components",children:"Related components"}),`
+`,o.jsx(d,{componentsNames:[x,u,j]})]})}function Ye(t={}){const{wrapper:i}={...c(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(h,{...t})}):h(t)}function r(t,i){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{Ye as default};
--- assets/Playground.stories-8HfJq2ks.js
+++
@@ -1,7 +0,0 @@
-import{R as o}from"./index-Hemj67b4.js";import{u as d,L as m,P as i}from"./index-DnoYtC_8.js";import"./lz-string-C2WXLmjK.js";import"./_commonjsHelpers-BosuxZz1.js";import"./jsx-runtime-DDzbWKUH.js";import"./index-OgocbbL1.js";import"./extends-B6xKY8K9.js";const p=(u,{parameters:n})=>{const e=d(),c=n.playground;return o.createElement(m,{code:e.jsx||"<></>",scope:c.components,enableTypeScript:!0},o.createElement(i,{css:e.css}))},b={title:"Playground",decorators:[p]},r={parameters:{chromatic:{disable:!0}}};var t,a,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  parameters: {
-    chromatic: {
-      disable: true
-    }
-  }
-}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const h=["Playground"];export{r as Playground,h as __namedExportsOrder,b as default};
