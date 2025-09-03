8cffb6091f061fcb8e5111c57182ce397f9dc139
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-09-03 09:33:55 +0000

    Deploying to gh-pages from @ mondaycom/vibe@8124998c5bcf2da19fef3ef2316cd0092c1a473a 🚀

---
+++ storybook-blocks/assets/paragraph-CrrWPXWK.js
@@ -0,0 +1,8 @@
+import{j as o}from"./jsx-runtime-B8889x5w.js";import{useMDXComponents as e}from"./index-ZXiX6yWz.js";import{M as s,C as a,a as i}from"./index-Bql0Dzd8.js";import{P as p,O as m}from"./paragraph.stories-DsK4ReGX.js";import"./index-CeXuxf-f.js";import"./iframe-BatqPVQS.js";import"./index-B3l35gtK.js";import"./index-B0HCylqX.js";import"./index-DrFu-skq.js";import"./paragraph-y8K-Wwnt.js";import"./index-De0AmtSO.js";function t(n){const r={code:"code",h1:"h1",h2:"h2",p:"p",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:p}),`
+`,o.jsx(r.h1,{id:"paragraph",children:"Paragraph"}),`
+`,o.jsx(r.h2,{id:"overview",children:"Overview"}),`
+`,o.jsxs(r.p,{children:[`Paragraph is a component that displays the blocks of text.
+This component can be used as a replacement for `,o.jsx(r.code,{children:"p"}),"."]}),`
+`,o.jsx(a,{of:m}),`
+`,o.jsx(r.h2,{id:"props",children:"Props"}),`
+`,o.jsx(i,{})]})}function g(n={}){const{wrapper:r}={...e(),...n.components};return r?o.jsx(r,{...n,children:o.jsx(t,{...n})}):t(n)}export{g as default};
