76c8259992eb8006a7a2c8bb4ea3743cbca66b3a
YossiSaadi <YossiSaadi@users.noreply.github.com>
2025-09-07 10:41:37 +0000

    Deploying to gh-pages from @ mondaycom/vibe@ad3e2e15f16d89e3a3a53ed677f789115e76c057 🚀

---
+++ assets/Box-BKo1eNvF.js
@@ -0,0 +1,61 @@
+import{j as o}from"./jsx-runtime-DDzbWKUH.js";import{useMDXComponents as s}from"./index-CXa8ra7y.js";import{M as d}from"./index-CWhCj3W_.js";import{F as a,x as c,d as h}from"./component-description-map-EC52k9bO.js";import{l}from"./tip-BKixvd9t.js";import{B as x,O as j,a as f,T as g,b as u,c as b,R,S as C,M as O,P as y,D}from"./Box.stories-DB-sv2PK.js";import{r as B}from"./usage-guidelines-CjZwxEOd.js";import"./index-Hemj67b4.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Br9k9u3y.js";import"./index-J7rFsFsg.js";import"./index-D63y3F3s.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./SplitButtonMenu-C5hDMj5f.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./test-ids-utils-CSfXomCJ.js";import"./index-BpvXyOxN.js";import"./DialogConstants-DkeX1sw_.js";import"./function-utils-DXYBjL2Y.js";import"./Tooltip-CPCyoO7B.js";import"./index-NFRdg0hY.js";import"./index-DLqmmtYJ.js";import"./ssr-utils-Do6V6AqB.js";import"./index-8zVcErkr.js";import"./index-8rJYV3Dp.js";import"./CSSTransition-usT7-fC7.js";import"./extends-B6xKY8K9.js";import"./camelCase-CO1PkVrH.js";import"./Icon-CFgxzlRS.js";import"./useMergeRef-DQ4XPFxl.js";import"./useIsomorphicLayoutEffect-FdvxY73P.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-BEu-Gd_z.js";import"./Clickable-A8m-5-d5.js";import"./useClickableProps-Cl-ObpBN.js";import"./Button-Blr1_T7S.js";import"./Loader-CPSZbvFA.js";import"./sizes-BOsbvv4u.js";import"./DialogContentContainer-lbi8em3I.js";import"./DropdownChevronDown-hX_A7Og1.js";import"./_tslib-Ct4JumdA.js";import"./useMenuItemKeyboardEvents-CHNh2wzx.js";import"./useFullKeyboardListeners-44yzp6oL.js";import"./index-CXrJR90Q.js";import"./MenuItem-DhyaoDMD.js";import"./useIsOverflowing-CkZDmDV4.js";import"./debounce-Bylel5eg.js";import"./Label-C9liG6JK.js";import"./Text-DgwpaQAG.js";import"./TypographyConstants-DfgIEzd_.js";import"./colors-vars-map-DOupSotq.js";import"./Divider-DLAtuxCi.js";import"./IconButton-oEYd-7Ub.js";import"./AddSmall-WZFkNTzJ.js";import"./DropdownChevronRight-DGHOJF1-.js";import"./Check-B_icfwyn.js";import"./Workspace-BIJf5qCK.js";import"./information-box-C2DnEpI4.js";import"./withStaticProps-DibdfTK_.js";import"./link-Dw3-1D-q.js";import"./ButtonGroup-D0qIWLFl.js";import"./Checkbox-COEFL6KU.js";import"./Remove-C7LZQ9Te.js";import"./RadioButton-T0qpPzRU.js";import"./Link-Cm6Ryn9m.js";import"./Counter-YUXpHtNF.js";import"./index-BhJ9w1Ot.js";import"./SwitchTransition-BQCJMqAO.js";import"./Chips-C23c6FsI.js";import"./Avatar-IXwfoNXn.js";import"./index-DxqC8EIR.js";import"./CloseSmall-CIab6kaf.js";import"./Toggle-DQrxWomd.js";import"./index-DS1BdwZI.js";import"./Dropdown-D_z33okR.js";import"./inheritsLoose-C4PSNlvh.js";import"./memoize-one.esm-CcMeOnPo.js";import"./TabList-DR_CZ4Mc.js";import"./useGridKeyboardNavigation-Baegmm3H.js";import"./BreadcrumbItem-BMbuB0j9.js";import"./NavigationChevronRight-BM1UJa0v.js";import"./Settings-DOfpeGYp.js";import"./Info-BB4o2YOm.js";import"./AttentionBox-BqIuMjRP.js";import"./AlertBannerText-u1qDtxFG.js";import"./Toast-CT7iHeBN.js";import"./MultiStepIndicator-l2p1msba.js";import"./HiddenText-9a3j3pQH.js";import"./TipseenWizard-CHbTNvFJ.js";import"./Steps-Qk9zF-Rq.js";import"./StepsGalleryHeader-DRE40DwL.js";import"./isArrayLike-CCcVsAV0.js";import"./toFinite-ClfBq1DX.js";import"./toNumber-DA38wwG6.js";import"./TextField-B-o5fy8T.js";import"./index-V5d9qz7E.js";import"./FieldLabel-C7Hird_h.js";import"./NumberField-Depd8x-Z.js";import"./BaseInput-D7E1OPHx.js";import"./Search-B7giCTNk.js";import"./Search-BxUBhs64.js";import"./Combobox-DWN3EdDn.js";import"./index-Bs0C4iL5.js";import"./index-BMFTbhVI.js";import"./VirtualizedList-BH52-Hmi.js";import"./index.esm-CsnITLvm.js";import"./assertThisInitialized-B9jnkVVz.js";import"./react-virtualized-auto-sizer.esm-CuRSY4Lt.js";import"./virtualized-service-DqzQFWKL.js";import"./person1-D9Wcho68.js";import"./person3-B60Gf7uf.js";import"./AvatarGroup-BDhREbs0.js";import"./MenuButton-Be0OFTQ3.js";import"./Skeleton-Bc-zNCgc.js";import"./Heading-D_N9ChHw.js";import"./LinearProgressBar-BMbwfN2m.js";import"./EditableHeading-56fhpk1c.js";import"./EditableTypography-m71ajX7B.js";import"./EditableText-DrigwV5P.js";import"./ListItem-C9Rbl8XI.js";import"./List-C57MCo16.js";import"./Box-D9cKzrT2.js";import"./AccordionItem-BdV19yUk.js";import"./ExpandCollapse-BRFH31fP.js";import"./VirtualizedList.stories.helpers-BcKCXt6n.js";import"./ColorPicker-Bp8pEI_l.js";import"./colors-utils-C1Q1nqOD.js";import"./Badge-BTXToAhy.js";import"./Slider--STsLPT0.js";import"./Table.stories.helpers-DL9Wj0KK.js";import"./Calendar-BX7mDXzx.js";import"./TableBody-BJoxOnR-.js";import"./SortDescending-B1UwxT6Q.js";import"./VirtualizedGrid.stories.helpers-ctc0JMsN.js";import"./useGridKeyboardNavigationContext.stories.helpers-BpRtUIhz.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./range-D3V44zw2.js";import"./MenuGridItem-DT6MPNgM.js";import"./Flex-DaTSFknt.js";import"./createStoryMetaSettingsDecorator-CZWEfz_r.js";import"./index-VC5anwLh.js";import"./Invite-CWwo6O26.js";import"./Table-DKnXfo9h.js";import"./Drag-po37gvvh.js";import"./Wand-Cv7iNunW.js";import"./NewTab-CFmtLVc3.js";import"./ThumbsUp-DnZbk6gE.js";import"./Upgrade-Jax58DJ-.js";const E=()=>o.jsx(l,{title:"Benefits of the Box component",children:"Easily add Vibe styles without adding css"});function e(t){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",h4:"h4",p:"p",pre:"pre",...s(),...t.components},{Canvas:r,PropsTable:n,RelatedComponents:m}=i;return r||p("Canvas"),n||p("PropsTable"),m||p("RelatedComponents"),o.jsxs(o.Fragment,{children:[o.jsx(d,{of:x}),`
+`,o.jsx(i.h1,{id:"box",children:"Box"}),`
+`,o.jsx(i.p,{children:"Box component is used as a wrapper component."}),`
+`,o.jsxs(i.p,{children:["Its purpose is to help scaffold compositions while using Vibe's prop keys without writing new CSS. ",o.jsx("br",{}),`It can be used as a container for
+atom based compositions, it can accept all Vibe style related props and have a semantic html tag.`]}),`
+`,o.jsx(r,{of:j}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { Box } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(n,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(B,{guidelines:["Use as a styled container","Use to add spacing, borders, and rounded corners to areas of content","Use as an inner component for spacing or styles"]}),`
+`,o.jsx(E,{}),`
+`,o.jsx(i.h2,{id:"utility-props",children:"Utility props"}),`
+`,o.jsx(i.h3,{id:"backgrounds-colors",children:"BACKGROUNDS COLORS"}),`
+`,o.jsx(r,{of:f}),`
+`,o.jsx(i.h3,{id:"text-colors",children:"TEXT COLORS"}),`
+`,o.jsx(r,{of:g}),`
+`,o.jsx(i.h3,{id:"border",children:"BORDER"}),`
+`,o.jsx(r,{of:u}),`
+`,o.jsx(i.h3,{id:"border-color",children:"BORDER COLOR"}),`
+`,o.jsx(r,{of:b}),`
+`,o.jsx(i.h3,{id:"rounded-corners",children:"ROUNDED CORNERS"}),`
+`,o.jsx(i.h4,{id:"size-props",children:"Size props"}),`
+`,o.jsx(r,{of:R}),`
+`,o.jsx(i.h3,{id:"shadow",children:"SHADOW"}),`
+`,o.jsx(r,{of:C}),`
+`,o.jsx(i.h3,{id:"margin",children:"MARGIN"}),`
+`,o.jsx(i.h4,{id:"size-props-1",children:"Size props"}),`
+`,o.jsx(r,{of:O}),`
+`,o.jsx(i.h4,{id:"property-variations-per-each-size",children:"Property variations per each size:"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{children:`margin
+marginX (x axis: left, right)
+marginY (y axis: top, bottom)
+marginTop
+marginEnd
+marginBottom
+marginStart
+`})}),`
+`,o.jsx(i.h4,{id:"aligning-auto-and-none",children:"Aligning Auto and none"}),`
+`,o.jsx(i.p,{children:"each property variation can hold additional values for:"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{children:`AUTO
+NONE
+`})}),`
+`,o.jsx(i.h3,{id:"padding",children:"PADDING"}),`
+`,o.jsx(i.h4,{id:"size-props-2",children:"Size props"}),`
+`,o.jsx(r,{of:y}),`
+`,o.jsx(i.h4,{id:"property-variations-per-each-size-1",children:"Property variations per each size:"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{children:`padding
+paddingX (x axis: left, right)
+paddingY (y axis: top, bottom)
+paddingTop
+paddingEnd
+paddingBottom
+paddingStart
+`})}),`
+`,o.jsx(i.h3,{id:"component-disabled",children:"Component Disabled"}),`
+`,o.jsx(r,{of:D}),`
+`,o.jsx(i.h2,{id:"related-components",children:"Related components"}),`
+`,o.jsx(m,{componentsNames:[a,c,h]})]})}function gr(t={}){const{wrapper:i}={...s(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(e,{...t})}):e(t)}function p(t,i){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{gr as default};
