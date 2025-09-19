54d7329c051736d42820386d61a8b16981c60793
talkor <talkor@users.noreply.github.com>
2025-09-19 14:25:20 +0000

    Deploying to gh-pages from @ mondaycom/vibe@7d71e17996446f71c784a2a98be92e69f716dd1e 🚀

---
+++ assets/Clickable.stories-B7dCRLSj.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-lwGtIXvq.js";import{C as r}from"./Clickable-HM5W4vbd.js";import{F as _}from"./Flex-D6jv3OvD.js";import{i as f,g as s,p as c,e as n}from"./interactions-utils-Dfct-TkD.js";import{r as v}from"./interactions-helper-BLVzu_Hd.js";import{N as d}from"./test-ids-utils-CSfXomCJ.js";import{B as i}from"./Box-CjIlZ8OJ.js";async function S(a){const C="clickable button",y="disabled clickable button",E=await s(a,C),x=await s(a,y);await c(d.TAB),n(document.activeElement).toEqual(E),await c(d.TAB),n(document.activeElement).not.toEqual(x),n(a).not.toContain(document.activeElement)}const o=f({tests:[S],afterEach:async()=>{await v()}});try{o.displayName="statesPlaySuite",o.__docgenInfo={description:"",displayName:"statesPlaySuite",props:{canvasElement:{defaultValue:null,description:"",name:"canvasElement",required:!0,type:{name:'Screen<typeof import("/home/runner/work/vibe/vibe/node_modules/@testing-library/react/node_modules/@testing-library/dom/types/queries")>'}},args:{defaultValue:null,description:"",name:"args",required:!0,type:{name:"Record<string, any>"}}}}}catch{}const B={title:"Accessibility/Clickable",component:r},L=a=>e.jsx(r,{onClick:()=>alert("clicked"),...a,children:e.jsx(i,{border:!0,padding:"small",rounded:"small",children:"I act like a button"})}),l={render:L.bind({}),name:"Overview",parameters:{docs:{liveEdit:{isEnabled:!1}}}},t={render:()=>e.jsxs(_,{gap:"medium",children:[e.jsx(r,{onClick:()=>alert("clicked"),ariaLabel:"clickable button",children:e.jsx(i,{border:!0,padding:"small",rounded:"small",children:"Regular clickable element"})}),e.jsx(r,{onClick:()=>alert("clicked"),disabled:!0,ariaLabel:"disabled clickable button",children:e.jsx(i,{border:!0,backgroundColor:"greyBackgroundColor",padding:"small",rounded:"small",children:"Disabled clickable element"})})]}),name:"States",play:o};var m,u,b;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: clickableTemplate.bind({}),
+  name: "Overview",
+  parameters: {
+    docs: {
+      liveEdit: {
+        isEnabled: false
+      }
+    }
+  }
+}`,...(b=(u=l.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};var p,k,g;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <Flex gap="medium">
+      <Clickable onClick={() => alert("clicked")} ariaLabel="clickable button">
+        <Box border padding="small" rounded="small">
+          Regular clickable element
+        </Box>
+      </Clickable>
+      <Clickable onClick={() => alert("clicked")} disabled ariaLabel="disabled clickable button">
+        <Box border backgroundColor="greyBackgroundColor" padding="small" rounded="small">
+          Disabled clickable element
+        </Box>
+      </Clickable>
+    </Flex>,
+  name: "States",
+  play: statesPlaySuite
+}`,...(g=(k=t.parameters)==null?void 0:k.docs)==null?void 0:g.source}}};const j=["Overview","States"],I=Object.freeze(Object.defineProperty({__proto__:null,Overview:l,States:t,__namedExportsOrder:j,default:B},Symbol.toStringTag,{value:"Module"}));export{I as C,l as O,t as S};
---
+++ assets/Table-DEjZRuOU.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as C}from"./index-BfNsOeqz.js";import{M as w}from"./index-DujXSKTw.js";import{T as s,a,b as l,c as d,d as p,e as t}from"./TableBody-D_tnfbAr.js";import{a as g}from"./Label-ByO7B-K0.js";import{d as m,a as T,s as y,T as c,b as h,c as x,e as v}from"./Table.stories.helpers-DtGW1hFL.js";import{T as R,O as D,S as k,B as z,a as B,L as H,b as M,V as L,c as O,H as A}from"./Table.stories-BY_P2lEO.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DOgmaNu6.js";import"./index-B1y4hBsf.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./index-BpvXyOxN.js";import"./test-ids-utils-CSfXomCJ.js";import"./useMergeRef-C008K4k9.js";import"./useIsomorphicLayoutEffect-CtfxbExD.js";import"./ssr-utils-Do6V6AqB.js";import"./withStaticProps-DfSG2La2.js";import"./Icon-B1P6-uxB.js";import"./index-DEnqPy1V.js";import"./index-nBaTNWgC.js";import"./function-utils-CSFuT5hl.js";import"./noop-DX6rZLP_.js";import"./IconButton-C4r7nQVz.js";import"./Tooltip-Cf28_m_C.js";import"./index-D4TxD6E0.js";import"./typesciptCssModulesHelper-oFWQo8l_.js";import"./index-Cgi7_mab.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./camelCase-CO1PkVrH.js";import"./DialogConstants-DkeX1sw_.js";import"./Flex-D6jv3OvD.js";import"./Clickable-HM5W4vbd.js";import"./useClickableProps-JhSuVvy2.js";import"./sizes-BOsbvv4u.js";import"./Button-lmknnYep.js";import"./Loader-BPB1GClv.js";import"./AddSmall-DFewX5rK.js";import"./_tslib-Ct4JumdA.js";import"./Text-1SYDbpP1.js";import"./TypographyConstants-B8jegsGx.js";import"./useIsOverflowing-CDKfAaU8.js";import"./debounce-D3NSP8gs.js";import"./Info-jvFNh0HS.js";import"./Skeleton-BWuSK9Pd.js";import"./SortDescending-DfYW2xR2.js";import"./colors-vars-map-DOupSotq.js";import"./Avatar-Bn0v7pS3.js";import"./Calendar-NzkLrIBg.js";import"./Workspace-DtDO7RvQ.js";import"./TableVirtualizedBody-C19Yd4W8.js";import"./index.esm-B4DTZ0aw.js";import"./assertThisInitialized-B9jnkVVz.js";import"./memoize-one.esm-CcMeOnPo.js";import"./react-virtualized-auto-sizer.esm-CJUwJr1b.js";import"./createStoryMetaSettingsDecorator-M9remOuO.js";import"./index-CkcRWdy2.js";import"./Invite-BJsiPZ1j.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-WHGZN_3r.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./ThumbsUp-WQ1WzAOd.js";import"./Search-ClaS4_rs.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";function S(n){const o={code:"code",div:"div",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...C(),...n.components},{Canvas:r,ComponentRules:u,PropsTable:b,RelatedComponents:f}=o;return r||j("Canvas"),u||j("ComponentRules"),b||j("PropsTable"),f||j("RelatedComponents"),e.jsxs(e.Fragment,{children:[e.jsx(w,{of:R}),`
+`,e.jsx(o.h1,{id:"table",children:"Table"}),`
+`,e.jsx(o.p,{children:"Tables are used to organize data, making it easier to understand."}),`
+`,e.jsx(r,{of:D}),`
+`,e.jsx(o.h3,{id:"import",children:"Import"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-js",children:`import { Table, TableHeader, TableHeaderCell TableBody, TableRow, TableCell } from "@vibe/core";
+`})}),`
+`,e.jsx(o.h2,{id:"props",children:"Props"}),`
+`,e.jsx(b,{}),`
+`,e.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(o.h3,{id:"sizes",children:"Sizes"}),`
+`,e.jsx(o.p,{children:"The table is available in 3 different row heights: small (32px) medium (40px) and large (48 px). Medium size is the default size."}),`
+`,e.jsx(r,{of:k}),`
+`,e.jsx(o.h3,{id:"borders",children:"Borders"}),`
+`,e.jsx(o.p,{children:"The table is available with or without an outer border. When using a table inside another component (like a modal or dialog), remove the table's outer border for a cleaner look."}),`
+`,e.jsx(r,{of:z}),`
+`,e.jsx(o.h2,{id:"table-header-functionality",children:"Table header functionality"}),`
+`,e.jsx(o.p,{children:"Sorting, Icons and Information added to selected columns"}),`
+`,e.jsx(r,{of:B}),`
+`,e.jsx(o.h2,{id:"loading",children:"Loading"}),`
+`,e.jsx(o.p,{children:"Using skeleton"}),`
+`,e.jsx(r,{of:H}),`
+`,e.jsx(o.h2,{id:"scroll",children:"Scroll"}),`
+`,e.jsx(o.p,{children:"Table with both vertical and horizontal scroll"}),`
+`,e.jsx(r,{of:M}),`
+`,e.jsx(o.h2,{id:"virtualized-scroll",children:"Virtualized Scroll"}),`
+`,e.jsx(o.p,{children:"This is an example of a table with 5000 rows"}),`
+`,e.jsx(r,{of:L}),`
+`,e.jsx(o.h2,{id:"sticky-column",children:"Sticky column"}),`
+`,e.jsx(o.p,{children:"Use sticky column in your table when you want to keep specific column visible while the users scroll horizontally."}),`
+`,e.jsx(r,{of:O}),`
+`,e.jsx(o.h2,{id:"highlighted-row",children:"Highlighted row"}),`
+`,e.jsx(o.p,{children:`Use a highlighted row to mark a single row of the table.
+A highlighted row allows adding additional information for the entire row, using a system trigger such as a side-panel or model.`}),`
+`,e.jsx(r,{of:A}),`
+`,e.jsx(o.h2,{id:"dos-and-donts",children:"Do’s and Don’ts"}),`
+`,e.jsx(u,{rules:[{positive:{component:e.jsx(o.div,{style:{width:"95%"},children:e.jsxs(s,{errorState:e.jsx(h,{}),emptyState:e.jsx(c,{}),columns:m,children:[e.jsx(a,{children:m.map(i=>e.jsx(l,{...i},i.id))}),e.jsx(d,{children:T.map(i=>e.jsxs(p,{children:[e.jsx(t,{children:i.sentOn}),e.jsx(t,{children:i.subject}),e.jsx(t,{children:e.jsx(g,{text:i.status,color:y[i.status]})})]},i.id))})]})}),description:"If there’s a need to insert an icon, use for all columns."},negative:{component:e.jsx(o.div,{style:{width:"95%"},children:e.jsxs(s,{errorState:e.jsx(h,{}),emptyState:e.jsx(c,{}),columns:m,children:[e.jsx(a,{children:m.map(i=>e.jsx(l,{...i,icon:i.id==="subject"?i.icon:void 0},i.id))}),e.jsx(d,{children:T.map(i=>e.jsxs(p,{children:[e.jsx(t,{children:i.sentOn}),e.jsx(t,{children:i.subject}),e.jsx(t,{children:e.jsx(g,{text:i.status,color:y[i.status]})})]},i.id))})]})}),description:"Don't use icons if not applied to all columns titles."}},{positive:{component:e.jsx(o.div,{style:{width:"95%"},children:e.jsxs(s,{errorState:e.jsx(h,{}),emptyState:e.jsx(c,{}),columns:x,withoutBorder:!0,children:[e.jsx(a,{children:x.map(i=>e.jsx(l,{...i},i.id))}),e.jsx(d,{children:v.map(i=>e.jsxs(p,{children:[e.jsx(t,{children:i.sentOn}),e.jsx(t,{children:i.subject}),e.jsx(t,{children:i.emailsSent})]},i.id))})]})}),description:"If there’s a need, remove only the outer border."},negative:{component:e.jsx(o.div,{style:{width:"95%","--layout-border-color":"transparent"},children:e.jsxs(s,{errorState:e.jsx(h,{}),emptyState:e.jsx(c,{}),columns:x,withoutBorder:!0,children:[e.jsx(a,{children:x.map(i=>e.jsx(l,{...i,icon:i.id==="subject"?i.icon:void 0},i.id))}),e.jsx(d,{children:v.map(i=>e.jsxs(p,{children:[e.jsx(t,{children:i.sentOn}),e.jsx(t,{children:i.subject}),e.jsx(t,{children:i.emailsSent})]},i.id))})]})}),description:"Don't remove border between the rows."}}]}),`
+`,e.jsx(o.h2,{id:"related-components",children:"Related components"}),`
+`,e.jsx(f,{componentsNames:["Label","IconButton","Skeleton"]})]})}function ni(n={}){const{wrapper:o}={...C(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(S,{...n})}):S(n)}function j(n,o){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{ni as default};
