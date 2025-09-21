5e38df614570a5696edf3e8cfde0eb13ca83a577
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-09-21 09:28:16 +0000

    Deploying to gh-pages from @ mondaycom/vibe@114409d20057bd96ded7c7583fb539541657da6d 🚀

---
+++ assets/BaseListItem.stories-CEUbbYfc.js
@@ -0,0 +1,20 @@
+import{B as r}from"./BaseListItem-D0c7WVcQ.js";import{a as p}from"./Settings-WHGZN_3r.js";import{r as n}from"./createComponentTemplate-B08h-OOW.js";import{c as a}from"./createStoryMetaSettingsDecorator-M9remOuO.js";import"./jsx-runtime-lwGtIXvq.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";import"./typesciptCssModulesHelper-oFWQo8l_.js";import"./Text-CYK--k2a.js";import"./TypographyConstants-CTtoteLy.js";import"./useMergeRef-C008K4k9.js";import"./useIsomorphicLayoutEffect-CtfxbExD.js";import"./ssr-utils-Do6V6AqB.js";import"./test-ids-utils-CSfXomCJ.js";import"./useIsOverflowing-CDKfAaU8.js";import"./debounce-D3NSP8gs.js";import"./Tooltip-BrDhcDk7.js";import"./index-BrqHMYbN.js";import"./function-utils-CSFuT5hl.js";import"./index-D4TxD6E0.js";import"./index-DEnqPy1V.js";import"./index-nBaTNWgC.js";import"./index-Cgi7_mab.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./camelCase-CO1PkVrH.js";import"./DialogConstants-DkeX1sw_.js";import"./Icon-B1P6-uxB.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-D6jv3OvD.js";import"./Clickable-HM5W4vbd.js";import"./useClickableProps-JhSuVvy2.js";import"./Avatar-B6jc8wDZ.js";import"./colors-vars-map-DOupSotq.js";import"./_tslib-Ct4JumdA.js";import"./index-CkcRWdy2.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-DtDO7RvQ.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./ThumbsUp-WQ1WzAOd.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";const s=""+new URL("person-02JXxArq.png",import.meta.url).href,o=a({component:r}),yt={title:"Internal/BaseListItem",component:r,argTypes:o.argTypes,decorators:o.decorators,tags:["internal"]},l=n(r),t={render:l.bind({}),args:{item:{value:"item1",label:"This is a list item",startElement:{type:"avatar",value:s},endElement:{type:"icon",value:p},tooltipProps:{content:"tooltip content"}}}};var i,e,m;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: baseListItemTemplate.bind({}),
+  args: {
+    item: {
+      value: "item1",
+      label: "This is a list item",
+      startElement: {
+        type: "avatar",
+        value: person1
+      },
+      endElement: {
+        type: "icon",
+        value: Email
+      },
+      tooltipProps: {
+        content: "tooltip content"
+      }
+    }
+  }
+}`,...(m=(e=t.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const bt=["Overview"];export{t as Overview,bt as __namedExportsOrder,yt as default};
---
+++ assets/Playground.stories-CVvV2FyJ.js
@@ -0,0 +1,7 @@
+import{R as o}from"./index-CTZeEbLr.js";import{u as d,L as m,P as i}from"./index-i-Hdf55_.js";import"./lz-string-C2WXLmjK.js";import"./index-B0s6aYCE.js";import"./_commonjsHelpers-BosuxZz1.js";import"./jsx-runtime-lwGtIXvq.js";import"./extends-B6xKY8K9.js";import"./iframe-y_JMgX8b.js";const p=(u,{parameters:n})=>{const e=d(),c=n.playground;return o.createElement(m,{code:e.jsx||"<></>",scope:c.components,enableTypeScript:!0},o.createElement(i,{css:e.css}))},h={title:"Playground",decorators:[p]},r={parameters:{chromatic:{disable:!0}}};var t,a,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  parameters: {
+    chromatic: {
+      disable: true
+    }
+  }
+}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const w=["Playground"];export{r as Playground,w as __namedExportsOrder,h as default};
