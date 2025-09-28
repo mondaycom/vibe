70096ebc5a30a0546d68f611093e5ebe1156f710
YossiSaadi <YossiSaadi@users.noreply.github.com>
2025-09-28 18:23:35 +0000

    Deploying to gh-pages from @ mondaycom/vibe@95ea4d98c02a26c59e8ae90fc0466ca858c57520 🚀

---
+++ assets/ListItem-BI-Pn1wt.js
@@ -0,0 +1,20 @@
+import{j as t}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as s}from"./index-BfNsOeqz.js";import{M as a,C as r}from"./index-ljzOf1Os.js";import{L as d,O as c,S as h,a as l,W as x,b as j}from"./ListItem.stories-D3PrFGkT.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DN78gZax.js";import"./index-YXLR5Xgo.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./ListItem-DxEJEwjh.js";import"./index-BpvXyOxN.js";import"./typesciptCssModulesHelper-oFWQo8l_.js";import"./Text-DYuGGc4e.js";import"./TypographyConstants-DUhdqI_I.js";import"./useMergeRef-C008K4k9.js";import"./useIsomorphicLayoutEffect-CtfxbExD.js";import"./ssr-utils-Do6V6AqB.js";import"./test-ids-utils-B1IbFLmr.js";import"./useIsOverflowing-CDKfAaU8.js";import"./debounce-D3NSP8gs.js";import"./Tooltip-C-Jn4wPI.js";import"./function-utils-CSFuT5hl.js";import"./index-D4TxD6E0.js";import"./index-DEnqPy1V.js";import"./index-nBaTNWgC.js";import"./index-Cgi7_mab.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./camelCase-CO1PkVrH.js";import"./DialogConstants-DkeX1sw_.js";import"./Icon-MuJFX10s.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-DfSG2La2.js";import"./Flex-Brm3wHG7.js";import"./Clickable-CzbLyDBE.js";import"./useClickableProps-DFA_eb5O.js";import"./sizes-BOsbvv4u.js";import"./ListItemIcon-CdG0j8Ny.js";import"./Avatar-CzJ-Evuw.js";import"./colors-vars-map-DOupSotq.js";import"./index-hfu5psMO.js";import"./createComponentTemplate-B08h-OOW.js";import"./index-CkcRWdy2.js";import"./_tslib-Ct4JumdA.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-DtDO7RvQ.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-WHGZN_3r.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./ThumbsUp-WQ1WzAOd.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./person1-D9Wcho68.js";import"./createStoryMetaSettingsDecorator-M9remOuO.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";function p(o){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...s(),...o.components},{PropsTable:e,UsageGuidelines:m}=i;return e||n("PropsTable"),m||n("UsageGuidelines"),t.jsxs(t.Fragment,{children:[t.jsx(a,{of:d}),`
+`,t.jsx(i.h1,{id:"listitem",children:"ListItem"}),`
+`,t.jsx(i.p,{children:"An item of a List component"}),`
+`,t.jsx(r,{of:c}),`
+`,t.jsx(i.h3,{id:"import",children:"Import"}),`
+`,t.jsx(i.pre,{children:t.jsx(i.code,{className:"language-js",children:`import { ListItem, ListItemIcon, ListItemAvatar } from "@vibe/core";
+`})}),`
+`,t.jsx(i.h2,{id:"props",children:"Props"}),`
+`,t.jsx(e,{}),`
+`,t.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(m,{guidelines:["List item can include an ListItemIcon or text.","List item icon should always be displayed before the item's text (left or right depending on the item's text language)"]}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"states",children:"States"}),`
+`,t.jsx(r,{of:h}),`
+`,t.jsx(i.h3,{id:"sizes",children:"Sizes"}),`
+`,t.jsx(r,{of:l}),`
+`,t.jsx(i.h3,{id:"list-item-with-an-icon",children:"List item with an icon"}),`
+`,t.jsx(r,{of:x}),`
+`,t.jsx(i.h3,{id:"list-item-with-an-avatar",children:"List item with an avatar"}),`
+`,t.jsx(r,{of:j})]})}function Pt(o={}){const{wrapper:i}={...s(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(p,{...o})}):p(o)}function n(o,i){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{Pt as default};
---
+++ assets/information-box-Bu6im9x4.js
@@ -0,0 +1,8 @@
+import{j as o}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as i}from"./index-BfNsOeqz.js";import{M as e,C as s,a as m}from"./index-ljzOf1Os.js";import{I as p,O as a}from"./information-box.stories-CFcMZprb.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DN78gZax.js";import"./index-YXLR5Xgo.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./index-BpvXyOxN.js";import"./link-CmdT0LNT.js";import"./Link-BQlPfOEJ.js";import"./withStaticProps-CwB-IjhW.js";function r(t){const n={h1:"h1",h2:"h2",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:p}),`
+`,o.jsx(n.h1,{id:"informationbox",children:"InformationBox"}),`
+`,o.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,o.jsx(n.p,{children:`The InformationBox component is used to display an information box.
+It allows you to provide the title, component, link and description.`}),`
+`,o.jsx(s,{of:a}),`
+`,o.jsx(n.h2,{id:"props",children:"Props"}),`
+`,o.jsx(m,{})]})}function D(t={}){const{wrapper:n}={...i(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(r,{...t})}):r(t)}export{D as default};
