9787fd577d03fbd188a4e0155b562986b615fa44
Yossi Saadi <yossisa@monday.com>
2025-09-28 17:15:28 +0300

    docs(IconButton): suggest using Info component

--- packages/core/src/components/IconButton/__stories__/IconButton.stories.helpers.tsx
+++ packages/core/src/components/IconButton/__stories__/IconButton.stories.helpers.tsx
@@ -11,9 +11,19 @@ export const TipCheckYourself = () => (
   </Tip>
 );
 
+export const TipInfo = () => (
+  <Tip>
+    If you need to use an icon as a button that opens info dialog with additional information, check out{" "}
+    <StorybookLink page="Components/Info" size={StorybookLink.sizes.SMALL}>
+      Info
+    </StorybookLink>{" "}
+    component.
+  </Tip>
+);
+
 export const TipMenuButton = () => (
   <Tip>
-    If you need to use an icon as a button that opens dialog or menu next to it, check out{" "}
+    If you need to use an icon as a button that opens menu next to it, check out{" "}
     <StorybookLink page="Components/MenuButton" size={StorybookLink.sizes.SMALL}>
       Menu button
     </StorybookLink>{" "}
