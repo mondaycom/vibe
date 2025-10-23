8a814fa2b951f4de27dd7154b6d50934c0127bae
Tal Koren <talko@monday.com>
2025-10-23 16:49:47 +0300

    remove files

--- packages/core/src/components/ThemeProvider/__stories__/product-themes/index.ts
+++
@@ -1,4 +0,0 @@
-export { productTheme1 } from "./product-theme-1";
-export { productTheme2 } from "./product-theme-2";
-export { productTheme4 } from "./product-theme-4";
-export { productTheme3 } from "./product-theme-3";
