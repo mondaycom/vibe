4e78c245286c3ac9a1558aaf062bbdcced220967
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-11-06 11:00:57 +0000

    Deploying to gh-pages from @ mondaycom/vibe@6c414e65054f03580caae3d776a0871f409d78d6 🚀

---
+++ assets/ModalMediaLayout-Dx8LvyZJ.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as c}from"./index-BfNsOeqz.js";import{M as h}from"./index-BQlb4TL5.js";import{M as u,O as x,W as f,A as g,H as j,a as w}from"./ModalMediaLayout.stories--KWke1I-.js";import{s as M}from"./Modal.stories.helpers-Bo4jpykM.js";import{l as y}from"./tip-CK87uV3P.js";import{m as p}from"./storybook-link-CMzDsKRG.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bm99eG_4.js";import"./index-BZVMxQPF.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./createStoryMetaSettingsDecorator-6la21Z9h.js";import"./index-BsvsTenc.js";import"./_tslib-Ct4JumdA.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-D_pV6T_J.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-DqG0P6p1.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Email-TZY0cRuW.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./ModalContent-lmwmNPUA.js";import"./index-BpvXyOxN.js";import"./test-ids-utils-BiWUJdxm.js";import"./motion-CPj5JhzT.js";import"./tslib.es6-DtQqIFM0.js";import"./Flex-Bmm6kMUW.js";import"./useMergeRef-Do2VYePL.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./Clickable-tjPirwzH.js";import"./useClickableProps-lXORseOQ.js";import"./function-utils-C5K4iOy1.js";import"./noop-DX6rZLP_.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./withStaticProps-DfSG2La2.js";import"./camelCase-CO1PkVrH.js";import"./Tooltip-CsLdkkzH.js";import"./index-Bt5DuqPA.js";import"./index-CkU0kzyk.js";import"./index-gB5zY9qh.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./DialogConstants-DkeX1sw_.js";import"./Icon-BKjd_X8Z.js";import"./Button-DTdAuCzP.js";import"./Loader-DOG4Zj1G.js";import"./StepsGalleryHeader-D-kV4Zxp.js";import"./range-D3V44zw2.js";import"./IconButton-BSIUJ3oq.js";import"./sizes-BOsbvv4u.js";import"./Heading-CjYMXRCF.js";import"./TypographyConstants-CngYclft.js";import"./useIsOverflowing-CqkoFCDi.js";import"./debounce-D3NSP8gs.js";import"./Text-BnWTa7DZ.js";import"./ModalMedia-BsmGf5Yx.js";import"./ModalMediaLayout-FRJzGud8.js";import"./useWizard-C20oyI_-.js";import"./Link-CJY64EMW.js";import"./Flex-qb9MRXYX.js";import"./withStaticProps-DibdfTK_.js";import"./_tslib-BMUU9Vyh.js";import"./link-CVYAeutz.js";import"./index-Cz_-5W6r.js";const b=""+new URL("ratio-do-CPswYwxJ.png",import.meta.url).href,v=""+new URL("ratio-dont-CfgM5DTX.png",import.meta.url).href,C=()=>e.jsx("div",{style:{marginTop:40},children:e.jsxs(y,{children:["If your content is scrollable or wide (you need more space), consider using"," ",e.jsx(p,{page:"Components/Modal [New]/Basic modal",size:p.sizes.SMALL,children:"Basic modal"}),"."]})});function l(t){const o={code:"code",h1:"h1",h2:"h2",h3:"h3",img:"img",p:"p",pre:"pre",...c(),...t.components},{Canvas:i,ComponentRules:r,PropsTable:a,RelatedComponents:s,StorybookLink:m,UsageGuidelines:d}=o;return i||n("Canvas"),r||n("ComponentRules"),a||n("PropsTable"),s||n("RelatedComponents"),m||n("StorybookLink"),d||n("UsageGuidelines"),e.jsxs(e.Fragment,{children:[e.jsx(h,{of:u}),`
+`,e.jsx(o.h1,{id:"media-modal",children:"Media Modal"}),`
+`,e.jsx(o.p,{children:"The Media Modal includes a highlighted media section, followed by a textual content area. This modal is intended for cases when you need to catch the user’s attention using visual elements before they interact with the text. It’s ideal for introducing new features or short onboarding flows."}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx(o.h3,{id:"import",children:"Import"}),`
+`,e.jsxs(o.p,{children:["All the Media Modal layout components can be imported from ",e.jsx(o.code,{children:"@vibe/core/next"}),"."]}),`
+`,e.jsxs(o.p,{children:["Notice not to import components directly from the ",e.jsx(o.code,{children:"@vibe/core"})," package, as they are deprecated."]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-ts",children:`import {
+  Modal,
+  ModalHeader,
+  ModalContent,
+  ModalMedia,
+  ModalFooter,
+  ModalFooterWizard,
+  ModalMediaLayout
+} from "@vibe/core/next";
+`})}),`
+`,e.jsx(o.h2,{id:"props",children:"Props"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(o.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(d,{guidelines:["Use when you want to prioritize images, videos, or other media before users engage with the text below.","Use for product highlights, feature releases, or any guides that require visuals first, followed by explanatory text."]}),`
+`,e.jsx(C,{}),`
+`,e.jsx(o.h2,{id:"use-cases-and-examples",children:"Use cases and examples"}),`
+`,e.jsx(o.h3,{id:"wizard-modal",children:"Wizard modal"}),`
+`,e.jsx(o.p,{children:"For modals with multiple steps, use the ModalFooterWizard."}),`
+`,e.jsx(i,{of:f}),`
+`,e.jsx(o.h3,{id:"announcement-modal",children:"Announcement modal"}),`
+`,e.jsx(o.p,{children:"Announcement modals used to effectively introduce new features or updates to users in a focused and engaging way. It is the media modal with predefine properties. To use an announcement modal, simply copy the code provided below."}),`
+`,e.jsx(i,{of:g}),`
+`,e.jsx(o.h3,{id:"header-with-extra-icon-button",children:"Header with extra icon button"}),`
+`,e.jsxs(o.p,{children:[`In case of a need of an icon button in the modal header, you can use our default header "Action slot".
+You can also use it as a `,e.jsx(m,{page:"Buttons/MenuButton",children:"menu button"})," component."]}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx(o.h3,{id:"animation",children:"Animation"}),`
+`,e.jsx(o.p,{children:"Each modal includes an animation type based on its entrance point, with wizard modals also featuring transition animations. The default is the element trigger animation, which can be replaced with a center pop animation if there's no specific trigger. Transition animation is used exclusively for wizard modals and cannot be changed or removed."}),`
+`,e.jsx(i,{of:w}),`
+`,e.jsx(o.h2,{id:"dos-and-donts",children:"Do's and don'ts"}),`
+`,e.jsx(r,{rules:[{componentContainerClassName:M.largeComponentRule,positive:{component:e.jsx(o.img,{src:b,alt:"modal with nice ratio between image to content and footer",style:{maxWidth:"80%"}}),description:"Keep a balanced ratio between the media section, to the content and footer section."},negative:{component:e.jsx(o.img,{src:v,alt:"modal with bad ratio between image to content and footer, image is very small",style:{maxWidth:"100%"}}),description:"Don't create a media that will be too small or too big for the modal width, as it create unbalanced look."}}]}),`
+`,e.jsx(o.h2,{id:"related-components",children:"Related components"}),`
+`,e.jsx(s,{componentsNames:["ModalSideBySideLayout","ModalBasicLayout","Tipseen"]})]})}function eo(t={}){const{wrapper:o}={...c(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(l,{...t})}):l(t)}function n(t,o){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{eo as default};
