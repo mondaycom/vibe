d278cafabc705e187b82ca0a1637d2f5f09fb02e
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-11-11 11:24:02 +0000

    Deploying to gh-pages from @ mondaycom/vibe@936d59d544718366bbb154d7ec6a722d594e50ef 🚀

---
+++ assets/Steps-2hw75C5n.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as m}from"./index-BfNsOeqz.js";import{M as a}from"./index-Dm1n4anH.js";import{S as d,O as c,T as h,a as l,b as x,c as j,N as u,d as f}from"./Steps.stories-BAB9Nk7-.js";import{r as g}from"./usage-guidelines-DaF3e-2f.js";import{a as y}from"./component-rules-Bcb_N_7h.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B7LUXjQL.js";import"./index-BO5_ldJP.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./Steps-DdQT6hJc.js";import"./index-BpvXyOxN.js";import"./function-utils-C5K4iOy1.js";import"./useMergeRef-Do2VYePL.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./test-ids-utils-BiWUJdxm.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./_tslib-Ct4JumdA.js";import"./camelCase-CO1PkVrH.js";import"./Button-DTdAuCzP.js";import"./Loader-DOG4Zj1G.js";import"./Icon-BKjd_X8Z.js";import"./noop-DX6rZLP_.js";import"./StepsGalleryHeader-D-kV4Zxp.js";import"./range-D3V44zw2.js";import"./Text-BnWTa7DZ.js";import"./TypographyConstants-CngYclft.js";import"./useIsOverflowing-CqkoFCDi.js";import"./debounce-D3NSP8gs.js";import"./Tooltip-CsLdkkzH.js";import"./index-Bt5DuqPA.js";import"./index-CkU0kzyk.js";import"./index-gB5zY9qh.js";import"./useClickableProps-lXORseOQ.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./DialogConstants-DkeX1sw_.js";import"./Flex-Bmm6kMUW.js";import"./Clickable-tjPirwzH.js";import"./withStaticProps-DfSG2La2.js";import"./createStoryMetaSettingsDecorator-6la21Z9h.js";import"./index-BsvsTenc.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-D_pV6T_J.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-DqG0P6p1.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Email-TZY0cRuW.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./Remove-B_sTt2Ao.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./TipseenWizard-B_9K8s5s.js";import"./IconButton-BSIUJ3oq.js";import"./sizes-BOsbvv4u.js";import"./_tslib-BMUU9Vyh.js";function n(o){const t={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...o.components},{Canvas:r,PropsTable:i,RelatedComponents:p}=t;return r||s("Canvas"),i||s("PropsTable"),p||s("RelatedComponents"),e.jsxs(e.Fragment,{children:[e.jsx(a,{of:d}),`
+`,e.jsx(t.h1,{id:"steps",children:"Steps"}),`
+`,e.jsx(t.p,{children:"Steps display progress through a sequence of logical and numbered steps. They may also be used for navigation."}),`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(t.h3,{id:"import",children:"Import"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-js",children:`import { Steps } from "@vibe/core";
+`})}),`
+`,e.jsx(t.h2,{id:"props",children:"Props"}),`
+`,e.jsx(i,{}),`
+`,e.jsx(t.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(g,{guidelines:["The stepper shows users where they are in the process, and can be used to navigate through the process by selecting steps.","If a task needs more than six steps, consider simplifying the process or breaking it up into multiple tasks."]}),`
+`,e.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.h3,{id:"types",children:"Types"}),`
+`,e.jsx(t.p,{children:"Steps with a number view or gallery view."}),`
+`,e.jsx(r,{of:h}),`
+`,e.jsx(t.h3,{id:"on-primary",children:"On primary"}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(t.h2,{id:"dos-and-donts",children:"Do’s and Don’ts"}),`
+`,e.jsx(y,{rules:[{positive:{component:e.jsx(j,{}),description:"Use steps with numbers type for use cases of steps with more than 5 steps."},negative:{component:e.jsx(x,{}),description:"Don't use the gallery type steps component for more than 5 steps."}}]}),`
+`,e.jsx(t.h2,{id:"use-cases-and-examples",children:"Use cases and examples"}),`
+`,e.jsx(t.h3,{id:"navigable-steps",children:"Navigable Steps"}),`
+`,e.jsx(t.p,{children:"Navigable steps with proper code example."}),`
+`,e.jsx(r,{of:u}),`
+`,e.jsx(t.h3,{id:"steps-inside-a-tipseen",children:"Steps inside a tipseen"}),`
+`,e.jsx(t.p,{children:"Our Tipseen component includes support for steps as content."}),`
+`,e.jsx(r,{of:f}),`
+`,e.jsx(t.h2,{id:"related-components",children:"Related components"}),`
+`,e.jsx(p,{componentsNames:["MultiStepIndicator","Breadcrumbs","Tabs"]})]})}function qe(o={}){const{wrapper:t}={...m(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}function s(o,t){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{qe as default};
