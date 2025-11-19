3cc7bf4209647f76e7b3aba1af42a2c7fbb5652f
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-11-19 15:10:33 +0000

    Deploying to gh-pages from @ mondaycom/vibe@0fd132d6d528a22a3fd08fd26e179ef2248e0a51 🚀

---
+++ assets/storybook-link-CxpR5p9a.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as i}from"./index-BfNsOeqz.js";import{M as e,C as s,a as m}from"./index-DO4PQ-4t.js";import{S as p,O as a}from"./storybook-link.stories-CNeYlFHk.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CbPlX4IU.js";import"./index-BMjThmUV.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./link-Ct1oq02L.js";import"./index-BpvXyOxN.js";import"./Link-jLiiF1iI.js";import"./withStaticProps-CwB-IjhW.js";import"./index-CelVuQn-.js";function r(t){const n={h1:"h1",h2:"h2",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:p}),`
+`,o.jsx(n.h1,{id:"storybooklink",children:"StorybookLink"}),`
+`,o.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,o.jsx(n.p,{children:"The StorybookLink component is an internal link to navigate between components/stories inside storybook."}),`
+`,o.jsx(s,{of:a}),`
+`,o.jsx(n.h2,{id:"props",children:"Props"}),`
+`,o.jsx(m,{})]})}function S(t={}){const{wrapper:n}={...i(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(r,{...t})}):r(t)}export{S as default};
