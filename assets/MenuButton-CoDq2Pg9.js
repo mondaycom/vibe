4b6257a565cf389e13ee06fab01b3e8b929b4d6b
talkor <talkor@users.noreply.github.com>
2025-12-17 19:05:02 +0000

    Deploying to gh-pages from @ mondaycom/vibe@105b2c6e724035b8a1a9b121a729ec073caa162f 🚀

---
+++ assets/MenuButton-CoDq2Pg9.js
@@ -0,0 +1,29 @@
+import{j as t}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as a}from"./index-BfNsOeqz.js";import{M as d}from"./index-BJ7kf-qu.js";import{M as c,O as h,S as l,D as u,W as x,a as j,b as f,C as g}from"./MenuButton.stories-DC_S7OSk.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bv4mPkvC.js";import"./index-CIEp_EIj.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./createStoryMetaSettingsDecorator-DphH9f_a.js";import"./index-C2UbYbNO.js";import"./_tslib-Ct4JumdA.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-CefzN9Lt.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-DqG0P6p1.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Email-TZY0cRuW.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./noop-DX6rZLP_.js";import"./MenuButton-Dvwd0e1H.js";import"./index-BpvXyOxN.js";import"./index-8JwjhRSi.js";import"./Tooltip-BlFnX8EL.js";import"./typesciptCssModulesHelper-DQ-2Bs84.js";import"./test-ids-utils-DBF55-X4.js";import"./constants-BFnbiwiw.js";import"./DialogConstants-C6vctR8T.js";import"./Flex-DIp2zxrn.js";import"./Clickable-DHJc0oio.js";import"./useClickableProps-f6_q31tC.js";import"./keyCodes-BtXLi1ea.js";import"./useMergeRef-DmpwoaL5.js";import"./function-utils-BT-tMqNc.js";import"./withStaticProps-BEcHOprC.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./Icon-CpEYCgaz.js";import"./useEventListener-CkU0kzyk.js";import"./index-Dedp4W2d.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./useMergeRef-Do2VYePL.js";import"./function-utils-CgdCy1w7.js";import"./DialogContentContainer-BeKFb57x.js";import"./withStaticProps-DfSG2La2.js";import"./useMenuItemKeyboardEvents-DmKbwaIK.js";import"./index-gB5zY9qh.js";import"./index-CkU0kzyk.js";import"./keyCodes-B-UZ88ng.js";import"./useFullKeyboardListeners-Bm0-g7hh.js";import"./index-BxdhJjph.js";import"./sizes-BOsbvv4u.js";import"./MenuItem-kivPwIiT.js";import"./useIsOverflowing-CqkoFCDi.js";import"./debounce-D3NSP8gs.js";import"./Label-5Ety79nk.js";import"./Text-5PiIofEG.js";import"./TypographyConstants-9tYecPzE.js";import"./colors-vars-map-B-3B1jIC.js";import"./Divider-kFkf3gfS.js";import"./IconButton-BpEGs1Nl.js";import"./Button-DE1bV8I3.js";import"./Loader-BabV-h63.js";function p(o){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...a(),...o.components},{Canvas:i,PropsTable:s,RelatedComponents:m,UsageGuidelines:r}=e;return i||n("Canvas"),s||n("PropsTable"),m||n("RelatedComponents"),r||n("UsageGuidelines"),t.jsxs(t.Fragment,{children:[t.jsx(d,{of:c}),`
+`,t.jsx(e.h1,{id:"menubutton",children:"MenuButton"}),`
+`,t.jsx(e.p,{children:"MenuButton is a component that opens a Dialog next to a button, the content of the dialog could be anything you want."}),`
+`,t.jsx(i,{of:h}),`
+`,t.jsx(e.h3,{id:"import",children:"Import"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-js",children:`import { MenuButton } from "@vibe/core";
+`})}),`
+`,t.jsx(e.h2,{id:"props",children:"Props"}),`
+`,t.jsx(s,{}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(r,{guidelines:["When you want to place content next to an element","When the content needs to be on top of adjacent content "]}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(r,{guidelines:[t.jsxs(t.Fragment,{children:["Provide an ",t.jsx(e.code,{children:"id"})," for the MenuButton to enable proper accessibility associations."]}),t.jsxs(t.Fragment,{children:["Use descriptive ",t.jsx(e.code,{children:"ariaLabel"})," values for MenuButtons that clearly indicate the menu's purpose."]}),t.jsxs(t.Fragment,{children:["Use ",t.jsx(e.code,{children:"ariaControls"})," prop to link the MenuButton to its menu content. Pass the ",t.jsx(e.code,{children:"id"})," of the menu element to establish the relationship between the button and its controlled menu."]})]}),`
+`,t.jsx(e.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(e.h3,{id:"sizes",children:"Sizes"}),`
+`,t.jsx(i,{of:l}),`
+`,t.jsx(e.h3,{id:"different-icon",children:"Different Icon"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(e.h3,{id:"with-text",children:"With Text"}),`
+`,t.jsx(i,{of:x}),`
+`,t.jsx(e.h3,{id:"with-text-and-icon-at-the-end",children:"With Text and Icon at the end"}),`
+`,t.jsx(i,{of:j}),`
+`,t.jsx(e.h3,{id:"disabled",children:"Disabled"}),`
+`,t.jsx(i,{of:f}),`
+`,t.jsx(e.h3,{id:"custom-trigger-element",children:"Custom trigger element"}),`
+`,t.jsxs(e.p,{children:["You can use any element as a trigger for the menu button, just pass it as a ",t.jsx("code",{children:"triggerElement"})," to the ",t.jsx("code",{children:"MenuButton"})," component."]}),`
+`,t.jsx(i,{of:g}),`
+`,t.jsx(e.h2,{id:"related-components",children:"Related components"}),`
+`,t.jsx(m,{componentsNames:["Button","IconButton","SplitButton"]})]})}function qt(o={}){const{wrapper:e}={...a(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(p,{...o})}):p(o)}function n(o,e){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{qt as default};
