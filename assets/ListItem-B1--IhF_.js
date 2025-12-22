175705c5d07e8fa8449fa3c0407ab566541bb2b4
YossiSaadi <YossiSaadi@users.noreply.github.com>
2025-12-22 16:53:01 +0000

    Deploying to gh-pages from @ mondaycom/vibe@24367dd19d2663f6c4e7abcea71725d83a126d47 🚀

---
+++ assets/ListItem-B1--IhF_.js
@@ -0,0 +1,20 @@
+import{j as t}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as s}from"./index-BfNsOeqz.js";import{M as a,C as r}from"./index-Btq2BhpS.js";import{L as d,O as c,S as h,a as l,W as x,b as j}from"./ListItem.stories-BUfmnviu.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BDlpdPz-.js";import"./index-BvUpIBDO.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./createStoryMetaSettingsDecorator-ByqFV25U.js";import"./index-B9Z9g-zy.js";import"./_tslib-Ct4JumdA.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-CefzN9Lt.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-DqG0P6p1.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Email-TZY0cRuW.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./person1-D9Wcho68.js";import"./createComponentTemplate-B08h-OOW.js";import"./ListItemIcon-jjs3UdVc.js";import"./index-BpvXyOxN.js";import"./useMergeRef-Do2VYePL.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./Avatar-GlG5KYPa.js";import"./typesciptCssModulesHelper-DQ-2Bs84.js";import"./test-ids-utils-DBF55-X4.js";import"./constants-BFnbiwiw.js";import"./colors-vars-map-B-3B1jIC.js";import"./withStaticProps-DfSG2La2.js";import"./withStaticProps-BEcHOprC.js";import"./Icon-CpEYCgaz.js";import"./useMergeRef-DmpwoaL5.js";import"./noop-DX6rZLP_.js";import"./useEventListener-CkU0kzyk.js";import"./keyCodes-BtXLi1ea.js";import"./Tooltip-BlFnX8EL.js";import"./DialogConstants-C6vctR8T.js";import"./Flex-DIp2zxrn.js";import"./Clickable-DHJc0oio.js";import"./useClickableProps-f6_q31tC.js";import"./function-utils-BT-tMqNc.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./index-Dedp4W2d.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./isNil-CHIgUVhi.js";import"./ListItem-BUlUWC9H.js";import"./Text-5PiIofEG.js";import"./TypographyConstants-9tYecPzE.js";import"./useIsOverflowing-CqkoFCDi.js";import"./debounce-D3NSP8gs.js";import"./function-utils-CgdCy1w7.js";import"./index-gB5zY9qh.js";import"./index-CkU0kzyk.js";import"./keyCodes-B-UZ88ng.js";import"./sizes-BOsbvv4u.js";function p(o){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...s(),...o.components},{PropsTable:m,UsageGuidelines:e}=i;return m||n("PropsTable"),e||n("UsageGuidelines"),t.jsxs(t.Fragment,{children:[t.jsx(a,{of:d}),`
+`,t.jsx(i.h1,{id:"listitem",children:"ListItem"}),`
+`,t.jsx(i.p,{children:"An item of a List component"}),`
+`,t.jsx(r,{of:c}),`
+`,t.jsx(i.h3,{id:"import",children:"Import"}),`
+`,t.jsx(i.pre,{children:t.jsx(i.code,{className:"language-js",children:`import { ListItem, ListItemIcon, ListItemAvatar } from "@vibe/core";
+`})}),`
+`,t.jsx(i.h2,{id:"props",children:"Props"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e,{guidelines:["List item can include an ListItemIcon or text.","List item icon should always be displayed before the item's text (left or right depending on the item's text language)"]}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"states",children:"States"}),`
+`,t.jsx(r,{of:h}),`
+`,t.jsx(i.h3,{id:"sizes",children:"Sizes"}),`
+`,t.jsx(r,{of:l}),`
+`,t.jsx(i.h3,{id:"list-item-with-an-icon",children:"List item with an icon"}),`
+`,t.jsx(r,{of:x}),`
+`,t.jsx(i.h3,{id:"list-item-with-an-avatar",children:"List item with an avatar"}),`
+`,t.jsx(r,{of:j})]})}function Ot(o={}){const{wrapper:i}={...s(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(p,{...o})}):p(o)}function n(o,i){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{Ot as default};
---
+++ assets/Loader-Dj3fajw2.js
@@ -0,0 +1,33 @@
+import{j as o}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as c}from"./index-BfNsOeqz.js";import{M as h}from"./index-Btq2BhpS.js";import{l as x}from"./tip-CK87uV3P.js";import{m as a}from"./storybook-link-BY3qZr6q.js";import{L as j,O as u,S as f,C as g,a as v,V as C,b as L,c as b}from"./Loader.stories-CcPxOnYN.js";import{r as z}from"./usage-guidelines-DaF3e-2f.js";import{a as w}from"./component-rules-Bcb_N_7h.js";import{F as m}from"./Flex-DIp2zxrn.js";import{H as p}from"./Heading-CEc9wMHn.js";import{L as d}from"./Loader-BabV-h63.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BDlpdPz-.js";import"./index-BvUpIBDO.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./index-BpvXyOxN.js";import"./Flex-qb9MRXYX.js";import"./withStaticProps-DibdfTK_.js";import"./_tslib-BMUU9Vyh.js";import"./link-CVYAeutz.js";import"./index-DJOlVVfh.js";import"./createStoryMetaSettingsDecorator-ByqFV25U.js";import"./index-B9Z9g-zy.js";import"./_tslib-Ct4JumdA.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-CefzN9Lt.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-DqG0P6p1.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Email-TZY0cRuW.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./Search-BfwBuPrP.js";import"./useMergeRef-Do2VYePL.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./test-ids-utils-DBF55-X4.js";import"./constants-BFnbiwiw.js";import"./index-BHzG1aTI.js";import"./noop-DX6rZLP_.js";import"./debounce-D3NSP8gs.js";import"./IconButton-BpEGs1Nl.js";import"./Tooltip-BlFnX8EL.js";import"./typesciptCssModulesHelper-DQ-2Bs84.js";import"./DialogConstants-C6vctR8T.js";import"./Icon-CpEYCgaz.js";import"./useMergeRef-DmpwoaL5.js";import"./useEventListener-CkU0kzyk.js";import"./keyCodes-BtXLi1ea.js";import"./withStaticProps-BEcHOprC.js";import"./function-utils-BT-tMqNc.js";import"./index-Dedp4W2d.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./sizes-BOsbvv4u.js";import"./Button-DE1bV8I3.js";import"./withStaticProps-DfSG2La2.js";import"./BaseInput-BvzZFTTo.js";import"./Text-5PiIofEG.js";import"./TypographyConstants-9tYecPzE.js";import"./useIsOverflowing-CqkoFCDi.js";import"./DialogContentContainer-BeKFb57x.js";import"./Clickable-DHJc0oio.js";import"./useClickableProps-f6_q31tC.js";const S=()=>o.jsxs(x,{children:["While loading content consider using"," ",o.jsx(a,{page:"Components/Skeleton",size:a.sizes.SMALL,children:"Skeleton loading"})]});function l(t){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...c(),...t.components},{Canvas:e,PropsTable:n,RelatedComponents:s}=i;return e||r("Canvas"),n||r("PropsTable"),s||r("RelatedComponents"),o.jsxs(o.Fragment,{children:[o.jsx(h,{of:j}),`
+`,o.jsx(i.h1,{id:"loader",children:"Loader"}),`
+`,o.jsx(i.p,{children:"Circular loader indicates to user waiting state."}),`
+`,o.jsx(e,{of:u}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { Loader } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(n,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(z,{guidelines:["Use this loader to load small parts of the system, not the whole pages.","Use sizes of loader in context to its position. The bigger the loading area gets, the bigger loader should be used.","Always place loader in context to the loading content."]}),`
+`,o.jsx(S,{}),`
+`,o.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,o.jsx(i.h3,{id:"size-variants",children:"Size variants"}),`
+`,o.jsx(e,{of:f}),`
+`,o.jsx(i.h3,{id:"color-variants",children:"Color variants"}),`
+`,o.jsx(e,{of:g}),`
+`,o.jsx(i.h3,{id:"custom-colors",children:"Custom colors"}),`
+`,o.jsxs(i.p,{children:["If there is a need for color customization, css ",o.jsx(i.code,{children:"color"})," attribute of a parent component can be used."]}),`
+`,o.jsx(e,{of:v}),`
+`,o.jsx(i.h3,{id:"visual-variants",children:"Visual variants"}),`
+`,o.jsx(e,{of:C}),`
+`,o.jsx(i.h2,{id:"dos-and-donts",children:"Do’s and Don’ts"}),`
+`,o.jsx(w,{rules:[{positive:{component:o.jsxs(m,{direction:"row",gap:"medium",children:[o.jsx(p,{children:"H1 heading component"}),o.jsx(d,{size:"small",hasBackground:!0})]}),description:"Place loader in context to the loading content and keep its size proportional to the content."},negative:{component:o.jsxs(m,{direction:"row",gap:"medium",children:[o.jsx(p,{children:"H1 heading component"}),o.jsx(d,{size:"large",hasBackground:!0})]}),description:"Don’t leave the size visually unmaintained."}}]}),`
+`,o.jsx(i.h2,{id:"use-cases-and-examples",children:"Use cases and examples"}),`
+`,o.jsx(i.h3,{id:"loader-in-text-field",children:"Loader in text field"}),`
+`,o.jsx(i.p,{children:"Use loader in search field while filtering results."}),`
+`,o.jsx(e,{of:L}),`
+`,o.jsx(i.h3,{id:"loader-in-button",children:"Loader in button"}),`
+`,o.jsx(i.p,{children:"Indicate the loading status in button if content or an action is loading."}),`
+`,o.jsx(e,{of:b}),`
+`,o.jsx(i.h2,{id:"related-components",children:"Related components"}),`
+`,o.jsx(s,{componentsNames:["Skeleton","LinearProgressBar","Button"]})]})}function ii(t={}){const{wrapper:i}={...c(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(l,{...t})}):l(t)}function r(t,i){throw new Error("Expected component `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{ii as default};
