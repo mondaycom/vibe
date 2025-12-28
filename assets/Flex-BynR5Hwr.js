b55ddf21478dee0095dcb022222828156b2f13e2
rivka-ungar <rivka-ungar@users.noreply.github.com>
2025-12-28 14:49:17 +0000

    Deploying to gh-pages from @ mondaycom/vibe@96981660b69c570fda4854724bbf84fc320b6e6b 🚀

---
+++ assets/Flex-BynR5Hwr.js
@@ -0,0 +1,36 @@
+import{j as o}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as a}from"./index-BfNsOeqz.js";import{M as l}from"./index-7J1KZgcv.js";import{F as c,O as d,D as x,H as h,V as u,a as f,b as j,c as g,d as b,S as y,e as w}from"./Flex.stories-BFaBMuht.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-x9BaM5iZ.js";import"./index-DooHX6eq.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./createStoryMetaSettingsDecorator-ByqFV25U.js";import"./index-B9Z9g-zy.js";import"./_tslib-Ct4JumdA.js";import"./Invite-BJsiPZ1j.js";import"./AddSmall-DFewX5rK.js";import"./Workspace-CefzN9Lt.js";import"./Calendar-NzkLrIBg.js";import"./Table-DaDp-EUK.js";import"./Check-CxyRTNy4.js";import"./CloseSmall-DUYWL2FE.js";import"./Settings-DqG0P6p1.js";import"./Drag-8qsbRXq_.js";import"./DropdownChevronDown-Ck7VmPfy.js";import"./DropdownChevronRight-CtJWrCfY.js";import"./Email-TZY0cRuW.js";import"./Wand-CS0YeGd1.js";import"./NewTab-CMglqP6P.js";import"./Info-jvFNh0HS.js";import"./NavigationChevronRight-DwBPRKWZ.js";import"./Remove-B_sTt2Ao.js";import"./Search-ClaS4_rs.js";import"./SortDescending-DfYW2xR2.js";import"./Upgrade-CC_w3yRG.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./story-description-B8U8K6Zm.js";import"./index-BpvXyOxN.js";import"./withStaticProps-DibdfTK_.js";import"./Flex-qb9MRXYX.js";import"./Flex-DIp2zxrn.js";import"./Clickable-DHJc0oio.js";import"./useClickableProps-f6_q31tC.js";import"./keyCodes-BtXLi1ea.js";import"./useMergeRef-DmpwoaL5.js";import"./function-utils-BT-tMqNc.js";import"./noop-DX6rZLP_.js";import"./withStaticProps-BEcHOprC.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./Box-CSuEa9xb.js";import"./Text-5PiIofEG.js";import"./typesciptCssModulesHelper-DQ-2Bs84.js";import"./TypographyConstants-9tYecPzE.js";import"./useMergeRef-Do2VYePL.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./test-ids-utils-DBF55-X4.js";import"./constants-BFnbiwiw.js";import"./useIsOverflowing-CqkoFCDi.js";import"./debounce-D3NSP8gs.js";import"./Tooltip-BlFnX8EL.js";import"./DialogConstants-C6vctR8T.js";import"./Icon-CpEYCgaz.js";import"./useEventListener-CkU0kzyk.js";import"./index-Dedp4W2d.js";import"./CSSTransition-CWtXmFMf.js";import"./extends-B6xKY8K9.js";import"./withStaticProps-DfSG2La2.js";import"./Chips-BsxGUg5t.js";import"./colors-vars-map-B-3B1jIC.js";import"./Avatar-GlG5KYPa.js";import"./isNil-CHIgUVhi.js";import"./IconButton-Dn6_e6fx.js";import"./sizes-BOsbvv4u.js";import"./Button-DwIU445G.js";import"./Loader-BabV-h63.js";import"./index-ChMa3XFl.js";import"./index-CkU0kzyk.js";import"./index-BxdhJjph.js";function m(e){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...a(),...e.components},{Canvas:t,PropsTable:r,RelatedComponents:s,UsageGuidelines:p}=i;return t||n("Canvas"),r||n("PropsTable"),s||n("RelatedComponents"),p||n("UsageGuidelines"),o.jsxs(o.Fragment,{children:[o.jsx(l,{of:c}),`
+`,o.jsx(i.h1,{id:"flex",children:"Flex"}),`
+`,o.jsx(i.p,{children:"Use Flex component to position group of sub-elements in one dimension, horizontal or vertical, without being dependent on a custom CSS file for positioning the sub-elements."}),`
+`,o.jsx(t,{of:d}),`
+`,o.jsx(i.h3,{id:"import",children:"Import"}),`
+`,o.jsx(i.pre,{children:o.jsx(i.code,{className:"language-js",children:`import { Flex } from "@vibe/core";
+`})}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsx(r,{}),`
+`,o.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(p,{guidelines:["Use flex component whenever you want to define a layout with one dimension.","Flex layout can be either horizontal or vertical.","You can defined the spacing between the layout children by using our fixed sizes - xs (4px), small (8px), medium (16px) or large( 24px)."]}),`
+`,o.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,o.jsx(i.h3,{id:"directions",children:"Directions"}),`
+`,o.jsx(t,{of:x}),`
+`,o.jsx(i.h3,{id:"horizontal-spacing-between-items",children:"Horizontal spacing between items"}),`
+`,o.jsx(t,{of:h}),`
+`,o.jsx(i.h3,{id:"vertical-spacing-between-items",children:"Vertical spacing between items"}),`
+`,o.jsx(t,{of:u}),`
+`,o.jsx(i.h3,{id:"horizontal-positions",children:"Horizontal positions"}),`
+`,o.jsx(t,{of:f}),`
+`,o.jsx(i.h3,{id:"horizontal-layout-using-flex",children:"Horizontal layout using flex"}),`
+`,o.jsx(t,{of:j}),`
+`,o.jsx(i.h3,{id:"vertical-positions",children:"Vertical positions"}),`
+`,o.jsx(t,{of:g}),`
+`,o.jsx(i.h3,{id:"vertical-layout-using-flex",children:"Vertical layout using flex"}),`
+`,o.jsx(t,{of:b}),`
+`,o.jsx(i.h3,{id:"support-multi-lines-layout",children:"Support multi lines layout"}),`
+`,o.jsx(i.p,{children:`You can display a layout that includes multiple lines using the flex component wrap mode.
+This mode allows the layout to break into multiple lines if all the component children cannot fit into one only.`}),`
+`,o.jsx(t,{of:y}),`
+`,o.jsx(i.h2,{id:"use-cases-and-examples",children:"Use cases and examples"}),`
+`,o.jsx(i.h3,{id:"flex-as-toolbar-container",children:"Flex as toolbar container"}),`
+`,o.jsx(i.p,{children:"You can use flex component for create responsive toolbars"}),`
+`,o.jsx(t,{of:w}),`
+`,o.jsx(i.h2,{id:"related-components",children:"Related components"}),`
+`,o.jsx(s,{componentsNames:["Menu","Tabs","List"]})]})}function Jo(e={}){const{wrapper:i}={...a(),...e.components};return i?o.jsx(i,{...e,children:o.jsx(m,{...e})}):m(e)}function n(e,i){throw new Error("Expected component `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{Jo as default};
--- assets/unstyled-list-C6honCFE.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as o}from"./index-BfNsOeqz.js";import{M as r,C as i,a as d}from"./index-BJBuKoUp.js";import{U as c,O as a}from"./unstyled-list.stories-CjzZPLGt.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CGRzfW_S.js";import"./index-DOLbsD_g.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";function s(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:c}),`
-`,e.jsx(n.h1,{id:"unstyledlist",children:"UnstyledList"}),`
-`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
-`,e.jsxs(n.p,{children:[`UnstyledList is a component that displays the items in an unordered list.
-This component can be used as a replacement for `,e.jsx(n.code,{children:"ul"})," and it's child component ",e.jsx(n.code,{children:"UnstyledListItem"})," can be used as a replacement for ",e.jsx(n.code,{children:"li"}),"."]}),`
-`,e.jsx(i,{of:a}),`
-`,e.jsx(n.h2,{id:"props",children:"Props"}),`
-`,e.jsx(d,{})]})}function M(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{M as default};
