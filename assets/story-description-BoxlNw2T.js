eb5ab7e151fe07e8674d9511da97fec5409cb3ff
rivka-ungar <rivka-ungar@users.noreply.github.com>
2026-01-04 11:59:21 +0000

    Deploying to gh-pages from @ mondaycom/vibe@e9c874b1cae9ee9d561d8a2048abee957bc5691d 🚀

---
+++ assets/story-description-BoxlNw2T.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as i}from"./index-BfNsOeqz.js";import{M as e,C as s,a as p}from"./index-BYJnOoku.js";import{S as m,O as a}from"./story-description.stories-BCoVXZq2.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-yJjQv8ft.js";import"./index-CDcywKeq.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./index-BpvXyOxN.js";import"./Flex-wmhmkp5A.js";import"./withStaticProps-CwB-IjhW.js";import"./status-tag-D5VNKzdF.js";function n(r){const o={h1:"h1",h2:"h2",p:"p",...i(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(o.h1,{id:"storydescription",children:"StoryDescription"}),`
+`,t.jsx(o.h2,{id:"overview",children:"Overview"}),`
+`,t.jsx(o.p,{children:"The StoryDescription component is used to add titles to the component states displayed in a story."}),`
+`,t.jsx(s,{of:a}),`
+`,t.jsx(o.h2,{id:"props",children:"Props"}),`
+`,t.jsx(p,{})]})}function O(r={}){const{wrapper:o}={...i(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(n,{...r})}):n(r)}export{O as default};
