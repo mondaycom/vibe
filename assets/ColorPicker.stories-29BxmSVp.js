c4bbd5dd1fe99b91461dcb5a5d536528f4c24f4e
talkor <talkor@users.noreply.github.com>
2026-05-20 09:17:54 +0000

    Deploying to gh-pages from @ mondaycom/vibe@bc01e96ec8a9cbeec84be41e4008b04e9e5621ad 🚀

---
+++ assets/ColorPicker.stories-29BxmSVp.js
@@ -0,0 +1,56 @@
+import{C as J}from"./Check-BhuJDOHf.js";import{h as k}from"./index-D1D0uiJ1.js";import{c as Q}from"./createStoryMetaSettingsDecorator-C07DgdqT.js";import{userEvent as u,within as X}from"./index-i7od9_os.js";import{i as z,r as F,t as V,e as S}from"./interactions-utils-CgUjUaHh.js";import{g as Y,C as Z}from"./constants-CenpDJY1.js";import{C as E}from"./ColorPicker-QLlcwBOH.js";import{r as $}from"./createComponentTemplate-B08h-OOW.js";const o={BRIGHT_GREEN:"bright-green",STEEL:"steel",DARK_PURPLE:"dark_purple",INDIGO:"indigo"};async function ee(e){await w(e,o.BRIGHT_GREEN),await n(e,o.BRIGHT_GREEN),await V("{arrowDown}",7),await C(e,o.STEEL),await u.keyboard(" "),await C(e,o.STEEL),await n(e,o.STEEL),await T(e,o.BRIGHT_GREEN),await u.keyboard("{arrowDown}"),await g(e,o.STEEL),await u.keyboard("{Enter}"),await T(e,o.STEEL)}const oe=z({tests:[ee],afterEach:async()=>{await F()}});async function re(e){await w(e,o.DARK_PURPLE),await n(e,o.DARK_PURPLE),await V("{arrowRight}",3),await C(e,o.INDIGO),await u.keyboard("{Enter}"),await n(e,o.DARK_PURPLE),await n(e,o.INDIGO),await C(e,o.INDIGO),await w(e,o.DARK_PURPLE),await T(e,o.DARK_PURPLE),await n(e,o.INDIGO),await g(e,o.DARK_PURPLE),await g(e,o.INDIGO)}const te=z({tests:[re],afterEach:async()=>{await F()}});function ae(e){return e.replace(/-|_/g," ").replace(/(?:^|\s)\S/g,r=>r.toUpperCase())}async function w(e,r){const t=await I(e,r),q=await X(t).findByLabelText(ae(r));await u.click(q)}async function n(e,r){const t=await I(e,r);S(t.getAttribute("class")).toContain("selectedColor")}async function T(e,r){const t=await I(e,r);S(t.getAttribute("class")).not.toContain("selectedColor")}async function C(e,r){const t=await I(e,r);S(t.getAttribute("class")).toContain("active")}async function g(e,r){const t=await I(e,r);S(t.getAttribute("class")).not.toContain("active")}async function I(e,r){return await e.findByTestId(Y(Z.COLOR_PICKER_ITEM,r))}const y=Q({component:E,iconPropNamesArray:["ColorIndicatorIcon","SelectedIndicatorIcon","NoColorIcon"],actionPropsArray:[{name:"onSave",linkedToPropValue:"value"}]}),ne={title:"Components/ColorPicker",component:E,argTypes:y.argTypes,decorators:y.decorators},a=$(E),c={render:a.bind({}),args:{id:"overview-color-picker"},name:"Overview"},i={render:a.bind({}),args:{id:"indicator-color-picker",ColorIndicatorIcon:k},name:"With Indicator"},s={render:a.bind({}),args:{id:"text-indication-color-picker",ColorIndicatorIcon:k,value:"peach",shouldRenderIndicatorWithoutBackground:!0},name:"Text Indication"},l={render:a.bind({}),args:{id:"selected-color-picker",ColorIndicatorIcon:k,colorStyle:"selected"},name:"Selected"},d={render:a.bind({}),args:{id:"no-color-picker",noColorText:"Clear color"},name:"No color",play:oe},p={render:a.bind({}),args:{id:"multiselect-color-picker",isMultiselect:!0,SelectedIndicatorIcon:J,value:"peach"},name:"Selected icon",play:te},m={render:a.bind({}),args:{id:"shapes-color-picker",colorShape:"circle"},name:"Shapes"};var P,b,h;c.parameters={...c.parameters,docs:{...(P=c.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "overview-color-picker"
+  },
+  name: "Overview"
+}`,...(h=(b=c.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};var R,f,_;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "indicator-color-picker",
+    ColorIndicatorIcon: TextColorIndicator
+  },
+  name: "With Indicator"
+}`,...(_=(f=i.parameters)==null?void 0:f.docs)==null?void 0:_.source}}};var N,x,A;s.parameters={...s.parameters,docs:{...(N=s.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "text-indication-color-picker",
+    ColorIndicatorIcon: TextColorIndicator,
+    value: "peach",
+    shouldRenderIndicatorWithoutBackground: true
+  },
+  name: "Text Indication"
+}`,...(A=(x=s.parameters)==null?void 0:x.docs)==null?void 0:A.source}}};var D,O,L;l.parameters={...l.parameters,docs:{...(D=l.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "selected-color-picker",
+    ColorIndicatorIcon: TextColorIndicator,
+    colorStyle: "selected"
+  },
+  name: "Selected"
+}`,...(L=(O=l.parameters)==null?void 0:O.docs)==null?void 0:L.source}}};var G,B,K;d.parameters={...d.parameters,docs:{...(G=d.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "no-color-picker",
+    noColorText: "Clear color"
+  },
+  name: "No color",
+  play: noColorInteractionSuite
+}`,...(K=(B=d.parameters)==null?void 0:B.docs)==null?void 0:K.source}}};var W,v,M;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "multiselect-color-picker",
+    isMultiselect: true,
+    SelectedIndicatorIcon: Check,
+    value: "peach"
+  },
+  name: "Selected icon",
+  play: multiSelectionInteractionSuite
+}`,...(M=(v=p.parameters)==null?void 0:v.docs)==null?void 0:M.source}}};var U,H,j;m.parameters={...m.parameters,docs:{...(U=m.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: colorPickerTemplate.bind({}),
+  args: {
+    id: "shapes-color-picker",
+    colorShape: "circle"
+  },
+  name: "Shapes"
+}`,...(j=(H=m.parameters)==null?void 0:H.docs)==null?void 0:j.source}}};const ce=["Overview","WithIndicator","TextIndication","Selected","NoColor","SelectedIcon","Shapes"],Ce=Object.freeze(Object.defineProperty({__proto__:null,NoColor:d,Overview:c,Selected:l,SelectedIcon:p,Shapes:m,TextIndication:s,WithIndicator:i,__namedExportsOrder:ce,default:ne},Symbol.toStringTag,{value:"Module"}));export{Ce as C,d as N,c as O,l as S,s as T,i as W,p as a,m as b};
