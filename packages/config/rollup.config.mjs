bd8cb504b5581afd24778b51e310c07adb71ebbf
Tal Koren <talkor@gmail.com>
2026-05-31 11:46:33 +0300

    Revert "chore: upgrade Rollup from v2 to v4 (#3371)"
    This reverts commit f04524b0a0284f2d6d9e966d3227c154b75e03e0.

--- packages/config/rollup.config.mjs
+++ packages/config/rollup.config.mjs
@@ -4,7 +4,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import terser from "@rollup/plugin-terser";
+import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import postCssImport from "postcss-import";
 import autoprefixer from "autoprefixer";
