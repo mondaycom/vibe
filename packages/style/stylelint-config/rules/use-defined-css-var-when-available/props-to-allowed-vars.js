36f476e9bc21062c1bbefc7f06114d52438424cc
Tal Koren <talko@monday.com>
2026-07-19 09:10:33 +0300

    feat(styles): add numeric border-radius tokens (#3435)

--- packages/style/stylelint-config/rules/use-defined-css-var-when-available/props-to-allowed-vars.js
+++ packages/style/stylelint-config/rules/use-defined-css-var-when-available/props-to-allowed-vars.js
@@ -13,7 +13,15 @@ const SPACINGS = [
   "--space-80"
 ];
 
-const BORDER_RADIUSES = ["--border-radius-small", "--border-radius-medium", "--border-radius-big"];
+const BORDER_RADIUSES = [
+  "--border-radius-small",
+  "--border-radius-medium",
+  "--border-radius-big",
+  "--border-radius-4",
+  "--border-radius-8",
+  "--border-radius-12",
+  "--border-radius-16"
+];
 
 const BORDER_WIDTHS = ["--border-width"];
 
@@ -148,7 +156,7 @@ function mapPropsToAllowedVars(propNames, allowedVars, recommended = undefined) 
 
 const PROPS_TO_ALLOWED_VARS = {
   ...mapPropsToAllowedVars(SPACING_PROPS, SPACINGS),
-    ...mapPropsToAllowedVars(BORDER_RADIUSES_PROPS, BORDER_RADIUSES),
+  ...mapPropsToAllowedVars(BORDER_RADIUSES_PROPS, BORDER_RADIUSES, ["--border-radius-4", "--border-radius-8", "--border-radius-16"]),
   ...mapPropsToAllowedVars(BORDER_WIDTHS_PROPS, BORDER_WIDTHS),
   ...mapPropsToAllowedVars(BORDER_STYLES_PROPS, BORDER_STYLES),
   ...mapPropsToAllowedVars(FONT_FAMILIES_PROPS, FONT_FAMILIES, ["--title-font-family"]),
