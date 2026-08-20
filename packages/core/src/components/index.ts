821a2f8cf2ba2690d2f52dc22367b7ae996d8091
Rivka Ungar <rivkaun@monday.com>
2026-08-20 16:38:17 +0300

    refactor: use export * from @vibe/tabs (no enums; drop stale TODO)

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -63,20 +63,7 @@ export * from "./Slider";
 export * from "./SplitButton";
 export * from "./Steps";
 export * from "./Table";
-// TODO: export * after removing enums
-export {
-  Tab,
-  type TabProps,
-  TabList,
-  type TabListProps,
-  TabPanel,
-  type TabPanelProps,
-  TabPanels,
-  type TabPanelsProps,
-  type TabPanelsAnimationDirection,
-  TabsContext,
-  type TabsContextProps
-} from "@vibe/tabs";
+export * from "@vibe/tabs";
 export { Text, type TextProps, type TextType, type TextWeight } from "@vibe/typography";
 export * from "./TextArea";
 export * from "./TextField";
