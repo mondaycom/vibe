21238f428919b2a31060c1ec6f9aaf9de1832b3a
rivka-ungar <rivka-ungar@users.noreply.github.com>
2026-08-25 12:06:05 +0000

    Deploying to gh-pages from @ mondaycom/vibe@c86702369b8b140d0db8269b58844cf10f01ef91 🚀

---
+++ assets/ListItem-jkPygOBo.js
@@ -0,0 +1,22 @@
+import{j as t}from"./jsx-runtime-lwGtIXvq.js";import{useMDXComponents as s}from"./index-BfNsOeqz.js";import{M as a,C as r}from"./index-B5_VHGkl.js";import{L as c,O as d,S as h,a as l,W as x,b as j}from"./ListItem.stories-BP6oiSLq.js";import{l as f}from"./tip-CK87uV3P.js";import{m as u}from"./storybook-link-CtsaiRGc.js";import"./index-CTZeEbLr.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-C1XqdcWx.js";import"./index-DY6vktWV.js";import"./index-BrqHMYbN.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./createStoryMetaSettingsDecorator-DHxXl1St.js";import"./index-2tdOESSL.js";import"./Invite-0V4zx4R3.js";import"./AddSmall-B_5viru5.js";import"./Workspace-BRy0vdK8.js";import"./Calendar-DUM0ORSW.js";import"./Table-COtoXgIc.js";import"./Check-BhuJDOHf.js";import"./CloseSmall-s783aDlP.js";import"./Delete-rsE7lq-b.js";import"./Drag-Ds0nT9eq.js";import"./Remove-BN6wnPjE.js";import"./DropdownChevronRight-C951c-JF.js";import"./Email-BmMi5Ll_.js";import"./Settings-Dllwc6G3.js";import"./Wand-CDIW39RM.js";import"./NewTab-BAQw6vct.js";import"./Info-LYrK74vc.js";import"./NavigationChevronRight-BDrJkoFj.js";import"./Person-C6oC2dzb.js";import"./Search-B-LsiQVE.js";import"./SortDescending-zUObFE7J.js";import"./Upgrade-CdQgCbE5.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./person1-D9Wcho68.js";import"./ListItemIcon-B682wOw3.js";import"./index-BpvXyOxN.js";import"./Avatar-BbSEznvw.js";import"./camelCase-CO1PkVrH.js";import"./Clickable-fYm4Nhj8.js";import"./useClickableProps-BM7QNjh1.js";import"./keyCodes-nZX5vvYt.js";import"./useMergeRef-Do2VYePL.js";import"./useIsomorphicLayoutEffect-BBiFUoiz.js";import"./ssr-utils-Do6V6AqB.js";import"./constants-BW-lD-kU.js";import"./noop-DX6rZLP_.js";import"./Icon-CL0Fp98D.js";import"./useEventListener-CkU0kzyk.js";import"./useKeyEvent-BeN1aNjK.js";import"./typesciptCssModulesHelper-Ji7rRrZn.js";import"./colors-vars-map-DuPRA0tY.js";import"./isNil-CHIgUVhi.js";import"./Tooltip-VkIKBJE6.js";import"./Flex-CRCq41Cp.js";import"./index-BKMrxRLL.js";import"./CSSTransition-Dhcv5TuQ.js";import"./extends-CCbyfPlC.js";import"./ListItem-XtOHDEUO.js";import"./Text-BEq4Om6j.js";import"./Typography-Di_TLYos.js";import"./index-CqkoFCDi.js";import"./debounce-D3NSP8gs.js";import"./keyCodes-DhPS-8Mm.js";import"./sizes-DwF_Bmpf.js";import"./createComponentTemplate-B08h-OOW.js";import"./Flex-qb9MRXYX.js";import"./withStaticProps-DibdfTK_.js";import"./_tslib-BMUU9Vyh.js";import"./link-CVYAeutz.js";import"./index-Bl42HU8t.js";function n(o){const i={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...s(),...o.components},{PropsTable:e,UsageGuidelines:m}=i;return e||p("PropsTable"),m||p("UsageGuidelines"),t.jsxs(t.Fragment,{children:[t.jsx(a,{of:c}),`
+`,t.jsx(i.h1,{id:"listitem",children:"ListItem"}),`
+`,t.jsx(f,{title:"New ListItem Available",emoji:"🚀",children:t.jsxs(i.p,{children:["This component is deprecated. A new ListItem component is available with a cleaner, data-driven API. Check out the"," ",`
+`,t.jsx(u,{page:"Components/List [New]/ListItem",children:"new ListItem component"}),"."]})}),`
+`,t.jsx(i.p,{children:"An item of a List component"}),`
+`,t.jsx(r,{of:d}),`
+`,t.jsx(i.h3,{id:"import",children:"Import"}),`
+`,t.jsx(i.pre,{children:t.jsx(i.code,{className:"language-js",children:`import { ListItem, ListItemIcon, ListItemAvatar } from "@vibe/core";
+`})}),`
+`,t.jsx(i.h2,{id:"props",children:"Props"}),`
+`,t.jsx(e,{}),`
+`,t.jsx(i.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(m,{guidelines:["List item can include an ListItemIcon or text.","List item icon should always be displayed before the item's text (left or right depending on the item's text language)"]}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"states",children:"States"}),`
+`,t.jsx(r,{of:h}),`
+`,t.jsx(i.h3,{id:"sizes",children:"Sizes"}),`
+`,t.jsx(r,{of:l}),`
+`,t.jsx(i.h3,{id:"list-item-with-an-icon",children:"List item with an icon"}),`
+`,t.jsx(r,{of:x}),`
+`,t.jsx(i.h3,{id:"list-item-with-an-avatar",children:"List item with an avatar"}),`
+`,t.jsx(r,{of:j})]})}function kt(o={}){const{wrapper:i}={...s(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(n,{...o})}):n(o)}function p(o,i){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{kt as default};
